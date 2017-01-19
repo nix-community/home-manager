35e0a339f8c14aed2c3e7230850b026befc02dc9
Robert Helgesson <robert@rycee.net>
2017-01-20 00:20:25 +0100

    dunst: support dbus activation

--- modules/services/dunst.nix
+++ modules/services/dunst.nix
@@ -16,20 +16,21 @@ with lib;
   };
 
   config = mkIf config.services.dunst.enable {
+    home.file.".local/share/dbus-1/services/org.knopwob.dunst.service".source =
+      "${pkgs.dunst}/share/dbus-1/services/org.knopwob.dunst.service";
+
     systemd.user.services.dunst = {
-        Unit = {
-          Description = "Dunst notification daemon";
-        };
+      Unit = {
+        Description = "Dunst notification daemon";
+        Requires = "graphical-session.target";
+        After = "graphical-session.target";
+      };
 
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
-
-        Service = {
-          # Type = "dbus";
-          # BusName = "org.freedesktop.Notifications";
-          ExecStart = "${pkgs.dunst}/bin/dunst";
-        };
+      Service = {
+        Type = "dbus";
+        BusName = "org.freedesktop.Notifications";
+        ExecStart = "${pkgs.dunst}/bin/dunst";
+      };
     };
   };
 }
