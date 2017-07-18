cdb2bec909cfdd808765778b3d8c447e4133b8c3
Robert Helgesson <robert@rycee.net>
2017-07-18 13:49:02 +0200

    syncthing: expand service description
    This models the user service on the upstream systemd file.

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -5,24 +5,28 @@ with lib;
 {
   options = {
     services.syncthing = {
-      enable = mkEnableOption "Syncthing";
+      enable = mkEnableOption "Syncthing continuous file synchronization";
     };
   };
 
   config = mkIf config.services.syncthing.enable {
     systemd.user.services.syncthing = {
-        Unit = {
-          Description = "Syncthing";
-          After = [ "network.target" ];
-        };
+      Unit = {
+        Description = "Syncthing - Open Source Continuous File Synchronization";
+        Documentation = "man:syncthing(1)";
+        After = [ "network.target" ];
+      };
 
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
+      Service = {
+        ExecStart = "${pkgs.syncthing}/bin/syncthing -no-browser -no-restart -logflags=0";
+        Restart = "on-failure";
+        SuccessExitStatus = [ 3 4 ];
+        RestartForceExitStatus = [ 3 4 ];
+      };
 
-        Service = {
-          ExecStart = "${pkgs.syncthing}/bin/syncthing -no-browser";
-        };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
     };
   };
 }
