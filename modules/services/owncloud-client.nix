ad9a26edee5ac00064a8563089bb654737b5f4be
Nikita Uvarov <uv.nikita@gmail.com>
2017-08-26 16:00:55 +0200

    owncloud-client: add module

---
+++ modules/services/owncloud-client.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  options = {
+    services.owncloud-client = {
+      enable = mkEnableOption "Owncloud Client";
+    };
+  };
+
+  config = mkIf config.services.owncloud-client.enable {
+    systemd.user.services.owncloud-client = {
+      Unit = {
+        Description = "Owncloud Client";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.owncloud-client}/bin/owncloud";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+  };
+}
