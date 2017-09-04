ef09f7841b4d9a3cd78cc5b32b499d101ab3fcac
Nikita Uvarov <uv.nikita@gmail.com>
2017-09-04 16:35:23 +0200

    feh: add module

---
+++ modules/programs/feh.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.programs.feh;
+
+  disableBinding = func: key: func;
+  enableBinding = func: key: "${func} ${key}";
+
+in
+
+{
+  options.programs.feh = {
+    enable = mkEnableOption "feh - a fast and light image viewer";
+
+    keybindings = mkOption {
+      default = {};
+      type = types.attrs;
+      example = { zoom_in = "plus"; zoom_out = "minus"; };
+      description = ''
+        Set keybindings.
+        See <link xlink:href="https://man.finalrewind.org/1/feh/#x4b455953"/> for
+        default bindings and available commands.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.feh ];
+
+    home.file.".config/feh/keys".text = ''
+      # Disable default keybindings
+      ${concatStringsSep "\n" (mapAttrsToList disableBinding cfg.keybindings)}
+
+      # Enable new keybindings
+      ${concatStringsSep "\n" (mapAttrsToList enableBinding cfg.keybindings)}
+    '';
+  };
+}
