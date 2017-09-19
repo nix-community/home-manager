76e0e09aca244fdbe54a5604a1fa4f26ff5a051b
Robert Helgesson <robert@rycee.net>
2017-09-19 23:32:01 +0200

    emacs: allow custom Emacs package

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -6,12 +6,24 @@ let
 
   cfg = config.programs.emacs;
 
+  # Copied from all-packages.nix.
+  emacsPackages = pkgs.emacsPackagesNgGen cfg.package;
+  emacsWithPackages = emacsPackages.emacsWithPackages;
+
 in
 
 {
   options = {
     programs.emacs = {
       enable = mkEnableOption "Emacs";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.emacs;
+        defaultText = "pkgs.emacs";
+        example = literalExample "pkgs.emacs25-nox";
+        description = "The Emacs package to use.";
+      };
 
       extraPackages = mkOption {
         default = self: [];
@@ -24,6 +36,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ (pkgs.emacsWithPackages cfg.extraPackages) ];
+    home.packages = [ (emacsWithPackages cfg.extraPackages) ];
   };
 }
