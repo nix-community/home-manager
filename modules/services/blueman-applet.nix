7e6f3364bcf0a0ec838aa4853f550a9a7b5ed027
Nikita Uvarov <uv.nikita@gmail.com>
2017-10-11 13:05:47 +0200

    blueman-applet: add note about required system service

--- modules/services/blueman-applet.nix
+++ modules/services/blueman-applet.nix
@@ -5,7 +5,15 @@ with lib;
 {
   options = {
     services.blueman-applet = {
-      enable = mkEnableOption "Blueman applet";
+      enable = mkEnableOption ''
+        Blueman applet.
+
+        Note, for the applet to work, 'blueman' package should also be installed system-wide
+        since it requires running 'blueman-mechanism' service activated via dbus.
+        You can add it to the dbus packages in system configuration:
+
+          services.dbus.packages = [ pkgs.blueman ];
+      '';
     };
   };
 
