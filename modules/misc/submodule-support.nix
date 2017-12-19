2bdaf4ae98fad26ded1b0d2a0219484b2252bf64
Robert Helgesson <robert@rycee.net>
2017-12-19 15:43:40 +0100

    nixos module: support NixOS user packages install
    When using the NixOS module we cannot guarantee that the Nix store
    will be writable during startup. Installing the user packages through
    `nix-env -i` will fail in these cases.
    
    This commit adds a NixOS option `home-manager.useUserPackages` that,
    when enabled, installs user packages through the NixOS
    
        users.users.<name?>.packages
    
    option.
    
    Note, when submodule support and external package install is enabled
    then the installed packages are not available in `~/.nix-profile`. We
    therefore set `home.profileDirectory` directly to the HM profile
    packages.
    
    (cherry picked from commit ef168979bf88310459464e5fe3fbcf2d3cbe49b5)

--- modules/misc/submodule-support.nix
+++ modules/misc/submodule-support.nix
@@ -15,5 +15,18 @@ with lib;
         in, for example, NixOS or nix-darwin.
       '';
     };
+
+    externalPackageInstall = mkOption {
+      type = types.bool;
+      default = false;
+      internal = true;
+      description = ''
+        Whether the packages of <option>home.packages</option> are
+        installed separately from the Home Manager activation script.
+        In NixOS, for example, this may be accomplished by installing
+        the packages through
+        <option>users.users.&lt;name?&gt;.packages</option>.
+      '';
+    };
   };
 }
--- nixos/default.nix
+++ nixos/default.nix
@@ -11,6 +11,7 @@ let
 
     config = {
       submoduleSupport.enable = true;
+      submoduleSupport.externalPackageInstall = cfg.useUserPackages;
       home.username = config.users.users.${name}.name;
       home.homeDirectory = config.users.users.${name}.home;
     };
@@ -20,16 +21,29 @@ in
 
 {
   options = {
-    home-manager.users = mkOption {
-      type = types.attrsOf hmModule;
-      default = {};
-      description = ''
-        Per-user Home Manager configuration.
+    home-manager = {
+      useUserPackages = mkEnableOption ''
+        installation of user packages through the
+        <option>users.users.&lt;name?&gt;.packages</option> option.
       '';
+
+      users = mkOption {
+        type = types.attrsOf hmModule;
+        default = {};
+        description = ''
+          Per-user Home Manager configuration.
+        '';
+      };
     };
   };
 
   config = mkIf (cfg.users != {}) {
+    users.users = mkIf cfg.useUserPackages (
+      mapAttrs (username: usercfg: {
+        packages = usercfg.home.packages;
+      }) cfg.users
+    );
+
     systemd.services = mapAttrs' (username: usercfg:
       nameValuePair ("home-manager-${utils.escapeSystemdPath username}") {
         description = "Home Manager environment for ${username}";
