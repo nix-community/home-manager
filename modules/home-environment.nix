e624b9aa6a7caf99eaacfd4ffd9c3ef839c68a66
Robert Helgesson <robert@rycee.net>
2018-01-04 13:15:22 +0100

    home-environment: install `hm-session-vars.sh` file
    This is a file containing all session variables exported using a
    Bourne-compatible syntax.

--- modules/home-environment.nix
+++ modules/home-environment.nix
@@ -267,6 +267,23 @@ in
         //
         (maybeSet "LC_TIME" cfg.language.time);
 
+    home.packages = [
+      # Provide a file holding all session variables.
+      (
+        pkgs.writeTextFile {
+          name = "hm-session-vars.sh";
+          destination = "/etc/profile.d/hm-session-vars.sh";
+          text = ''
+            # Only source this once.
+            if [ -n "$__HM_SESS_VARS_SOURCED" ]; then return; fi
+            export __HM_SESS_VARS_SOURCED=1
+
+            ${config.lib.shell.exportAll cfg.sessionVariables}
+          '';
+        }
+      )
+    ];
+
     # A dummy entry acting as a boundary between the activation
     # script's "check" and the "write" phases.
     home.activation.writeBoundary = dag.entryAnywhere "";
