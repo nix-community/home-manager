b6fd064f67da5049f0ca78eac427e546d92654b2
Will Fancher <elvishjerricco@gmail.com>
2018-07-27 13:38:19 -0400

    nix-darwin: Run home-manager in extraActivation

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -32,24 +32,10 @@ in
   };
 
   config = mkIf (cfg.users != {}) {
-    launchd.daemons = mapAttrs' (username: usercfg:
-      let escapeSystemdPath = s:
-        replaceChars ["/" "-" " "] ["-" "\\x2d" "\\x20"]
-          (if hasPrefix "/" s then substring 1 (stringLength s) s else s);
-      in nameValuePair ("home-manager-${escapeSystemdPath username}") {
-        # The activation script is run by a login shell to make sure
-        # that the user is given a sane Nix environment.
-        script = "${pkgs.writeScript "activate-${username}" ''
-          #! ${pkgs.stdenv.shell} -el
-          echo Activating home-manager configuration for ${username}
-          exec ${usercfg.home.activationPackage}/activate
-        ''}";
-
-        serviceConfig = {
-          UserName = username;
-          RunAtLoad = true;
-        };
-      }
-    ) cfg.users;
+    system.activationScripts.extraActivation.text =
+      lib.concatStringsSep "\n" (lib.mapAttrsToList (username: usercfg: ''
+        echo Activating home-manager configuration for ${username}
+        ${usercfg.home.activationPackage}/activate
+      '') cfg.users);
   };
 }
