78ed7c0dfcd2fb9dc62d54dacbeed11823ca0193
Roman Volosatovs <rvolosatovs@riseup.net>
2018-08-18 16:13:33 +0200

    go: add module

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -793,6 +793,13 @@ in
           A new module is available: 'programs.zathura'.
         '';
       }
+
+      {
+        time = "2018-09-19:50:07+00:00";
+        message = ''
+          A new module is available: 'programs.go'.
+        '';
+      }
     ];
   };
 }
