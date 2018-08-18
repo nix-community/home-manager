f9ac73732b25dedabb6a0b0fd6ace6598a1dd601
LightDiscord <root@arnaud.sh>
2018-08-18 20:41:44 +0200

    awesome: fix missing concatenation

--- modules/services/window-managers/awesome.nix
+++ modules/services/window-managers/awesome.nix
@@ -9,7 +9,7 @@ let
   getLuaPath = lib: dir: "${lib}/${dir}/lua/${pkgs.luaPackages.lua.luaversion}";
   makeSearchPath = lib.concatMapStrings (path:
     " --search ${getLuaPath path "share"}"
-    " --search ${getLuaPath path "lib"}"
+    + " --search ${getLuaPath path "lib"}"
   );
 
 in
