49c5d791a05b84350616c362b26dd0d30ebba478
Robert Helgesson <robert@rycee.net>
2018-09-14 23:33:43 +0200

    wip

--- modules/programs/alot-accounts.nix
+++ modules/programs/alot-accounts.nix
@@ -1,26 +1,24 @@
 { config, lib, ... }:
 
 with lib;
+
 {
-
   options.alot = {
-    enable = mkEnableOption "alot";
-
-    contactCompletionCommand = mkOption {
-      type = types.nullOr types.str;
-      default = null;
-      description = ''
-        Command to run to send a message.
-        See <link xlink:href="https://alot.readthedocs.io/en/latest/configuration/contacts_completion.html" /> for more advice.
-        '';
-    };
-
     sendMailCommand = mkOption {
       type = types.nullOr types.str;
-      default = null;
       description = ''
-        Command to send a mail.
+        Command to send a mail. If msmtp is enabled for the account,
+        then this is set to
+        <command>msmtpq --account=&lt;name&gt; -t</command>.
       '';
     };
+  };
+
+  config = mkIf config.notmuch.enable {
+    alot.sendMailCommand = mkOptionDefault (
+      if config.msmtp.enable
+      then "msmtpq --account=${config.name} -t"
+      else null
+    );
   };
 }
