8d4c65f259a2f5508d6a1c667f02facafb3b3010
Robert Helgesson <robert@rycee.net>
2018-10-30 00:09:20 +0100

    fzf: only enable when line editing is available
    Fixes #401
    
    Suggested-by: Alex Vorobiev
    Suggested-by: Mario Rodas

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -122,13 +122,17 @@ in
       );
 
     programs.bash.initExtra = mkIf cfg.enableBashIntegration ''
-      . ${pkgs.fzf}/share/fzf/completion.bash
-      . ${pkgs.fzf}/share/fzf/key-bindings.bash
+      if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
+        . ${pkgs.fzf}/share/fzf/completion.bash
+        . ${pkgs.fzf}/share/fzf/key-bindings.bash
+      fi
     '';
 
     programs.zsh.initExtra = mkIf cfg.enableZshIntegration ''
-      . ${pkgs.fzf}/share/fzf/completion.zsh
-      . ${pkgs.fzf}/share/fzf/key-bindings.zsh
+      if [[ $options[zle] = on ]]; then
+        . ${pkgs.fzf}/share/fzf/completion.zsh
+        . ${pkgs.fzf}/share/fzf/key-bindings.zsh
+      fi
     '';
   };
 }
