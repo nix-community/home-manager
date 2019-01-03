39992ef3a06cdcb34dd9672b2e9fd1be728d001d
wedens <kirill.wedens@gmail.com>
2019-01-03 16:12:41 +0700

    lesspipe: add colorizer option

--- modules/programs/lesspipe.nix
+++ modules/programs/lesspipe.nix
@@ -8,12 +8,25 @@ with lib;
   options = {
     programs.lesspipe = {
       enable = mkEnableOption "lesspipe preprocessor for less";
+
+      colorizer = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        example = literalExample "${pkgs.python3Packages.pygments}/bin/pygmentize";
+        description = ''
+          Highlight syntax in less using specified highlighter.
+        '';
+      };
     };
   };
 
   config = mkIf config.programs.lesspipe.enable {
     home.sessionVariables = {
       LESSOPEN = "|${pkgs.lesspipe}/bin/lesspipe.sh %s";
+    }
+    //
+    optionalAttrs (config.colorizer != null) {
+      LESSCOLORIZER = config.colorizer;
     };
   };
 }
