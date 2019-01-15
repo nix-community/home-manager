9ae70ebfe9d1e60d655dc1b8ea455ea32a561439
Adam Washington <rprospero@gmail.com>
2019-01-15 14:29:06 +0000

    Added requested changes

--- modules/programs/matplotlib.nix
+++ modules/programs/matplotlib.nix
@@ -7,29 +7,29 @@ let
   cfg = config.programs.matplotlib;
 
   formatLine = o: n: v:
-    let
-      formatValue = v:
-        if isBool v then (if v then "True" else "False")
-        else toString v;
-    in
-      if isAttrs v
-      then concatStringsSep "\n" ([] ++ mapAttrsToList (formatLine "${o}${n}.") v)
-      else "${o}${n}: ${formatValue v}";
+  let
+    formatValue = v:
+      if isBool v then (if v then "True" else "False")
+      else toString v;
+  in
+    if isAttrs v
+    then concatStringsSep "\n" ([] ++ mapAttrsToList (formatLine "${o}${n}.") v)
+    else (if v == "" then "" else "${o}${n}: ${formatValue v}");
 in
 
 {
-    meta.maintainers = [ maintainers.rprospero];
+  meta.maintainers = [ maintainers.rprospero];
 
-    options.programs.matplotlib = {
-      enable = mkEnableOption ''
-        matplotlib, a plotting library for python'';
+  options.programs.matplotlib = {
+    enable = mkEnableOption "matplotlib, a plotting library for python";
 
-      rc = mkOption {
-        default = {};
-        type = with types; attrsOf (either attrs (either str (either bool int)));
-        description = ''
-          Add terms to the matplotlibrc file, control the default matplotlib dehavior'';
-        example = {
+    config = mkOption {
+      default = {};
+      type = with types; attrsOf (either attrs (either str (either bool int)));
+      description = ''
+        Add terms to the matplotlibrc file, control the default matplotlib behavior.'';
+      example = literalExample  ''
+        {
           backend = "Qt5Agg";
           axes = {
             grid = true;
@@ -38,22 +38,23 @@ in
           };
           grid.color = "FF9900";
         };
-      };
-
-      extraConfig = mkOption {
-        type = types.lines;
-        default = "";
-        description = ''
-          Additional commands for matplotlib that will be added to the
-          <filename>matplotlibrc</filename> file.
         '';
-      };
     };
-    config = mkIf cfg.enable {
-      xdg.configFile."matplotlib/matplotlibrc".text =
-      concatStringsSep "\n" ([]
-        ++ mapAttrsToList (formatLine "") cfg.rc
-        ++ optional (cfg.extraConfig != "") cfg.extraConfig
-      ) + "\n";
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = ''
+        Additional commands for matplotlib that will be added to the
+        <filename>matplotlibrc</filename> file.
+      '';
+    };
+  };
+  config = mkIf cfg.enable {
+    xdg.configFile."matplotlib/matplotlibrc".text =
+    concatStringsSep "\n" ([]
+      ++ mapAttrsToList (formatLine "") cfg.config
+      ++ optional (cfg.extraConfig != "") cfg.extraConfig
+    ) + "\n";
   };
 }
