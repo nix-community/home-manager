45cadbd4f304a3dcb7b3fcd47032a39fac715f6f
Robert Helgesson <robert@rycee.net>
2019-01-29 18:17:24 +0100

    git: quote sendemail section header
    This will allow, e.g., the character `@` in the email identity.
    
    Also adds a test case.
    
    Fixes #557

---
+++ tests/modules/accounts/email-test-accounts.nix
@@ -0,0 +1,27 @@
+{ ... }:
+
+{
+  accounts.email = {
+    maildirBasePath = "Mail";
+
+    accounts = {
+      "hm@example.com" = {
+        address = "hm@example.com";
+        userName = "home.manager";
+        realName = "H. M. Test";
+        passwordCommand = "password-command";
+        imap.host = "imap.example.com";
+        smtp.host = "smtp.example.com";
+      };
+
+      hm-account = {
+        address = "hm@example.org";
+        userName = "home.manager.jr";
+        realName = "H. M. Test Jr.";
+        passwordCommand = "password-command 2";
+        imap.host = "imap.example.org";
+        smtp.host = "smtp.example.org";
+      };
+    };
+  };
+}
---
+++ tests/modules/programs/git-with-email.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  imports = [ ../accounts/email-test-accounts.nix ];
+
+  config = {
+    programs.git = {
+      enable = true;
+      userEmail = "hm@example.com";
+      userName = "H. M. Test";
+    };
+
+    nmt.script = ''
+      function assertGitConfig() {
+        local value
+        value=$(${pkgs.git}/bin/git config \
+          --file $TESTED/home-files/.config/git/config \
+          --get $1)
+        if [[ $value != $2 ]]; then
+          fail "Expected option '$1' to have value '$2' but it was '$value'"
+        fi
+      }
+
+      assertFileExists home-files/.config/git/config
+      assertFileContent home-files/.config/git/config ${./git-with-email-expected.conf}
+
+      assertGitConfig "sendemail.hm@example.com.from" "hm@example.com"
+      assertGitConfig "sendemail.hm-account.from" "hm@example.org"
+    '';
+  };
+}
