f89522362fd3f0e9487eebe32acafb0fc11d2504
Robert Helgesson <robert@rycee.net>
2019-02-01 01:06:18 +0100

    git: use `attrsOf` instead of `attrs`
    This makes sure that values added to
    
        programs.git.aliases
    
    or
    
        programs.git.extraConfig
    
    are merged as expected.
    
    Also add a few option examples.
    
    (cherry picked from commit 445c0b1482c38172a9f8294ee16a7ca7462388e5)

--- tests/modules/programs/git.nix
+++ tests/modules/programs/git.nix
@@ -4,33 +4,41 @@ with lib;
 
 {
   config = {
-    programs.git = {
-      enable = true;
-      aliases = {
-        a1 = "foo";
-        a2 = "bar";
-      };
-      extraConfig = {
-        extra = {
-          name = "value";
+    programs.git = mkMerge [
+      {
+        enable = true;
+        aliases = {
+          a1 = "foo";
+          a2 = "bar";
         };
-      };
-      ignores = [ "*~" "*.swp" ];
-      includes = [
-        { path = "~/path/to/config.inc"; }
-        {
-          path = "~/path/to/conditional.inc";
-          condition = "gitdir:~/src/dir";
-        }
-      ];
-      signing = {
-        gpgPath = "path-to-gpg";
-        key = "00112233445566778899AABBCCDDEEFF";
-        signByDefault = true;
-      };
-      userEmail = "user@example.org";
-      userName = "John Doe";
-    };
+        extraConfig = {
+          extra = {
+            name = "value";
+          };
+        };
+        ignores = [ "*~" "*.swp" ];
+        includes = [
+          { path = "~/path/to/config.inc"; }
+          {
+            path = "~/path/to/conditional.inc";
+            condition = "gitdir:~/src/dir";
+          }
+        ];
+        signing = {
+          gpgPath = "path-to-gpg";
+          key = "00112233445566778899AABBCCDDEEFF";
+          signByDefault = true;
+        };
+        userEmail = "user@example.org";
+        userName = "John Doe";
+      }
+
+      {
+        aliases.a2 = mkForce "baz";
+        extraConfig.extra.boolean = true;
+        extraConfig.extra.integer = 38;
+      }
+    ];
 
     nmt.script = ''
       assertFileExists home-files/.config/git/config
