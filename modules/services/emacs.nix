67c4474a02183d8ba9f2ad6efa66b844dbf4ae93
Ben Sima <ben@bsima.me>
2019-02-07 16:14:28 -0800

    emacs: formatting

--- modules/services/emacs.nix
+++ modules/services/emacs.nix
@@ -3,19 +3,22 @@
 with lib;
 
 let
+
   cfg = config.services.emacs;
   emacs = config.programs.emacs;
+
 in
 {
   options.services.emacs = {
     enable = mkEnableOption "Emacs daemon systemd service";
   };
+
   config = mkIf cfg.enable {
     systemd.user.services.emacs = {
       Unit = {
         Description = "Emacs: the extensible, self-documenting text editor";
         Documentation = "info:emacs man:emacs(1) https://gnu.org/software/emacs/";
-        };
+      };
 
       Service = {
         Type = "simple";
