8980e08240eb5a9be98e3c7beebb4cb18029698c
Robert Helgesson <robert@rycee.net>
2019-02-17 23:25:42 +0100

    msmtp: use `<parameter>` for CLI options in description
    (cherry picked from commit 93f5fcae1e5c664050d3e75bd3cba5e6a6a95e53)

--- modules/programs/msmtp-accounts.nix
+++ modules/programs/msmtp-accounts.nix
@@ -11,14 +11,14 @@ with lib;
         Whether to enable msmtp.
         </para><para>
         If enabled then it is possible to use the
-        <option>--account</option> command line option to send a
-        message for a given account using the <command>msmtp</command>
-        or <command>msmtpq</command> tool. For example,
-        <command>msmtp --account=private</command>
-        would send using the account defined in
+        <parameter class="command">--account</parameter> command line
+        option to send a message for a given account using the
+        <command>msmtp</command> or <command>msmtpq</command> tool.
+        For example, <command>msmtp --account=private</command> would
+        send using the account defined in
         <option>accounts.email.accounts.private</option>. If the
-        <option>--account</option> option is not given then the
-        primary account will be used.
+        <parameter class="command">--account</parameter> option is not
+        given then the primary account will be used.
       '';
     };
 
