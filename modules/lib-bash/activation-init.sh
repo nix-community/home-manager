47f93faf344691fb9dcf7f04218da22a7d13b10e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-20 22:53:35 -0800

    create the profile/gc-root

--- modules/lib-bash/activation-init.sh
+++ modules/lib-bash/activation-init.sh
@@ -5,39 +5,30 @@ function setupVars() {
     local gcPath="/nix/var/nix/gcroots/per-user/$USER"
     local greatestGenNum
 
-    newGenNum=1
-
-    if [[ -d "${profilesPath}" ]]; then
-        greatestGenNum=$( \
-            find "$profilesPath" -name 'home-manager-*-link' \
-                | sed 's/^.*-\([0-9]*\)-link$/\1/' \
-                | sort -rn \
-                | head -1)
-
-        if [[ -n $greatestGenNum ]] ; then
-            oldGenNum=$greatestGenNum
-            newGenNum=$((oldGenNum + 1))
-        fi
+    if [[ ! -d "${profilesPath}" ]]; then
+        mkdir -p "${profilesPath}"
     fi
+
+    if [[ ! -d "${gcPath}" ]]; then
+        mkdir -p "${gcPath}"
+    fi
+
+    greatestGenNum=$( \
+        find "$profilesPath" -name 'home-manager-*-link' \
+            | sed 's/^.*-\([0-9]*\)-link$/\1/' \
+            | sort -rn \
+            | head -1)
+
+    if [[ -n $greatestGenNum ]] ; then
+        oldGenNum=$greatestGenNum
+    else
+        oldGenNum=0
+    fi
+    newGenNum=$((oldGenNum + 1))
 
     if [[ -e $gcPath/current-home ]] ; then
         oldGenPath="$(readlink -e "$gcPath/current-home")"
     fi
-
-    $VERBOSE_ECHO "Sanity checking oldGenNum and oldGenPath"
-    if [[ -v oldGenNum && ! -v oldGenPath
-            || ! -v oldGenNum && -v oldGenPath ]]; then
-        errorEcho "Invalid profile number and GC root values! These must be"
-        errorEcho "either both empty or both set but are now set to"
-        errorEcho "    '${oldGenNum:-}' and '${oldGenPath:-}'"
-        errorEcho "If you don't mind losing previous profile generations then"
-        errorEcho "the easiest solution is probably to run"
-        errorEcho "   rm $profilesPath/home-manager*"
-        errorEcho "   rm $gcPath/current-home"
-        errorEcho "and trying home-manager switch again. Good luck!"
-        exit 1
-    fi
-
 
     genProfilePath="$profilesPath/home-manager"
     newGenPath="@GENERATION_DIR@";
