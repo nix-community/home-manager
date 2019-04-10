776dd45451c54965549f77357e4a4e792be09062
Nick Hu <me@nickhu.co.uk>
2019-04-10 14:24:35 +0100

    make notmuch search.exclude_tags configurable

--- modules/programs/notmuch.nix
+++ modules/programs/notmuch.nix
@@ -46,7 +46,7 @@ let
           };
 
         search = {
-          exclude_tags = [ "deleted" "spam" ];
+          exclude_tags = cfg.search.excludeTags;
         };
       }
       cfg.extraConfig;
@@ -135,6 +135,19 @@ in
           default = true;
           description = ''
             Whether to synchronize Maildir flags.
+          '';
+        };
+      };
+
+      search = {
+        excludeTags = mkOption {
+          type = types.listOf types.str;
+          default = [ "deleted" "spam" ];
+          example = [ "trash" "spam" ];
+          description = ''
+            A  list  of  tags  that  will be excluded from search results by
+            default. Using an excluded tag in a  query  will  override  that
+            exclusion.
           '';
         };
       };
