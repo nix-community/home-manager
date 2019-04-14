0ef19ce5646c2e3e6a6cf666d68ee9d8449c1d22
Robert Helgesson <robert@rycee.net>
2019-04-14 14:25:47 +0200

    Random fixes

--- modules/programs/broot.nix
+++ modules/programs/broot.nix
@@ -20,7 +20,10 @@ let
       '';
 
   brootConf = {
-    verbs = mapAttrsToList (name: value: value // { invocation = name; }) cfg.verbs;
+    verbs =
+      mapAttrsToList
+        (name: value: value // { invocation = name; })
+        cfg.verbs;
     skin = cfg.skin;
   };
 
@@ -30,9 +33,7 @@ in
   meta.maintainers = [ maintainers.aheaume ];
 
   options.programs.broot = {
-    enable = mkEnableOption ''
-      Broot, a better way to navigate directories
-    '';
+    enable = mkEnableOption "Broot, a better way to navigate directories";
 
     enableBashIntegration = mkOption {
       default = true;
@@ -59,89 +60,93 @@ in
     };
 
     verbs = mkOption {
-      type = types.attrsOf types.attrs;
+      type = with types; attrsOf (attrsOf (either bool str));
       default = {
         "p" = { execution = ":parent"; };
         "edit" = { shortcut = "e"; execution = "$EDITOR {file}" ; };
         "create {subpath}" = { execution = "$EDITOR {directory}/{subpath}"; };
         "view" = { execution = "less {file}"; };
       };
-      example = {
-        "p" = { execution = ":parent"; };
-        "edit" = { shortcut = "e"; execution = "$EDITOR {file}" ; };
-        "create {subpath}" = { execution = "$EDITOR {directory}/{subpath}"; };
-        "view" = { execution = "less {file}"; };
-        "blop {name}\\.{type}" = {
-          execution = "/bin/mkdir {parent}/{type} && /usr/bin/nvim {parent}/{type}/{name}.{type}";
-          from_shell = true;
-        };
-      };
+      example = literalExample ''
+        {
+          "p" = { execution = ":parent"; };
+          "edit" = { shortcut = "e"; execution = "$EDITOR {file}" ; };
+          "create {subpath}" = { execution = "$EDITOR {directory}/{subpath}"; };
+          "view" = { execution = "less {file}"; };
+          "blop {name}\\.{type}" = {
+            execution = "/bin/mkdir {parent}/{type} && /usr/bin/nvim {parent}/{type}/{name}.{type}";
+            from_shell = true;
+          };
+        }
+      '';
       description = ''
-        Define new verbs.
+        Define new verbs. The attribute name indicates how the verb is
+        called by the user, with placeholders for arguments.
         </para><para>
         The possible attributes are:
         </para>
 
-        <para><variablelist>
-          <varlistentry>
-            <term><literal>invocation</literal> (mandatory)</term>
-            <listitem><para>how the verb is called by the user, with placeholders for arguments</para></listitem>
-          </varlistentry>
+        <para>
+        <variablelist>
           <varlistentry>
             <term><literal>execution</literal> (mandatory)</term>
             <listitem><para>how the verb is executed</para></listitem>
           </varlistentry>
           <varlistentry>
             <term><literal>shortcut</literal> (optional)</term>
-            <listitem><para>an alternate way to call the verb (without the arguments part)</para></listitem>
+            <listitem><para>an alternate way to call the verb (without
+            the arguments part)</para></listitem>
           </varlistentry>
           <varlistentry>
             <term><literal>leave_broot</literal> (optional)</term>
-            <listitem><para>whether to quit broot on execution (default: <literal>true</literal>)</para></listitem>
+            <listitem><para>whether to quit broot on execution
+            (default: <literal>true</literal>)</para></listitem>
           </varlistentry>
           <varlistentry>
             <term><literal>from_shell</literal> (optional)</term>
-            <listitem><para>whether the verb must be executed from the parent shell (needs br, default: <literal>false</literal>)</para></listitem>
+            <listitem><para>whether the verb must be executed from the
+            parent shell (default:
+            <literal>false</literal>)</para></listitem>
           </varlistentry>
         </variablelist>
       '';
-
     };
 
     skin = mkOption {
       type = types.attrsOf types.str;
       default = {};
-      example = {
-        status_normal_fg = "grayscale(18)";
-        status_normal_bg = "grayscale(3)";
-        status_error_fg = "red";
-        status_error_bg = "yellow";
-        tree_fg = "red";
-        selected_line_bg = "grayscale(7)";
-        permissions_fg = "grayscale(12)";
-        size_bar_full_bg = "red";
-        size_bar_void_bg = "black";
-        directory_fg = "lightyellow";
-        input_fg = "cyan";
-        flag_value_fg = "lightyellow";
-        table_border_fg = "red";
-        code_fg = "lightyellow";
-      };
+      example = literalExample ''
+        {
+          status_normal_fg = "grayscale(18)";
+          status_normal_bg = "grayscale(3)";
+          status_error_fg = "red";
+          status_error_bg = "yellow";
+          tree_fg = "red";
+          selected_line_bg = "grayscale(7)";
+          permissions_fg = "grayscale(12)";
+          size_bar_full_bg = "red";
+          size_bar_void_bg = "black";
+          directory_fg = "lightyellow";
+          input_fg = "cyan";
+          flag_value_fg = "lightyellow";
+          table_border_fg = "red";
+          code_fg = "lightyellow";
+        }
+      '';
       description = ''
-        Change the colors.
+        Color configuration.
         </para><para>
-        Complete list of keys (expected to change before the v1 of broot)
-        </para>
+        Complete list of keys (expected to change before the v1 of broot):
 
-        <para><itemizedlist>
+        <itemizedlist>
           <listitem><para><literal>char_match</literal></para></listitem>
           <listitem><para><literal>code</literal></para></listitem>
           <listitem><para><literal>directory</literal></para></listitem>
+          <listitem><para><literal>exe</literal></para></listitem>
           <listitem><para><literal>file</literal></para></listitem>
           <listitem><para><literal>file_error</literal></para></listitem>
           <listitem><para><literal>flag_label</literal></para></listitem>
           <listitem><para><literal>flag_value</literal></para></listitem>
-          <listitem><para><literal>exe</literal></para></listitem>
           <listitem><para><literal>input</literal></para></listitem>
           <listitem><para><literal>link</literal></para></listitem>
           <listitem><para><literal>permissions</literal></para></listitem>
@@ -158,7 +163,8 @@ in
         </itemizedlist></para>
 
         <para>
-        Add _fg for a foreground color while _bg is for background colors.
+        Add <literal>_fg</literal> for a foreground color and
+        <literal>_bg</literal> for a background colors.
       '';
     };
   };
@@ -166,9 +172,7 @@ in
   config = mkIf cfg.enable {
     home.packages = [ pkgs.broot ];
 
-    xdg.configFile."broot/conf.toml" = {
-      source = configFile brootConf;
-    };
+    xdg.configFile."broot/conf.toml".source = configFile brootConf;
 
     # Dummy file to prevent broot from trying to reinstall itself
     xdg.configFile."broot/launcher/installed".text = "";
