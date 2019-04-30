nix-community/home-manager

commit c94eaa0e6c2cba37e38ff5e137d87b97d67b79b9

files: replace unnecessary use of xargs

modules/files.nix
@@ -74,8 +74,8 @@ in
         function checkNewGenCollision() {
           local newGenFiles
           newGenFiles="$(readlink -e "$newGenPath/home-files")"
-          find "$newGenFiles" -type f -print0 -or -type l -print0 \
-                  | xargs -0 bash ${check} "$newGenFiles"
+          find "$newGenFiles" -type f -or -type l \
+              -exec bash ${check} "$newGenFiles" {} +
         }
 
         checkNewGenCollision || exit 1
@@ -155,8 +155,8 @@ in
 
             local newGenFiles
             newGenFiles="$(readlink -e "$newGenPath/home-files")"
-            find "$newGenFiles" -type f -print0 -or -type l -print0 \
-              | xargs -0 bash ${link} "$newGenFiles"
+            find "$newGenFiles" -type f -or -type l \
+              -exec bash ${link} "$newGenFiles" {} +
           }
 
           function cleanOldGen() {

commit 9577a477c6028aaa5fbb4f8908febf8d8d89983f

fix "find" invocation broken in c94eaa0e
Add parens to expression so the `-exec` includes
files matching both.

Otherwise (before this change) the `-exec` is
only invoked for links (`-type l`):

file or (link -> doexec)
=>
(file or link) -> doexec

modules/files.nix
@@ -74,7 +74,7 @@ in
         function checkNewGenCollision() {
           local newGenFiles
           newGenFiles="$(readlink -e "$newGenPath/home-files")"
-          find "$newGenFiles" -type f -or -type l \
+          find "$newGenFiles" \( -type f -or -type l \) \
               -exec bash ${check} "$newGenFiles" {} +
         }
 
@@ -155,7 +155,7 @@ in
 
             local newGenFiles
             newGenFiles="$(readlink -e "$newGenPath/home-files")"
-            find "$newGenFiles" -type f -or -type l \
+            find "$newGenFiles" \( -type f -or -type l \) \
               -exec bash ${link} "$newGenFiles" {} +
           }
 