29824a8cf6eabb32343a4af63d27ad6775c238ff
Sebastián Estrella <sestrella.me@gmail.com>
2019-06-04 06:21:20 -0500

    tmux: Disable confirmation prompt

--- tests/modules/programs/tmux/default.nix
+++ tests/modules/programs/tmux/default.nix
@@ -3,4 +3,5 @@
   tmux-not-enabled = ./not-enabled.nix;
   tmux-vi-all-true = ./vi-all-true.nix;
   tmux-secure-socket-enabled = ./secure-socket-enabled.nix;
+  tmux-disable-confirmation-prompt = ./disable-confirmation-prompt.nix;
 }
