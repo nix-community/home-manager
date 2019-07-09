d1bf1ba7543be22cd494867ac5d92b44561837b9
Robert Helgesson <robert@rycee.net>
2019-07-10 01:52:36 +0200

    wip

--- modules/programs/khal-calendar-accounts.nix
+++ modules/programs/khal-calendar-accounts.nix
@@ -6,7 +6,18 @@ with lib;
   options.khal = {
     type = mkOption {
       type = types.nullOr (types.enum [ "calendar" "discover"]);
+      default = null;
       description = ''
+        There is no description of this option.
+      '';
+    };
+
+    glob = mkOption {
+      type = types.str;
+      default = "*";
+      description = ''
+        The glob expansion to be searched for events or birthdays when
+        type is set to discover.
       '';
     };
   };
