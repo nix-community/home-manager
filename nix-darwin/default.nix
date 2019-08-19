071f5495015a20bd1a9a048f0990929fcb8d2eae
toonn <toonn@toonn.io>
2019-08-19 17:22:27 +0200

    nix-darwin: run sudo with `-s`
    Currently activation is run with `sudo -i` this defaults to the user's
    login shell. This can lead to problems if the user's shell isn't set
    properly.
    
    By passing `-s` rather than `-i`, `sudo` runs `activate` in `SHELL`
    instead. We assume that at this point in the activation `SHELL`
    contains the path to a bash in the nix store. This should always be a
    valid shell to run the `activate` script with.
    
    From the `sudo` manual it seems like this cannot be fixed if `SHELL`
    isn't set at this point or by passing a command to `-s` because that
    command is then passed to the user's shell.

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -99,7 +99,7 @@ in
     system.activationScripts.postActivation.text =
       concatStringsSep "\n" (mapAttrsToList (username: usercfg: ''
         echo Activating home-manager configuration for ${username}
-        sudo -u ${username} -i ${pkgs.writeShellScript "activation-${username}" ''
+        sudo -u ${username} -s ${pkgs.writeShellScript "activation-${username}" ''
           ${lib.optionalString (cfg.backupFileExtension != null)
             "export HOME_MANAGER_BACKUP_EXT=${lib.escapeShellArg cfg.backupFileExtension}"}
           ${lib.optionalString cfg.verbose "export VERBOSE=1"}
