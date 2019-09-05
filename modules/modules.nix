aa5ba177cc55b89aab8622e7dd011f1092cc3bdc
pacien <pacien.trangirard@pacien.net>
2019-09-05 11:18:43 +0200

    xdg-user-dirs: add module

--- modules/modules.nix
+++ modules/modules.nix
@@ -34,6 +34,7 @@ let
     (loadModule ./misc/submodule-support.nix { })
     (loadModule ./misc/version.nix { })
     (loadModule ./misc/xdg.nix { })
+    (loadModule ./misc/xdg-user-dirs.nix { condition = hostPlatform.isLinux; })
     (loadModule ./programs/afew.nix { })
     (loadModule ./programs/alacritty.nix { })
     (loadModule ./programs/alot.nix { })
