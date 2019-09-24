43852e9228958c56db8e58028ad1ee71b4091274
Ryan Orendorff <ryan@orendorff.io>
2019-09-24 01:19:35 -0700

    fish: plugins uses pluginModule type, add example

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -110,12 +110,35 @@ in
     };
 
     programs.fish.plugins = mkOption {
-      type = types.listOf types.package;
+      type = types.listOf pluginModule;
       default = [];
+      example = literalExample ''
+        [
+          {
+            name = "z";
+            src = pkgs.fetchFromGitHub {
+              owner = "jethrokuan";
+              repo = "z";
+              rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+              sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+            };
+          }
+
+          # oh-my-fish plugins are stored in their own repositories, which
+          # makes them simple to import into home-manager.
+          {
+            name = "fasd";
+            src = pkgs.fetchFromGitHub {
+              owner = "oh-my-fish";
+              repo = "plugin-fasd";
+              rev = "38a5b6b6011106092009549e52249c6d6f501fba";
+              sha256 = "06v37hqy5yrv5a6ssd1p3cjd9y3hnp19d3ab7dag56fs1qmgyhbs";
+            };
+          }
+        ]
+      '';
       description = ''
-        The plugins to add to fish.
-        Built with <varname>buildFishPlugin</varname>.
-        Overrides manually installed ones.
+        The plugins to source in conf.d/99plugins.fish
       '';
     };
 
