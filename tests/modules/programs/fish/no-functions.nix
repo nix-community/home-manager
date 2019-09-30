111011b2c29243a41363e9f71ce29c93cd74c20f
Ryan Orendorff <ryan@orendorff.io>
2019-09-30 00:11:36 -0700

    fish: add some tests
    - If a function is defined, check that the function file exists and
      that the contents matches a given string.
    
    - If no functions exists, the functions folder should not exist.
    
    - Verify plugin functionality.

---
+++ tests/modules/programs/fish/no-functions.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+
+with lib;
+
+{
+  config = {
+    programs.fish = {
+      enable = true;
+
+      functions = { };
+    };
+
+    nmt = {
+      description =
+        "if fish.functions is blank, the functions folder should not exist.";
+      script = ''
+        assertPathNotExists home-files/.config/fish/functions
+      '';
+
+    };
+  };
+}
