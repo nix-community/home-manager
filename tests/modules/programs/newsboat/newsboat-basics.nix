4113cb764551027a8bbab64d01b6b1a747822113
Robert Helgesson <robert@rycee.net>
2019-10-07 23:51:12 +0200

    newsboat: support feed titles
    Fixes #861

---
+++ tests/modules/programs/newsboat/newsboat-basics.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  config = {
+    programs.newsboat = {
+      enable = true;
+
+      urls = [
+        {
+          url = "http://example.org/feed.xml";
+          tags = [ "tag1" "tag2" ];
+          title = "Cool feed";
+        }
+
+        {
+          url = "http://example.org/feed2.xml";
+        }
+      ];
+
+      queries = {
+        "foo" = "rssurl =~ \"example.com\"";
+      };
+    };
+
+    nixpkgs.overlays = [
+      (self: super: {
+        newsboat = pkgs.writeScriptBin "dummy-newsboat" "";
+      })
+    ];
+
+    nmt.script = ''
+      assertFileContent \
+        home-files/.newsboat/urls \
+        ${./newsboat-basics-urls.txt}
+    '';
+  };
+}
