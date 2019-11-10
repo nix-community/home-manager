595150be86639cd1e41c9eba1cf02dc503d10c23
Pasquale <p3dimaria@hotmail.it>
2019-11-11 00:43:42 +0100

    vscode: correct base path from which to pull extensions

--- modules/programs/vscode.nix
+++ modules/programs/vscode.nix
@@ -71,12 +71,15 @@ in
     # Adapted from https://discourse.nixos.org/t/vscode-extensions-setup/1801/2
     home.file =
       let
-        toPaths = p:
-          # Links every dir in p to the extension path.
-          mapAttrsToList (k: v:
-            {
-              "${extensionPath}/${k}".source = "${p}/${k}";
-            }) (builtins.readDir p);
+        toPaths = path:
+          let
+            p = "${path}/share/vscode/extensions";
+          in
+            # Links every dir in p to the extension path.
+            mapAttrsToList (k: v:
+              {
+                "${extensionPath}/${k}".source = "${p}/${k}";
+              }) (builtins.readDir p);
         toSymlink = concatMap toPaths cfg.extensions;
       in
         foldr
