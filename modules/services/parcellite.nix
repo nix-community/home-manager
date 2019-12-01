fba3331eb70c249fbef05e0e805a998a03d263c4
Robin Stumm <serverkorken@gmail.com>
2019-12-01 15:50:30 +0100

    parcellite: remove obsolete backward compatibility fixes
    This also fixes that the `gtk.theme` option was not picked up due to hardcoded XDG_DATA_DIRS.

--- modules/services/parcellite.nix
+++ modules/services/parcellite.nix
@@ -33,12 +33,6 @@ in
       };
 
       Service = {
-        # PATH have been added in nixpkgs.parcellite, keeping it here for
-        # backward compatibility. XDG_DATA_DIRS is necessary to make it pick up
-        # icons correctly.
-        Environment = ''
-          PATH=${package}/bin:${pkgs.which}/bin:${pkgs.xdotool}/bin XDG_DATA_DIRS=${pkgs.hicolor_icon_theme}/share
-        '';
         ExecStart = "${package}/bin/parcellite";
         Restart = "on-abort";
       };
