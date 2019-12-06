e79d4d9b0ac4721034ddefecf7fa8bf2987356f5
Vojtěch Káně <vojtech.kane@gmail.com>
2019-12-06 21:14:31 +0100

    readline: reformat

--- modules/programs/readline.nix
+++ modules/programs/readline.nix
@@ -1,8 +1,13 @@
 { config, lib, pkgs, ... }:
+
 with lib;
+
 let
+
   cfg = config.programs.readline;
+
 in
+
 {
   options.programs.readline = {
     enable = mkEnableOption "readline";
@@ -11,34 +16,38 @@ in
       default = {};
       type = types.attrsOf types.str;
       example = { "\C-h" = "backward-kill-word"; };
-      description = "Readline bindings";
+      description = "Readline bindings.";
     };
 
-    includeSystem = mkOption {
+    includeSystemConfig = mkOption {
       type = types.bool;
       default = true;
-      description = "Whether to include the system-wide config";
+      description = "Whether to include the system-wide configuration.";
     };
 
     extraConfig = mkOption {
       type = types.lines;
       default = "";
-      description = "Config lines appended to the end unchanged. Useful for configuration not yet supported by home-manager.";
+      description = ''
+        Configuration lines appended unchanged to the end of the
+        <filename>~/.inputrc</filename> file.
+      '';
     };
   };
+
   config = mkIf cfg.enable {
     home.packages = [ pkgs.readline ];
     home.file.".inputrc".text =
-    let
-      includeSystemStr = if cfg.includeSystem then "$include /etc/inputrc" else "";
-      bindingsStr = concatStringsSep "\n" (
-        mapAttrsToList (k: v: "\"${k}\": ${v}") cfg.bindings
-      );
-    in
-      ''
-        ${includeSystemStr}
-        ${bindingsStr}
-        ${cfg.extraConfig}
-      '';
+      let
+        includeSystemStr = if cfg.includeSystem then "$include /etc/inputrc" else "";
+        bindingsStr = concatStringsSep "\n" (
+          mapAttrsToList (k: v: "\"${k}\": ${v}") cfg.bindings
+        );
+      in
+        ''
+          ${includeSystemStr}
+          ${bindingsStr}
+          ${cfg.extraConfig}
+        '';
   };
 }
