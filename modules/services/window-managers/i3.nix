42309437abcf1501bbd97b5755af05e58ece4c12
Michael Hoang <Enzime@users.noreply.github.com>
2019-12-27 15:24:43 +1100

    i3: Update default i3 key bindings
    Update to match the default i3 key bindings
    
    https://github.com/i3/i3/blob/master/etc/config

--- modules/services/window-managers/i3.nix
+++ modules/services/window-managers/i3.nix
@@ -455,26 +455,33 @@ let
 
           "${cfg.config.modifier}+Shift+space" = "floating toggle";
           "${cfg.config.modifier}+space" = "focus mode_toggle";
+          
+          "${cfg.config.modifier}+a" = "focus parent";
+          
+          "${cfg.config.modifier}+Shift+minus" = "move scratchpad";
+          "${cfg.config.modifier}+minus" = "scratchpad show";
 
-          "${cfg.config.modifier}+1" = "workspace 1";
-          "${cfg.config.modifier}+2" = "workspace 2";
-          "${cfg.config.modifier}+3" = "workspace 3";
-          "${cfg.config.modifier}+4" = "workspace 4";
-          "${cfg.config.modifier}+5" = "workspace 5";
-          "${cfg.config.modifier}+6" = "workspace 6";
-          "${cfg.config.modifier}+7" = "workspace 7";
-          "${cfg.config.modifier}+8" = "workspace 8";
-          "${cfg.config.modifier}+9" = "workspace 9";
+          "${cfg.config.modifier}+1" = "workspace number 1";
+          "${cfg.config.modifier}+2" = "workspace number 2";
+          "${cfg.config.modifier}+3" = "workspace number 3";
+          "${cfg.config.modifier}+4" = "workspace number 4";
+          "${cfg.config.modifier}+5" = "workspace number 5";
+          "${cfg.config.modifier}+6" = "workspace number 6";
+          "${cfg.config.modifier}+7" = "workspace number 7";
+          "${cfg.config.modifier}+8" = "workspace number 8";
+          "${cfg.config.modifier}+9" = "workspace number 9";
+          "${cfg.config.modifier}+0" = "workspace number 10";
 
-          "${cfg.config.modifier}+Shift+1" = "move container to workspace 1";
-          "${cfg.config.modifier}+Shift+2" = "move container to workspace 2";
-          "${cfg.config.modifier}+Shift+3" = "move container to workspace 3";
-          "${cfg.config.modifier}+Shift+4" = "move container to workspace 4";
-          "${cfg.config.modifier}+Shift+5" = "move container to workspace 5";
-          "${cfg.config.modifier}+Shift+6" = "move container to workspace 6";
-          "${cfg.config.modifier}+Shift+7" = "move container to workspace 7";
-          "${cfg.config.modifier}+Shift+8" = "move container to workspace 8";
-          "${cfg.config.modifier}+Shift+9" = "move container to workspace 9";
+          "${cfg.config.modifier}+Shift+1" = "move container to workspace number 1";
+          "${cfg.config.modifier}+Shift+2" = "move container to workspace number 2";
+          "${cfg.config.modifier}+Shift+3" = "move container to workspace number 3";
+          "${cfg.config.modifier}+Shift+4" = "move container to workspace number 4";
+          "${cfg.config.modifier}+Shift+5" = "move container to workspace number 5";
+          "${cfg.config.modifier}+Shift+6" = "move container to workspace number 6";
+          "${cfg.config.modifier}+Shift+7" = "move container to workspace number 7";
+          "${cfg.config.modifier}+Shift+8" = "move container to workspace number 8";
+          "${cfg.config.modifier}+Shift+9" = "move container to workspace number 9";
+          "${cfg.config.modifier}+Shift+0" = "move container to workspace number 10";
 
           "${cfg.config.modifier}+Shift+c" = "reload";
           "${cfg.config.modifier}+Shift+r" = "restart";
