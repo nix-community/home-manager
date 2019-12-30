af2303526d425b9aed15134f28efdf29b038daaf
Robert Helgesson <robert@rycee.net>
2019-12-30 22:32:27 +0100

    install: add state version to initial configuration
    This sets the state version in recent installs to the latest released
    version. It is beneficial for people to be aware of this option and it
    is also good to help new users get a more recent setup.
    
    (cherry picked from commit 54f367b1196cb23c6803bc48359512ccc39f4136)

--- home-manager/install.nix
+++ home-manager/install.nix
@@ -21,6 +21,16 @@ runCommand
       {
         # Let Home Manager install and manage itself.
         programs.home-manager.enable = true;
+
+        # This value determines the Home Manager release that your
+        # configuration is compatible with. This helps avoid breakage
+        # when a new Home Manager release introduces backwards
+        # incompatible changes.
+        #
+        # You can update Home Manager without changing this value. See
+        # the Home Manager release notes for a list of state version
+        # changes in each release.
+        home.stateVersion = "19.09";
       }
       EOF
       fi
