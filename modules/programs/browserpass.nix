8ace1ab1b0847ba25e54a8c0c25abcd8feb8d613
Robert Helgesson <robert@rycee.net>
2020-01-11 19:26:32 +0100

    browserpass: use attribute set to define files
    To avoid warning message concerning deprecation of the `loaOf` type.

--- modules/programs/browserpass.nix
+++ modules/programs/browserpass.nix
@@ -24,57 +24,58 @@ in {
   };
 
   config = mkIf config.programs.browserpass.enable {
-    home.file = builtins.concatLists (with pkgs.stdenv; map (x:
-      if x == "chrome" then
-        let dir = if isDarwin
-          then "Library/Application Support/Google/Chrome/NativeMessagingHosts"
-          else ".config/google-chrome/NativeMessagingHosts";
-        in [
-          {
-            target = "${dir}/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          }
-          {
-            target = "${dir}/../policies/managed/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-          }
-        ]
-      else if x == "chromium" then
-        let dir = if isDarwin
-          then "Library/Application Support/Chromium/NativeMessagingHosts"
-          else ".config/chromium/NativeMessagingHosts";
-        in [
-          {
-            target = "${dir}/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          }
-          {
-            target = "${dir}/../policies/managed/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-          }
-        ]
-      else if x == "firefox" then
-        [ {
-          target = (if isDarwin
+    home.file =
+      foldl' (a: b: a // b) {}
+      (concatMap (x: with pkgs.stdenv;
+        if x == "chrome" then
+          let dir = if isDarwin
+            then "Library/Application Support/Google/Chrome/NativeMessagingHosts"
+            else ".config/google-chrome/NativeMessagingHosts";
+          in [
+            {
+              "${dir}/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
+              "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
+            }
+          ]
+        else if x == "chromium" then
+          let dir = if isDarwin
+            then "Library/Application Support/Chromium/NativeMessagingHosts"
+            else ".config/chromium/NativeMessagingHosts";
+          in [
+            {
+              "${dir}/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
+            }
+            {
+              "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
+            }
+          ]
+        else if x == "firefox" then
+          let dir = if isDarwin
             then "Library/Application Support/Mozilla/NativeMessagingHosts"
-            else ".mozilla/native-messaging-hosts")
-            + "/com.github.browserpass.native.json";
-          source = "${pkgs.browserpass}/lib/browserpass/hosts/firefox/com.github.browserpass.native.json";
-        } ]
-      else if x == "vivaldi" then
-        let dir = if isDarwin
-          then "Library/Application Support/Vivaldi/NativeMessagingHosts"
-          else ".config/vivaldi/NativeMessagingHosts";
-        in [
-          {
-            target = "${dir}/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          }
-          {
-            target = "${dir}/../policies/managed/com.github.browserpass.native.json";
-            source = "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
-          }
-        ]
-      else throw "unknown browser ${x}") config.programs.browserpass.browsers);
+            else ".mozilla/native-messaging-hosts";
+          in [
+            {
+              "${dir}/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/hosts/firefox/com.github.browserpass.native.json";
+            }
+          ]
+        else if x == "vivaldi" then
+          let dir = if isDarwin
+            then "Library/Application Support/Vivaldi/NativeMessagingHosts"
+            else ".config/vivaldi/NativeMessagingHosts";
+          in [
+            {
+              "${dir}/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
+              "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+                "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
+            }
+          ]
+        else throw "unknown browser ${x}") config.programs.browserpass.browsers
+      );
   };
 }
