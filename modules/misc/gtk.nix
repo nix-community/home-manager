ded46ecf70cc29f1d52aa54fdfde7707ac18ae4e
ivann <ivann@0xf.re>
2020-01-22 16:53:52 +0100

    create a generic fontType

--- modules/misc/gtk.nix
+++ modules/misc/gtk.nix
@@ -27,28 +27,7 @@ let
     in
       "${n} = ${v'}";
 
-  fontType = types.submodule {
-    options = {
-      package = mkOption {
-        type = types.nullOr types.package;
-        default = null;
-        example = literalExample "pkgs.dejavu_fonts";
-        description = ''
-          Package providing the font. This package will be installed
-          to your profile. If <literal>null</literal> then the font
-          is assumed to already be available in your profile.
-        '';
-      };
-
-      name = mkOption {
-        type = types.str;
-        example = "DejaVu Sans 8";
-        description = ''
-          The family name and size of the font within the package.
-        '';
-      };
-    };
-  };
+  fontType = (import ../lib/font-type.nix { inherit lib; }).fontType;
 
   themeType = types.submodule {
     options = {
