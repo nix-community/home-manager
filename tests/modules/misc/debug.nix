f72c8e33efebd53e32583eb6b66e732e294c7bf5
Maximilian Bosch <maximilian@mbosch.me>
2020-02-27 00:24:25 +0100

    misc/debug: Exclude gdb testcase on darwin
    Don't have a mac to figure out how to get gdb with Nix on darwin
    running, let's exclude the testcase on darwin for now to get the build
    working.

--- tests/modules/misc/debug.nix
+++ tests/modules/misc/debug.nix
@@ -14,11 +14,13 @@
       assertFileRegex home-path/etc/profile.d/hm-session-vars.sh \
           'NIX_DEBUG_INFO_DIRS=.*/lib/debug'
 
-      # We need to override NIX_DEBUG_INFO_DIRS here as $HOME evalutes to the home
-      # of the user who executes this testcase :/
-      { echo quit | PATH="$TESTED/home-path/bin''${PATH:+:}$PATH" NIX_DEBUG_INFO_DIRS=$TESTED/home-path/lib/debug \
-        gdb nix 2>&1 | \
-        grep 'Reading symbols from ${builtins.storeDir}/'; } || fail "Failed to read debug symbols from nix in gdb"
+      ${lib.optionalString (!pkgs.stdenv.isDarwin) ''
+        # We need to override NIX_DEBUG_INFO_DIRS here as $HOME evalutes to the home
+        # of the user who executes this testcase :/
+        { echo quit | PATH="$TESTED/home-path/bin''${PATH:+:}$PATH" NIX_DEBUG_INFO_DIRS=$TESTED/home-path/lib/debug \
+          gdb nix 2>&1 | \
+          grep 'Reading symbols from ${builtins.storeDir}/'; } || fail "Failed to read debug symbols from nix in gdb"
+      ''}
     '';
   };
 }
