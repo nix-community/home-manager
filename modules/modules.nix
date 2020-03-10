5ea66cdfcc47192b3ce0a23d6905bc496296d0dc
William Carroll <wpcarro@gmail.com>
2020-03-10 22:28:48 +0000

    Support keynav service
    keynav lets Linux users control their mouse with the keyboard.
    
    For most users, `services.keynav.enable = true` should support their workflows.
    
    However, keynav offers many configuration options, which home-manager could
    eventually support. For now, why not support the default configuration and allow
    open source contributors to fill in the rest as needed?

--- modules/modules.nix
+++ modules/modules.nix
@@ -155,6 +155,7 @@ let
     (loadModule ./services/taskwarrior-sync.nix { condition = hostPlatform.isLinux; })
     (loadModule ./services/udiskie.nix { })
     (loadModule ./services/unclutter.nix { })
+    (loadModule ./services/keynav.nix { })
     (loadModule ./services/unison.nix { condition = hostPlatform.isLinux; })
     (loadModule ./services/window-managers/awesome.nix { })
     (loadModule ./services/window-managers/bspwm/default.nix { condition = hostPlatform.isLinux; })
