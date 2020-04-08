ed22cb3752eb3c8eb479660649fb9e5732cfa561
Vika <vika@fireburn.ru>
2020-04-09 01:16:35 +0300

    Added standalone home-manager support - the build tools are currently WIP

--- flake.nix
+++ flake.nix
@@ -7,5 +7,21 @@
 
     nixosModules.home-manager = import ./nixos nixpkgs;
 
+    lib = {
+      homeManagerConfiguration = {
+        configuration, system, homeDirectory, username,
+        pkgs ? builtins.getAttr system nixpkgs.outputs.legacyPackages,
+        check ? true
+      }@args: import ./modules nixpkgs {
+        pkgs = builtins.getAttr system nixpkgs.outputs.legacyPackages;
+        configuration = { ... }: {
+          imports = [configuration];
+          home = {
+            inherit homeDirectory username;
+          };
+        };
+        inherit check;
+      };
+    };
   };
 }
