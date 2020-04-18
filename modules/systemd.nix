0b6cb1d04be1a78fc864c25e2b86be227a4b7fd1
Cole Helbling <cole.e.helbling@outlook.com>
2020-04-18 16:01:45 -0700

    systemd: don't page failed user units
    Otherwise, the pager (typically `less`) pauses execution of
    `home-manager switch` until the pager is dismissed, if the content is
    larger than would fit on the screen.

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -249,7 +249,7 @@ in
             if [[ $systemdStatus == 'running' || $systemdStatus == 'degraded' ]]; then
               if [[ $systemdStatus == 'degraded' ]]; then
                 warnEcho "The user systemd session is degraded:"
-                ${systemctl} --user --state=failed
+                ${systemctl} --user --no-pager --state=failed
                 warnEcho "Attempting to reload services anyway..."
               fi
 
