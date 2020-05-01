e15843c57ce187c786e4e16bf47a65393a5cd181
ivann <ivann@0xf.re>
2020-05-02 00:48:03 +0200

    qutebrowser: add aliases, searchEngines and keyMappings options

--- modules/programs/qutebrowser.nix
+++ modules/programs/qutebrowser.nix
@@ -24,9 +24,35 @@ let
     else
       "${o}${n} = ${formatValue v}";
 
+  formatDictLine = o: n: v: ''${o}['${n}'] = "${v}"'';
+
 in {
   options.programs.qutebrowser = {
     enable = mkEnableOption "qutebrowser";
+
+    aliases = mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+      description = ''
+        Aliases for commands.
+      '';
+    };
+
+    searchEngines = mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+      description = ''
+        Search engines which can be used via the address bar. Maps a search engine name (such as <literal>DEFAULT</literal>, or <literal>ddg</literal>) to a URL with a <literal>{}</literal> placeholder. The placeholder will be replaced by the search term, use <literal>{{</literal> and <literal>}}</literal> for literal <literal>{/}</literal> signs. The search engine named <literal>DEFAULT</literal> is used when <literal>url.auto_search</literal> is turned on and something else than a URL was entered to be opened. Other search engines can be used by prepending the search engine name to the search term, e.g. <literal>:open google qutebrowser</literal>.
+      '';
+      example = literalExample ''
+        {
+          w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
+          aw = "https://wiki.archlinux.org/?search={}";
+          nw = "https://nixos.wiki/index.php?search={}";
+          g = "https://www.google.com/search?hl=en&q={}";
+        }
+      '';
+    };
 
     settings = mkOption {
       type = types.attrs;
@@ -50,6 +76,14 @@ in {
       '';
     };
 
+    keyMappings = mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+      description = ''
+        This setting can be used to map keys to other keys. When the key used as dictionary-key is pressed, the binding for the key used as dictionary-value is invoked instead. This is useful for global remappings of keys, for example to map Ctrl-[ to Escape. Note that when a key is bound (via <literal>bindings.default</literal> or <literal>bindings.commands</literal>), the mapping is ignored.
+      '';
+    };
+
     extraConfig = mkOption {
       type = types.lines;
       default = "";
@@ -64,6 +98,9 @@ in {
 
     xdg.configFile."qutebrowser/config.py".text = concatStringsSep "\n" ([ ]
       ++ mapAttrsToList (formatLine "c.") cfg.settings
-      ++ optional (cfg.extraConfig != "") cfg.extraConfig);
+      ++ mapAttrsToList (formatDictLine "c.aliases") cfg.aliases
+      ++ mapAttrsToList (formatDictLine "c.url.searchengines") cfg.searchEngines
+      ++ mapAttrsToList (formatDictLine "c.bindings.key_mappings")
+      cfg.keyMappings ++ optional (cfg.extraConfig != "") cfg.extraConfig);
   };
 }
