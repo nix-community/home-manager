b8cdd3da0aa1b830ae53bd3845cc55fc1845ef71
ivann <ivann@0xf.re>
2020-05-02 15:27:40 +0200

    qutebrowser: add test for keybindings

---
+++ tests/modules/programs/qutebrowser/keybindings.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  config = {
+    programs.qutebrowser = {
+      enable = true;
+
+      keybindings = {
+        normal = {
+          "<Ctrl-v>" = "spawn mpv {url}";
+          ",l" = ''config-cycle spellcheck.languages ["en-GB"] ["en-US"]'';
+        };
+        prompt = { "<Ctrl-y>" = "prompt-yes"; };
+      };
+    };
+
+    nixpkgs.overlays = [
+      (self: super: {
+        qutebrowser = pkgs.writeScriptBin "dummy-qutebrowser" "";
+      })
+    ];
+
+    nmt.script = ''
+      assertFileContent \
+        home-files/.config/qutebrowser/config.py \
+        ${
+          pkgs.writeText "qutebrowser-expected-config.py" ''
+            config.bind(",l", "config-cycle spellcheck.languages [\"en-GB\"] [\"en-US\"]", mode="normal")
+            config.bind("<Ctrl-v>", "spawn mpv {url}", mode="normal")
+            config.bind("<Ctrl-y>", "prompt-yes", mode="prompt")''
+        }
+    '';
+  };
+}
