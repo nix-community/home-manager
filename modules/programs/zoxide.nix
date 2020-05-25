e2782b0736929c4cc6303835386c84c3e2d5db9a
Mario Rodas <marsam@users.noreply.github.com>
2020-05-25 04:20:00 -0500

    zoxide: add module

---
+++ modules/programs/zoxide.nix
@@ -0,0 +1,79 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.programs.zoxide;
+
+in {
+  meta.maintainers = [ maintainers.marsam ];
+
+  options.programs.zoxide = {
+    enable = mkEnableOption "zoxide";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.zoxide;
+      defaultText = literalExample "pkgs.zoxide";
+      description = ''
+        Zoxide package to install.
+      '';
+    };
+
+    options = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "--no-aliases" ];
+      description = ''
+        List of options to pass to zoxide.
+      '';
+    };
+
+    enableBashIntegration = mkOption {
+      default = true;
+      type = types.bool;
+      description = ''
+        Whether to enable Bash integration.
+      '';
+    };
+
+    enableZshIntegration = mkOption {
+      default = true;
+      type = types.bool;
+      description = ''
+        Whether to enable Zsh integration.
+      '';
+    };
+
+    enableFishIntegration = mkOption {
+      default = true;
+      type = types.bool;
+      description = ''
+        Whether to enable Fish integration.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    programs.bash.initExtra = mkIf cfg.enableBashIntegration ''
+      eval "$(${cfg.package}/bin/zoxide init bash ${
+        concatStringsSep " " cfg.options
+      })"
+    '';
+
+    programs.zsh.initExtra = mkIf cfg.enableZshIntegration ''
+      eval "$(${cfg.package}/bin/zoxide init zsh ${
+        concatStringsSep " " cfg.options
+      })"
+    '';
+
+    programs.fish.shellInit = mkIf cfg.enableFishIntegration ''
+      ${cfg.package}/bin/zoxide init fish ${
+        concatStringsSep " " cfg.options
+      } | source
+    '';
+  };
+}
