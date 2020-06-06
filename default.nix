aa7339a0f04c8e74412d31d123d1244b94bedbcf
Damien Cassou <damien@cassou.me>
2020-06-06 22:08:02 +0200

    home-manager: Add doc-related attributes to root `default.nix`
    Add html-manual and manPages to default.nix. These attributes provide
    an easy way to check changes to the documentation before sending a
    Pull Request.

--- default.nix
+++ default.nix
@@ -1,12 +1,22 @@
 { pkgs ? import <nixpkgs> { } }:
 
-rec {
+let
+  extendedLib = import ./modules/lib/stdlib-extended.nix pkgs.lib;
+  doc = (import ./doc {
+    lib = extendedLib;
+    pkgs = pkgs;
+  });
+in rec {
   home-manager = pkgs.callPackage ./home-manager { path = toString ./.; };
 
   install =
     pkgs.callPackage ./home-manager/install.nix { inherit home-manager; };
 
   nixos = import ./nixos;
+
+  html-manual = doc.manual.html;
+
+  manPages = doc.manPages;
 
   path = ./.;
 }
