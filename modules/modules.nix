78457729797493ca06899b6f44b55c517647035e
cwyc <cwyc@users.noreply.github.com>
2020-06-15 21:47:58 -0400

    ne: add module
    Added a simple module to place configuration files for ne: the nice editor

--- modules/modules.nix
+++ modules/modules.nix
@@ -89,6 +89,7 @@ let
     (loadModule ./programs/mercurial.nix { })
     (loadModule ./programs/mpv.nix { })
     (loadModule ./programs/msmtp.nix { })
+    (loadModule ./programs/ne.nix { })
     (loadModule ./programs/neomutt.nix { })
     (loadModule ./programs/neovim.nix { })
     (loadModule ./programs/newsboat.nix { })
---
+++ tests/modules/programs/ne/defprefs.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  defpref = ''
+    defined through defaultPreferences
+  '';
+  autopref = ''
+    defined through automaticPreferences
+  '';
+in {
+  config = {
+    programs.ne = {
+      enable = true;
+      defaultPreferences = defpref;
+      automaticPreferences.".default" = autopref;
+    };
+    nmt = {
+      description =
+        "Check that it gracefully handles the case of both defaultPreferences and automaticPreferences.'.default' being set, defaulting to the former.";
+      script = ''
+        assertFileExists home-files/.ne/.default#ap
+        assertFileContent home-files/.ne/.default#ap ${
+          builtins.toFile "defpref" defpref
+        }
+      '';
+
+    };
+  };
+}
