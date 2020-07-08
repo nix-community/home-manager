abc46c63fa57e2978199538be3efcd72f777727e
Cole Mickens <cole.mickens@gmail.com>
2020-07-08 14:48:53 -0700

    tests: i3/sway: add followmouse tests

---
+++ tests/modules/services/window-managers/sway/sway-followmouse.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  config = {
+    wayland.windowManager.sway = {
+      enable = true;
+
+      config = {
+        focus.followMouse = "always";
+        menu = "${pkgs.dmenu}/bin/dmenu_run";
+        bars = [ ];
+      };
+    };
+
+    nixpkgs.overlays = [
+      (self: super: {
+        dmenu = super.dmenu // { outPath = "@dmenu@"; };
+        rxvt-unicode-unwrapped = super.rxvt-unicode-unwrapped // {
+          outPath = "@rxvt-unicode-unwrapped@";
+        };
+        sway = super.sway // { outPath = "@sway@"; };
+      })
+    ];
+
+    nmt.script = ''
+      assertFileExists home-files/.config/sway/config
+      assertFileContent home-files/.config/sway/config \
+        ${./sway-followmouse-expected.conf}
+    '';
+  };
+}
