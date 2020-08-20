1932c72becb2c16cf5563eda9b18b88d06db9940
cwyc <cwyc@users.noreply.github.com>
2020-08-19 20:50:29 -0400

    xdg-desktop-entries: add module
    rebase

--- modules/modules.nix
+++ modules/modules.nix
@@ -41,6 +41,7 @@ let
     (loadModule ./misc/version.nix { })
     (loadModule ./misc/vte.nix { })
     (loadModule ./misc/xdg-system-dirs.nix { condition = hostPlatform.isLinux; })
+    (loadModule ./misc/xdg-desktop-entries.nix { condition = hostPlatform.isLinux; })
     (loadModule ./misc/xdg-mime.nix { condition = hostPlatform.isLinux; })
     (loadModule ./misc/xdg-mime-apps.nix { condition = hostPlatform.isLinux; })
     (loadModule ./misc/xdg-user-dirs.nix { condition = hostPlatform.isLinux; })
