a97ff1534eb7c92fde5290919a141118dfa301bc
Tony Olagbaiye <me@fron.io>
2020-08-26 07:13:05 +0100

    flakes: add flake.nix
    No flake.lock, because the only input (nixpkgs) will almost always be overridden, and currently h-m's testing and verification is not flake-based

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Home Manager for Nix";
+
+  outputs = { self, nixpkgs }: rec {
+
+    nixosModules.home-manager = import ./nixos;
+
+    lib = {
+      homeManagerConfiguration = { configuration, system, homeDirectory
+        , username
+        , pkgs ? builtins.getAttr system nixpkgs.outputs.legacyPackages
+        , check ? true }@args:
+        import ./modules {
+          inherit pkgs check;
+          configuration = { ... }: {
+            imports = [ configuration ];
+            home = { inherit homeDirectory username; };
+          };
+        };
+    };
+  };
+}
