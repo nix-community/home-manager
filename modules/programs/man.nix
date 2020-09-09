812b64d4d3c243bf7b6d08e9d210750f53e39e22
Damien Cassou <damien@cassou.me>
2020-09-09 08:11:36 +0200

    man: prepare for new `programs.man` options

--- modules/programs/man.nix
+++ modules/programs/man.nix
@@ -4,14 +4,16 @@ with lib;
 
 {
   options = {
-    programs.man.enable = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Whether to enable manual pages and the <command>man</command>
-        command. This also includes "man" outputs of all
-        <literal>home.packages</literal>.
-      '';
+    programs.man = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Whether to enable manual pages and the <command>man</command>
+          command. This also includes "man" outputs of all
+          <literal>home.packages</literal>.
+        '';
+      };
     };
   };
 
