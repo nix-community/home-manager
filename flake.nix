a68898e9d60b514b0c3611150800d16f5d9131d2
Sebastian Zivota <sebastian.zivota@mailbox.org>
2020-09-22 13:15:55 +0200

    Add flake support
    flake.nix from bqv-flakes branch

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Home Manager for Nix";
+
+  outputs = { self, nixpkgs }: rec {
+
+    nixosModules.home-manager = import ./nixos nixpkgs;
+
+    lib = {
+      homeManagerConfiguration = { configuration, system, homeDirectory
+        , username
+        , pkgs ? builtins.getAttr system nixpkgs.outputs.legacyPackages
+        , check ? true }@args:
+        import ./modules nixpkgs {
+          pkgs = builtins.getAttr system nixpkgs.outputs.legacyPackages;
+          configuration = { ... }: {
+            imports = [ configuration ];
+            home = { inherit homeDirectory username; };
+          };
+          inherit check;
+        };
+    };
+  };
+}
