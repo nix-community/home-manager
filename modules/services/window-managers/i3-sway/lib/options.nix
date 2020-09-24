690d93c22a8812c303a52786022f3f0441dfd06a
Alex Rice <alexrice999@hotmail.co.uk>
2020-09-24 10:16:54 +0100

    sway: fix startup example (#1517)
    Fixes #1515. Example for `wayland.windowManager.config.startup`
    referenced options `notification` and `workspace` which are not valid
    for sway.

--- modules/services/window-managers/i3-sway/lib/options.nix
+++ modules/services/window-managers/i3-sway/lib/options.nix
@@ -645,13 +645,22 @@ in {
 
       See <link xlink:href="https://i3wm.org/docs/userguide.html#_automatically_starting_applications_on_i3_startup"/>.
     '';
-    example = literalExample ''
-      [
-      { command = "systemctl --user restart polybar"; always = true; notification = false; }
-      { command = "dropbox start"; notification = false; }
-      { command = "firefox"; workspace = "1: web"; }
-      ];
-    '';
+    example = if moduleName == "i3" then
+      literalExample ''
+        [
+        { command = "systemctl --user restart polybar"; always = true; notification = false; }
+        { command = "dropbox start"; notification = false; }
+        { command = "firefox"; workspace = "1: web"; }
+        ];
+      ''
+    else
+      literalExample ''
+        [
+        { command = "systemctl --user restart waybar"; always = true; }
+        { command = "dropbox start"; }
+        { command = "firefox"; }
+        ]
+      '';
   };
 
   gaps = mkOption {
