c5e47e25a6cbb37feb3ff47fe83e5a1db097bf16
dawidsowa <dawid_sowa@posteo.net>
2020-10-19 15:37:49 +0200

    firefox: add enableGnomeExtensions option (#1552)

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -220,6 +220,16 @@ in
         default = false;
         description = "Whether to enable the unfree Adobe Flash plugin.";
       };
+
+      enableGnomeExtensions = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable the GNOME Shell native host connector.
+          You need set NixOS's `services.gnome3.chrome-gnome-shell.enable`
+          to <literal>true</literal>.
+        '';
+      };
     };
   };
 
@@ -262,6 +272,7 @@ in
         # The configuration expected by the Firefox wrapper.
         fcfg = {
           enableAdobeFlash = cfg.enableAdobeFlash;
+          enableGnomeExtensions = cfg.enableGnomeExtensions;
         };
 
         # A bit of hackery to force a config into the wrapper.
