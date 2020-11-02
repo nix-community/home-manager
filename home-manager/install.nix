072a6ea333a5999298cec45e8ea92f9821d52e60
Robert Helgesson <robert@rycee.net>
2020-11-02 22:42:49 +0100

    docs: change stable from 20.03 to 20.09

--- home-manager/install.nix
+++ home-manager/install.nix
@@ -45,7 +45,7 @@ runCommand "home-manager-install" {
       # You can update Home Manager without changing this value. See
       # the Home Manager release notes for a list of state version
       # changes in each release.
-      home.stateVersion = "20.09";
+      home.stateVersion = "21.03";
     }
     EOF
     fi
