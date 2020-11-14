7c3c64208e5696cac0e5bb70ac7d4cd04427882d
Robert Helgesson <robert@rycee.net>
2020-11-14 10:38:58 +0100

    sway: use configured package in reload script
    Fixes unnecessary download of the plain `sway` package.

--- modules/services/window-managers/i3-sway/sway.nix
+++ modules/services/window-managers/i3-sway/sway.nix
@@ -399,7 +399,7 @@ in {
         swaySocket=''${XDG_RUNTIME_DIR:-/run/user/$UID}/sway-ipc.$UID.$(${pkgs.procps}/bin/pgrep -x sway || ${pkgs.coreutils}/bin/true).sock
         if [ -S $swaySocket ]; then
           echo "Reloading sway"
-          $DRY_RUN_CMD ${pkgs.sway}/bin/swaymsg -s $swaySocket reload
+          $DRY_RUN_CMD ${cfg.package}/bin/swaymsg -s $swaySocket reload
         fi
       '';
     };
