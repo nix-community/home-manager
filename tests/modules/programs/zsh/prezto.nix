b5e7817de2a2c2a3735d8620d0fe67b327a5e95d
Robert Helgesson <robert@rycee.net>
2020-11-14 10:37:35 +0100

    zsh-prezto: remove package dependency in test

--- tests/modules/programs/zsh/prezto.nix
+++ tests/modules/programs/zsh/prezto.nix
@@ -6,6 +6,18 @@ with lib;
   config = {
     programs.zsh.prezto.enable = true;
 
+    nixpkgs.overlays = [
+      (self: super: {
+        zsh-prezto = super.runCommandLocal "dummy-zsh-prezto" { } ''
+          mkdir -p $out/runcoms
+          echo '# zprofile' > $out/runcoms/zprofile
+          echo '# zlogin' > $out/runcoms/zlogin
+          echo '# zlogout' > $out/runcoms/zlogout
+          echo '# zshenv' > $out/runcoms/zshenv
+        '';
+      })
+    ];
+
     nmt.script = ''
       assertFileExists home-files/.zpreztorc
     '';
