c7d2c501c436b4a4a79de8d27349188b25463ff5
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-11-15 17:58:55 -0500

    i3: validate configuration at build time

--- tests/modules/services/window-managers/i3/i3-keybindings.nix
+++ tests/modules/services/window-managers/i3/i3-keybindings.nix
@@ -19,9 +19,10 @@ with lib;
     nixpkgs.overlays = [
       (self: super: {
         dmenu = super.dmenu // { outPath = "@dmenu@"; };
-
-        i3 = super.i3 // { outPath = "@i3@"; };
-
+        i3 = super.writeScriptBin "i3" "" // { outPath = "@i3@"; };
+        i3-gaps = super.writeScriptBin "i3-gaps" "" // {
+          outPath = "@i3-gaps@";
+        };
         i3status = super.i3status // { outPath = "@i3status@"; };
       })
     ];
