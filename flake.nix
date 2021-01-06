0b8061c8803b6dc5f2f2609f4de829eb686106bd
mlatus <wqseleven@gmail.com>
2021-01-06 10:31:01 +0800

    flake: add extraSpecialArgs
    This allows flake user to pass extra arguments to all modules.

--- flake.nix
+++ flake.nix
@@ -9,11 +9,11 @@
     lib = {
       hm = import ./modules/lib { lib = nixpkgs.lib; };
       homeManagerConfiguration = { configuration, system, homeDirectory
-        , username
+        , username, extraSpecialArgs ? { }
         , pkgs ? builtins.getAttr system nixpkgs.outputs.legacyPackages
         , check ? true }@args:
         import ./modules {
-          inherit pkgs check;
+          inherit pkgs check extraSpecialArgs;
           configuration = { ... }: {
             imports = [ configuration ];
             home = { inherit homeDirectory username; };
