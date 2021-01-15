4e86d65aee028d5bca9a56fdcd1ef8c703e68c32
Tino Breddin <t@tolbrino.com>
2021-01-15 11:57:58 +0100

    nextcloud-client: add `package` option

--- modules/services/nextcloud-client.nix
+++ modules/services/nextcloud-client.nix
@@ -2,12 +2,25 @@
 
 with lib;
 
-{
+let
+
+  cfg = config.services.nextcloud-client;
+
+in {
   options = {
-    services.nextcloud-client = { enable = mkEnableOption "Nextcloud Client"; };
+    services.nextcloud-client = {
+      enable = mkEnableOption "Nextcloud Client";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.nextcloud-client;
+        defaultText = literalExample "pkgs.nextcloud-client";
+        description = "The package to use for the nextcloud client binary.";
+      };
+    };
   };
 
-  config = mkIf config.services.nextcloud-client.enable {
+  config = mkIf cfg.enable {
     systemd.user.services.nextcloud-client = {
       Unit = {
         Description = "Nextcloud Client";
@@ -17,7 +30,7 @@ with lib;
 
       Service = {
         Environment = "PATH=${config.home.profileDirectory}/bin";
-        ExecStart = "${pkgs.nextcloud-client}/bin/nextcloud";
+        ExecStart = "${cfg.package}/bin/nextcloud";
       };
 
       Install = { WantedBy = [ "graphical-session.target" ]; };
