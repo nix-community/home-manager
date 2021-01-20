9d680ec66280d7843164f12be3d1f182b27cb198
Robert Helgesson <robert@rycee.net>
2021-01-20 19:48:52 +0100

    kakoune: disable the `kakoune-use-plugins` test

--- tests/modules/programs/kakoune/default.nix
+++ tests/modules/programs/kakoune/default.nix
@@ -1,6 +1,8 @@
 {
   kakoune-no-plugins = ./no-plugins.nix;
-  kakoune-use-plugins = ./use-plugins.nix;
+  # Temporarily disabled until https://github.com/NixOS/nixpkgs/pull/110196
+  # reaches the unstable channel.
+  # kakoune-use-plugins = ./use-plugins.nix;
   kakoune-whitespace-highlighter = ./whitespace-highlighter.nix;
   kakoune-whitespace-highlighter-corner-cases =
     ./whitespace-highlighter-corner-cases.nix;
