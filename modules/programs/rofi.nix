6f7074d21dfa50e2e30b3e087cca60abd1c0f9f8
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-23 12:30:34 -0300

    rofi: migrate to rasi configuration format (#1736)
    * rofi: migrate to rasi configuration format
    
    The Xresources configuration format is deprecated in Rofi. For example,
    using Rofi from unstable (1.6.1 as of now) you get the following
    warnings when starting the application:
    
    ```
    (process:9272): Rofi-WARNING **: 01:38:48.596: The old Xresources based configuration format is deprecated.
    
    (process:9272): Rofi-WARNING **: 01:38:48.596: Please upgrade: rofi -upgrade-config.
    ``````
    
    So this commit migrates it for its new configuration format, called rasi
    instead.
    
    This new implementation uses attrsets manipulation instead of using
    strings, making the code clearer and also fixing some bugs found during
    the way. To make sure everything is right, I also created some tests.
    
    If someone wants to validate if the generated config is correct, just
    run in terminal:
    
    ```
    $ rofi -dump-config
    ```
    
    And rofi will dump the current configuration file, including all
    unsetted options.
    
    * docs: document programs.rofi.extraConfig changes
    
    * rofi: add thiagokokada as maintainer
    
    * rofi: add toRasi function

--- modules/programs/rofi.nix
+++ modules/programs/rofi.nix
@@ -71,20 +71,8 @@ let
     };
   };
 
-  valueToString = value:
-    if isBool value then (if value then "true" else "else") else toString value;
-
   windowColorsToString = window:
     concatStringsSep ", " (with window; [ background border separator ]);
-
-  rowsColorsToString = rows: ''
-    ${optionalString (rows.normal != null)
-    (setOption "color-normal" (rowColorsToString rows.normal))}
-    ${optionalString (rows.active != null)
-    (setOption "color-active" (rowColorsToString rows.active))}
-    ${optionalString (rows.urgent != null)
-    (setOption "color-urgent" (rowColorsToString rows.urgent))}
-  '';
 
   rowColorsToString = row:
     concatStringsSep ", " (with row; [
@@ -95,14 +83,45 @@ let
       highlight.foreground
     ]);
 
-  setOption = name: value:
-    optionalString (value != null) "rofi.${name}: ${valueToString value}";
+  mkColorScheme = colors:
+    if colors != null then
+      with colors; {
+        color-window =
+          if (window != null) then (windowColorsToString window) else null;
+        color-normal = if (rows != null && rows.normal != null) then
+          (rowColorsToString rows.normal)
+        else
+          null;
+        color-active = if (rows != null && rows.active != null) then
+          (rowColorsToString rows.active)
+        else
+          null;
+        color-urgent = if (rows != null && rows.active != null) then
+          (rowColorsToString rows.urgent)
+        else
+          null;
+      }
+    else
+      { };
 
-  setColorScheme = colors:
-    optionalString (colors != null) ''
-      ${optionalString (colors.window != null) setOption "color-window"
-      (windowColorsToString colors.window)}
-      ${optionalString (colors.rows != null) (rowsColorsToString colors.rows)}
+  mkValueString = value:
+    if isBool value then
+      if value then "true" else "false"
+    else if isInt value then
+      toString value
+    else
+      ''"${toString value}"'';
+
+  mkKeyValue = name: value: "${name}: ${mkValueString value};";
+
+  toRasi = section: config:
+    let
+      # Remove null values so the resulting config does not have empty lines
+      configStr = generators.toKeyValue { inherit mkKeyValue; }
+        (attrsets.filterAttrs (m: v: v != null) config);
+    in ''
+      ${section} {
+      ${configStr}}
     '';
 
   locationsMap = {
@@ -119,12 +138,12 @@ let
 
   themeName = if (cfg.theme == null) then
     null
-  else if (lib.isString cfg.theme) then
+  else if (isString cfg.theme) then
     cfg.theme
   else
-    lib.removeSuffix ".rasi" (baseNameOf cfg.theme);
+    removeSuffix ".rasi" (baseNameOf cfg.theme);
 
-  themePath = if (lib.isString cfg.theme) then null else cfg.theme;
+  themePath = if (isString cfg.theme) then null else cfg.theme;
 
 in {
   options.programs.rofi = {
@@ -220,7 +239,7 @@ in {
 
     location = mkOption {
       default = "center";
-      type = types.enum (builtins.attrNames locationsMap);
+      type = types.enum (attrNames locationsMap);
       description = "The location rofi appears on the screen.";
     };
 
@@ -284,15 +303,22 @@ in {
     };
 
     configPath = mkOption {
-      default = "${config.xdg.configHome}/rofi/config";
-      defaultText = "$XDG_CONFIG_HOME/rofi/config";
+      default = "${config.xdg.configHome}/rofi/config.rasi";
+      defaultText = "$XDG_CONFIG_HOME/rofi/config.rasi";
       type = types.str;
       description = "Path where to put generated configuration file.";
     };
 
     extraConfig = mkOption {
-      default = "";
-      type = types.lines;
+      default = { };
+      example = literalExample ''
+        {
+          modi = "drun,emoji,ssh";
+          kb-primary-paste = "Control+V,Shift+Insert";
+          kb-secondary-paste = "Control+v,Insert";
+        }
+      '';
+      type = with types; attrsOf (oneOf [ int str bool ]);
       description = "Additional configuration to add.";
     };
 
@@ -308,31 +334,29 @@ in {
 
     home.packages = [ cfg.package ];
 
-    home.file."${cfg.configPath}".text = ''
-      ${setOption "width" cfg.width}
-      ${setOption "lines" cfg.lines}
-      ${setOption "font" cfg.font}
-      ${setOption "bw" cfg.borderWidth}
-      ${setOption "eh" cfg.rowHeight}
-      ${setOption "padding" cfg.padding}
-      ${setOption "separator-style" cfg.separator}
-      ${setOption "hide-scrollbar"
-      (if (cfg.scrollbar != null) then (!cfg.scrollbar) else cfg.scrollbar)}
-      ${setOption "terminal" cfg.terminal}
-      ${setOption "cycle" cfg.cycle}
-      ${setOption "fullscreen" cfg.fullscreen}
-      ${setOption "location" (builtins.getAttr cfg.location locationsMap)}
-      ${setOption "xoffset" cfg.xoffset}
-      ${setOption "yoffset" cfg.yoffset}
-
-      ${setColorScheme cfg.colors}
-      ${setOption "theme" themeName}
-
-      ${cfg.extraConfig}
-    '';
+    home.file."${cfg.configPath}".text = toRasi "configuration" ({
+      width = cfg.width;
+      lines = cfg.lines;
+      font = cfg.font;
+      bw = cfg.borderWidth;
+      eh = cfg.rowHeight;
+      padding = cfg.padding;
+      separator-style = cfg.separator;
+      hide-scrollbar =
+        if (cfg.scrollbar != null) then (!cfg.scrollbar) else null;
+      terminal = cfg.terminal;
+      cycle = cfg.cycle;
+      fullscreen = cfg.fullscreen;
+      location = (getAttr cfg.location locationsMap);
+      xoffset = cfg.xoffset;
+      yoffset = cfg.yoffset;
+      theme = themeName;
+    } // (mkColorScheme cfg.colors) // cfg.extraConfig);
 
     xdg.dataFile = mkIf (themePath != null) {
       "rofi/themes/${themeName}.rasi".source = themePath;
     };
   };
+
+  meta.maintainers = with maintainers; [ thiagokokada ];
 }
