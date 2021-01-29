e072c8767aaa4e6cc7402b6d978387f94345448f
midchildan <git@midchildan.org>
2021-01-29 22:55:09 +0900

    targets/darwin: add new option 'targets.darwin.defaultSchema'
    This option would make it possible to add additional descriptions, type
    infomation, and default values for individual options in
    'targets.darwin.defaultSchema'.

--- modules/targets/darwin/default.nix
+++ modules/targets/darwin/default.nix
@@ -21,31 +21,54 @@ let
 in {
   imports = [ ./keybindings.nix ./linkapps.nix ./search.nix ];
 
-  options.targets.darwin.defaults = mkOption {
-    type = types.submodule ./options.nix;
-    default = { };
-    example = {
-      "com.apple.desktopservices" = {
-        DSDontWriteNetworkStores = true;
-        DSDontWriteUSBStores = true;
+  options.targets.darwin = {
+    defaults = mkOption {
+      type = types.submoduleWith { modules = cfg.defaultsSchema; };
+      default = { };
+      example = {
+        "com.apple.desktopservices" = {
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
       };
-    };
-    description = ''
-      Set macOS user defaults. Values set to <literal>null</literal> are
-      ignored.
+      description = ''
+        Set macOS user defaults. Values set to <literal>null</literal> are
+        ignored.
 
-      <warning>
-        <para>
-          Some settings might require a re-login to take effect.
-        </para>
-      </warning>
-    '';
+        <warning>
+          <para>
+            Some settings might require a re-login to take effect.
+          </para>
+        </warning>
+      '';
+    };
+
+    defaultsSchema = mkOption {
+      type = with types; listOf anything;
+      default = [ ];
+      example = [
+        ({ ... }: { option."org.nixos.foo".enable = mkEnableOption "foo"; })
+      ];
+      description = ''
+        A list of modules to use for defining the option type
+        <option>targets.darwin.defaults</option>. This can be used to add
+        additional descriptions and type information to individual suboptions as
+        well as make the suboptions <literal>mkOverride</literal>-able.
+
+        The modules can be an attribute set, a function returning an attribute
+        set, or a path to a file containing such a value.
+      '';
+    };
   };
 
-  config = mkIf (isDarwin && cfg.defaults != { }) {
-    home.activation.setDarwinDefaults = hm.dag.entryAfter [ "writeBoundary" ] ''
-      $VERBOSE_ECHO "Configuring macOS user defaults"
-      ${concatStringsSep "\n" activationCmds}
-    '';
+  config = mkIf isDarwin {
+    targets.darwin.defaultsSchema = [ ./options.nix ];
+
+    home.activation = mkIf (cfg.defaults != { }) {
+      setDarwinDefaults = hm.dag.entryAfter [ "writeBoundary" ] ''
+        $VERBOSE_ECHO "Configuring macOS user defaults"
+        ${concatStringsSep "\n" activationCmds}
+      '';
+    };
   };
 }
