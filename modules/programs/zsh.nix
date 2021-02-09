0c712a4fd1d613d455b5944491ae1a5c5a47f5a2
Nick Hu <me@nickhu.co.uk>
2021-02-09 01:58:05 +0000

    zsh-prezto: fix tests, small tidyup

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -452,7 +452,7 @@ in
         '') cfg.plugins)}
 
         # Oh-My-Zsh/Prezto calls compinit during initialization,
-        # calling it twice causes sight start up slowdown
+        # calling it twice causes slight start up slowdown
         # as all $fpath entries will be traversed again.
         ${optionalString (cfg.enableCompletion && !cfg.oh-my-zsh.enable && !cfg.prezto.enable)
           "autoload -U compinit && compinit"
