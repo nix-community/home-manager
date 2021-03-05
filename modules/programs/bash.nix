1b3ca5b10fcfef1062dad10040396f0ede16525f
Naïm Favier <n@monade.li>
2021-03-05 10:21:12 +0100

    bash: improve initialisation
    Resolves #1843. Allows aliases to be expanded in initExtra, and adds a
    visible bashrcExtra option for commands that should be run in ~/.bashrc
    even by non-interactive shells.

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -111,23 +111,21 @@ in
         '';
       };
 
-      bashrcExtra = mkOption {
-        # Hide for now, may want to rename in the future.
-        visible = false;
-        default = "";
-        type = types.lines;
-        description = ''
-          Extra commands that should be added to
-          <filename>~/.bashrc</filename>.
-        '';
-      };
-
       initExtra = mkOption {
         default = "";
         type = types.lines;
         description = ''
           Extra commands that should be run when initializing an
           interactive shell.
+        '';
+      };
+
+      bashrcExtra = mkOption {
+        default = "";
+        type = types.lines;
+        description = ''
+          Extra commands that should be placed in <filename>~/.bashrc</filename>.
+          Note that these commands will be run even in non-interactive shells.
         '';
       };
 
@@ -171,19 +169,6 @@ in
           }
         ));
     in mkIf cfg.enable {
-      programs.bash.bashrcExtra = ''
-        # Commands that should be applied only for interactive shells.
-        if [[ $- == *i* ]]; then
-          ${historyControlStr}
-
-          ${shoptsStr}
-
-          ${aliasesStr}
-
-          ${cfg.initExtra}
-        fi
-      '';
-
       home.file.".bash_profile".text = ''
         # -*- mode: sh -*-
 
@@ -208,6 +193,17 @@ in
         # -*- mode: sh -*-
 
         ${cfg.bashrcExtra}
+
+        # Commands that should be applied only for interactive shells.
+        [[ $- == *i* ]] || return
+
+        ${historyControlStr}
+
+        ${shoptsStr}
+
+        ${aliasesStr}
+
+        ${cfg.initExtra}
       '';
 
       home.file.".bash_logout" = mkIf (cfg.logoutExtra != "") {
