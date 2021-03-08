5fbb33cff5a722b267a561b3559d1e4c3b5ec156
midchildan <git@midchildan.org>
2021-03-09 05:41:23 +0900

    targets/genericLinux: set TERMINFO_DIRS (#1819)
    This makes terminfo descriptions in installed packages available to
    shell sessions. Not needed for NixOS, which does the same thing already.

--- tests/modules/targets-linux/generic-linux.nix
+++ tests/modules/targets-linux/generic-linux.nix
@@ -11,12 +11,20 @@ with lib;
 
     nmt.script = ''
       assertFileExists home-path/etc/profile.d/hm-session-vars.sh
+
       assertFileContains \
         home-path/etc/profile.d/hm-session-vars.sh \
         'export XDG_DATA_DIRS="''${NIX_STATE_DIR:-/nix/var/nix}/profiles/default/share:/home/hm-user/.nix-profile/share:/foo''${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"'
       assertFileContains \
         home-path/etc/profile.d/hm-session-vars.sh \
         '. "${pkgs.nix}/etc/profile.d/nix.sh"'
+
+      assertFileContains \
+        home-path/etc/profile.d/hm-session-vars.sh \
+        'export TERMINFO_DIRS="/home/hm-user/.nix-profile/share/terminfo:$TERMINFO_DIRS''${TERMINFO_DIRS:+:}/etc/terminfo:/lib/terminfo:/usr/share/terminfo"'
+      assertFileContains \
+        home-path/etc/profile.d/hm-session-vars.sh \
+        'export TERM="$TERM"'
     '';
   };
 }
