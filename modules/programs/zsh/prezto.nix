nix-community/home-manager

commit b220d5c4461173ed734ef354cfb976b937155691

prezto: fix #1773 (#1778)
* zsh: update prezto path structure

The path structure was changed in Nixpkgs and this commit updates
the module to match.

Fixes #1773

* zsh-prezto: fix tests, small tidyup

Co-authored-by: Nick Hu <me@nickhu.co.uk>

modules/programs/zsh/prezto.nix
@@ -351,16 +351,16 @@ in {
     };
   };
   config = mkIf cfg.enable (mkMerge [{
-    home.file."${relToDotDir ".zprofile"}".text =
-      builtins.readFile "${pkgs.zsh-prezto}/runcoms/zprofile";
-    home.file."${relToDotDir ".zlogin"}".text =
-      builtins.readFile "${pkgs.zsh-prezto}/runcoms/zlogin";
-    home.file."${relToDotDir ".zlogout"}".text =
-      builtins.readFile "${pkgs.zsh-prezto}/runcoms/zlogout";
+    home.file."${relToDotDir ".zprofile"}".source =
+      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zprofile";
+    home.file."${relToDotDir ".zlogin"}".source =
+      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogin";
+    home.file."${relToDotDir ".zlogout"}".source =
+      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogout";
     home.packages = with pkgs; [ zsh-prezto ];
 
-    home.file."${relToDotDir ".zshenv"}".text =
-      (builtins.readFile "${pkgs.zsh-prezto}/runcoms/zshenv");
+    home.file."${relToDotDir ".zshenv"}".source =
+      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zshenv";
     home.file."${relToDotDir ".zpreztorc"}".text = ''
       # Generated by Nix
       ${optionalString (cfg.caseSensitive != null) ''

tests/modules/programs/zsh/prezto.nix
@@ -9,11 +9,11 @@ with lib;
     nixpkgs.overlays = [
       (self: super: {
         zsh-prezto = super.runCommandLocal "dummy-zsh-prezto" { } ''
-          mkdir -p $out/runcoms
-          echo '# zprofile' > $out/runcoms/zprofile
-          echo '# zlogin' > $out/runcoms/zlogin
-          echo '# zlogout' > $out/runcoms/zlogout
-          echo '# zshenv' > $out/runcoms/zshenv
+          mkdir -p $out/share/zsh-prezto/runcoms
+          echo '# zprofile' > $out/share/zsh-prezto/runcoms/zprofile
+          echo '# zlogin' > $out/share/zsh-prezto/runcoms/zlogin
+          echo '# zlogout' > $out/share/zsh-prezto/runcoms/zlogout
+          echo '# zshenv' > $out/share/zsh-prezto/runcoms/zshenv
         '';
       })
     ];

modules/programs/zsh.nix
@@ -452,7 +452,7 @@ in
         '') cfg.plugins)}
 
         # Oh-My-Zsh/Prezto calls compinit during initialization,
-        # calling it twice causes sight start up slowdown
+        # calling it twice causes slight start up slowdown
         # as all $fpath entries will be traversed again.
         ${optionalString (cfg.enableCompletion && !cfg.oh-my-zsh.enable && !cfg.prezto.enable)
           "autoload -U compinit && compinit"
@@ -483,7 +483,7 @@ in
         ''}
 
         ${optionalString cfg.prezto.enable
-            (builtins.readFile "${pkgs.zsh-prezto}/runcoms/zshrc")}
+            (builtins.readFile "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zshrc")}
 
         ${concatStrings (map (plugin: ''
           if [ -f "$HOME/${pluginsDir}/${plugin.name}/${plugin.file}" ]; then

commit daf3f81d1982191f021108c55e00b69b52d411e0

prezto: fix environment files being overwritten
Fixed the breakage for prezto introduced in #1778.

The previous method created issues where certain configuration files would get
replaced by prezto's variants instead of being merged as before. This led to
issues like no config being loaded if `home.zsh.dotDir` was set.

The old method of loading these files has been restored. This fixes the issue.

modules/programs/zsh/prezto.nix
@@ -351,16 +351,16 @@ in {
     };
   };
   config = mkIf cfg.enable (mkMerge [{
-    home.file."${relToDotDir ".zprofile"}".source =
-      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zprofile";
-    home.file."${relToDotDir ".zlogin"}".source =
-      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogin";
-    home.file."${relToDotDir ".zlogout"}".source =
-      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogout";
+    home.file."${relToDotDir ".zprofile"}".text =
+      builtins.readFile "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zprofile";
+    home.file."${relToDotDir ".zlogin"}".text =
+      builtins.readFile "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogin";
+    home.file."${relToDotDir ".zlogout"}".text =
+      builtins.readFile "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zlogout";
     home.packages = with pkgs; [ zsh-prezto ];
 
-    home.file."${relToDotDir ".zshenv"}".source =
-      "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zshenv";
+    home.file."${relToDotDir ".zshenv"}".text =
+      builtins.readFile "${pkgs.zsh-prezto}/share/zsh-prezto/runcoms/zshenv";
     home.file."${relToDotDir ".zpreztorc"}".text = ''
       # Generated by Nix
       ${optionalString (cfg.caseSensitive != null) ''