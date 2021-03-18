f9b5172d956c79add5f6c8acccea2c7ca7f904c9
Dany Marcoux <dmarcoux@posteo.de>
2021-03-18 23:19:42 +0100

    jq: Use default upstream values for colors (#1870)
    As documented here: https://stedolan.github.io/jq/manual/#Colors

--- modules/programs/jq.nix
+++ modules/programs/jq.nix
@@ -51,12 +51,12 @@ in {
 
         default = {
           null = "1;30";
-          false = "0;39";
-          true = "0;39";
-          numbers = "0;39";
+          false = "0;37";
+          true = "0;37";
+          numbers = "0;37";
           strings = "0;32";
-          arrays = "1;39";
-          objects = "1;39";
+          arrays = "1;37";
+          objects = "1;37";
         };
 
         type = colorsType;
