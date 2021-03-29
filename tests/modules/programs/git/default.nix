9c7a14f913a8ddf716365519103f5e865abf5d8a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-03-29 06:04:30 +0300

    Git: Add tests for config with and without signing key id

--- tests/modules/programs/git/default.nix
+++ tests/modules/programs/git/default.nix
@@ -3,4 +3,6 @@
   git-with-most-options = ./git.nix;
   git-with-msmtp = ./git-with-msmtp.nix;
   git-with-str-extra-config = ./git-with-str-extra-config.nix;
+  git-with-signing-key-id = ./git-with-signing-key-id.nix;
+  git-without-signing-key-id = ./git-without-signing-key-id.nix;
 }
