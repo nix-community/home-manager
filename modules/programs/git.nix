f24f6163e5983ac8c99cc56535299699694303d9
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-03-29 05:10:22 +0300

    Git: Make signing key id be optional
    Thus by default the signing key is selected by commit’s author.

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -61,7 +61,8 @@ let
   signModule = types.submodule {
     options = {
       key = mkOption {
-        type = types.str;
+        type = types.nullOr types.str;
+        default = null;
         description = "The default GPG signing key fingerprint.";
       };
 
@@ -303,7 +304,7 @@ in {
 
     (mkIf (cfg.signing != null) {
       programs.git.iniContent = {
-        user.signingKey = cfg.signing.key;
+        user.signingKey = mkIf (cfg.signing.key != null) cfg.signing.key;
         commit.gpgSign = cfg.signing.signByDefault;
         gpg.program = cfg.signing.gpgPath;
       };
