a913120f7d96832d98a6867fa75f1f53fafa9355
Matthieu Coudron <mcoudron@hotmail.com>
2021-04-23 20:24:35 +0200

    programs.neovim: use new nixpkgs API
    init.vim is wrapped by default so we need to disabled it.

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -222,10 +222,13 @@ in {
   };
 
   config = let
+    mergedConfigure = cfg.configure // moduleConfigure;
     neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
       inherit (cfg)
-        extraPython3Packages withPython3 withNodeJs withRuby viAlias vimAlias;
-      configure = cfg.configure // moduleConfigure;
+        extraPython3Packages withPython3 extraPythonPackages withPython
+        withNodeJs withRuby viAlias vimAlias;
+      configure = mergedConfigure;
+      customRC = cfg.extraConfig;
       plugins = cfg.plugins;
     };
 
@@ -241,11 +244,14 @@ in {
 
     home.packages = [ cfg.finalPackage ];
 
-    xdg.configFile."nvim/init.vim".text = neovimConfig.neovimRcContent;
+    xdg.configFile = mkIf (neovimConfig.neovimRcContent != "") {
+      "nvim/init.vim".text = neovimConfig.neovimRcContent;
+    };
     programs.neovim.finalPackage = pkgs.wrapNeovimUnstable cfg.package
       (neovimConfig // {
         wrapperArgs = (lib.escapeShellArgs neovimConfig.wrapperArgs) + " "
           + extraMakeWrapperArgs;
+        wrapRc = false;
       });
 
     programs.bash.shellAliases = mkIf cfg.vimdiffAlias { vimdiff = "nvim -d"; };
