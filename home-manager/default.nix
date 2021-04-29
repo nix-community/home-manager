7d6b3c8b2cf2de9343710bf3545e28b4e7a29c65
Robert Helgesson <robert@rycee.net>
2021-04-30 01:29:39 +0200

    home-manager: add basic i18n support

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{ runCommand, lib, bash, coreutils, findutils, gnused, less
+{ runCommand, lib, bash, coreutils, findutils, gettext, gnused, less
 
 # Extra path to Home Manager. If set then this path will be tried
 # before `$HOME/.config/nixpkgs/home-manager` and
@@ -12,6 +12,7 @@ let
 in runCommand "home-manager" {
   preferLocalBuild = true;
   allowSubstitutes = false;
+  nativeBuildInputs = [ gettext ];
   meta = with lib; {
     description = "A user environment configurator";
     maintainers = [ maintainers.rycee ];
@@ -25,12 +26,17 @@ in runCommand "home-manager" {
     --subst-var-by bash "${bash}" \
     --subst-var-by coreutils "${coreutils}" \
     --subst-var-by findutils "${findutils}" \
+    --subst-var-by gettext "${gettext}" \
     --subst-var-by gnused "${gnused}" \
     --subst-var-by less "${less}" \
-    --subst-var-by HOME_MANAGER_PATH '${pathStr}'
+    --subst-var-by HOME_MANAGER_PATH '${pathStr}' \
+    --subst-var-by TEXTDOMAINDIR "$out/share/locale"
 
   install -D -m755 ${./completion.bash} \
     $out/share/bash-completion/completions/home-manager
   install -D -m755 ${./completion.zsh} \
     $out/share/zsh/site-functions/_home-manager
+
+  mkdir -p $out/share/locale/sv/LC_MESSAGES
+  msgfmt -o $out/share/locale/sv/LC_MESSAGES/home-manager.mo ${./po/sv.po}
 ''
