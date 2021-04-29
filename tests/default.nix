b11358949c45c87ffd6dc8c9f6afd24cf7fed5b9
Sumner Evans <me@sumnerevans.com>
2021-04-29 11:34:14 -0600

    format: remove exception for tests/defuault.nix

--- tests/default.nix
+++ tests/default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
 
@@ -14,25 +14,21 @@ let
   modules = import ../modules/modules.nix {
     inherit lib pkgs;
     check = false;
-  } ++ [
-    {
-      # Fix impurities. Without these some of the user's environment
-      # will leak into the tests through `builtins.getEnv`.
-      xdg.enable = true;
-      home.username = "hm-user";
-      home.homeDirectory = "/home/hm-user";
+  } ++ [{
+    # Fix impurities. Without these some of the user's environment
+    # will leak into the tests through `builtins.getEnv`.
+    xdg.enable = true;
+    home.username = "hm-user";
+    home.homeDirectory = "/home/hm-user";
 
-      # Avoid including documentation since this will cause
-      # unnecessary rebuilds of the tests.
-      manual.manpages.enable = false;
+    # Avoid including documentation since this will cause
+    # unnecessary rebuilds of the tests.
+    manual.manpages.enable = false;
 
-      imports = [ ./asserts.nix ];
-    }
-  ];
+    imports = [ ./asserts.nix ];
+  }];
 
-in
-
-import nmt {
+in import nmt {
   inherit lib pkgs modules;
   testedAttrPath = [ "home" "activationPackage" ];
   tests = builtins.foldl' (a: b: a // (import b)) { } ([
@@ -81,40 +77,40 @@ import nmt {
     ./modules/programs/zplug
     ./modules/programs/zsh
     ./modules/xresources
-  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-    ./modules/targets-darwin
-  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-    ./modules/config/i18n
-    ./modules/misc/debug
-    ./modules/misc/numlock
-    ./modules/misc/pam
-    ./modules/misc/qt
-    ./modules/misc/xdg
-    ./modules/misc/xsession
-    ./modules/programs/abook
-    ./modules/programs/autorandr
-    ./modules/programs/firefox
-    ./modules/programs/getmail
-    ./modules/programs/i3status-rust
-    ./modules/programs/ncmpcpp-linux
-    ./modules/programs/neovim   # Broken package dependency on Darwin.
-    ./modules/programs/rofi
-    ./modules/programs/rofi-pass
-    ./modules/programs/waybar
-    ./modules/services/dropbox
-    ./modules/services/emacs
-    ./modules/services/fluidsynth
-    ./modules/services/kanshi
-    ./modules/services/lieer
-    ./modules/services/redshift-gammastep
-    ./modules/services/pbgopy
-    ./modules/services/playerctld
-    ./modules/services/polybar
-    ./modules/services/sxhkd
-    ./modules/services/window-managers/i3
-    ./modules/services/window-managers/sway
-    ./modules/services/wlsunset
-    ./modules/systemd
-    ./modules/targets-linux
-  ]);
+  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin
+    [ ./modules/targets-darwin ]
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+      ./modules/config/i18n
+      ./modules/misc/debug
+      ./modules/misc/numlock
+      ./modules/misc/pam
+      ./modules/misc/qt
+      ./modules/misc/xdg
+      ./modules/misc/xsession
+      ./modules/programs/abook
+      ./modules/programs/autorandr
+      ./modules/programs/firefox
+      ./modules/programs/getmail
+      ./modules/programs/i3status-rust
+      ./modules/programs/ncmpcpp-linux
+      ./modules/programs/neovim # Broken package dependency on Darwin.
+      ./modules/programs/rofi
+      ./modules/programs/rofi-pass
+      ./modules/programs/waybar
+      ./modules/services/dropbox
+      ./modules/services/emacs
+      ./modules/services/fluidsynth
+      ./modules/services/kanshi
+      ./modules/services/lieer
+      ./modules/services/redshift-gammastep
+      ./modules/services/pbgopy
+      ./modules/services/playerctld
+      ./modules/services/polybar
+      ./modules/services/sxhkd
+      ./modules/services/window-managers/i3
+      ./modules/services/window-managers/sway
+      ./modules/services/wlsunset
+      ./modules/systemd
+      ./modules/targets-linux
+    ]);
 }
