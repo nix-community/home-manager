5709b5f9536354a5cfdf80f31d82abcd4432edb6
oxalica <oxalicc@pm.me>
2021-05-02 04:33:45 +0800

    zsh: add history.ignorePatterns option (#1933)
    The corresponding variable is `HISTORY_IGNORE` described in zshparam(1).

--- tests/modules/programs/zsh/default.nix
+++ tests/modules/programs/zsh/default.nix
@@ -4,5 +4,6 @@
   zsh-history-path-new-custom = ./history-path-new-custom.nix;
   zsh-history-path-old-default = ./history-path-old-default.nix;
   zsh-history-path-old-custom = ./history-path-old-custom.nix;
+  zsh-history-ignore-pattern = ./history-ignore-pattern.nix;
   zsh-prezto = ./prezto.nix;
 }
