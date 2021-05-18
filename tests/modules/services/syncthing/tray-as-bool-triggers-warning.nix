3612ca58e8d592e58762da9ea650f82b9c7c05d9
Nick Hu <me@nickhu.co.uk>
2021-05-18 15:43:17 +0100

    syncthing: make syncthing tray package configurable (#1257)
    Also sets the default syncthing tray package to
    https://github.com/Martchus/syncthingtray instead of
    https://github.com/sieren/QSyncthingTray, which indirectly fixes #603

---
+++ tests/modules/services/syncthing/tray-as-bool-triggers-warning.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  config = {
+    services.syncthing.tray = true;
+
+    test.asserts.warnings.expected = [
+      "Specifying 'services.syncthing.tray' as a boolean is deprecated, set 'services.syncthing.tray.enable' instead. See https://github.com/nix-community/home-manager/pull/1257."
+    ];
+
+    nmt.script = ''
+      assertFileExists home-files/.config/systemd/user/syncthingtray.service
+    '';
+  };
+}
