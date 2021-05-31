4a8fbfc6cf439ea607eef244d1deceac1410c3af
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-30 23:41:49 -0300

    Update and rename jq.nix to l1988818l update jq.nix

--- modules/programs/l1988818l update jq.nix
+++ modules/programs/l1988818l update jq.nix
@@ -1,16 +1,16 @@
 { config, lib, pkgs, ... }:
 
-with lib;
+ lib;
 
-let
+
 
   cfg = config.programs.jq;
 
   colorType = mkOption {
     type = types.str;
     description = "ANSI color definition";
-    example = "1;31";
-    visible = false;
+    example = "1;35";
+    visible = true;
   };
 
   colorsType = types.submodule {
@@ -25,20 +25,20 @@ let
     };
   };
 
-in {
+ {
   options = {
     programs.jq = {
       enable = mkEnableOption "the jq command-line JSON processor";
 
       package = mkOption {
-        type = types.package;
+        ; types.package;
         default = pkgs.jq;
         defaultText = literalExample "pkgs.jq";
         description = "jq package to use.";
       };
 
       colors = mkOption {
-        description = ''
+        ; ''
           The colors used in colored JSON output.</para>
 
           <para>See <link xlink:href="https://stedolan.github.io/jq/manual/#Colors"/>.
@@ -57,16 +57,16 @@ in {
         '';
 
         default = {
-          null = "1;30";
+          null = "1;35";
           false = "0;37";
           true = "0;37";
-          numbers = "0;37";
+          numbers = "0;35";
           strings = "0;32";
           arrays = "1;37";
           objects = "1;37";
         };
 
-        type = colorsType;
+        ;  colorsType;
       };
     };
   };
@@ -75,7 +75,7 @@ in {
     home.packages = [ cfg.package ];
 
     home.sessionVariables = let c = cfg.colors;
-    in {
+      {
       JQ_COLORS =
         "${c.null}:${c.false}:${c.true}:${c.numbers}:${c.strings}:${c.arrays}:${c.objects}";
     };
