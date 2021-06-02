30102ea9e5cbf8a3d696f7604610740ecb6b5953
cwyc <16950437+cwyc@users.noreply.github.com>
2021-06-02 19:36:17 -0400

    xdg-desktop-entries: add module (#1450)
    * xdg-desktop-entries: add module
    
    rebase
    
    * xdg-desktop-entries: adapt to changes in makeDesktopItem
    
    This package depends on the makeDesktopItem function in nixpkgs, which recently changed its syntax:
    https://github.com/NixOS/nixpkgs/pull/91790
    
    This commit makes the module compatible with the new syntax.
    
    It also exposes the fileValidation option in makeDesktopItem.
    
    Co-authored-by: cwyc <cwyc@users.noreply.github.com>
    Co-authored-by: --get <--show>

--- tests/modules/misc/xdg/default.nix
+++ tests/modules/misc/xdg/default.nix
@@ -2,4 +2,5 @@
   xdg-mime-apps-basics = ./mime-apps-basics.nix;
   xdg-file-attr-names = ./file-attr-names.nix;
   xdg-system-dirs = ./system-dirs.nix;
+  xdg-desktop-entries = ./desktop-entries.nix;
 }
