95da56b783e4ccc8ded71137e4add780b239dd46
Basti <mail@sebastian-sellmeier.de>
2021-06-03 02:03:20 +0200

    i3,sway: workspace output assignment (#2003)
    (cherry picked from commit ea3ff797c87313e71f2ed2fd2b932a18a4b4400b)

--- tests/modules/services/window-managers/i3/default.nix
+++ tests/modules/services/window-managers/i3/default.nix
@@ -5,4 +5,5 @@
   i3-keybindings = ./i3-keybindings.nix;
   i3-null-config = ./i3-null-config.nix;
   i3-workspace-default = ./i3-workspace-default.nix;
+  i3-workspace-output = ./i3-workspace-output.nix;
 }
