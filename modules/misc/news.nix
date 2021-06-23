49864a4370fcf4114aef839bfb0b81876b6ae8e5
Nicolas Berbiche <nicolas@normie.dev>
2021-06-22 20:36:47 -0400

    xmonad: document breakage of recompilation (#2024)
    Co-authored-by: Markus Scherer <markus.f.scherer@gmail.com>

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2084,6 +2084,25 @@ in
           A new module is available: 'programs.mangohud'.
         '';
       }
+
+      {
+        time = "2021-06-16T01:26:16+00:00";
+        message = ''
+          The xmonad module now compiles the configuration before
+          linking the binary to the place xmonad expects to find
+          the compiled configuration (the binary).
+
+          This breaks recompilation of xmonad (i.e. the 'q' binding or
+          'xmonad --recompile').
+
+          If this behavior is undesirable, do not use the
+          'xsession.windowManager.xmonad.config' option. Instead, set the
+          contents of the configuration file with
+          'home.file.".xmonad/config.hs".text = "content of the file"'
+          or 'home.file.".xmonad/config.hs".source = ./path-to-config'.
+        '';
+      }
+
     ];
   };
 }
