9ea5fa31dc8a0f5943e73e18023e8d4dd0050317
axion <i@axionl.me>
2021-07-16 20:32:53 +0800

    add some new options

--- home-manager/completion.fish
+++ home-manager/completion.fish
@@ -21,9 +21,11 @@ function __home_manager_generations --description "Get all generations"
     end
 end
 
+
 ### SubCommands
 complete -c home-manager -n "__fish_use_subcommand" -f -a "help" -d "Print home-manager help"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "edit" -d "Open the home configuration in $EDITOR"
+complete -c home-manager -n "__fish_use_subcommand" -f -a "option" -d "Inspect configuration option"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "build" -d "Build configuration into result directory"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "instantiate" -d "Instantiate the configuration and print the resulting derivation"
 complete -c home-manager -n "__fish_use_subcommand" -f -a "switch" -d "Build and activate configuration"
@@ -39,18 +41,22 @@ complete -c home-manager -n "__fish_use_subcommand" -x -a "expire-generations" -
 
 ### Options
 complete -c home-manager -F -s f -l "file" -d "The home configuration file"
-complete -c home-manager -x -s A -d "Optional attribute that selects a configuration expression in the configuration file"
+complete -c home-manager -x -s A -d "Select an expression in the configuration file"
 complete -c home-manager -F -s I -d "Add a path to the Nix expression search path"
+complete -c home-manager -F -l "flake" -d "Use home-manager configuration at specified flake-uri"
 complete -c home-manager -F -s b -d "Move existing files to new path rather than fail"
 complete -c home-manager -f -s v -l "verbose" -d "Verbose output"
 complete -c home-manager -f -s n -l "dry-run" -d "Do a dry run, only prints what actions would be taken"
 complete -c home-manager -f -s h -l "help" -d "Print this help"
 
-complete -c home-manager -x -l "cores" -d "NUM"
-complete -c home-manager -f -l "keep-failed"
-complete -c home-manager -f -l "keep-going"
-complete -c home-manager -x -s j -l "max-jobs" -d "NUM"
-complete -c home-manager -x -l "option" -d "NAME VALUE"
-complete -c home-manager -f -l "show-trace"
+complete -c home-manager -x -l "arg" -d "Override inputs passed to home-manager.nix"
+complete -c home-manager -x -l "argstr" -d "Like --arg but the value is a string"
+complete -c home-manager -x -l "cores" -d "Threads per job (e.g. -j argument to make)"
+complete -c home-manager -x -l "debug"
+complete -c home-manager -f -l "keep-failed" -d "Keep temporary directory used by failed builds"
+complete -c home-manager -f -l "keep-going" -d "Keep going in case of failed builds"
+complete -c home-manager -x -s j -l "max-jobs" -d "Max number of build jobs in parallel"
+complete -c home-manager -x -l "option" -d "Set Nix configuration option"
+complete -c home-manager -f -l "show-trace" -d "Print stack trace of evaluation errors"
 complete -c home-manager -f -l "substitute"
 complete -c home-manager -f -l "no-substitute"
