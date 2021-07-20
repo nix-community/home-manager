7c19bcb822a351cbcdda245710a6e17d296f0647
zseri <zseri.devel@ytrizja.de>
2021-07-20 14:40:45 +0200

    nixos: remove indirection via activate-${username}
    Fixes #2191

--- nixos/default.nix
+++ nixos/default.nix
@@ -144,11 +144,8 @@ in {
 
           # The activation script is run by a login shell to make sure
           # that the user is given a sane Nix environment.
-          ExecStart = pkgs.writeScript "activate-${username}" ''
-            #! ${pkgs.runtimeShell} -el
-            echo Activating home-manager configuration for ${username}
-            exec ${usercfg.home.activationPackage}/activate
-          '';
+          ExecStart =
+            "${pkgs.runtimeShell} -l ${usercfg.home.activationPackage}/activate";
         };
       }) cfg.users;
   };
