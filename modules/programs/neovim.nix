e12ffa6282846afa9fc045e2cb1f215dbe42b897
t4ccer <t4ccer@gmail.com>
2021-07-25 15:56:36 +0200

    neovim: Update coc example

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -226,26 +226,28 @@ in {
         settings = mkOption {
           type = jsonFormat.type;
           default = { };
-          example = {
-            "suggest.noselect" = true;
-            "suggest.enablePreview" = true;
-            "suggest.enablePreselect" = false;
-            "suggest.disableKind" = true;
-            languageserver = {
-              haskell = {
-                command = "haskell-language-server-wrapper";
-                args = [ "--lsp" ];
-                rootPatterns = [
-                  "*.cabal"
-                  "stack.yaml"
-                  "cabal.project"
-                  "package.yaml"
-                  "hie.yaml"
-                ];
-                filetypes = [ "haskell" "lhaskell" ];
+          example = literalExample ''
+            {
+              "suggest.noselect" = true;
+              "suggest.enablePreview" = true;
+              "suggest.enablePreselect" = false;
+              "suggest.disableKind" = true;
+              languageserver = {
+                haskell = {
+                  command = "haskell-language-server-wrapper";
+                  args = [ "--lsp" ];
+                  rootPatterns = [
+                    "*.cabal"
+                    "stack.yaml"
+                    "cabal.project"
+                    "package.yaml"
+                    "hie.yaml"
+                  ];
+                  filetypes = [ "haskell" "lhaskell" ];
+                };
               };
             };
-          };
+          '';
           description = ''
             Extra configuration lines to add to
             <filename>$XDG_CONFIG_HOME/nvim/coc-settings.json</filename>
