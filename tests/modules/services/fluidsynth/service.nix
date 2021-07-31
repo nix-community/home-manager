72394f6d6b1cee26021c3e319fa249122ad33d82
Nicholas Coltharp <coltharp@pdx.edu>
2021-07-31 06:05:27 -0500

    fluidsynth: add sound service option
    Fluidsynth's systemd unit currently has a hard dependency on the
    pulseaudio systemd service. Since fluidsynth can use other sound
    services (e.g., pipewire-pulse), this should be configurable. This
    commit adds the relevant option.
    
    PR #2238

--- tests/modules/services/fluidsynth/service.nix
+++ tests/modules/services/fluidsynth/service.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   config = {
     services.fluidsynth.enable = true;
+    services.fluidsynth.soundService = "pipewire-pulse";
     services.fluidsynth.soundFont = "/path/to/soundFont";
     services.fluidsynth.extraOptions = [ "--sample-rate 96000" ];
 
@@ -19,6 +20,12 @@
 
       assertFileContains $serviceFile \
         'ExecStart=@fluidsynth@/bin/fluidsynth -a pulseaudio -si --sample-rate 96000 /path/to/soundFont'
+
+      assertFileContains $serviceFile \
+        'After=pipewire-pulse.service'
+
+      assertFileContains $serviceFile \
+        'BindsTo=pipewire-pulse.service'
     '';
   };
 }
