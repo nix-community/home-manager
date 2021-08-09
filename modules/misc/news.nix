2c4234cb79684646657f9cfcd4075c3122845670
Ivan Malison <IvanMalison@gmail.com>
2021-08-08 18:29:36 -0600

    notify-osd: init (#2240)
    Daemon that displays passive pop-up notifications

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2149,6 +2149,14 @@ in
           A new module is available: 'services.trayer'.
         '';
       }
+
+      {
+        time = "2021-07-19T01:30:46+00:00";
+        condition = hostPlatform.isLinux;
+        message = ''
+          A new module is available: 'services.notify-osd'.
+        '';
+      }
     ];
   };
 }
---
+++ modules/services/notify-osd.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.notify-osd;
+
+in {
+  meta.maintainers = [ maintainers.imalison ];
+
+  options = {
+    services.notify-osd = {
+      enable = mkEnableOption "notify-osd";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.notify-osd;
+        defaultText = literalExample "pkgs.notify-osd";
+        description = ''
+          Package containing the <command>notify-osd</command> program.
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [
+      (lib.hm.assertions.assertPlatform "services.notify-osd" pkgs
+        lib.platforms.linux)
+    ];
+
+    systemd.user.services.notify-osd = {
+      Unit = {
+        Description = "notify-osd";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install.WantedBy = [ "graphical-session.target" ];
+
+      Service = {
+        ExecStart = "${cfg.package}/bin/notify-osd";
+        Restart = "on-abort";
+      };
+    };
+  };
+}
