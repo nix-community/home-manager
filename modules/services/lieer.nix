f347ea34f8755739b7993a9b9027b7c9747a93ea
arcnmx <arcnmx@users.noreply.github.com>
2021-11-16 09:20:56 -0800

    lieer: use configured package in service

--- modules/services/lieer.nix
+++ modules/services/lieer.nix
@@ -24,7 +24,7 @@ let
 
       Service = {
         Type = "oneshot";
-        ExecStart = "${pkgs.gmailieer}/bin/gmi sync";
+        ExecStart = "${config.programs.lieer.package}/bin/gmi sync";
         WorkingDirectory = account.maildir.absPath;
         Environment =
           "NOTMUCH_CONFIG=${config.xdg.configHome}/notmuch/notmuchrc";
