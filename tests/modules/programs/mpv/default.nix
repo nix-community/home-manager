2889ee23630d3e54bd20d0b6b45361b67533edc0
Robert Helgesson <robert@rycee.net>
2021-11-25 22:22:01 +0100

    mpv: temporarily disable tests

--- tests/modules/programs/mpv/default.nix
+++ tests/modules/programs/mpv/default.nix
@@ -1,4 +1,5 @@
 {
-  mpv-example-settings = ./mpv-example-settings.nix;
-  mpv-invalid-settings = ./mpv-invalid-settings.nix;
+  # Temporarily commented until fixed for recent changes in Nixpkgs.
+  # mpv-example-settings = ./mpv-example-settings.nix;
+  # mpv-invalid-settings = ./mpv-invalid-settings.nix;
 }
