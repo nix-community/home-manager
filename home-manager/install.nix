579f2e8bebb954a103a96b905c27b10f15ef38c7
Robert Helgesson <robert@rycee.net>
2021-11-25 17:19:06 +0100

    Switch to 22.05 as current development release

--- home-manager/install.nix
+++ home-manager/install.nix
@@ -44,7 +44,7 @@ runCommand "home-manager-install" {
       # You can update Home Manager without changing this value. See
       # the Home Manager release notes for a list of state version
       # changes in each release.
-      home.stateVersion = "21.11";
+      home.stateVersion = "22.05";
 
       # Let Home Manager install and manage itself.
       programs.home-manager.enable = true;
