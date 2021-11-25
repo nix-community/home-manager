c27c8f49c0bccaff91f5f637ad727d440b769997
Robert Helgesson <robert@rycee.net>
2021-11-25 08:32:19 +0100

    taskwarrior: clean up news entry

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2244,10 +2244,12 @@ in
 
       {
         time = "2021-11-23T20:26:37+00:00";
+        condition = config.programs.taskwarrior.enable;
         message = ''
-          Taskwarrior version 2.6.0 respects XDG Specification for the config file now.
-          Option 'opt-programs.taskwarrior.config' and friends now generate the config file at
-          '$XDG_CONFIG_HOME/task/taskrc' instead of '~/.taskrc'.
+          Taskwarrior version 2.6.0 respects XDG Specification for the config
+          file now. Option 'programs.taskwarrior.config' and friends now
+          generate the config file at '$XDG_CONFIG_HOME/task/taskrc' instead of
+          '~/.taskrc'.
         '';
       }
     ];
