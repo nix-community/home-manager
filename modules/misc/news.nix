1aaa1a033bae6c53773ad4374756ac72ba25b729
Nicolas Berbiche <nicolas@normie.dev>
2021-12-01 23:22:41 -0500

    docs: add note about Waybar modules

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2269,6 +2269,17 @@ in
           macOS.
         '';
       }
+
+      {
+        time = "2021-12-02T02:59:59+00:00";
+        condition = config.programs.waybar.enable;
+        message = ''
+          The Waybar module now allows defining modules directly under the 'settings'
+          option instead of nesting the modules under 'settings.modules'.
+          The Waybar module will also stop reporting errors about unused or misnamed
+          modules.
+        '';
+      }
     ];
   };
 }
