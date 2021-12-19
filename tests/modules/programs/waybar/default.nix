3db603677509eb0b8c396a3234b1d4b70d023894
Bruno Inec <7051978+sweenu@users.noreply.github.com>
2021-12-19 06:21:15 +0100

    waybar: allow using attrs for settings (#2547)
    Co-authored-by: Bruno Inec <binec@scaleway.com>

--- tests/modules/programs/waybar/default.nix
+++ tests/modules/programs/waybar/default.nix
@@ -3,5 +3,6 @@
     ./systemd-with-graphical-session-target.nix;
   waybar-styling = ./styling.nix;
   waybar-settings-complex = ./settings-complex.nix;
+  waybar-settings-with-attrs = ./settings-with-attrs.nix;
   waybar-deprecated-modules-option = ./deprecated-modules-option.nix;
 }
--- tests/modules/programs/waybar/settings-complex.nix
+++ tests/modules/programs/waybar/settings-complex.nix
@@ -9,43 +9,52 @@ with lib;
     programs.waybar = {
       package = config.lib.test.mkStubPackage { outPath = "@waybar@"; };
       enable = true;
-      settings = [{
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [ "DP-1" ];
-        modules-left = [ "sway/workspaces" "sway/mode" "custom/my-module" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [
-          "idle_inhibitor"
-          "pulseaudio"
-          "network"
-          "cpu"
-          "memory"
-          "backlight"
-          "tray"
-          "battery#bat1"
-          "battery#bat2"
-          "clock"
-        ];
+      settings = [
+        {
+          layer = "top";
+          position = "top";
+          height = 30;
+          output = [ "DP-1" ];
+          modules-left = [ "sway/workspaces" "sway/mode" "custom/my-module" ];
+          modules-center = [ "sway/window" ];
+          modules-right = [
+            "idle_inhibitor"
+            "pulseaudio"
+            "network"
+            "cpu"
+            "memory"
+            "backlight"
+            "tray"
+            "battery#bat1"
+            "battery#bat2"
+            "clock"
+          ];
 
-        modules = {
-          "sway/workspaces" = {
-            disable-scroll = true;
-            all-outputs = true;
+          modules = {
+            "sway/workspaces" = {
+              disable-scroll = true;
+              all-outputs = true;
+            };
+            "sway/mode" = { tooltip = false; };
+            "sway/window" = { max-length = 120; };
+            "idle_inhibitor" = { format = "{icon}"; };
+            "custom/my-module" = {
+              format = "hello from {}";
+              exec = let
+                dummyScript =
+                  config.lib.test.mkStubPackage { outPath = "@dummy@"; };
+              in "${dummyScript}/bin/dummy";
+            };
           };
-          "sway/mode" = { tooltip = false; };
-          "sway/window" = { max-length = 120; };
-          "idle_inhibitor" = { format = "{icon}"; };
-          "custom/my-module" = {
-            format = "hello from {}";
-            exec = let
-              dummyScript =
-                config.lib.test.mkStubPackage { outPath = "@dummy@"; };
-            in "${dummyScript}/bin/dummy";
-          };
-        };
-      }];
+        }
+        {
+          position = "bottom";
+          output = [ "!DP-1" ];
+          modules-left = [ "sway/mode" ];
+          modules-center = [ "clock" ];
+          modules = { "sway/mode" = { tooltip = true; }; };
+        }
+      ];
     };
 
     nmt.script = ''
