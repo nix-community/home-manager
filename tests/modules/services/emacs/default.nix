f3be3cda6a69365f2acecc201b3cd1ee4f6d4614
rcerc <88944439+rcerc@users.noreply.github.com>
2022-01-10 02:17:13 -0500

    services/emacs: add option to set `emacsclient` as the default editor (#2545)
    * services/emacs: add `defaultEditor` option
    
    Sets `emacsclient` as the default editor using the `EDITOR` environment variable.

--- tests/modules/services/emacs/default.nix
+++ tests/modules/services/emacs/default.nix
@@ -3,4 +3,5 @@
   emacs-service-28 = ./emacs-service-28.nix;
   emacs-socket-27 = ./emacs-socket-27.nix;
   emacs-socket-28 = ./emacs-socket-28.nix;
+  emacs-default-editor = ./emacs-default-editor.nix;
 }
