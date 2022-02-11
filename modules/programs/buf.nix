f0ba13164a3e81bc06cf69b2410e00a2de49908e
Luc Perkins <lucperkins@gmail.com>
2022-02-10 16:28:39 -0800

    Remove shell completion options

--- modules/programs/buf.nix
+++ modules/programs/buf.nix
@@ -15,45 +15,9 @@ in {
       defaultText = literalExpression "pkgs.buf";
       description = "Package providing <command>buf</command>.";
     };
-
-    enableBashIntegration = mkOption {
-      default = false;
-      type = types.bool;
-      description = ''
-        Whether to load Bash autocomplete scripts for <command>buf</command>.
-      '';
-    };
-
-    enableFishIntegration = mkOption {
-      default = false;
-      type = types.bool;
-      description = ''
-        Whether to load Fish autocomplete scripts for <command>buf</command>.
-      '';
-    };
-
-    enableZshIntegration = mkOption {
-      default = false;
-      type = types.bool;
-      description = ''
-        Whether to load zsh autocomplete scripts for <command>buf</command>.
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
-
-    programs.bash.initExtra = mkIf cfg.enableBashIntegration ''
-      eval "$(${pkgs.buf}/bin/buf completion bash)"
-    '';
-
-    programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration ''
-      ${pkgs.buf}/bin/buf completion fish | source
-    '';
-
-    programs.zsh.initExtra = mkIf cfg.enableZshIntegration ''
-      eval "$(${pkgs.buf}/bin/buf completion zsh)"
-    '';
   };
 }
