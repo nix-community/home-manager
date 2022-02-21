21da41f34060fd09092e6c6301349193d385c996
Gregory Oakes <2153080+gcoakes@users.noreply.github.com>
2022-02-20 19:51:20 -0600

    vscode: add mutable user settings option.
    User settings could not be modified once home-manager took over managing
    settings. This setting allows user settings to diverge from what is
    configured in the user's home-manager config. New settings are still
    applied but changed or removed settings are not.

--- modules/programs/vscode.nix
+++ modules/programs/vscode.nix
@@ -135,6 +135,16 @@ in {
           or by Visual Studio Code.
         '';
       };
+
+      mutableUserSettings = mkOption {
+        type = types.bool;
+        default = false;
+        example = false;
+        description = ''
+          Whether user settings can be changed manually or by Visual
+          Studio Code.
+        '';
+      };
     };
   };
 
@@ -142,7 +152,7 @@ in {
     home.packages = [ cfg.package ];
 
     home.file = mkMerge [
-      (mkIf (cfg.userSettings != { }) {
+      (mkIf (!cfg.mutableUserSettings && cfg.userSettings != { }) {
         "${configFilePath}".source =
           jsonFormat.generate "vscode-user-settings" cfg.userSettings;
       })
@@ -172,5 +182,23 @@ in {
         "${extensionPath}".source = extensionsFolder;
       }))
     ];
+
+    home.activation =
+      mkIf (cfg.mutableUserSettings && cfg.userSettings != { }) {
+        injectVscodeSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          tmp="$(mktemp)"
+          if [[ -v DRY_RUN ]]; then
+            echo tmp="\$(mktemp)"
+            echo ${pkgs.jq}/bin/jq -s "'reduce .[] as \$x ({}; . * \$x)'" "${
+              jsonFormat.generate "vscode-user-settings" cfg.userSettings
+            }" "${configFilePath}" ">" "$tmp"
+          else
+            ${pkgs.jq}/bin/jq -s 'reduce .[] as $x ({}; . * $x)' "${
+              jsonFormat.generate "vscode-user-settings" cfg.userSettings
+            }" "${configFilePath}" > "$tmp"
+          fi
+          $DRY_RUN_CMD mv "$tmp" "${configFilePath}"
+        '';
+      };
   };
 }
