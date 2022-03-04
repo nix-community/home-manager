bf0003da18124b0d0227768cd46e3e1369b7ad62
Yt <raphael@megzari.com>
2022-03-03 22:32:59 -0500

    i3status-rust: change default to newer version (#2774)

--- modules/programs/i3status-rust.nix
+++ modules/programs/i3status-rust.nix
@@ -144,8 +144,8 @@ in {
             {
               block = "memory";
               display_type = "memory";
-              format_mem = "{Mup}%";
-              format_swap = "{SUp}%";
+              format_mem = "{mem_used_percents}";
+              format_swap = "{swap_used_percents}";
             }
             {
               block = "cpu";
