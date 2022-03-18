a4b0a3faa4055521f2a20cfafe26eb85e6954751
Naïm Favier <n@monade.li>
2022-03-18 03:22:53 +0100

    xdg: add `XDG_*_HOME` variables to `systemd.user.sessionVariables` (#2790)
    Make sure those variables are set in systemd user services.
    This mirrors the situation for `XDG_*_DIRS` variables.

--- tests/modules/systemd/session-variables.nix
+++ tests/modules/systemd/session-variables.nix
@@ -15,6 +15,10 @@
           LOCALE_ARCHIVE_2_27=${pkgs.glibcLocales}/lib/locale/locale-archive
           V_int=1
           V_str=2
+          XDG_CACHE_HOME=/home/hm-user/.cache
+          XDG_CONFIG_HOME=/home/hm-user/.config
+          XDG_DATA_HOME=/home/hm-user/.local/share
+          XDG_STATE_HOME=/home/hm-user/.local/state
         ''
       }
     '';
