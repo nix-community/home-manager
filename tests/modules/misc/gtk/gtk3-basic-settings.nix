9970d23218d2215fb7e6e2cb74a3533e2b8863ad
Robert Helgesson <robert@rycee.net>
2022-03-24 00:08:37 +0100

    gtk: fix incorrect test assertion

--- tests/modules/misc/gtk/gtk3-basic-settings.nix
+++ tests/modules/misc/gtk/gtk3-basic-settings.nix
@@ -15,7 +15,7 @@ with lib;
     test.stubs.dconf = { };
 
     nmt.script = ''
-      assertPathExists home-files/.config/gtk-3.0
+      assertFileExists home-files/.config/gtk-3.0/settings.ini
 
       assertFileContent home-files/.config/gtk-3.0/settings.ini \
         ${./gtk3-basic-settings-expected.ini}
