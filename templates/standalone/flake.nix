69345217a0d9b822af940105e94a73c9e5ac7801
MaxCan-Code <35848843+MaxCan-Code@users.noreply.github.com>
2022-04-13 16:39:52 -0400

    flake: add templates
    Add example flakes from `docs/nix-flakes.adoc` as templates.
    Format standalone flake in `docs/nix-flakes.adoc`

---
+++ templates/standalone/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Home Manager configuration of Jane Doe";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.jdoe = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
