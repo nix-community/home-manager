65bcfaccc5584e3c04ac883dc0501faf1ec9025f
Michael Forster <email@michael-forster.de>
2022-05-29 18:53:49 +0200

    micro: add module
    This adds initial bare-bones support for the `micro` editor.

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -561,6 +561,13 @@ in
           A new module is available: 'programs.mujmap'.
         '';
       }
+
+      {
+        time = "2022-06-24T17:18:32+00:00";
+        message = ''
+          A new module is available: 'programs.micro'.
+        '';
+      }
     ];
   };
 }
