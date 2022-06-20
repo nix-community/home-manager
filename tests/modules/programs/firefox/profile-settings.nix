3746c462c21107f35da9123aafcb443f44d6943f
h7x4 <h7x4abk3g@protonmail.com>
2022-06-20 16:58:26 +0200

    firefox: add support for nested bookmarks
    Change type of `firefox.profile.<name>.bookmarks` to
    allow for nested bookmarks with user defined order.

--- tests/modules/programs/firefox/profile-settings.nix
+++ tests/modules/programs/firefox/profile-settings.nix
@@ -15,13 +15,30 @@ lib.mkIf config.test.enableBig {
     profiles.bookmarks = {
       id = 2;
       settings = { "general.smoothScroll" = false; };
-      bookmarks = {
-        wikipedia = {
+      bookmarks = [
+        {
+          name = "wikipedia";
           keyword = "wiki";
           url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-        };
-        "kernel.org" = { url = "https://www.kernel.org"; };
-      };
+        }
+        {
+          name = "kernel.org";
+          url = "https://www.kernel.org";
+        }
+        {
+          name = "Nix sites";
+          bookmarks = [
+            {
+              name = "homepage";
+              url = "https://nixos.org/";
+            }
+            {
+              name = "wiki";
+              url = "https://nixos.wiki/";
+            }
+          ];
+        }
+      ];
     };
   };
 
