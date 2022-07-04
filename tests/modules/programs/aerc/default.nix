0f4f2d18a8d776fb1ecc6df4a2544e1ba2bc6c9e
Lukas Nagel <lukasngl@mail.upb.de>
2022-07-04 13:59:44 +0200

    aerc: add accounts config
    This adds support for configuring email accounts, with automatic smtp, imap,
    sendmail (msmpt) and maildir (mbsync, offlineimap) setup in aerc,
    via `accounts.email`.

--- tests/modules/programs/aerc/default.nix
+++ tests/modules/programs/aerc/default.nix
@@ -1,8 +1,4 @@
 {
   aerc-noSettings = ./noSettings.nix;
-  aerc-extraAccounts = ./extraAccounts.nix;
-  aerc-extraConfig = ./extraConfig.nix;
-  aerc-extraBinds = ./extraBinds.nix;
-  aerc-stylesets = ./stylesets.nix;
-  aerc-templates = ./templates.nix;
+  aerc-settings = ./settings.nix;
 }
--- tests/modules/programs/aerc/noSettings.nix
+++ tests/modules/programs/aerc/noSettings.nix
@@ -4,16 +4,15 @@ with lib;
 
 {
   config = {
-    programs.aerc.enable = true;
-
-    test.stubs.aerc = { };
-
-    nmt.script = 
-      let dir = "home-files/.config/aerc"; in ''
+    nmt.script = let dir = "home-files/.config/aerc";
+    in ''
       assertPathNotExists ${dir}/accounts.conf
       assertPathNotExists ${dir}/aerc.conf
       assertPathNotExists ${dir}/binds.conf
       assertPathNotExists ${dir}/stylesets
     '';
+    programs.aerc.enable = true;
+
+    test.stubs.aerc = { };
   };
 }
