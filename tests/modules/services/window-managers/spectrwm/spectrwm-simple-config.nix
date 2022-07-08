a3b778e672e2a24f9307190da320782aa654b712
Loïc Reynier <loic@loicreynier.fr>
2022-07-08 19:31:25 +0200

    spectrwm: add module
    PR #3077

---
+++ tests/modules/services/window-managers/spectrwm/spectrwm-simple-config.nix
@@ -0,0 +1,25 @@
+{ ... }:
+
+{
+  xsession.windowManager.spectrwm = {
+    enable = true;
+    settings = {
+      bar_enabled = true;
+      bar_format = "+S [+R:+I] %a %b %d [%R]";
+      bar_justify = "center";
+      modkey = "Mod4";
+    };
+    programs = { term = "alacritty"; };
+    bindings = { term = "MOD+Shift+Return"; };
+    unbindings = [ "MOD+Return" ];
+  };
+
+  test.stubs.spectrwm = { };
+
+  nmt.script = ''
+    assertFileContent \
+      home-files/.config/spectrwm/spectrwm.conf ${
+        ./spectrwm-simple-config-expected-spectrwm.conf
+      }
+  '';
+}
