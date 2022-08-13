7bb4576f46f7f7590347e21b14d2f6a2dbc76638
AndersonTorres <torres.anderson.85@protonmail.com>
2022-08-12 23:37:04 -0300

    pueue: add module

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -653,6 +653,14 @@ in
           A new module is available: 'programs.bashmount'.
         '';
       }
+
+      {
+        time = "2022-08-25T21:01:37+00:00";
+        condition = hostPlatform.isLinux;
+        message = ''
+          A new module is available: 'services.pueue'.
+        '';
+      }
     ];
   };
 }
