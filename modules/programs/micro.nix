1910e009e37d669c229d009fc3d8080014e22ccf
Peder Bergebakken Sundt <pbsds@hotmail.com>
2022-09-14 20:04:15 +0200

    micro: add bindings, ensurePlugins, and enableTrueColor options

--- modules/programs/micro.nix
+++ modules/programs/micro.nix
@@ -9,11 +9,18 @@ let
   jsonFormat = pkgs.formats.json { };
 
 in {
-  meta.maintainers = [ hm.maintainers.mforster ];
+  meta.maintainers = [ hm.maintainers.mforster maintainers.pbsds ];
 
   options = {
     programs.micro = {
       enable = mkEnableOption "micro, a terminal-based text editor";
+
+      trueColor = mkOption {
+        type = types.bool;
+        default = true;
+        description =
+          "Enables support for the whole color range, should the terminal allow.";
+      };
 
       settings = mkOption {
         type = jsonFormat.type;
@@ -31,13 +38,72 @@ in {
           for supported values.
         '';
       };
+
+      bindings = mkOption {
+        type = jsonFormat.type;
+        default = { };
+        example = literalExpression ''
+          {
+            "Alt-d" = "SpawnMultiCursor";
+            "Escape" = "RemoveAllMultiCursors";
+            "CtrlDown" = "None";
+            "CtrlUp" = "None";
+            "Shift-PageDown" = "SelectPageDown";
+            "Shift-PageUp" = "SelectPageUp";
+          }
+        '';
+        description = ''
+          Configuration written to
+          <filename>$XDG_CONFIG_HOME/micro/bindings.json</filename>. See
+          <link xlink:href="https://github.com/zyedidia/micro/blob/master/runtime/help/keybindings.md"/>
+          for supported values.
+        '';
+      };
+
+      ensurePlugins = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        example = literalExpression ''
+          [
+            "aspell"
+          ]
+        '';
+        description = ''
+          Install micro plugins during activation. See
+          <link xlink:href="https://micro-editor.github.io/plugins.html"/>
+          for a listing of available plugins.
+        '';
+
+      };
     };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ pkgs.micro ];
 
+    home.sessionVariables = mkIf cfg.trueColor { MICRO_TRUECOLOR = "1"; };
+
     xdg.configFile."micro/settings.json".source =
       jsonFormat.generate "micro-settings" cfg.settings;
+
+    xdg.configFile."micro/bindings.json".source =
+      jsonFormat.generate "micro-bindings" cfg.bindings;
+
+    home.activation = let
+      mkInstall = pluginName: ''
+        if ! test -d ${config.xdg.configHome}/micro/plug/${
+          lib.escapeShellArg pluginName
+        }; then
+          (set -x
+            $DRY_RUN_CMD ${pkgs.micro}/bin/micro -plugin install ${
+              lib.escapeShellArg pluginName
+            }
+          )
+        fi
+      '';
+      installs = lib.concatStringsSep "\n" (map mkInstall cfg.ensurePlugins);
+    in mkIf (cfg.ensurePlugins != [ ]) {
+      microPluginSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] installs;
+    };
   };
 }
--- tests/modules/programs/micro/micro.nix
+++ tests/modules/programs/micro/micro.nix
@@ -8,17 +8,33 @@
       autosu = false;
       cursorline = false;
     };
+
+    bindings = {
+      Alt-d = "SpawnMultiCursor";
+      Escape = "RemoveAllMultiCursors";
+    };
   };
 
   test.stubs.micro = { };
 
   nmt.script = ''
-    assertFileContent home-files/.config/micro/settings.json \
-    ${builtins.toFile "micro-expected-settings.json" ''
-      {
-        "autosu": false,
-        "cursorline": false
-      }
-    ''}
+     assertFileContent home-files/.config/micro/settings.json \
+     ${
+       builtins.toFile "micro-expected-settings.json" ''
+         {
+           "autosu": false,
+           "cursorline": false
+         }
+       ''
+     }
+    assertFileContent home-files/.config/micro/bindings.json \
+     ${
+       builtins.toFile "micro-expected-settings.json" ''
+         {
+           "Alt-d": "SpawnMultiCursor",
+           "Escape": "RemoveAllMultiCursors"
+         }
+       ''
+     }
   '';
 }
