65c599f9653f24ed2336fbde7220a6180537ebf0
Matthieu Coudron <mcoudron@hotmail.com>
2022-09-20 01:50:47 +0200

    fixup! programs.neovim: default to init.lua

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -368,13 +368,11 @@ in {
           "nvim/init.vim" = mkIf (neovimConfig.neovimRcContent != "") {
             text = neovimConfig.neovimRcContent;
           };
-          "nvim/init.lua" =
-            mkIf (hasLuaConfig || neovimConfig.neovimRcContent != "") {
-              text = lib.optionalString hasLuaConfig
-                "vim.cmd.source ${config.xdg.configFile."nvim/init.vim"}"
-                + lib.optionalString hasLuaConfig
-                config.programs.neovim.generatedConfigs.lua;
-            };
+          "nvim/init.lua".text =
+            lib.optionalString (neovimConfig.neovimRcContent != "")
+            "vim.cmd.source ${config.xdg.configHome}/nvim/init.vim"
+            + lib.optionalString hasLuaConfig
+            config.programs.neovim.generatedConfigs.lua;
           "nvim/coc-settings.json" = mkIf cfg.coc.enable {
             source = jsonFormat.generate "coc-settings.json" cfg.coc.settings;
           };
