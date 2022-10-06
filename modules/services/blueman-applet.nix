7757c602ac5e4f0124ffd588ecd06a4647b2b8a3
Adam Stephens <adam@valkor.net>
2022-10-06 15:54:01 -0400

    blueman-applet: fix service name in example

--- modules/services/blueman-applet.nix
+++ modules/services/blueman-applet.nix
@@ -13,7 +13,7 @@ with lib;
           be enabled system-wide. You can enable it in the system
           configuration using
           <programlisting language="nix">
-            services.blueman.enable = true;
+            services.blueman-applet.enable = true;
           </programlisting>
         '';
       };
