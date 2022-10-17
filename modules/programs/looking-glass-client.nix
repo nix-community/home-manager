8a539df77e7edef3ba1d3ea925874df22d681441
j-brn <me@bricker.io>
2022-10-17 20:07:00 +0200

    looking-glass-client: format code properly

--- modules/programs/looking-glass-client.nix
+++ modules/programs/looking-glass-client.nix
@@ -45,15 +45,16 @@ in {
   };
 
   config = mkIf cfg.enable {
-    assertions =
-      [ (hm.assertions.assertPlatform "programs.looking-glass-client" pkgs platforms.linux) ];
+    assertions = [
+      (hm.assertions.assertPlatform "programs.looking-glass-client" pkgs
+        platforms.linux)
+    ];
 
     home.packages = [ cfg.package ];
 
-    xdg.configFile."looking-glass/client.ini" =
-      mkIf (cfg.settings != { }) {
-        source = settingsFormat.generate ("looking-glass-client.ini")
-          cfg.settings;
-      };
+    xdg.configFile."looking-glass/client.ini" = mkIf (cfg.settings != { }) {
+      source =
+        settingsFormat.generate ("looking-glass-client.ini") cfg.settings;
+    };
   };
 }
