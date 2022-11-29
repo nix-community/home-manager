c814aa1b5d1c812572e9a4aa230d7c4993583b19
ThinkChaos <ThinkChaos@users.noreply.github.com>
2022-11-28 20:57:10 -0500

    nixos: use `lib` argument instead of `pkgs.lib`
    This allows users to pass a lib with custom attributes and have those
    available to custom HM modules.

--- nixos/common.nix
+++ nixos/common.nix
@@ -9,7 +9,7 @@ let
 
   cfg = config.home-manager;
 
-  extendedLib = import ../modules/lib/stdlib-extended.nix pkgs.lib;
+  extendedLib = import ../modules/lib/stdlib-extended.nix lib;
 
   hmModule = types.submoduleWith {
     description = "Home Manager module";
