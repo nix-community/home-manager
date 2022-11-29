cd2a826f33ee96f705e8c07b01fd1346b2eccbc0
Robert Helgesson <robert@rycee.net>
2022-11-29 13:25:48 +0100

    ssh: make news item conditional

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -846,6 +846,7 @@ in
 
       {
         time = "2022-11-27T13:14:01+00:00";
+        condition = config.programs.ssh.enable;
         message = ''
           'programs.ssh.matchBlocks.*' now supports literal 'Match' blocks via
           'programs.ssh.matchBlocks.*.match' option as an alternative to plain
