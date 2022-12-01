5e8352c97f9e15502b2b41db90a90b45cdd3ca32
Chris Montgomery <chris@cdom.io>
2022-12-01 14:42:34 -0500

    gpg-agent: add @montchr as maintainer

--- modules/services/gpg-agent.nix
+++ modules/services/gpg-agent.nix
@@ -89,7 +89,7 @@ let
   in hexString: (foldl' go initState (splitChars hexString)).ret;
 
 in {
-  meta.maintainers = [ maintainers.rycee ];
+  meta.maintainers = with maintainers; [ rycee montchr ];
 
   options = {
     services.gpg-agent = {
