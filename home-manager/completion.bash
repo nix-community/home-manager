89a8ba0b5b43b3350ff2e3ef37b66736b2ef8706
Anders Kaseorg <andersk@mit.edu>
2022-12-24 22:48:45 -0800

    home-manager: pass -L/--print-build-logs to nix build
    When building from a flake, `nix build` hides the build output by
    default, with a `-L`/`--print-build-logs` option to show it. Pass this
    option along from `home-manager` if the user provides it.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    (cherry picked from commit c55fa26ce05fee8e063db22918d05a73d430b2ea)

--- home-manager/completion.bash
+++ home-manager/completion.bash
@@ -293,6 +293,7 @@ _home-manager_completions ()
     Options=( "-f" "--file" "-b" "-A" "-I" "-h" "--help" "-n" "--dry-run" "-v" \
               "--verbose" "--cores" "--debug" "--impure" "--keep-failed" \
               "--keep-going" "-j" "--max-jobs" "--no-substitute" "--no-out-link" \
+              "-L" "--print-build-logs" \
               "--show-trace" "--substitute" "--builders" "--version" \
               "--update-input" "--override-input" "--experimental-features" \
               "--extra-experimental-features" )
