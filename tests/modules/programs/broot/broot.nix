fd9091efcae83b75e4121294799b5f1291d1e956
Robert Helgesson <robert@rycee.net>
2022-12-28 20:49:19 +0100

    broot: simplify test slightly
    (cherry picked from commit d7a3c268542eadfc480c0dd362cd7d4b6eb2a536)

--- tests/modules/programs/broot/broot.nix
+++ tests/modules/programs/broot/broot.nix
@@ -1,30 +1,26 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
+{ ... }:
 
 {
-  config = {
-    programs.broot = {
-      enable = true;
-      settings.modal = true;
-    };
-
-    nmt.script = ''
-      assertFileExists home-files/.config/broot/conf.toml
-      assertFileContent home-files/.config/broot/conf.toml ${
-        pkgs.writeText "broot.expected" ''
-          content_search_max_file_size = "10MB"
-          imports = ["verbs.hjson", {file = "dark-blue-skin.hjson", luma = ["dark", "unknown"]}, {file = "white-skin.hjson", luma = "light"}]
-          modal = true
-          show_selection_mark = true
-          verbs = []
-
-          [skin]
-
-          [special_paths]
-          "/media" = "no-enter"
-        ''
-      }
-    '';
+  programs.broot = {
+    enable = true;
+    settings.modal = true;
   };
+
+  nmt.script = ''
+    assertFileExists home-files/.config/broot/conf.toml
+    assertFileContent home-files/.config/broot/conf.toml ${
+      builtins.toFile "broot.expected" ''
+        content_search_max_file_size = "10MB"
+        imports = ["verbs.hjson", {file = "dark-blue-skin.hjson", luma = ["dark", "unknown"]}, {file = "white-skin.hjson", luma = "light"}]
+        modal = true
+        show_selection_mark = true
+        verbs = []
+
+        [skin]
+
+        [special_paths]
+        "/media" = "no-enter"
+      ''
+    }
+  '';
 }
