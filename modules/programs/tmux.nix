419b4b38e179a4710de4493677abc49dcab88ca4
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-01-14 22:12:14 +0300

    tmux: fix secureSocket environment variable

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -333,7 +333,7 @@ in {
 
     (mkIf cfg.secureSocket {
       home.sessionVariables = {
-        TMUX_TMPDIR = ''''${XDG_RUNTIME_DIR:-"/run/user/\$(id -u)"}'';
+        TMUX_TMPDIR = ''''${XDG_RUNTIME_DIR:-"/run/user/$(id -u)"}'';
       };
     })
 
--- tests/modules/programs/tmux/secure-socket-enabled.nix
+++ tests/modules/programs/tmux/secure-socket-enabled.nix
@@ -12,7 +12,7 @@ with lib;
     nmt.script = ''
       assertFileExists home-path/etc/profile.d/hm-session-vars.sh
       assertFileContains home-path/etc/profile.d/hm-session-vars.sh \
-        'export TMUX_TMPDIR="''${XDG_RUNTIME_DIR:-"/run/user/\$(id -u)"}"'
+        'export TMUX_TMPDIR="''${XDG_RUNTIME_DIR:-"/run/user/$(id -u)"}"'
     '';
   };
 }
