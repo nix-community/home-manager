04de4129e1b111db0676cf574fbb9907895bca35
Zack <43693810+zaporter@users.noreply.github.com>
2023-02-01 05:25:13 -0500

    home-manager: edit `homeManagerConfiguration` err (#3632)
    * home-manager: edit `homeManagerConfiguration` err
    
    This adds https://nix-community.github.io/home-manager/release-notes.html#sec-release-22.11-highlights
    to the homeManagerConfiguration arguments error.
    
    * home-manager: `homeManagerConfiguration` indent
    
    Moved URL of the release notes for 22.11 to the same line so that
    ./format doesn't add extra whitespace
    
    ---------
    
    Co-authored-by: zaporter <opensource@zackporter.com>

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
                 - 'system'
 
               have been removed. Instead use the arguments 'pkgs' and
-              'modules'. See the 22.11 release notes for more.
+              'modules'. See the 22.11 release notes for more: https://nix-community.github.io/home-manager/release-notes.html#sec-release-22.11-highlights 
             '';
 
             throwForRemovedArgs = v:
