612779dad393551f4dd6f7a08ec75be4e51f79af
Robert Helgesson <robert@rycee.net>
2023-02-07 22:19:10 +0100

    scmpuff: clean up tests
    Stub the scmpuff package. Also remove unnecessary `config` wrapping.

--- tests/modules/programs/scmpuff/bash.nix
+++ tests/modules/programs/scmpuff/bash.nix
@@ -1,15 +1,17 @@
-{ pkgs, ... }: {
-  config = {
-    programs = {
-      scmpuff.enable = true;
-      bash.enable = true;
-    };
+{ ... }:
 
-    nmt.script = ''
-      assertFileExists home-files/.bashrc
-      assertFileContains \
-        home-files/.bashrc \
-        'eval "$(${pkgs.scmpuff}/bin/scmpuff init -s)"'
-    '';
+{
+  programs = {
+    scmpuff.enable = true;
+    bash.enable = true;
   };
+
+  test.stubs.scmpuff = { };
+
+  nmt.script = ''
+    assertFileExists home-files/.bashrc
+    assertFileContains \
+      home-files/.bashrc \
+      'eval "$(@scmpuff@/bin/scmpuff init -s)"'
+  '';
 }
