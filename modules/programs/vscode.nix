f8ac7b627fac4dd28ce7b8e322913b2eb46f5b76
Nathan van Doorn <nvd1234@gmail.com>
2023-02-08 10:38:13 +0100

    Use lib.versionAtLeast
    Co-authored-by: Naïm Favier <n@monade.li>

--- modules/programs/vscode.nix
+++ modules/programs/vscode.nix
@@ -220,7 +220,7 @@ in {
           else
             builtins.attrNames (builtins.readDir (ext + "/${subDir}")));
       in if cfg.mutableExtensionsDir then
-        mkMerge (concatMap toPaths cfg.extensions ++ lib.optional (lib.version >= "1.74.0") [{
+        mkMerge (concatMap toPaths cfg.extensions ++ lib.optional (lib.versionAtLeast vscodeVersion "1.74.0") [{
           # Whenever our immutable extensions.json changes, force VSCode to regenerate
           # extensions.json with both mutable and immutable extensions.
           "${extensionPath}/.extensions-immutable.json" = {
@@ -236,7 +236,7 @@ in {
         "${extensionPath}".source = let
           combinedExtensionsDrv = pkgs.buildEnv {
             name = "vscode-extensions";
-            paths = cfg.extensions ++ lib.optional (vscodeVersion >= "1.74.0") [ extensionJsonFile ];
+            paths = cfg.extensions ++ lib.optional (lib.versionAtLeast vscodeVersion "1.74.0") [ extensionJsonFile ];
           };
         in "${combinedExtensionsDrv}/${subDir}";
       }))
