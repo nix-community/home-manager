239335da388be790d3bdcef13dd42f362e9aa67a
polykernel <81340136+polykernel@users.noreply.github.com>
2023-02-09 08:59:23 -0500

    home.pointerCursor: set common XCURSOR_* environment variables by default
    The `XCURSOR_*` environment variables specified in libxcursor
    are used by many applications and libraries to load and configure
    cursor settings. Setting these variables is a no-op if ignored but
    is useful as a fallback when other sources of configuration are
    unreliable.
    
    This commit sets some commonly used `XCURSOR_*` environment variables
    (i.e XCURSOR_THEME, XCURSOR_SIZE) by default when `home.pointerCursor`
    is enabled.

--- modules/config/home-cursor.nix
+++ modules/config/home-cursor.nix
@@ -133,6 +133,8 @@ in {
       home.sessionVariables = {
         XCURSOR_PATH = mkDefault ("$XCURSOR_PATH\${XCURSOR_PATH:+:}"
           + "${config.home.profileDirectory}/share/icons");
+        XCURSOR_SIZE = mkDefault cfg.size;
+        XCURSOR_THEME = mkDefault cfg.name;
       };
     }
 
