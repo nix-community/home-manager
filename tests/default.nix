4f0d4df5544e99d77b813b94a9dc6ec29ce40833
Sumner Evans <me@sumnerevans.com>
2023-02-09 02:37:26 -0700

    tests: fix formatting (tab -> spaces) (#3660)
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- tests/default.nix
+++ tests/default.nix
@@ -122,7 +122,7 @@ import nmt {
     ./modules/programs/tmate
     ./modules/programs/tmux
     ./modules/programs/topgrade
-	./modules/programs/vim-vint
+    ./modules/programs/vim-vint
     ./modules/programs/vscode
     ./modules/programs/watson
     ./modules/programs/wezterm
