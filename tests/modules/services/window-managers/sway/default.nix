5fcf91b1169f123d7baa5757fe5442ac84b60242
Sefa Eyeoglu <contact@scrumplex.net>
2023-03-06 16:37:51 +0100

    sway: add support for XDG autostart using systemd
    Using the option wayland.windowManager.sway.systemd.xdgAutostart, users
    can now choose to start applications present in
    $XDG_CONFIG_HOME/autostart when starting their sway session.
    
    This change also renames wayland.windowManager.sway.systemdIntegration
    to wayland.windowManager.sway.systemd.enable;
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- tests/modules/services/window-managers/sway/default.nix
+++ tests/modules/services/window-managers/sway/default.nix
@@ -10,6 +10,7 @@
   sway-null-config = ./sway-null-config.nix;
   sway-null-package = ./sway-null-package.nix;
   sway-post-2003 = ./sway-post-2003.nix;
+  sway-systemd-autostart = ./sway-systemd-autostart.nix;
   sway-workspace-default = ./sway-workspace-default.nix;
   sway-workspace-output = ./sway-workspace-output.nix;
   swaynag-example-settings = ./swaynag-example-settings.nix;
