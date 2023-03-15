4f349932b7aee93e9b97a35955d3917f94196640
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-03-15 23:23:08 +0000

    news: document the i3status-rust changes

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -931,6 +931,23 @@ in
           A new module is available: 'services.avizo'.
         '';
       }
+
+      {
+        time = "2023-03-16:12:00+00:00";
+        condition = config.programs.i3status-rust.enable;
+        message = ''
+          Module 'i3status-rust' was updated to support the new configuration
+          format from 0.30.x releases, that introduces many breaking changes.
+          The documentation was updated with examples from 0.30.x to help
+          the transition.
+
+          See https://github.com/greshake/i3status-rust/blob/v0.30.0/NEWS.md
+          for instructions on how to migrate.
+
+          Users that don't want to migrate yet can set
+          'programs.i3status-rust.package' to an older version.
+        '';
+      }
     ];
   };
 }
