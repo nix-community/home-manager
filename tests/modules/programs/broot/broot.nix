7da74ff66dea3daa090bb3776be3548a7de51fd2
Robert Helgesson <robert@rycee.net>
2023-04-03 21:20:42 +0200

    ci: use the right channel
    Also revert bad backport of broot test.

--- tests/modules/programs/broot/broot.nix
+++ tests/modules/programs/broot/broot.nix
@@ -10,16 +10,12 @@
     assertFileExists home-files/.config/broot/conf.toml
     assertFileContent home-files/.config/broot/conf.toml ${
       builtins.toFile "broot.expected" ''
-        content_search_max_file_size = "10MB"
         imports = ["verbs.hjson", {file = "dark-blue-skin.hjson", luma = ["dark", "unknown"]}, {file = "white-skin.hjson", luma = "light"}]
         modal = true
         show_selection_mark = true
         verbs = []
 
         [skin]
-
-        [special_paths]
-        "/media" = "no-enter"
       ''
     }
   '';
