474f738674d57e87c6118390306fb9bd93c5ce50
Neon-44 <78902360+Neon-44@users.noreply.github.com>
2023-04-07 15:45:44 +0000

    ssh: made the ssh config file path modular
    made a new option programs.ssh.userConfigFile with default ".ssh/config" and
    replaced home.file.".ssh/config".text with home.file.${cfg.userConfigFile}.text

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -392,6 +392,21 @@ in
       '';
     };
 
+    userConfigFile = mkOption {
+      type = types.str;
+      default = ".ssh/config";
+      description = ''
+      tells Home-Manager where in your home you want your ssh config to be.
+      Example: <filename>.ssh/config<filename> will put it into 
+      <filename>~/.ssh/config<filename>
+      while <filename>.config/ssh/config<filename> will put it into 
+      <filename>~/.config/ssh/config<filename>
+      file is put into <filename>~/.ssh/config</filename> by default.
+      can be paired with a alias 'ssh = "ssh -F"' to declutter your 
+      home folder.
+      '';
+    };
+
     userKnownHostsFile = mkOption {
       type = types.str;
       default = "~/.ssh/known_hosts";
@@ -510,7 +525,7 @@ in
       }
     ];
 
-    home.file.".ssh/config".text =
+    home.file.${cfg.userConfigFile}.text =
       let
         sortedMatchBlocks = hm.dag.topoSort cfg.matchBlocks;
         sortedMatchBlocksStr = builtins.toJSON sortedMatchBlocks;
