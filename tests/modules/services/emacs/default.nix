f714b170315770f5cb34107b17f2925f0aed7dd4
Jian Lin <75130626+jian-lin@users.noreply.github.com>
2023-05-11 19:58:37 +0800

    emacs: extend startWithUserSession to start after graphical session (#3010)

--- tests/modules/services/emacs/default.nix
+++ tests/modules/services/emacs/default.nix
@@ -1,6 +1,8 @@
 {
   emacs-service-27 = ./emacs-service-27.nix;
   emacs-service-28 = ./emacs-service-28.nix;
+  emacs-service-28-after-graphical-session-target =
+    ./emacs-service-28-after-graphical-session-target.nix;
   emacs-socket-27 = ./emacs-socket-27.nix;
   emacs-socket-28 = ./emacs-socket-28.nix;
   emacs-default-editor = ./emacs-default-editor.nix;
