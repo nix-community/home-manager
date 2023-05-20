e7fdcb40b2ddb79ed0b166e9694b361581c3dddb
rasmus-kirk <mail@rasmuskirk.com>
2023-05-20 12:26:09 +0200

    joshuto: add the joshuto file manager
    PR #4004

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1062,6 +1062,13 @@ in
           A new module is available: 'programs.imv'.
         '';
       }
+
+      {
+        time = "2023-06-09T19:18:42+00:00";
+        message = ''
+          A new module is available: 'programs.joshuto'.
+        '';
+      }
     ];
   };
 }
