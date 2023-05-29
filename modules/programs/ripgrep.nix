edacbe80b65e018c4b9852aa9c88c5230c5c41b8
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2023-05-29 02:27:24 +0200

    ripgrep: Apply suggestions from code review
    Co-authored-by: Naïm Favier <n@monade.li>

--- modules/programs/ripgrep.nix
+++ modules/programs/ripgrep.nix
@@ -10,30 +10,23 @@ in {
     programs.ripgrep = {
       enable = mkEnableOption "Ripgrep";
 
-      package = mkOption {
-        type = types.package;
-        default = pkgs.ripgrep;
-        defaultText = literalExpression "pkgs.ripgrep";
-        description =
-          "Package containing the <command>ripgrep</command> program.";
-      };
+      package = mkPackageOption pkgs "ripgrep" { };
 
       configDir = mkOption {
         type = types.str;
         default = "${config.xdg.configHome}/ripgrep";
-        defaultText = literalExpression "\${config.xdg.configHome}/ripgrep";
+        defaultText = literalExpression "\"\${config.xdg.configHome}/ripgrep\"";
         description = ''
-          Directory where the <filename>ripgreprc</filename> file will be stored (Inside $HOME).
+          Directory where the <filename>ripgreprc</filename> file will be stored.
         '';
       };
 
       config = mkOption {
         type = with types; listOf str;
         default = [ ];
-        defaultText = literalExpression "[]";
         example = [ "--max-columns-preview" "--colors=line:style:bold" ];
         description = ''
-          List of arguments to pass to ripgrep. Each line is given to ripgrep as a single command line argument verbatim.
+          List of arguments to pass to ripgrep. Each item is given to ripgrep as a single command line argument verbatim.
 
           See <link xlink:href="https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file"/>
           for an example configuration.
@@ -47,7 +40,7 @@ in {
       packages = [ cfg.package ];
 
       file."${cfg.configDir}/ripgreprc" = mkIf (cfg.config != [ ]) {
-        text = (builtins.concatStringsSep "\n" cfg.config) + "\n";
+        text = lib.concatLines cfg.config;
       };
 
       sessionVariables = {
