3e830960b8d6a54d6403abb000e7b0f0e444aca6
midchildan <git@midchildan.org>
2023-05-30 01:27:25 +0900

    nix: add more tests

--- tests/modules/misc/nix/default.nix
+++ tests/modules/misc/nix/default.nix
@@ -2,4 +2,5 @@
   nix-empty-settings = ./empty-settings.nix;
   nix-example-settings = ./example-settings.nix;
   nix-example-registry = ./example-registry.nix;
+  nix-keep-old-nix-path = ./keep-old-nix-path.nix;
 }
