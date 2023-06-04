79e03fbe24dde6fe10662723870400d87a9cb40d
Robert Helgesson <robert@rycee.net>
2023-06-04 09:49:30 +0200

    lib: remove listOrDagOf type
    It has been deprecated for a long time and should be safe to remove.

--- tests/lib/types/default.nix
+++ tests/lib/types/default.nix
@@ -1,7 +1,6 @@
 {
   lib-types-dag-submodule = ./dag-submodule.nix;
   lib-types-dag-merge = ./dag-merge.nix;
-  lib-types-list-or-dag-merge = ./list-or-dag-merge.nix;
 
   lib-types-gvariant-merge = ./gvariant-merge.nix;
 }
