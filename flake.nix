3ac8421e1fe38894e6f1964d5c9c6625458176b5
Robert Helgesson <robert@rycee.net>
2023-06-25 00:24:57 +0200

    flake: switch Nixpkgs input to 23.05 branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home Manager for Nix";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs, ... }:
     {
