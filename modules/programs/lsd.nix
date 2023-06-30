17ce23ea56aeecbd01dfeb08f6a7902c80f27311
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-06-30 21:47:03 +0200

    lsd: use -A instead of -a in aliases (#4173)
    The current `lla` alias, together with the `total-size` option try to get
    the size of the `..` directory, and for this has to recursively open all
    sibling folders. This may be super slow if some of those siblings
    contain too many files, and raise a ton of useless errors if some of
    those siblings contains non-readable files.
    
    I'm suggesting to use `-A` instead, which will skip the obvious `.` and
    `..` folders.
    
    While here, I think we could also add `llt`.

--- modules/programs/lsd.nix
+++ modules/programs/lsd.nix
@@ -11,9 +11,10 @@ let
   aliases = {
     ls = "${pkgs.lsd}/bin/lsd";
     ll = "${pkgs.lsd}/bin/lsd -l";
-    la = "${pkgs.lsd}/bin/lsd -a";
+    la = "${pkgs.lsd}/bin/lsd -A";
     lt = "${pkgs.lsd}/bin/lsd --tree";
-    lla = "${pkgs.lsd}/bin/lsd -la";
+    lla = "${pkgs.lsd}/bin/lsd -lA";
+    llt = "${pkgs.lsd}/bin/lsd -l --tree";
   };
 
 in {
