9af726faca32b0f47c8481ec307acf9aa1818936
Olmo Kramer <olmo@codegrade.com>
2023-07-03 22:06:38 +0200

    khard: add module

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1251,6 +1251,13 @@ in
           A new module is available: 'programs.wpaperd'.
         '';
       }
+
+      {
+        time = "2023-10-01T07:28:45+00:00";
+        message = ''
+          A new module is available: 'programs.khard'.
+        '';
+      }
     ];
   };
 }
