af715ed857f9d94086e4ef833949944bf0322521
Robert Helgesson <robert@rycee.net>
2023-07-08 10:12:35 +0200

    tests: some minor cleanups

--- tests/modules/programs/qutebrowser/quickmarks.nix
+++ tests/modules/programs/qutebrowser/quickmarks.nix
@@ -1,33 +1,29 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
+{ pkgs, ... }:
 
 {
-  config = {
-    programs.qutebrowser = {
-      enable = true;
+  programs.qutebrowser = {
+    enable = true;
 
-      quickmarks = {
-        nixpkgs = "https://github.com/NixOS/nixpkgs";
-        home-manager = "https://github.com/nix-community/home-manager";
-      };
+    quickmarks = {
+      nixpkgs = "https://github.com/NixOS/nixpkgs";
+      home-manager = "https://github.com/nix-community/home-manager";
     };
-
-    test.stubs.qutebrowser = { };
-
-    nmt.script = let
-      quickmarksFile = if pkgs.stdenv.hostPlatform.isDarwin then
-        ".qutebrowser/quickmarks"
-      else
-        ".config/qutebrowser/quickmarks";
-    in ''
-      assertFileContent \
-        home-files/${quickmarksFile} \
-        ${
-          pkgs.writeText "qutebrowser-expected-quickmarks" ''
-            home-manager https://github.com/nix-community/home-manager
-            nixpkgs https://github.com/NixOS/nixpkgs''
-        }
-    '';
   };
+
+  test.stubs.qutebrowser = { };
+
+  nmt.script = let
+    quickmarksFile = if pkgs.stdenv.hostPlatform.isDarwin then
+      ".qutebrowser/quickmarks"
+    else
+      ".config/qutebrowser/quickmarks";
+  in ''
+    assertFileContent \
+      home-files/${quickmarksFile} \
+      ${
+        pkgs.writeText "qutebrowser-expected-quickmarks" ''
+          home-manager https://github.com/nix-community/home-manager
+          nixpkgs https://github.com/NixOS/nixpkgs''
+      }
+  '';
 }
--- tests/modules/programs/terminator/config-file.nix
+++ tests/modules/programs/terminator/config-file.nix
@@ -1,24 +1,24 @@
-{ config, lib, pkgs, ... }: {
-  config = {
-    programs.terminator = {
-      enable = true;
-      config = {
-        global_config.borderless = true;
-        profiles.default.background_color = "#002b36";
-      };
+{ pkgs, ... }:
+
+{
+  programs.terminator = {
+    enable = true;
+    config = {
+      global_config.borderless = true;
+      profiles.default.background_color = "#002b36";
     };
-
-    test.stubs.terminator = { };
-
-    nmt.script = ''
-      assertFileContent home-files/.config/terminator/config ${
-        pkgs.writeText "expected" ''
-          [global_config]
-          borderless = True
-          [profiles]
-          [[default]]
-          background_color = "#002b36"''
-      }
-    '';
   };
+
+  test.stubs.terminator = { };
+
+  nmt.script = ''
+    assertFileContent home-files/.config/terminator/config ${
+      pkgs.writeText "expected" ''
+        [global_config]
+        borderless = True
+        [profiles]
+        [[default]]
+        background_color = "#002b36"''
+    }
+  '';
 }
