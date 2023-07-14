d2e47de53650d48fd95eb0ec92049d1cb7f09520
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-07-14 19:25:46 +0000

    home-cursor.nix: enable gtk module when enabling gtk config generation (#4144)
    * home-cursor.nix: enable gtk module when enabling gtk config generation
    
    The gtk configurations are not generated unless config.gtk is enabled.
    This is a point of confusion because config.home.pointerCursor.gtk can essentially be disabled,
    despite having it enabled.
    
    * home-cursor.nix: Add note to gtk config generation description instead of enabling gtk module
    
    * home-cursor.nix: Add note about applying pointerCursor configs to main submodule desc
    
    * home-cursor.nix: Change tabs to spaces

--- modules/config/home-cursor.nix
+++ modules/config/home-cursor.nix
@@ -104,6 +104,12 @@ in {
         backends, you can toggle them via the enable option. For example,
         <xref linkend="opt-home.pointerCursor.x11.enable"/>
         will enable x11 cursor configurations.
+        </para><para>
+        Note that this will merely generate the cursor configurations.
+        To apply the configurations, the relevant subsytems must also be configured.
+        For example, <xref linkend="opt-home.pointerCursor.gtk.enable"/> will generate
+        the gtk cursor configuration, but <xref linkend="opt-gtk.enable"/> needs
+        to be set for it to be applied.
       '';
     };
   };
