627c2e9c772d20ebec58ae6d968287d1711e093c
musjj <72612857+musjj@users.noreply.github.com>
2023-08-15 05:31:22 +0700

    home-manager: allow the tool to be installed in submodule setups

--- modules/programs/home-manager.nix
+++ modules/programs/home-manager.nix
@@ -27,7 +27,7 @@ in {
     };
   };
 
-  config = mkIf (cfg.enable && !config.submoduleSupport.enable) {
+  config = mkIf cfg.enable {
     home.packages =
       [ (pkgs.callPackage ../../home-manager { inherit (cfg) path; }) ];
   };
