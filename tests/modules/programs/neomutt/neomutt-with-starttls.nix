9e70a3df87548aabbeac02a91b89e8df1542ccc5
Franz Pletz <fpletz@fnordicwalking.de>
2023-08-29 23:56:29 +0200

    neomutt: fix STARTTLS
    When smtps is used as a protocol, neomutt expects TLS but will if
    STARTTLS should be used. When using STARTTLS, smtp has to be used as
    protocol and `ssl_force_tls` is set.
    
    See <https://neomutt.org/guide/optionalfeatures#2-1-%C2%A0starttls>.

---
+++ tests/modules/programs/neomutt/neomutt-with-starttls.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  imports = [ ../../accounts/email-test-accounts.nix ];
+
+  config = {
+    accounts.email.accounts = {
+      "hm@example.com" = {
+        notmuch.enable = true;
+        neomutt = {
+          enable = true;
+          extraConfig = ''
+            color status cyan default
+          '';
+        };
+        imap.port = 143;
+        smtp.tls.useStartTls = true;
+      };
+    };
+
+    programs.neomutt = {
+      enable = true;
+      vimKeys = false;
+    };
+
+    test.stubs.neomutt = { };
+
+    nmt.script = ''
+      assertFileExists home-files/.config/neomutt/neomuttrc
+      assertFileExists home-files/.config/neomutt/hm@example.com
+      assertFileContent home-files/.config/neomutt/neomuttrc ${
+        ./neomutt-expected.conf
+      }
+      assertFileContent home-files/.config/neomutt/hm@example.com ${
+        ./hm-example.com-starttls-expected
+      }
+    '';
+  };
+}
