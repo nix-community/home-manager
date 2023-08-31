9c390452eee427b58cf774bd8ff1ac656d317c92
nicoo <nicoo@mur.at>
2023-08-31 11:28:44 +0000

    tmpfiles: use only `xdg.configFile`
    This is a small refactor, and minimises the impact of home-manager#4398.

--- modules/misc/tmpfiles.nix
+++ modules/misc/tmpfiles.nix
@@ -27,8 +27,8 @@ in {
         platforms.linux)
     ];
 
-    xdg = {
-      dataFile."user-tmpfiles.d/home-manager.conf" = {
+    xdg.configFile = {
+      "user-tmpfiles.d/home-manager.conf" = {
         text = ''
           # This file is created automatically and should not be modified.
           # Please change the option ‘systemd.user.tmpfiles.rules’ instead.
@@ -36,16 +36,14 @@ in {
         '';
         onChange = "${pkgs.systemd}/bin/systemd-tmpfiles --user --create";
       };
-      configFile = {
-        "systemd/user/basic.target.wants/systemd-tmpfiles-setup.service".source =
-          "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-setup.service";
-        "systemd/user/systemd-tmpfiles-setup.service".source =
-          "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-setup.service";
-        "systemd/user/timers.target.wants/systemd-tmpfiles-clean.timer".source =
-          "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-clean.timer";
-        "systemd/user/systemd-tmpfiles-clean.service".source =
-          "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-clean.service";
-      };
+      "systemd/user/basic.target.wants/systemd-tmpfiles-setup.service".source =
+        "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-setup.service";
+      "systemd/user/systemd-tmpfiles-setup.service".source =
+        "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-setup.service";
+      "systemd/user/timers.target.wants/systemd-tmpfiles-clean.timer".source =
+        "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-clean.timer";
+      "systemd/user/systemd-tmpfiles-clean.service".source =
+        "${pkgs.systemd}/example/systemd/user/systemd-tmpfiles-clean.service";
     };
   };
 }
