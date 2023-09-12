ee67f243be2848eb8f74c7a06972ddd08ae36ddc
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-12 18:11:39 +0100

    news: add services.home-manager.autoExpire entry

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2201,6 +2201,16 @@ in {
           services into a video player.
         '';
       }
+
+      {
+        time = "2025-04-02T00:00:00+00:00";
+        message = ''
+          A new service is available: 'services.home-manager.autoExpire'.
+
+          A service that allow to automatically expire (and optionally clean-up
+          Nix's store) old Home-Manager generations.
+        '';
+      }
     ];
   };
 }
