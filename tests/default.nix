c8589e7fb14386fb52115003de41fe01453a77ed
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-12 17:25:31 +0100

    home-manager-auto-expire: init
    This commit introduces a new service that expires old Home-Manager
    generations.
    
    (cherry picked from commit 28242a60d3cdcb6675dfe68ebbb64bf43c7be4e0)

--- tests/default.nix
+++ tests/default.nix
@@ -252,6 +252,7 @@ in import nmtSrc {
     ./modules/services/git-sync
     ./modules/services/glance
     ./modules/services/gromit-mpx
+    ./modules/services/home-manager-auto-expire
     ./modules/services/home-manager-auto-upgrade
     ./modules/services/hypridle
     ./modules/services/hyprpaper
