95fee92056fa222afc84677dcd4d351207a2759d
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-12 18:11:39 +0100

    news: add services.home-manager.autoExpire entry
    (cherry picked from commit 5ee44bc7c2e853f144390a12ebe5174ad7e3b9e0)

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1878,6 +1878,16 @@ in {
           speed, features, or native UIs. Ghostty provides all three.
         '';
       }
+
+      {
+        time = "2025-04-02T00:00:00+00:00";
+        message = ''
+          A new service is available: 'services.home-manager.autoExpire'.
+
+          A service that allow to automatically expire (and optionally clean-up
+          Nix's store) old Home-Manager generations.
+        '';
+      }
     ];
   };
 }
