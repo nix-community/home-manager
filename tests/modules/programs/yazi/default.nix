f5e514774459c00328a09b6c46f8911157504e1b
XYenon <i@xyenon.bid>
2023-09-22 19:00:16 +0800

    yazi: add fish and nushell integration

--- tests/modules/programs/yazi/default.nix
+++ tests/modules/programs/yazi/default.nix
@@ -2,4 +2,6 @@
   yazi-settings = ./settings.nix;
   yazi-bash-integration-enabled = ./bash-integration-enabled.nix;
   yazi-zsh-integration-enabled = ./zsh-integration-enabled.nix;
+  yazi-fish-integration-enabled = ./fish-integration-enabled.nix;
+  yazi-nushell-integration-enabled = ./nushell-integration-enabled.nix;
 }
