2e44116afcd1a80bd68d9a8e0b554271a9ad37c8
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-18 12:54:36 +0100

    qt: export QT_PLUGIN_PATH/QML2_IMPORT_PATH
    Those 2 variables are necessary to make e.g.: qt6ct work correctly.
    
    See issue: https://github.com/NixOS/nixpkgs/issues/239909.

--- tests/modules/misc/qt/qt-platform-theme-gtk.nix
+++ tests/modules/misc/qt/qt-platform-theme-gtk.nix
@@ -10,6 +10,10 @@
     nmt.script = ''
       assertFileRegex home-path/etc/profile.d/hm-session-vars.sh \
         'QT_QPA_PLATFORMTHEME="gtk2"'
+      assertFileRegex home-path/etc/profile.d/hm-session-vars.sh \
+        'QT_PLUGIN_PATH'
+      assertFileRegex home-path/etc/profile.d/hm-session-vars.sh \
+        'QML2_IMPORT_PATH'
     '';
   };
 }
