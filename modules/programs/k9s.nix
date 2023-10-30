23a3d53e88ee355a7b89ed5461b24d4cfce3b740
liyangau <li.yang@konghq.com>
2023-10-30 15:15:37 +1100

    k9s: add hotkey option
    This PR adds an option to customise k9s hotkeys.
    The keyword `hotKey` must be in camel case on the user config.

--- modules/programs/k9s.nix
+++ modules/programs/k9s.nix
@@ -8,7 +8,7 @@ let
   yamlFormat = pkgs.formats.yaml { };
 
 in {
-  meta.maintainers = [ hm.maintainers.katexochen ];
+  meta.maintainers = [ hm.maintainers.katexochen maintainers.liyangau ];
 
   options.programs.k9s = {
     enable =
@@ -49,6 +49,29 @@ in {
         };
       '';
     };
+
+    hotkey = mkOption {
+      type = yamlFormat.type;
+      default = { };
+      description = ''
+        hotkeys written to
+        {file}`$XDG_CONFIG_HOME/k9s/hotkey.yml`. See
+        <https://k9scli.io/topics/hotkeys/>
+        for supported values.
+      '';
+      example = literalExpression ''
+        hotkey = {
+          # Make sure this is camel case
+          hotKey = {
+            shift-0 = {
+              shortCut = "Shift-0";
+              description = "Viewing pods";
+              command = "pods";
+            };
+          };
+        };
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
@@ -60,6 +83,10 @@ in {
 
     xdg.configFile."k9s/skin.yml" = mkIf (cfg.skin != { }) {
       source = yamlFormat.generate "k9s-skin" cfg.skin;
+    };
+
+    xdg.configFile."k9s/hotkey.yml" = mkIf (cfg.hotkey != { }) {
+      source = yamlFormat.generate "k9s-hotkey" cfg.hotkey;
     };
   };
 }
