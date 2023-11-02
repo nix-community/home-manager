eef22e2ab99bfde5122cac1e629a167af123522a
musjj <72612857+musjj@users.noreply.github.com>
2023-11-02 15:54:57 +0700

    kitty: properly export `KITTY_SHELL_INTEGRATION`

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -40,6 +40,7 @@ let
     '';
     zsh = ''
       if test -n "$KITTY_INSTALLATION_DIR"; then
+        export KITTY_SHELL_INTEGRATION="enabled"
         autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
         kitty-integration
         unfunction kitty-integration
