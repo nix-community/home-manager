b9b9b5aab0cc6d23f5c31521e23158736995e7ad
Sebastian Zivota <loewenheim@users.noreply.github.com>
2023-11-03 16:33:15 +0100

    thefuck: add fish integration (#4535)
    Co-authored-by: Sumner Evans <me@sumnerevans.com>

--- modules/programs/thefuck.nix
+++ modules/programs/thefuck.nix
@@ -21,6 +21,10 @@ with lib;
       '';
     };
 
+    enableFishIntegration = mkEnableOption "Fish integration" // {
+      default = true;
+    };
+
     enableZshIntegration = mkOption {
       default = true;
       type = types.bool;
@@ -45,6 +49,21 @@ with lib;
     home.packages = [ cfg.package ];
 
     programs.bash.initExtra = mkIf cfg.enableBashIntegration shEvalCmd;
+
+    programs.fish.functions = mkIf cfg.enableFishIntegration {
+      fuck = {
+        description = "Correct your previous console command";
+        body = ''
+          set -l fucked_up_command $history[1]
+          env TF_SHELL=fish TF_ALIAS=fuck PYTHONIOENCODING=utf-8 ${cfg.package}/bin/thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv | read -l unfucked_command
+          if [ "$unfucked_command" != "" ]
+            eval $unfucked_command
+            builtin history delete --exact --case-sensitive -- $fucked_up_command
+            builtin history merge
+          end
+        '';
+      };
+    };
 
     programs.zsh.initExtra = mkIf cfg.enableZshIntegration shEvalCmd;
   };
