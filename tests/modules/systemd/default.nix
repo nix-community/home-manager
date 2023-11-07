6a8444467c83c961e2f5ff64fb4f422e303c98d3
Olli Helenius <liff@iki.fi>
2023-11-07 16:55:17 +0200

    systemd: add settings option (#4276)
    The `systemd.user.extraConfig` provides a way to generate a
    `systemd-user.conf(5)` file for the user.
    
    This is the home-manager equivalent of NixOS’s option of the same
    name, with the difference that NixOS’s option generates a `user.conf`
    file that is shared between all users.

--- tests/modules/systemd/default.nix
+++ tests/modules/systemd/default.nix
@@ -2,6 +2,7 @@
   systemd-services = ./services.nix;
   systemd-services-disabled-for-root = ./services-disabled-for-root.nix;
   systemd-session-variables = ./session-variables.nix;
+  systemd-user-config = ./user-config.nix;
   systemd-slices = ./slices.nix;
   systemd-timers = ./timers.nix;
 }
