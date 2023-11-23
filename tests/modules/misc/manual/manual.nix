f48a0062dfe9bbf42050b557a0a5de0f4706ac75
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-11-23 23:18:27 +0900

    docs: update paths to make manual tests pass

--- tests/modules/misc/manual/manual.nix
+++ tests/modules/misc/manual/manual.nix
@@ -12,8 +12,8 @@
       assertFileExists home-path/share/doc/home-manager/index.html
       assertFileExists home-path/share/doc/home-manager/options.html
       assertFileExists home-path/share/doc/home-manager/options.json
-      assertFileExists home-path/share/man/man1/home-manager.1.gz
-      assertFileExists home-path/share/man/man5/home-configuration.nix.5.gz
+      assertFileExists home-path/share/man/man1/home-manager.1
+      assertFileExists home-path/share/man/man5/home-configuration.nix.5
     '';
   };
 }
