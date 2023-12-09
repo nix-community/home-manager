defbb9c5857e157703e8fc7cf3c2ceb01cb95883
Nara Díaz Viñolas <rdvdev2@gmail.com>
2023-12-09 13:26:07 +0100

    hyprland: add option sourceFirst
    Add the option sourceFirst to the hyprland module. When this option is
    enabled source entries will be put near the top of the file, so that
    the variables declared in other files can be used by the other
    configuration entries.
    
    Add "source" to the list of important prefixes when the former option
    is enabled.
    
    Resolves #4729

--- tests/modules/services/window-managers/hyprland/simple-config.nix
+++ tests/modules/services/window-managers/hyprland/simple-config.nix
@@ -8,6 +8,8 @@
     plugins =
       [ "/path/to/plugin1" (config.lib.test.mkStubPackage { name = "foo"; }) ];
     settings = {
+      source = [ "sourced.conf" ];
+
       decoration = {
         shadow_offset = "0 5";
         "col.shadow" = "rgba(00000099)";
