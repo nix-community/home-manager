6fc71dc563c78f999386a55b1892c05039199e8f
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-12-16 14:49:25 +0900

    docs: add release-notes as appendix
    The release notes used to be an appendix in the manual. After
    converting to markdown that appendix got lost. This commit
    reintroduces the release notes into the manual.

--- tests/modules/misc/manual/manual.nix
+++ tests/modules/misc/manual/manual.nix
@@ -12,6 +12,7 @@
       assertFileExists home-path/share/doc/home-manager/index.xhtml
       assertFileExists home-path/share/doc/home-manager/options.html
       assertFileExists home-path/share/doc/home-manager/options.json
+      assertFileExists home-path/share/doc/home-manager/release-notes.xhtml
       assertFileExists home-path/share/man/man1/home-manager.1
       assertFileExists home-path/share/man/man5/home-configuration.nix.5
     '';
