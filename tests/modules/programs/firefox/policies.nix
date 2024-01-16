nix-community/home-manager

commit 3feeb7715584fd45ed1389cec8fb15f6930e8dab

firefox: add support for specifying policies (#4626)

modules/programs/firefox.nix
@@ -169,7 +169,10 @@ let
     else if isDarwin then
       package
     else if versionAtLeast config.home.stateVersion "19.09" then
-      package.override (old: { cfg = old.cfg or { } // fcfg; })
+      package.override (old: {
+        cfg = old.cfg or { } // fcfg;
+        extraPolicies = cfg.policies;
+      })
     else
       (pkgs.wrapFirefox.override { config = bcfg; }) package { };
 
@@ -228,6 +231,17 @@ in {
         type = with types; nullOr package;
         readOnly = true;
         description = "Resulting Firefox package.";
+      };
+
+      policies = mkOption {
+        type = types.attrsOf jsonFormat.type;
+        default = { };
+        description =
+          "[See list of policies](https://mozilla.github.io/policy-templates/).";
+        example = {
+          DefaultDownloadDirectory = "\${home}/Downloads";
+          BlockAboutConfig = true;
+        };
       };
 
       profiles = mkOption {

tests/modules/programs/firefox/policies.nix
@@ -1,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./setup-firefox-mock-overlay.nix ];
+
+  config = lib.mkIf config.test.enableBig {
+    home.stateVersion = "23.05";
+
+    programs.firefox = {
+      enable = true;
+      policies = { BlockAboutConfig = true; };
+    };
+
+    nmt.script = ''
+      jq=${lib.getExe pkgs.jq}
+      config_file="${config.programs.firefox.finalPackage}/lib/firefox/distribution/policies.json"
+
+      assertFileExists "$config_file"
+      blockAboutConfig_actual_value="$($jq ".policies.BlockAboutConfig" $config_file)"
+
+      if [[ $blockAboutConfig_actual_value != "true" ]]; then
+        fail "Expected '$config_file' to set 'policies.BlockAboutConfig' to true"
+      fi
+    '';
+  };
+}

tests/modules/programs/firefox/default.nix
@@ -4,4 +4,5 @@
   firefox-deprecated-native-messenger = ./deprecated-native-messenger.nix;
   firefox-duplicate-profile-ids = ./duplicate-profile-ids.nix;
   firefox-duplicate-container-ids = ./duplicate-container-ids.nix;
+  firefox-policies = ./policies.nix;
 }

commit bf4b576f84e1ce54ec886836bae7695738aa5a6c

firefox: restore compatibility for extraPolicies
This commit makes it possible to specify Firefox' extraPolicies
through:

    programs.firefox.package = pkgs.firefox.override {
      extraPolicies = {... }
    }

This was possible in the past but was broken by:

  3feeb7715584fd45ed1389cec8fb15f6930e8dab
  firefox: add support for specifying policies (#4626)

modules/programs/firefox.nix
@@ -172,7 +172,7 @@ let
     else if versionAtLeast config.home.stateVersion "19.09" then
       package.override (old: {
         cfg = old.cfg or { } // fcfg;
-        extraPolicies = cfg.policies;
+        extraPolicies = (old.extraPolicies or { }) // cfg.policies;
       })
     else
       (pkgs.wrapFirefox.override { config = bcfg; }) package { };

tests/modules/programs/firefox/policies.nix
@@ -9,6 +9,9 @@
     programs.firefox = {
       enable = true;
       policies = { BlockAboutConfig = true; };
+      package = pkgs.firefox.override {
+        extraPolicies = { DownloadDirectory = "/foo"; };
+      };
     };
 
     nmt.script = ''
@@ -16,10 +19,17 @@
       config_file="${config.programs.firefox.finalPackage}/lib/firefox/distribution/policies.json"
 
       assertFileExists "$config_file"
+
       blockAboutConfig_actual_value="$($jq ".policies.BlockAboutConfig" $config_file)"
 
       if [[ $blockAboutConfig_actual_value != "true" ]]; then
         fail "Expected '$config_file' to set 'policies.BlockAboutConfig' to true"
+      fi
+
+      downloadDirectory_actual_value="$($jq ".policies.DownloadDirectory" $config_file)"
+
+      if [[ $downloadDirectory_actual_value != "\"/foo\"" ]]; then
+        fail "Expected '$config_file' to set 'policies.DownloadDirectory' to \"/foo\""
       fi
     '';
   };