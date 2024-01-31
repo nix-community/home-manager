9eeece97ea829603d79f113077470eb3cf4113c6
William Boulanger <willbou2@gmail.com>
2024-01-31 18:08:00 -0500

    vifm: added new module to modules.nix and fixed tests

--- modules/modules.nix
+++ modules/modules.nix
@@ -227,6 +227,7 @@ let
     ./programs/translate-shell.nix
     ./programs/urxvt.nix
     ./programs/vdirsyncer.nix
+    ./programs/vifm.nix
     ./programs/vim.nix
     ./programs/vim-vint.nix
     ./programs/vscode.nix
