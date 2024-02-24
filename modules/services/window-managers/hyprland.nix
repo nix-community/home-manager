ae7a3b5137ca3522f2802b3183de8fe5287b13d2
Mihai Fufezan <fufexan@protonmail.com>
2024-02-24 22:04:47 +0200

    hyprland: fix reloading
    Fix hyprctl erroring out due to HIS.

--- modules/services/window-managers/hyprland.nix
+++ modules/services/window-managers/hyprland.nix
@@ -247,10 +247,8 @@ in {
       onChange = lib.mkIf (cfg.package != null) ''
         ( # Execute in subshell so we don't poision environment with vars
           if [[ -d "/tmp/hypr" ]]; then
-            # This var must be set for hyprctl to function, but the value doesn't matter.
-            export HYPRLAND_INSTANCE_SIGNATURE="bogus"
             for i in $(${cfg.finalPackage}/bin/hyprctl instances -j | jq ".[].instance" -r); do
-              HYPRLAND_INSTANCE_SIGNATURE=$i ${cfg.finalPackage}/bin/hyprctl reload config-only
+              ${cfg.finalPackage}/bin/hyprctl -i "$i" reload config-only
             done
           fi
         )
