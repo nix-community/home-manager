6c7547cbcb0e79007a596a70426da29595a46a61
Felipe Silva <github@felipeqq2.rocks>
2024-03-03 14:12:42 -0300

    khal: fix contact integration (#4836)
    * khal: fix contact integration
    
    - Add tests for contact+khal
    - Make options `color`/`priority` available for contact accounts
    
    * khal: add separate calendar for each contact collection
    
    A contact account may have multiple VCARD collections, but Khal doesn't
    search recursively. Collection folder names must be hardcoded, and each
    has its own calendar.
    
    - Add khal.collections option for contact accounts
    - Default to previous setup for accounts with a single collection
    - Add tests
    
    * khal: specify how priority is defined by Khal
    
    See https://khal.readthedocs.io/en/latest/configure.html

--- modules/programs/khal-accounts.nix
+++ modules/programs/khal-accounts.nix
@@ -13,5 +13,39 @@ with lib;
         Keep khal from making any changes to this account.
       '';
     };
+
+    color = mkOption {
+      type = types.nullOr (types.enum [
+        "black"
+        "white"
+        "brown"
+        "yellow"
+        "dark gray"
+        "dark green"
+        "dark blue"
+        "light gray"
+        "light green"
+        "light blue"
+        "dark magenta"
+        "dark cyan"
+        "dark red"
+        "light magenta"
+        "light cyan"
+        "light red"
+      ]);
+      default = null;
+      description = ''
+        Color in which events in this calendar are displayed.
+      '';
+      example = "light green";
+    };
+
+    priority = mkOption {
+      type = types.int;
+      default = 10;
+      description = ''
+        Priority of a calendar used for coloring (calendar with highest priority is preferred).
+      '';
+    };
   };
 }
