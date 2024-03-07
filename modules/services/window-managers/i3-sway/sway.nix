bcb7b94e12c8eba80d0240829ec547f38f6b4f83
novenary <novenary@kwak.zip>
2024-03-07 17:48:31 +0200

    sway: stop sway-session.target on exit
    This ensures graphical-session.target is properly cycled, and that all
    of its members are restarted on consecutive runs.

--- modules/services/window-managers/i3-sway/sway.nix
+++ modules/services/window-managers/i3-sway/sway.nix
@@ -411,7 +411,11 @@ in {
 
       extraCommands = mkOption {
         type = types.listOf types.str;
-        default = [ "systemctl --user start sway-session.target" ];
+        default = [
+          "systemctl --user start sway-session.target"
+          "swaymsg -mt subscribe '[]' || true"
+          "systemctl --user stop sway-session.target"
+        ];
         description = ''
           Extra commands to run after D-Bus activation.
         '';
