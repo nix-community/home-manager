643c84f271bd03a1f1af0de53388f7cab23ac21c
Lin Xianyi <iynaix@gmail.com>
2024-03-10 10:48:04 +0800

    pqiv: add extraConfig option

--- modules/programs/pqiv.nix
+++ modules/programs/pqiv.nix
@@ -3,12 +3,10 @@
 with lib;
 
 let
-
   cfg = config.programs.pqiv;
   iniFormat = pkgs.formats.ini { };
-
 in {
-  meta.maintainers = with lib.maintainers; [ donovanglover ];
+  meta.maintainers = with lib.maintainers; [ donovanglover iynaix ];
 
   options.programs.pqiv = {
     enable = mkEnableOption "pqiv image viewer";
@@ -24,10 +22,9 @@ in {
       type = iniFormat.type;
       default = { };
       description = ''
-        Configuration written to
-        <filename>$XDG_CONFIG_HOME/pqivrc</filename>. See <link
-        xlink:href="https://github.com/phillipberndt/pqiv/blob/master/pqiv.1"/>
-        for a list of available options. To set a boolean flag, set the value to 1.
+        Configuration written to {file}`$XDG_CONFIG_HOME/pqivrc`. See
+        {manpage}`pqiv(1)` for a list of available options. To set a
+        boolean flag, set the value to 1.
       '';
       example = literalExpression ''
         {
@@ -41,6 +38,25 @@ in {
         };
       '';
     };
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = ''
+        Extra lines to be added to {file}`$XDG_CONFIG_HOME/pqivrc`. See
+        {manpage}`pqiv(1)` for a list of available options.
+      '';
+      example = literalExpression ''
+        [actions]
+        set_cursor_auto_hide(1)
+
+        [keybindings]
+        t { montage_mode_enter() }
+        @MONTAGE {
+          t { montage_mode_return_cancel() }
+        }
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
@@ -49,8 +65,12 @@ in {
 
     home.packages = [ cfg.package ];
 
-    xdg.configFile."pqivrc" = mkIf (cfg.settings != { }) {
-      source = iniFormat.generate "pqivrc" cfg.settings;
-    };
+    xdg.configFile."pqivrc" =
+      mkIf (cfg.settings != { } && cfg.extraConfig != "") {
+        text = lib.concatLines [
+          (generators.toINI { } cfg.settings)
+          cfg.extraConfig
+        ];
+      };
   };
 }
