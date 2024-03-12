c8847ac1cf5bbf1001568f2a069d2d8654e63933
zorrobert <118135271+zorrobert@users.noreply.github.com>
2024-03-12 11:06:35 +0100

    joplin-desktop: fixed formatting via ./format

--- modules/programs/joplin-desktop.nix
+++ modules/programs/joplin-desktop.nix
@@ -40,14 +40,26 @@ in {
     ### Sync
     sync = {
       target = lib.mkOption {
-        type = lib.types.enum [ null "none" "file-system" "onedrive" "nextcloud" "webdav" "dropbox" "s3" "joplin-server" "joplin-cloud" ];
+        type = lib.types.enum [
+          null
+          "none"
+          "file-system"
+          "onedrive"
+          "nextcloud"
+          "webdav"
+          "dropbox"
+          "s3"
+          "joplin-server"
+          "joplin-cloud"
+        ];
         default = null;
         description = "What is the type of sync target.";
         example = "dropbox";
       };
 
       interval = lib.mkOption {
-        type = lib.types.enum [ null "disabled" "5m" "10m" "30m" "1h" "12h" "1d" ];
+        type =
+          lib.types.enum [ null "disabled" "5m" "10m" "30m" "1h" "12h" "1d" ];
         default = null;
         description = ''
           Set the Synchronisation interval. The following values can be used: "disabled" "5m" "10m" "30m" "1h" "12h" "1d"
@@ -61,47 +73,43 @@ in {
     home.packages = [ cfg.package ];
 
     home.activation = {
-      hm-activate-joplin-desktop-config =
-        let
-          newConfig = jsonFormat.generate "joplin-settings.json" (
-            lib.attrsets.filterAttrs (n: v: (v != null) && (v != "")) (
-              {
-                # TODO: find a better way to convert nix attribute names to strings:
-                # sync.interval = ... -> "sync.interval" = ...
+      hm-activate-joplin-desktop-config = let
+        newConfig = jsonFormat.generate "joplin-settings.json"
+          (lib.attrsets.filterAttrs (n: v: (v != null) && (v != "")) ({
+            # TODO: find a better way to convert nix attribute names to strings:
+            # sync.interval = ... -> "sync.interval" = ...
 
-                "editor" = config.programs.joplin-desktop.general.editor;
+            "editor" = config.programs.joplin-desktop.general.editor;
 
-                "sync.target" = {
-                  "none"          = 0;
-                  "file-system"   = 2;
-                  "onedrive"      = 3;
-                  "nextcloud"     = 5;
-                  "webdav"        = 6;
-                  "dropbox"       = 7;
-                  "s3"            = 8;
-                  "joplin-server" = 9;
-                  "joplin-cloud"  = 10;
-                }.${config.programs.joplin-desktop.sync.target} or null;
+            "sync.target" = {
+              "none" = 0;
+              "file-system" = 2;
+              "onedrive" = 3;
+              "nextcloud" = 5;
+              "webdav" = 6;
+              "dropbox" = 7;
+              "s3" = 8;
+              "joplin-server" = 9;
+              "joplin-cloud" = 10;
+            }.${config.programs.joplin-desktop.sync.target} or null;
 
-                "sync.interval" = {
-                  "disabled"  =     0;
-                  "5m"        =   300;
-                  "10m"       =   600;
-                  "30m"       =  1800;
-                  "1h"        =  3600;
-                  "12h"       = 43200;
-                  "1d"        = 86400;
-                }.${config.programs.joplin-desktop.sync.interval} or null;
-              } // config.programs.joplin-desktop.extraConfig
-            )
-          );
-        in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
-          # ensure that settings.json exists
-          touch ${configPath}/settings.json
-          # config has to be written to temporary variable because jq cannot edit files in place
-          config="$(jq -s '.[0] + .[1]' ${configPath}/settings.json ${newConfig})"
-          printf '%s\n' "$config" > ${configPath}/settings.json
-        '';
+            "sync.interval" = {
+              "disabled" = 0;
+              "5m" = 300;
+              "10m" = 600;
+              "30m" = 1800;
+              "1h" = 3600;
+              "12h" = 43200;
+              "1d" = 86400;
+            }.${config.programs.joplin-desktop.sync.interval} or null;
+          } // config.programs.joplin-desktop.extraConfig));
+      in lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+        # ensure that settings.json exists
+        touch ${configPath}/settings.json
+        # config has to be written to temporary variable because jq cannot edit files in place
+        config="$(jq -s '.[0] + .[1]' ${configPath}/settings.json ${newConfig})"
+        printf '%s\n' "$config" > ${configPath}/settings.json
+      '';
     };
   };
 
