nix-community/home-manager

commit 01e4a5143e92251272850a8e0fbb4518bd099087

gpg-agent: migrate to 'pinentryPackage'
This follows upstream's module change [1], which allows setting any
package as a pinentry program.

[1]: https://github.com/NixOS/nixpkgs/pull/133542

modules/services/gpg-agent.nix
@@ -81,6 +81,11 @@ let
 in {
   meta.maintainers = [ maintainers.rycee ];
 
+  imports = [
+    (mkRemovedOptionModule [ "services" "gpg-agent" "pinentryFlavor" ]
+      "Use services.gpg-agent.pinentryPackage instead")
+  ];
+
   options = {
     services.gpg-agent = {
       enable = mkEnableOption "GnuPG private key agent";
@@ -192,10 +197,9 @@ in {
           configuration file.
         '';
       };
-
-      pinentryFlavor = mkOption {
-        type = types.nullOr (types.enum pkgs.pinentry.flavors);
-        example = "gnome3";
+      pinentryPackage = mkOption {
+        type = types.nullOr types.package;
+        example = literalExpression "pkgs.pinentry-gnome3";
         default = null;
         description = ''
           Which pinentry interface to use. If not
@@ -243,8 +247,8 @@ in {
           "max-cache-ttl ${toString cfg.maxCacheTtl}"
           ++ optional (cfg.maxCacheTtlSsh != null)
           "max-cache-ttl-ssh ${toString cfg.maxCacheTtlSsh}"
-          ++ optional (cfg.pinentryFlavor != null)
-          "pinentry-program ${pkgs.pinentry.${cfg.pinentryFlavor}}/bin/pinentry"
+          ++ optional (cfg.pinentryPackage != null)
+          "pinentry-program ${lib.getExe pinentryPackage}"
           ++ [ cfg.extraConfig ]);
 
       home.sessionVariablesExtra = optionalString cfg.enableSshSupport ''

tests/modules/services/gpg-agent/default-homedir.nix
@@ -5,7 +5,7 @@ with lib;
 {
   config = {
     services.gpg-agent.enable = true;
-    services.gpg-agent.pinentryFlavor = null; # Don't build pinentry package.
+    services.gpg-agent.pinentryPackage = null; # Don't build pinentry package.
     programs.gpg.enable = true;
 
     test.stubs.gnupg = { };

tests/modules/services/gpg-agent/override-homedir.nix
@@ -5,7 +5,7 @@ with lib;
 {
   config = {
     services.gpg-agent.enable = true;
-    services.gpg-agent.pinentryFlavor = null; # Don't build pinentry package.
+    services.gpg-agent.pinentryPackage = null; # Don't build pinentry package.
     programs.gpg = {
       enable = true;
       homedir = "/path/to/hash";

commit e44cb2322abe9f8d186c0108d7959fae666672e6

gpg-agent: fix pinentry-program configuration
Fies a build error introduced in
https://github.com/nix-community/home-manager/commit/01e4a5143e92251272850a8e0fbb4518bd099087.

modules/services/gpg-agent.nix
@@ -248,7 +248,7 @@ in {
           ++ optional (cfg.maxCacheTtlSsh != null)
           "max-cache-ttl-ssh ${toString cfg.maxCacheTtlSsh}"
           ++ optional (cfg.pinentryPackage != null)
-          "pinentry-program ${lib.getExe pinentryPackage}"
+          "pinentry-program ${lib.getExe cfg.pinentryPackage}"
           ++ [ cfg.extraConfig ]);
 
       home.sessionVariablesExtra = optionalString cfg.enableSshSupport ''