c67b06ff923a4e7eb5c93fe0277a966c894afdc4
Giel van Schijndel <giel@mortis.eu>
2024-04-12 14:14:03 +0200

    zsh: support attrsets as associative arrays in programs.zsh.localVariables

--- tests/modules/programs/zsh/local-variables.nix
+++ tests/modules/programs/zsh/local-variables.nix
@@ -12,7 +12,13 @@ with lib;
         V2 = false;
         V3 = "some-string";
         V4 = 42;
-        V5 = [ V1 V2 V3 V4 ];
+        V5 = builtins.attrValues V6;
+        V6 = {
+          a = V1;
+          b = V2;
+          c = V3;
+          d = V4;
+        };
       };
     };
 
@@ -20,11 +26,15 @@ with lib;
 
     nmt.script = ''
       assertFileExists home-files/.zshrc
-      assertFileContains home-files/.zshrc 'V1=true'
-      assertFileContains home-files/.zshrc 'V2=false'
-      assertFileContains home-files/.zshrc 'V3="some-string"'
-      assertFileContains home-files/.zshrc 'V4="42"'
-      assertFileContains home-files/.zshrc 'V5=(true false "some-string" "42")'
-      '';
+      assertFileRegex home-files/.zshrc '^V1=true$'
+      assertFileRegex home-files/.zshrc '^V2=false$'
+      assertFileRegex home-files/.zshrc '^V3="some-string"$'
+      assertFileRegex home-files/.zshrc '^V4="42"$'
+      assertFileRegex home-files/.zshrc '^V5=[(]true false "some-string" "42"[)]$'
+      assertFileContains home-files/.zshrc ${
+        lib.escapeShellArg ''
+          typeset -A V6=(['a']=true ['b']=false ['c']="some-string" ['d']="42")''
+      }
+    '';
   };
 }
