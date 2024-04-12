0360bc8c5bee93fff7afa52f8b26c93ef35ee01d
Divit Mittal <64.69.76.69.74.m@gmail.com>
2024-04-12 17:20:23 +0530

    yazi: reformated using nixfmt-rfc

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -137,33 +137,33 @@ in {
     };
 
     extraLuaConfig = mkOption {
-        type = types.lines;
-        default = "";
-        example = literalExpression ''
-          '''
-          -- Add symlink target to the status bar
-          function Status:name()
-            local h = cx.active.current.hovered
-            if h == nil then
-              return ui.Span("")
-            end
-
-            local linked = ""
-            if h.link_to ~= nil then
-              linked = " -> " .. tostring(h.link_to)
-            end
-            return ui.Span(" " .. h.name .. linked)
+      type = types.lines;
+      default = "";
+      example = literalExpression ''
+        '''
+        -- Add symlink target to the status bar
+        function Status:name()
+          local h = cx.active.current.hovered
+          if h == nil then
+            return ui.Span("")
           end
-          '''
-        '';
-        description = ''
-          Configuration written to
-          {file}`$XDG_CONFIG_HOME/yazi/init.lua`.
 
-          See <https://yazi-rs.github.io/docs/tips>
-          for more information
-        '';
-      };
+          local linked = ""
+          if h.link_to ~= nil then
+            linked = " -> " .. tostring(h.link_to)
+          end
+          return ui.Span(" " .. h.name .. linked)
+        end
+        '''
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/yazi/init.lua`.
+
+        See <https://yazi-rs.github.io/docs/tips>
+        for more information
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
