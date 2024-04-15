59d50bc582bdf439df096a9ae1781e6c9c8a7523
Nathan Henrie <n8henrie@users.noreply.github.com>
2024-04-15 01:40:27 -0600

    espanso: enable module on darwin

--- tests/default.nix
+++ tests/default.nix
@@ -163,6 +163,7 @@ in import nmtSrc {
     ./modules/xresources
   ] ++ lib.optionals isDarwin [
     ./modules/launchd
+    ./modules/services/espanso-darwin
     ./modules/services/git-sync-darwin
     ./modules/services/imapnotify-darwin
     ./modules/services/nix-gc-darwin
