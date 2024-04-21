bfa7c06436771e3a0c666ccc6ee01e815d4c33aa
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-04-21 02:55:00 -0400

    swayosd: add custom style option

--- tests/modules/services/swayosd/swayosd.nix
+++ tests/modules/services/swayosd/swayosd.nix
@@ -8,6 +8,7 @@
       outPath = "@swayosd@";
     };
     display = "DISPLAY";
+    stylePath = "/etc/xdg/swayosd/style.css";
     topMargin = 0.1;
   };
 
@@ -20,7 +21,7 @@
           WantedBy=graphical-session.target
 
           [Service]
-          ExecStart=@swayosd@/bin/swayosd-server --display DISPLAY --top-margin 0.100000
+          ExecStart=@swayosd@/bin/swayosd-server --display DISPLAY --style '/etc/xdg/swayosd/style.css' --top-margin 0.100000
           Restart=always
           Type=simple
 
