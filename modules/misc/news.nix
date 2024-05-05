fdaaf543bad047639ef0b356ea2e6caec2f1215c
Austin Horstman <khaneliman12@gmail.com>
2024-05-05 04:13:18 -0500

    hypridle: add module

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1584,6 +1584,17 @@ in {
           https://conky.cc/ for more.
         '';
       }
+
+      {
+        time = "2024-05-05T07:22:01+00:00";
+        condition = hostPlatform.isLinux;
+        message = ''
+          A new module is available: 'services.hypridle'.
+
+          Hypridle is a program that monitors user activity and runs commands
+          when idle or active. See https://github.com/hyprwm/hypridle for more.
+        '';
+      }
     ];
   };
 }
