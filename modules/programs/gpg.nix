f37049e85f1a7152746f1869bddf5e66a9f2fdf3
北雁 Cryolitia <Cryolitia@gmail.com>
2024-05-09 05:26:01 +0800

    gpg: delete default settings
    fix https://github.com/nix-community/home-manager/issues/5383

--- modules/programs/gpg.nix
+++ modules/programs/gpg.nix
@@ -240,28 +240,10 @@ in {
   };
 
   config = mkIf cfg.enable {
+    # To prevent error: The option `programs.gpg.settings' is used but not defined.
     programs.gpg.settings = {
-      personal-cipher-preferences = mkDefault "AES256 AES192 AES";
-      personal-digest-preferences = mkDefault "SHA512 SHA384 SHA256";
-      personal-compress-preferences = mkDefault "ZLIB BZIP2 ZIP Uncompressed";
-      default-preference-list = mkDefault
-        "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-      cert-digest-algo = mkDefault "SHA512";
-      s2k-digest-algo = mkDefault "SHA512";
-      s2k-cipher-algo = mkDefault "AES256";
-      charset = mkDefault "utf-8";
-      fixed-list-mode = mkDefault true;
-      no-comments = mkDefault true;
-      no-emit-version = mkDefault true;
-      keyid-format = mkDefault "0xlong";
-      list-options = mkDefault "show-uid-validity";
-      verify-options = mkDefault "show-uid-validity";
-      with-fingerprint = mkDefault true;
-      require-cross-certification = mkDefault true;
-      no-symkey-cache = mkDefault true;
-      use-agent = mkDefault true;
+      # no defaults for gpg
     };
-
     programs.gpg.scdaemonSettings = {
       # no defaults for scdaemon
     };
