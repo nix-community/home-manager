c21fc2c0a84bcef551fdb681a4446e23f2c6b48d
libewa <libewa.git@icloud.com>
2024-05-26 12:40:12 +0200

    zed-editor: add ability to configure user settings and keymaps

--- modules/programs/zed-editor.nix
+++ modules/programs/zed-editor.nix
@@ -4,6 +4,14 @@ with lib;
 
 let
   cfg = config.programs.zed-editor;
+  jsonFormat = pkgs.formats.json { };
+
+  configDir = if pkgs.stdenv.hostPlatform.isDarwin then
+      "Library/Application Support/zed" #TODO: see if this is actually the correct directory
+    else
+      "${config.xdg.configHome}/zed";
+  configFilePath = "${configDir}/settings.json";
+  keymapFilePath = "${configDir}/keymap.json";
 in
 {
   options = {
@@ -16,9 +24,59 @@ in
         description = ''
           Another package to install instead of zed
         '';
-    };
-    config = mkIf cfg.enable {
-      home.packages = [ cfg.package ];
+      };
+      userSettings = mkOption {
+        type = jsonFormat.type;
+        default = { };
+        example = literalExpression ''
+        {
+          "features": {
+            "copilot": false
+          },
+          "telemetry": {
+            "metrics": false
+          },
+          "vim_mode": false,
+          "ui_font_size": 16,
+          "buffer_font_size": 16
+        }
+        '';
+        description = ''
+          Configuration written to Zed's {file}`settings.json`.
+        '';
+      };
+      userKeymaps = mkOption {
+        type = jsonFormat.type;
+        default = { };
+        example = literalExpression ''
+          [
+            {
+              "context" = "Workspace";
+              "bindings" = {
+                "ctrl-shift-t" = "workspace::NewTerminal";
+              };
+            };
+          ]
+        '';
+        description = ''
+          Configuration written to Zed's {file}`keymap.json`.
+        '';
+      };
+      #TODO: add vscode option parity (installing extensions, configuring keybinds with nix etc.)
     };
   };
+
+  config = mkIf cfg.enable {
+      home.packages = [ cfg.package ];
+      home.file = mkMerge [
+        (mkIf (cfg.userSettings != { }) {
+          "${configFilePath}".source =
+            jsonFormat.generate "zed-user-settings" cfg.userSettings;
+        })
+        (mkIf (cfg.userKeymaps != { }) {
+          "${keymapFilePath}".source = 
+            jsonFormat.generate "zed-user-keymaps" cfg.userKeymaps;
+        })
+      ];
+    };
 }
