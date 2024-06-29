c2f806e60ac55c604708250ba0ebcf96bccbbafe
Robert Helgesson <robert@rycee.net>
2024-06-29 17:20:18 +0200

    pulseeffects: fix test evaluation
    Fixes #5588

--- modules/lib/maintainers.nix
+++ modules/lib/maintainers.nix
@@ -173,6 +173,13 @@
     github = "iosmanthus";
     githubId = 16307070;
   };
+  jonringer = {
+    email = "jonringer117@gmail.com";
+    matrix = "@jonringer:matrix.org";
+    github = "jonringer";
+    githubId = 7673602;
+    name = "Jonathan Ringer";
+  };
   kalhauge = {
     name = "Christian Gram Kalhauge";
     email = "kalhauge@users.noreply.github.com";
