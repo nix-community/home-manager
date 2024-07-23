26059c1477116d6a3ea486eed3bb4e806ff5aaec
jirafey <jirafey@tuta.io>
2024-07-23 20:42:27 +0200

    firefox: show NixOS wiki search engine icon
    Before it was referencing:
    `https://wiki.nixos.org/favicon.png`, which is not existing.
    Now both icons for NixOS packages and NixOS wiki are the same

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -546,7 +546,7 @@ in {
 
                     "NixOS Wiki" = {
                       urls = [{ template = "https://wiki.nixos.org/index.php?search={searchTerms}"; }];
-                      iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+                      iconUpdateURL = "https://wiki.nixos.org/nixos.png";
                       updateInterval = 24 * 60 * 60 * 1000; # every day
                       definedAliases = [ "@nw" ];
                     };
