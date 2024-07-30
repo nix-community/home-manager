718f450b97d57234dc0c801c76b856b6b615cde6
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-30 20:19:04 +0900

    sheldon: fix setting to simple

--- tests/modules/programs/sheldon/default.nix
+++ tests/modules/programs/sheldon/default.nix
@@ -5,48 +5,10 @@
       settings = {
         shell = "zsh";
         plugins = {
-          async = {
-            local = "~/.config/sheldon/async";
-            use = [ "*.zsh" ];
-            apply = [ "defer" ];
-          };
-          sync = {
-            local = "~/.config/sheldon/sync";
-            use = [ "*.zsh" ];
-            apply = [ "source" ];
-          };
-          zsh-defer = {
-            github = "romkatv/zsh-defer";
-            apply = [ "source" ];
-          };
-          add-zsh-hook = { inline = "autoload -U add-zsh-hook"; };
-          anyframe = { github = "mollifier/anyframe"; };
-          colors = { inline = "autoload -U colors && zsh-defer colors"; };
-          compinit = {
-            inline = "autoload -U compinit && zsh-defer compinit -C";
-          };
-          fzf = { github = "junegunn/fzf"; };
-          predict = { inline = "autoload -U predict-on && predict-on"; };
-          starship = {
-            inline = ''
-              eval "$(starship init zsh)"
-            '';
-          };
-          zcalc = { inline = "autoload -U zcalc"; };
-          zsh-async = { github = "mafredri/zsh-async"; };
-          zsh-complations = {
-            github = "zsh-users/zsh-completions";
-            apply = [ "defer" ];
-          };
-          zsh-history-substring-search = {
-            github = "zsh-users/zsh-history-substring-search";
-            apply = [ "defer" ];
-          };
           zsh-syntax-highlighting = {
             github = "zsh-users/zsh-syntax-highlighting";
             apply = [ "defer" ];
           };
-          zsh-terminfo = { inline = "zmodload zsh/terminfo"; };
         };
         templates = {
           defer = ''
