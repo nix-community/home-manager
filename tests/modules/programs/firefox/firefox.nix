75c08056d90180d3bfbbda250ace19a33d31124c
Kira Bruneau <kira.bruneau@pm.me>
2024-08-01 14:53:58 -0400

    firefox: organize tests by submodule

--- tests/modules/programs/firefox/firefox.nix
+++ tests/modules/programs/firefox/firefox.nix
@@ -1,11 +1,16 @@
 let name = "firefox";
 
 in builtins.mapAttrs (test: module: import module [ "programs" name ]) {
-  "${name}-profile-settings" = ./profile-settings.nix;
-  "${name}-state-version-19_09" = ./state-version-19_09.nix;
   "${name}-deprecated-native-messenger" = ./deprecated-native-messenger.nix;
-  "${name}-duplicate-profile-ids" = ./duplicate-profile-ids.nix;
-  "${name}-duplicate-container-ids" = ./duplicate-container-ids.nix;
-  "${name}-container-id-out-of-range" = ./container-id-out-of-range.nix;
   "${name}-policies" = ./policies.nix;
+  "${name}-profile-bookmarks" = ./profiles/bookmarks;
+  "${name}-profile-containers" = ./profiles/containers;
+  "${name}-profile-containers-duplicate-ids" =
+    ./profiles/containers/duplicate-ids.nix;
+  "${name}-profile-containers-id-out-of-range" =
+    ./profiles/containers/id-out-of-range.nix;
+  "${name}-profile-duplicate-ids" = ./profiles/duplicate-ids.nix;
+  "${name}-profile-search" = ./profiles/search;
+  "${name}-profile-settings" = ./profiles/settings;
+  "${name}-state-version-19_09" = ./state-version-19_09.nix;
 }
