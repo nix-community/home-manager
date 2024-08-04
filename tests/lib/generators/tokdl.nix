7b5f3727de663abaf0c7a37efea6fd0f4a4b0d1a
shimun <shimun@shimun.net>
2024-08-04 11:57:46 +0200

    test(generators/toKDL): added case for _args

--- tests/lib/generators/tokdl.nix
+++ tests/lib/generators/tokdl.nix
@@ -1,51 +1,81 @@
-{ config, lib, ... }:
-
-{
-  home.file."tokdl-result.txt".text = lib.hm.generators.toKDL { } {
-    a = 1;
-    b = "string";
-    c = ''
-      multiline string
-      with special characters:
-      \t \n \" "
-    '';
-    unsafeString = " \" \n 	 ";
-    flatItems = [ 1 2 "asdf" true null ];
-    bigFlatItems = [
-      23847590283751
-      1.239
-      ''
-        multiline " " "
-        string
-      ''
-      null
-    ];
-    repeated = [ [ 1 2 ] [ true false ] [ ] [ null ] ];
-    extraAttrs = {
-      _args = [ 2 true ];
-      _props = {
-        arg1 = 1;
-        arg2 = false;
+{ config, lib, ... }: {
+  home.file = {
+    "tokdl-result.txt".text = lib.hm.generators.toKDL { } {
+      a = 1;
+      b = "string";
+      c = ''
+        multiline string
+        with special characters:
+        \t \n \" "
+      '';
+      unsafeString = " \" \n 	 ";
+      flatItems = [ 1 2 "asdf" true null ];
+      bigFlatItems = [
+        23847590283751
+        1.239
+        ''
+          multiline " " "
+          string
+        ''
+        null
+      ];
+      repeated = [ [ 1 2 ] [ true false ] [ ] [ null ] ];
+      extraAttrs = {
+        _args = [ 2 true ];
+        _props = {
+          arg1 = 1;
+          arg2 = false;
+        };
+        nested = {
+          a = 1;
+          b = null;
+        };
       };
-      nested = {
-        a = 1;
-        b = null;
+      listInAttrsInList = {
+        list1."-" = [
+          { a = 1; }
+          { b = true; }
+          {
+            c = null;
+            d."-" = [{ e = "asdfadfasdfasdf"; }];
+          }
+        ];
+        list2 = [{ a = 8; }];
       };
     };
-    listInAttrsInList = {
-      list1."-" = [
-        { a = 1; }
-        { b = true; }
+    "tokdl-attrs.txt".text = lib.hm.generators.toKDL { } {
+      resize.bind = [
         {
-          c = null;
-          d."-" = [{ e = "asdfadfasdfasdf"; }];
+          _args = [ "k" "Up" ];
+          Resize = "Increase Up";
+        }
+        {
+          _args = [ "j" "Down" ];
+          Resize = "Increase Down";
         }
       ];
-      list2 = [{ a = 8; }];
+    };
+    "tokdl-list.txt".text = lib.hm.generators.toKDL { } {
+      resize.bind."-" = [
+        {
+          _args = [ "k" "Up" ];
+          Resize = "Increase Up";
+        }
+        {
+          _args = [ "j" "Down" ];
+          Resize = "Increase Down";
+        }
+      ];
     };
   };
 
   nmt.script = ''
+    assertFileContent \
+      home-files/tokdl-attrs.txt \
+      ${./tokdl-attrs.txt}
+    assertFileContent \
+      home-files/tokdl-list.txt \
+      ${./tokdl-list.txt}
     assertFileContent \
       home-files/tokdl-result.txt \
       ${./tokdl-result.txt}
