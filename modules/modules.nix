cbedc75a3ce2805449a1576bccec517ff4ca278d
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-08-07 14:47:26 +0100

    jqp: add module

--- modules/modules.nix
+++ modules/modules.nix
@@ -140,6 +140,7 @@ let
     ./programs/java.nix
     ./programs/jetbrains-remote.nix
     ./programs/jq.nix
+    ./programs/jqp.nix
     ./programs/jujutsu.nix
     ./programs/joshuto.nix
     ./programs/joplin-desktop.nix
