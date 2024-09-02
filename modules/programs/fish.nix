efd70b81aae6877ddb619911ea4b307aebc925cd
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2024-09-02 14:34:07 +0200

    fish : move aliases after initializations
    This allow programs needing initialization to be aliased

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -471,11 +471,11 @@ in {
           # Abbreviations
           ${abbrsStr}
 
-          # Aliases
-          ${aliasesStr}
-
           # Interactive shell initialisation
           ${cfg.interactiveShellInit}
+
+          # Aliases
+          ${aliasesStr}
 
         end
 
