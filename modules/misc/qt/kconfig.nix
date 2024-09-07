f917b026e20798789ba43a2692c045882e6a0bc5
Louis Opter <louis@opter.org>
2024-09-07 13:25:40 -0700

    qt: use kwriteconfig6 in qt.kde.settings and actually call the tool
    Update configuration tool for plasma6.
    
    Not sure why we had this test -f in the activation code?

--- modules/misc/qt/kconfig.nix
+++ modules/misc/qt/kconfig.nix
@@ -17,13 +17,13 @@ in {
       powermanagementprofilesrc.AC.HandleButtonEvents.lidAction = 32;
     };
     description = ''
-      A set of values to be modified by {command}`kwriteconfig5`.
+      A set of values to be modified by {command}`kwriteconfig6`.
 
       The example value would cause the following command to run in the
       activation script:
 
       ``` shell
-      kwriteconfig5 --file $XDG_CONFIG_HOME/powermanagementprofilesrc \
+      kwriteconfig6 --file $XDG_CONFIG_HOME/powermanagementprofilesrc \
                     --group AC \
                     --group HandleButtonEvents \
                     --group lidAction \
@@ -53,7 +53,7 @@ in {
             lib.mapAttrsToList
             (group: value: toLine file (path ++ [ group ]) value) value
           else
-            "run test -f '${configHome}/${file}' && run ${pkgs.libsForQt5.kconfig}/bin/kwriteconfig5 --file '${configHome}/${file}' ${
+            "run ${pkgs.kdePackages.kconfig}/bin/kwriteconfig6 --file '${configHome}/${file}' ${
               lib.concatMapStringsSep " " (x: "--group ${x}")
               (lib.lists.init path)
             } --key '${lib.lists.last path}' ${toValue value}";
@@ -62,7 +62,7 @@ in {
       in builtins.concatStringsSep "\n" lines}
 
       # TODO: some way to only call the dbus calls needed
-      run ${pkgs.libsForQt5.qttools.bin}/bin/qdbus org.kde.KWin /KWin reconfigure || echo "KWin reconfigure failed"
+      run ${pkgs.kdePackages.qttools}/bin/qdbus org.kde.KWin /KWin reconfigure || echo "KWin reconfigure failed"
       # the actual values are https://github.com/KDE/plasma-workspace/blob/c97dddf20df5702eb429b37a8c10b2c2d8199d4e/kcms/kcms-common_p.h#L13
       for changeType in {0..10}; do
         # even if one of those calls fails the others keep running
