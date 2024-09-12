ea244c5ae2205f0fced24dde2e555440303d63bc
Chet Gurevitch <chet@gurevitch.net>
2024-09-12 20:00:29 +0000

    helix: remove outdated language-server comment

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -75,7 +75,6 @@ in {
       default = { };
       example = literalExpression ''
         {
-          # the language-server option currently requires helix from the master branch at https://github.com/helix-editor/helix/
           language-server.typescript-language-server = with pkgs.nodePackages; {
             command = "''${typescript-language-server}/bin/typescript-language-server";
             args = [ "--stdio" "--tsserver-path=''${typescript}/lib/node_modules/typescript/lib" ];
