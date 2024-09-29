bdd40dfd539d24b2f3fbc68de92637e3244729f8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-09-29 22:28:41 +0200

    wpaperd: test service file content

--- tests/modules/programs/wpaperd/wpaperd-example-settings.nix
+++ tests/modules/programs/wpaperd/wpaperd-example-settings.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   config = {
@@ -21,6 +21,25 @@
     nmt.script = ''
       assertFileContent home-files/.config/wpaperd/wallpaper.toml \
         ${./wpaperd-expected-settings.toml}
+
+      serviceFile=home-files/.config/systemd/user/wpaperd.service
+      assertFileExists $serviceFile
+      assertFileContent $serviceFile \
+        ${
+          pkgs.writeText "wpaperd-expected.service" ''
+            [Install]
+            WantedBy=graphical-session.target
+
+            [Service]
+            ExecStart=${pkgs.wpaperd}/bin/wpaperd
+
+            [Unit]
+            After=graphical-session-pre.target
+            Description=Modern wallpaper daemon for Wayland
+            PartOf=graphical-session.target
+          ''
+        }
+
     '';
   };
 }
