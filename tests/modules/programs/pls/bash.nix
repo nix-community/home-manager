d57112db877f07387ce7104b5ac346ede556d2d7
Nabeen Tiwaree <nabeen0x01@gmail.com>
2024-10-12 05:43:52 -0400

    pls: fixed perm argument to pass via pls

--- tests/modules/programs/pls/bash.nix
+++ tests/modules/programs/pls/bash.nix
@@ -23,7 +23,7 @@ with lib;
         "alias ls=@pls@/bin/pls"
       assertFileContains \
         home-files/.bashrc \
-        "alias ll='@pls@/bin/pls -d perms -d user -d group -d size -d mtime -d git'"
+        "alias ll='@pls@/bin/pls -d perm -d user -d group -d size -d mtime -d git'"
     '';
   };
 }
