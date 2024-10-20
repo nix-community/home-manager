ad4701e2df861fe6fb6a1c3caa363f8175712749
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-20 21:30:14 +1100

    eww: added improvements

--- modules/programs/eww.nix
+++ modules/programs/eww.nix
@@ -23,12 +23,64 @@ in {
       '';
     };
 
+    configYuck = mkOption {
+      type = types.lines;
+      example = ''
+        (defwindow example
+           :monitor 0
+           :geometry (geometry :x "0%"
+                               :y "20px"
+                               :width "90%"
+                               :height "30px"
+                               :anchor "top center")
+           :stacking "fg"
+           :reserve (struts :distance "40px" :side "top")
+           :windowtype "dock"
+           :wm-ignore false
+        "example content")
+      '';
+      description = ''
+        The content that gets symlinked to
+        {file} `$XDG_CONFIG_HOME/eww/eww.yuck`.
+      '';
+    };
+
+    configScss = mkOption {
+      type = types.lines;
+      example = ''
+        window {
+          background: pink;
+        }
+      '';
+      description = ''
+        The directory that gets symlinked to
+        {file} `$XDG_CONFIG_HOME/eww/eww.scss`.
+      '';
+    };
+
     configDir = mkOption {
       type = types.path;
       example = literalExpression "./eww-config-dir";
       description = ''
         The directory that gets symlinked to
         {file}`$XDG_CONFIG_HOME/eww`.
+
+        This Option is now deprecated. Please use `programs.eww.configYuck` & `programs.eww.configScss` instead.
+      '';
+    };
+
+    systemd.enable = mkEnableOption "Launches Eww Daemon";
+
+    systemd.target = mkOption {
+      type = types.str;
+      default = "graphical-session.target";
+      example = "sway-session.target";
+      description = ''
+        The systemd target that will automatically start the Eww service.
+
+        When setting this value to `"sway-session.target"`,
+        make sure to also enable {option}`wayland.windowManager.sway.systemd.enable`,
+        otherwise the service may never be started.
       '';
     };
 
@@ -45,26 +97,51 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-    xdg.configFile."eww".source = cfg.configDir;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      warnings = optional (isList cfg.configDir) ''
+        `programs.eww.configDir` is now deprecated. Please use `programs.eww.configYuck` & `programs.eww.configScss` instead.
+      '';
+      home.packages = [ cfg.package ];
+      xdg.configFile."eww".source = cfg.configDir;
 
-    programs.bash.initExtra = mkIf cfg.enableBashIntegration ''
-      if [[ $TERM != "dumb" ]]; then
-        eval "$(${ewwCmd} shell-completions --shell bash)"
-      fi
-    '';
+      programs.bash.initExtra = let ewwCmd = "${cfg.package}/bin/eww";
+      in mkIf cfg.enableBashIntegration ''
+        if [[ $TERM != "dumb" ]]; then
+          eval "$(${ewwCmd} shell-completions --shell bash)"
+        fi
+      '';
 
-    programs.zsh.initExtra = mkIf cfg.enableZshIntegration ''
-      if [[ $TERM != "dumb" ]]; then
-        eval "$(${ewwCmd} shell-completions --shell zsh)"
-      fi
-    '';
+      programs.zsh.initExtra = mkIf cfg.enableZshIntegration ''
+        if [[ $TERM != "dumb" ]]; then
+          eval "$(${ewwCmd} shell-completions --shell zsh)"
+        fi
+      '';
 
-    programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration ''
-      if test "$TERM" != "dumb"
-        eval "$(${ewwCmd} shell-completions --shell fish)"
-      end
-    '';
-  };
+      programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration ''
+        if test "$TERM" != "dumb"
+          eval "$(${ewwCmd} shell-completions --shell fish)"
+        end
+      '';
+    }
+
+    (mkIf cfg.systemd.enable {
+      systemd.user.services.eww = {
+        Unit = {
+          Description = "ElKowars wacky widgets";
+          Documentation = "https://elkowar.github.io/eww/";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${cfg.package} daemon --no-damonize";
+          ExecStop = "${cfg.package} kill";
+          ExecReload = "${cfg.package} reload";
+        };
+
+        Install = { WantedBy = [ cfg.systemd.target ]; };
+      };
+    })
+  ]);
 }
