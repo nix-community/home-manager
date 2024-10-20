639d707130dc061d6642ca05d3e74ffae8ec14a8
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-20 21:30:14 +1100

    eww: added improvements
    Co-authored-by: Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>

--- tests/modules/programs/eww/default.nix
+++ tests/modules/programs/eww/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }:
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   eww-basic-config = ./basic-config.nix;
-  eww-null-config = ./null-config.nix;
+  eww-empty-config = ./empty-config.nix;
 }
