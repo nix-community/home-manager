da8f12a2eb31174adf882f9c6d6a2383db5d74bc
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-10-26 11:04:42 -0300

    zen-browser: add tests

--- tests/default.nix
+++ tests/default.nix
@@ -196,6 +196,7 @@ in import nmtSrc {
     ./modules/programs/boxxy
     ./modules/programs/firefox/firefox.nix
     ./modules/programs/firefox/floorp.nix
+    ./modules/programs/firefox/zen-browser.nix
     ./modules/programs/foot
     ./modules/programs/freetube
     ./modules/programs/fuzzel
