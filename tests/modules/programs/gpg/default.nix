ceadcc27465e4d25fd8c3d1a15cdb16fa05cb5cf
Hoang Nguyen <folliekazetani@protonmail.com>
2024-11-02 00:00:00 +0700

    gpg: add dirmngrSettings and gpgsmSettings options

--- tests/modules/programs/gpg/default.nix
+++ tests/modules/programs/gpg/default.nix
@@ -3,4 +3,5 @@
   gpg-mutable-keyfiles = ./mutable-keyfiles.nix;
   gpg-multiple-keys-trust = ./multiple-keys-trust.nix;
   gpg-override-defaults = ./override-defaults.nix;
+  gpg-other-settings = ./other-settings.nix;
 }
