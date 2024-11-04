d8615de008988e2a995f6dd5fa5e7858422bae6f
Dawid Dziurla <dawidd0811@gmail.com>
2024-11-04 11:36:05 +0100

    podman: install package if enabled and create config files

--- modules/services/podman-linux/default.nix
+++ modules/services/podman-linux/default.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
-
-{
+let
+  cfg = config.services.podman;
+  toml = pkgs.formats.toml { };
+in {
   meta.maintainers = with lib.hm.maintainers; [ bamhm182 n-hass ];
 
   imports =
@@ -8,10 +10,87 @@
 
   options.services.podman = {
     enable = lib.mkEnableOption "Podman, a daemonless container engine";
+
+    containersConf.settings = lib.mkOption {
+      type = toml.type;
+      default = { };
+      description = "containers.conf configuration";
+    };
+
+    storage.settings = lib.mkOption {
+      type = toml.type;
+      description = "storage.conf configuration";
+    };
+
+    registries = {
+      search = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ "docker.io" ];
+        description = ''
+          List of repositories to search.
+        '';
+      };
+
+      insecure = lib.mkOption {
+        default = [ ];
+        type = lib.types.listOf lib.types.str;
+        description = ''
+          List of insecure repositories.
+        '';
+      };
+
+      block = lib.mkOption {
+        default = [ ];
+        type = lib.types.listOf lib.types.str;
+        description = ''
+          List of blocked repositories.
+        '';
+      };
+    };
+
+    policy = lib.mkOption {
+      default = { };
+      type = lib.types.attrs;
+      example = lib.literalExpression ''
+        {
+          default = [ { type = "insecureAcceptAnything"; } ];
+          transports = {
+            docker-daemon = {
+              "" = [ { type = "insecureAcceptAnything"; } ];
+            };
+          };
+        }
+      '';
+      description = ''
+        Signature verification policy file.
+        If this option is empty the default policy file from
+        `skopeo` will be used.
+      '';
+    };
   };
 
-  config = lib.mkIf config.services.podman.enable {
+  config = lib.mkIf cfg.enable {
     assertions =
       [ (lib.hm.assertions.assertPlatform "podman" pkgs lib.platforms.linux) ];
+
+    home.packages = [ cfg.package ];
+
+    services.podman.storage.settings = {
+      storage.driver = lib.mkDefault "overlay";
+    };
+
+    xdg.configFile = {
+      "containers/policy.json".source = if cfg.policy != { } then
+        pkgs.writeText "policy.json" (builtins.toJSON cfg.policy)
+      else
+        "${pkgs.skopeo.policy}/default-policy.json";
+      "containers/registries.conf".source = toml.generate "registries.conf" {
+        registries = lib.mapAttrs (n: v: { registries = v; }) cfg.registries;
+      };
+      "containers/storage.conf".source =
+        toml.generate "storage.conf" cfg.storage.settings;
+      "containers/containers.conf".source =
+        toml.generate "containers.conf" cfg.containersConf.settings;
+    };
   };
 }
