04b689f957a0528cd98189168e064f1489237065
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-11-06 00:08:15 +0100

    vivid: add module

--- modules/modules.nix
+++ modules/modules.nix
@@ -248,6 +248,7 @@ let
     ./programs/vifm.nix
     ./programs/vim-vint.nix
     ./programs/vim.nix
+    ./programs/vivid.nix
     ./programs/vscode.nix
     ./programs/vscode/haskell.nix
     ./programs/pywal.nix
