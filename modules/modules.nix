2c901492bc98b04cee2cbe3b5e221cbf735c2383
hauskens <hauskens-git@disp.lease>
2024-11-29 20:44:26 +0100

    lazydocker: add module

--- modules/modules.nix
+++ modules/modules.nix
@@ -143,6 +143,7 @@ let
     ./programs/khard.nix
     ./programs/kitty.nix
     ./programs/kodi.nix
+    ./programs/lazydocker.nix
     ./programs/lazygit.nix
     ./programs/ledger.nix
     ./programs/less.nix
