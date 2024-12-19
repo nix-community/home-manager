1395379a7a36e40f2a76e7b9936cc52950baa1be
Robert Helgesson <robert@rycee.net>
2024-12-19 12:27:52 +0100

    home-manager: improve path handling when building news
    Fixes #6217

--- tests/integration/default.nix
+++ tests/integration/default.nix
@@ -10,6 +10,7 @@ let
     };
 
   tests = {
+    home-with-symbols = runTest ./standalone/home-with-symbols.nix;
     kitty = runTest ./standalone/kitty.nix;
     nixos-basics = runTest ./nixos/basics.nix;
     standalone-flake-basics = runTest ./standalone/flake-basics.nix;
