dddbc0896db3f7828ab6bccfe02cd434c2c54f8f
Yash Raj <work.velocity806@passinbox.com>
2024-12-30 02:33:02 +0530

    [module/services ] polkit-gnome : format with nixpkgs-fmt

--- modules/services/polkit-gnome.nix
+++ modules/services/polkit-gnome.nix
@@ -1,27 +1,28 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
+{ lib
+, pkgs
+, config
+, ...
+}:
+let
   inherit (lib) mkIf mkEnableOption mkOption mkPackageOption;
   cfg = config.services.polkit-gnome;
-in {
-  meta.maintainers = [];
+in
+{
+  meta.maintainers = [ ];
 
   options.services.polkit-gnome = {
     enable = mkEnableOption "polkit-gnome";
     package = mkPackageOption pkgs "polkit_gnome";
-    systemd = mkEnableOption "systemd service for polkit-gnome" // mkOption {default = true;};
+    systemd = mkEnableOption "systemd service for polkit-gnome" // mkOption { default = true; };
   };
 
   config = mkIf cfg.enable {
     systemd.user = mkIf cfg.systemd {
       services.polkit-gnome-authentication-agent-1 = {
         description = "polkit-gnome-authentication-agent-1";
-        wantedBy = ["graphical-session.target"];
-        wants = ["graphical-session.target"];
-        after = ["graphical-session.target"];
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
         serviceConfig = {
           Type = "simple";
           ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
