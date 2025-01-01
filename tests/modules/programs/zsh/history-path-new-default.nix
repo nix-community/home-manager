9fca4915873d488f267253b00998669697a26453
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-01-01 02:48:58 +0000

    zsh: improve histfile handling
    Previously, a stateVersion check for 20.03 determined whether or not the input to
    `programs.zsh.history.path` would be prepended with `$HOME`. However, this was not
    communicated in the documentation, which stated the version check determined whether
    the default histfile location would be in `programs.zsh.dotDir` or
    `home.homeDirectory`.
    
    The current change simplifies matters and brings path handling in-line with that of
    the preceding work on dotDir path handling. If a relative path is provided, it is
    parsed as being relative to `home.homeDirectory`. Both absolute and relative paths
    are supported, and are cleaned before being passed to other functions.
    
    Tests have been rewritten for the new logic, with case handling for reusability.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/zsh/history-path-new-default.nix
+++
@@ -1,9 +0,0 @@
-{
-  home.stateVersion = "20.03";
-  programs.zsh.enable = true;
-
-  nmt.script = ''
-    assertFileRegex home-files/.zshrc \
-      '^HISTFILE="${config.home.homeDirectory}/.zsh_history"$'
-  '';
-}
