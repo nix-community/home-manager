3e2c84d94f2f4eaddb7887c65125c40d2ce94ea2
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-01-01 02:48:58 +0000

    zsh: improve histfile handling
    Previously, a stateVersion check for 20.03 determined whether or not the input to
    `programs.zsh.history.path` would be prepended with `$HOME`. However, this was not
    communicated in the documentation, which stated the version check determined whether
    the default histfile location would be in `programs.zsh.dotDir` or
    `home.homeDirectory`.
    
    The current change simplifies matters and brings path handling in-line with that of
    the preceding work on dotDir path handling. If a relative path is provided, it is
    parsed as being relative to `home.homeDirectory`. Both absolute and relative paths
    are supported, and are cleaned before being passed to other functions.
    
    Tests have been rewritten for the new logic, with case handling for reusability.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/zsh/default.nix
+++ tests/modules/programs/zsh/default.nix
@@ -5,10 +5,9 @@
   zsh-dotdir-default = import ./dotdir.nix "default";
   zsh-dotdir-relative = import ./dotdir.nix "relative";
   zsh-history-ignore-pattern = ./history-ignore-pattern.nix;
-  zsh-history-path-new-custom = ./history-path-new-custom.nix;
-  zsh-history-path-new-default = ./history-path-new-default.nix;
-  zsh-history-path-old-default = ./history-path-old-default.nix;
-  zsh-history-path-old-custom = ./history-path-old-custom.nix;
+  zsh-history-path-absolute = import ./history-path.nix "absolute";
+  zsh-history-path-default = import ./history-path.nix "default";
+  zsh-history-path-relative = import ./history-path.nix "relative";
   zsh-history-substring-search = ./history-substring-search.nix;
   zsh-plugins = ./plugins.nix;
   zsh-prezto = ./prezto.nix;
