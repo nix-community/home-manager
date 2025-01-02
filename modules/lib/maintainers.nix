6948b7bc94c020d6292ef69f699cf37b9683eec9
Madeleine Sydney <lomiskiam@gmail.com>
2025-01-02 04:22:29 -0700

    mpdscribble: add module

--- modules/lib/maintainers.nix
+++ modules/lib/maintainers.nix
@@ -276,6 +276,12 @@
     github = "mifom";
     githubId = 23462908;
   };
+  msyds = {
+    name = "Madeleine Sydney Ślaga";
+    email = "65362461+msyds@users.noreply.github.com";
+    github = "msyds";
+    githubId = 65362461;
+  };
   nikp123 = {
     name = "nikp123";
     email = "nikp123@users.noreply.github.com";
