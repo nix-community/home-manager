6b8cea647349f53f2d57399535478001bebed053
hauskens <hauskens-git@disp.lease>
2025-01-05 18:03:51 +0100

    easyeffects: add option to import presets
    added options to import presets
    added tests
    added hausken as maintainer

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -2140,6 +2140,14 @@ in {
           you can use `lib.mkOrder` to specify the order of the content you want to insert.
         '';
       }
+      {
+        time = "2025-03-19T18:10:56+00:00";
+        condition = config.services.easyeffects.enable;
+        message = ''
+          The Easyeffects module now supports adding json formatted presets
+          under '$XDG_CONFIG_HOME/easyeffects/{input,output}/'.
+        '';
+      }
     ];
   };
 }
