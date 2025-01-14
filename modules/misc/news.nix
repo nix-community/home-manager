4481a16d1ac5bff4a77c608cefe08c9b9efe840d
Ismaïl Senhaji <134528+iSma@users.noreply.github.com>
2025-01-14 17:52:56 +0100

    yazi: improve fish integration
    Calls yazi as `command yazi`, allowing to use "yazi" as
    `shellWrapperName`. Also defines the wrapper with
    `programs.fish.functions` instead of `interactiveShellInit`.

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1960,6 +1960,19 @@ in {
           as well as wf-shell.
         '';
       }
+
+      {
+        time = "2025-01-21T17:28:13+00:00";
+        condition = with config.programs.yazi; enable && enableFishIntegration;
+        message = ''
+          Yazi's fish shell integration wrapper now calls the 'yazi' executable
+          directly, ignoring any shell aliases with the same name.
+
+          Your configuration may break if you rely on the wrapper calling a
+          'yazi' alias.
+        '';
+      }
     ];
   };
 }
+
