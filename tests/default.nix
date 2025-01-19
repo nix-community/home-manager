f8bc52c87f27ad364579c06a38a19d7633be2039
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-01-19 17:44:05 +0000

    mods: add basic tests

--- tests/default.nix
+++ tests/default.nix
@@ -189,6 +189,7 @@ in import nmtSrc {
     ./modules/programs/mbsync
     ./modules/programs/micro
     ./modules/programs/mise
+    ./modules/programs/mods
     ./modules/programs/mpv
     ./modules/programs/mu
     ./modules/programs/mujmap
