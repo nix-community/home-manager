3934ad0bc63d9b8f08cd14535a48362c5e54f2a1
Robert Helgesson <robert@rycee.net>
2025-01-24 19:59:46 +0100

    home-manager: move tests into new test flake
    Having the tests available in the main Nix Flake introduces
    unnecessary evaluation for non-developer users and, worse, a
    dependency on the nmt library.
    
    Fixes #6354

--- flake.nix
+++ flake.nix
@@ -89,12 +89,6 @@
     } // (let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          tests = import ./tests { inherit pkgs; };
-        in tests.run);
-
       formatter = forAllSystems (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in pkgs.linkFarm "format" [{
@@ -105,23 +99,12 @@
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          lib = pkgs.lib;
           releaseInfo = nixpkgs.lib.importJSON ./release.json;
           docs = import ./docs {
             inherit pkgs;
             inherit (releaseInfo) release isReleaseBranch;
           };
           hmPkg = pkgs.callPackage ./home-manager { path = "${./.}"; };
-
-          testPackages = let
-            tests = import ./tests { inherit pkgs; };
-            renameTestPkg = n: lib.nameValuePair "test-${n}";
-          in lib.mapAttrs' renameTestPkg tests.build;
-
-          integrationTestPackages = let
-            tests = import ./tests/integration { inherit pkgs; };
-            renameTestPkg = n: lib.nameValuePair "integration-test-${n}";
-          in lib.mapAttrs' renameTestPkg tests;
         in {
           default = hmPkg;
           home-manager = hmPkg;
@@ -129,7 +112,7 @@
           docs-html = docs.manual.html;
           docs-json = docs.options.json;
           docs-manpages = docs.manPages;
-        } // testPackages // integrationTestPackages);
+        });
 
       defaultPackage = forAllSystems (system: self.packages.${system}.default);
     });
