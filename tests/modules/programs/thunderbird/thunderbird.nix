625f0220833fd8cd2cc2a6e0d64c69885ff4a427
Austin Horstman <khaneliman12@gmail.com>
2025-01-29 16:22:06 -0600

    Revert "thunderbird: add native host support (#6292)"
    This reverts commit 608b26d16ee69384580b1b14ac947d81d0240beb.

--- tests/modules/programs/thunderbird/thunderbird.nix
+++ tests/modules/programs/thunderbird/thunderbird.nix
@@ -41,9 +41,6 @@
     # Disable warning so that platforms' behavior is the same
     darwinSetupWarning = false;
 
-    # Darwin doesn't support wrapped Thunderbird, using unwrapped instead
-    package = pkgs.thunderbird-unwrapped;
-
     profiles = {
       first = {
         isDefault = true;
@@ -65,31 +62,18 @@
       };
     };
 
-    nativeMessagingHosts = with pkgs;
-      [
-        # NOTE: this is not a real Thunderbird native host module but Firefox; no
-        # native hosts are currently packaged for nixpkgs or elsewhere, so we
-        # have to improvise. Packaging wise, Firefox and Thunderbird native hosts
-        # are identical though. Good news is that the test will still pass as
-        # long as we don't attempt to run the mail client itself with the host.
-        # (Which we don't.)
-        browserpass
-      ];
-
     settings = {
       "general.useragent.override" = "";
       "privacy.donottrackheader.enabled" = true;
     };
   };
 
+  test.stubs.thunderbird = { };
+
   nmt.script = let
     isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
     configDir = if isDarwin then "Library/Thunderbird" else ".thunderbird";
     profilesDir = if isDarwin then "${configDir}/Profiles" else "${configDir}";
-    nativeHostsDir = if isDarwin then
-      "Library/Mozilla/NativeMessagingHosts"
-    else
-      ".mozilla/native-messaging-hosts";
     platform = if isDarwin then "darwin" else "linux";
   in ''
     assertFileExists home-files/${configDir}/profiles.ini
@@ -111,7 +95,5 @@
     assertFileExists home-files/${profilesDir}/first/chrome/userContent.css
     assertFileContent home-files/${profilesDir}/first/chrome/userContent.css \
       <(echo "* { color: red !important; }")
-
-    assertFileExists home-files/${nativeHostsDir}/com.github.browserpass.native.json
   '';
 }
