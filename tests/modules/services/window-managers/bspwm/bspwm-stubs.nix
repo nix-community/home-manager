c4f4b1e2fa335844aaa98d65066a5a434fa9ce22
Austin Horstman <khaneliman12@gmail.com>
2025-01-30 18:50:50 -0600

    bspwm-stubs: add stubs for bspwm tests
    Reducing closure from 500MB to 357MB

---
+++ tests/modules/services/window-managers/bspwm/bspwm-stubs.nix
@@ -0,0 +1,13 @@
+{ config, lib, ... }: {
+  # Avoid unnecessary downloads in CI jobs and/or make out paths constant, i.e.,
+  # not containing hashes, version numbers etc.
+  test.stubs = { bspwm = { }; };
+
+  nixpkgs.overlays = [
+    (_final: _prev: {
+      dbus = config.lib.test.mkStubPackage { name = "dbus"; };
+      systemd =
+        lib.makeOverridable (_attrs: config.lib.test.mkStubPackage { }) { };
+    })
+  ];
+}
