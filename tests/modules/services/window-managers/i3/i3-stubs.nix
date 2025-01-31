69053fb6abcba9ccd3baf1a3ad18c327352a2159
Austin Horstman <khaneliman12@gmail.com>
2025-01-30 18:38:43 -0600

    i3-stubs: add more stubs
    Reducing closure from 500MB to 357MB

--- tests/modules/services/window-managers/i3/i3-stubs.nix
+++ tests/modules/services/window-managers/i3/i3-stubs.nix
@@ -1,4 +1,4 @@
-{
+{ config, lib, ... }: {
   # Avoid unnecessary downloads in CI jobs and/or make out paths constant, i.e.,
   # not containing hashes, version numbers etc.
   test.stubs = {
@@ -16,4 +16,12 @@
 
     i3status = { };
   };
+
+  nixpkgs.overlays = [
+    (_final: _prev: {
+      dbus = config.lib.test.mkStubPackage { name = "dbus"; };
+      systemd =
+        lib.makeOverridable (_attrs: config.lib.test.mkStubPackage { }) { };
+    })
+  ];
 }
