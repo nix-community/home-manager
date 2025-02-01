18c649fb786a9cf0835cedcb59aa397eedb2e309
Oughie <montgalletsabinpantin@gmail.com>
2025-02-01 22:25:43 +0100

    clock-rs: add module
    clock-rs is a modern, digital clock that effortlessly runs in your terminal

--- tests/default.nix
+++ tests/default.nix
@@ -353,6 +353,7 @@ import nmtSrc {
       ./modules/programs/btop
       ./modules/programs/carapace
       ./modules/programs/cava
+      ./modules/programs/clock-rs
       ./modules/programs/cmus
       ./modules/programs/comodoro
       ./modules/programs/darcs
