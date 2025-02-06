5eb04fcfa887740925fbec109466a19dde5bcc70
Coutinho de Souza <dev@onemoresuza.com>
2025-02-06 21:50:03 +0000

    wayland: create tray.target if xsession is not enabled (#6332)
    Create a internal/read-only trayTarget option for the xsession, which is
    also used in wayland's config, if the former is not enabled.
    
    Remove all other definitions of `systemd.user.targets.tray`, i. e, the
    ones from the following modules: hyprland, sway, river and wayfire.

--- modules/services/window-managers/hyprland.nix
+++ modules/services/window-managers/hyprland.nix
@@ -312,12 +312,5 @@ in {
           [ "xdg-desktop-autostart.target" ];
       };
     };
-
-    systemd.user.targets.tray = {
-      Unit = {
-        Description = "Home Manager System Tray";
-        Requires = [ "graphical-session-pre.target" ];
-      };
-    };
   };
 }
