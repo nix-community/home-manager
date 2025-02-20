55844b1fc8e4f18c34410a71fd387a0ffad6dc03
David Thievon <pdkfan@gmail.com>
2025-02-20 19:10:05 +0100

    swayimg: add tests

--- tests/default.nix
+++ tests/default.nix
@@ -306,6 +306,7 @@ in import nmtSrc {
     ./modules/programs/rbw
     ./modules/programs/rofi
     ./modules/programs/rofi-pass
+    ./modules/programs/swayimg
     ./modules/programs/swaylock
     ./modules/programs/swayr
     ./modules/programs/terminator
