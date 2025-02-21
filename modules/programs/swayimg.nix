2453b30a68b0deb3d10b4a64c952b7cb249d80d3
Austin Horstman <khaneliman12@gmail.com>
2025-02-21 11:36:44 -0600

    swayimg: remove with lib

--- modules/programs/swayimg.nix
+++ modules/programs/swayimg.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.programs.swayimg;
   iniFormat = pkgs.formats.ini { };
@@ -9,21 +7,21 @@ in {
   meta.maintainers = with lib.maintainers; [ dod-101 ];
 
   options.programs.swayimg = {
-    enable = mkEnableOption "swayimg";
-    package = mkOption {
-      type = types.package;
+    enable = lib.mkEnableOption "swayimg";
+    package = lib.mkOption {
+      type = lib.types.package;
       default = pkgs.swayimg;
-      defaultText = literalExpression "pkgs.swayimg";
+      defaultText = lib.literalExpression "pkgs.swayimg";
       description = "The swayimg package to install";
     };
-    settings = mkOption {
+    settings = lib.mkOption {
       type = iniFormat.type;
       default = { };
       description = ''
         Configuration written to
         {file}`$XDG_CONFIG_HOME/swayimg/config`. See <https://github.com/artemsen/swayimg/blob/master/extra/swayimgrc> for a list of available options.
       '';
-      example = literalExpression ''
+      example = lib.literalExpression ''
         {
           viewer = {
             window = "#10000010";
@@ -40,14 +38,15 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     assertions = [
-      (hm.assertions.assertPlatform "programs.swayimg" pkgs platforms.linux)
+      (lib.hm.assertions.assertPlatform "programs.swayimg" pkgs
+        lib.platforms.linux)
     ];
 
     home.packages = [ cfg.package ];
 
-    xdg.configFile."swayimg/config" = mkIf (cfg.settings != { }) {
+    xdg.configFile."swayimg/config" = lib.mkIf (cfg.settings != { }) {
       source = iniFormat.generate "config" cfg.settings;
     };
   };
