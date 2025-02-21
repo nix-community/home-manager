3c82285348bc811b723014cf4dba2f87e7ffc885
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-02-21 10:48:51 -0300

    vinegar: add module (#6494)

--- modules/modules.nix
+++ modules/modules.nix
@@ -259,6 +259,7 @@ let
     ./programs/vifm.nix
     ./programs/vim-vint.nix
     ./programs/vim.nix
+    ./programs/vinegar.nix
     ./programs/vscode.nix
     ./programs/vscode/haskell.nix
     ./programs/pywal.nix
