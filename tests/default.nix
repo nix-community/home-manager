bdf73272a8408fedc7ca86d5ea47192f6d2dad54
David Thievon <131907205+DOD-101@users.noreply.github.com>
2025-02-21 19:15:02 +0100

    swayimg: add module (#6506)

--- tests/default.nix
+++ tests/default.nix
@@ -307,6 +307,7 @@ in import nmtSrc {
     ./modules/programs/rbw
     ./modules/programs/rofi
     ./modules/programs/rofi-pass
+    ./modules/programs/swayimg
     ./modules/programs/swaylock
     ./modules/programs/swayr
     ./modules/programs/terminator
