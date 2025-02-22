9f30068456dc7e43676a42520374f127c7ca9d34
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-02-22 09:15:44 -0500

    tests: test librewolf the same way firefox is tested
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- tests/default.nix
+++ tests/default.nix
@@ -291,6 +291,7 @@ in import nmtSrc {
     ./modules/programs/firefox
     ./modules/programs/firefox/firefox.nix
     ./modules/programs/firefox/floorp.nix
+    ./modules/programs/firefox/librewolf.nix
     ./modules/programs/foot
     ./modules/programs/freetube
     ./modules/programs/fuzzel
