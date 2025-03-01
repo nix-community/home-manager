b95cfa13c6423b9d5f43263c4d90418417d58bc8
Austin Horstman <khaneliman12@gmail.com>
2025-02-28 22:21:33 -0500

    tests: move xdg to cross platform tests
    Has cross platform functionality, gate linux only behind separate test
    path.

--- tests/modules/misc/xdg/default.nix
+++ tests/modules/misc/xdg/default.nix
@@ -1,13 +1,6 @@
 {
-  xdg-mime-apps-basics = ./mime-apps-basics.nix;
-  xdg-system-dirs = ./system-dirs.nix;
-  xdg-desktop-entries = ./desktop-entries.nix;
   xdg-file-gen = ./file-gen.nix;
   xdg-default-locations = ./default-locations.nix;
-  xdg-user-dirs-null = ./user-dirs-null.nix;
-  xdg-portal = ./portal.nix;
-  xdg-mime = ./mime.nix;
   xdg-mime-disabled = ./mime-disabled.nix;
-  xdg-mime-package = ./mime-packages.nix;
   xdg-autostart = ./autostart.nix;
 }
