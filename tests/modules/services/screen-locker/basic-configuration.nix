5871e21c1145f3f5e278545d1c09539b52183ccd
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-03-12 19:31:54 +0700

    screen-locker: add lockCmdEnv option (#6592)

--- tests/modules/services/screen-locker/basic-configuration.nix
+++ tests/modules/services/screen-locker/basic-configuration.nix
@@ -5,6 +5,7 @@
     enable = true;
     inactiveInterval = 5;
     lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c AA0000";
+    lockCmdEnv = [ "DISPLAY=:0" "XAUTHORITY=/custom/path/.Xauthority" ];
     xss-lock = { extraOptions = [ "-test" ]; };
     xautolock = {
       enable = true;
@@ -19,6 +20,8 @@
 
     assertFileExists $xssService
     assertFileRegex $xssService 'ExecStart=.*/bin/xss-lock.*-test.*i3lock -n -c AA0000'
+    assertFileRegex $xssService 'Environment=DISPLAY=:0'
+    assertFileRegex $xssService 'Environment=XAUTHORITY=/custom/path/.Xauthority'
     assertFileRegex $xssService 'Restart=always'
     assertFileExists $xautolockService
     assertFileRegex $xautolockService 'ExecStart=.*/bin/xautolock.*-time 5.*-detectsleep.*-test.*'
