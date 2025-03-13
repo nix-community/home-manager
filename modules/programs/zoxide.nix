44a40b3a6d63e8789cade7e0a3253b5773f23df2
Austin Horstman <khaneliman12@gmail.com>
2025-03-13 09:25:14 -0500

    zoxide: remove with lib

--- modules/programs/zoxide.nix
+++ modules/programs/zoxide.nix
@@ -1,30 +1,25 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
-
   cfg = config.programs.zoxide;
 
-  cfgOptions = concatStringsSep " " cfg.options;
-
+  cfgOptions = lib.concatStringsSep " " cfg.options;
 in {
   meta.maintainers = [ ];
 
   options.programs.zoxide = {
-    enable = mkEnableOption "zoxide";
+    enable = lib.mkEnableOption "zoxide";
 
-    package = mkOption {
-      type = types.package;
+    package = lib.mkOption {
+      type = lib.types.package;
       default = pkgs.zoxide;
-      defaultText = literalExpression "pkgs.zoxide";
+      defaultText = lib.literalExpression "pkgs.zoxide";
       description = ''
         Zoxide package to install.
       '';
     };
 
-    options = mkOption {
-      type = types.listOf types.str;
+    options = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
       default = [ ];
       example = [ "--no-cmd" ];
       description = ''
@@ -45,22 +40,24 @@ in {
       lib.hm.shell.mkZshIntegrationOption { inherit config; };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
 
-    programs.bash.initExtra = mkIf cfg.enableBashIntegration (mkOrder 2000 ''
-      eval "$(${cfg.package}/bin/zoxide init bash ${cfgOptions})"
-    '');
+    programs.bash.initExtra = lib.mkIf cfg.enableBashIntegration
+      (lib.mkOrder 2000 ''
+        eval "$(${cfg.package}/bin/zoxide init bash ${cfgOptions})"
+      '');
 
-    programs.zsh.initContent = mkIf cfg.enableZshIntegration (mkOrder 2000 ''
-      eval "$(${cfg.package}/bin/zoxide init zsh ${cfgOptions})"
-    '');
+    programs.zsh.initContent = lib.mkIf cfg.enableZshIntegration
+      (lib.mkOrder 2000 ''
+        eval "$(${cfg.package}/bin/zoxide init zsh ${cfgOptions})"
+      '');
 
-    programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration ''
+    programs.fish.interactiveShellInit = lib.mkIf cfg.enableFishIntegration ''
       ${cfg.package}/bin/zoxide init fish ${cfgOptions} | source
     '';
 
-    programs.nushell = mkIf cfg.enableNushellIntegration {
+    programs.nushell = lib.mkIf cfg.enableNushellIntegration {
       extraEnv = ''
         let zoxide_cache = "${config.xdg.cacheHome}/zoxide"
         if not ($zoxide_cache | path exists) {
