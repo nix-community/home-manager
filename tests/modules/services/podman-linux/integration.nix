bb72d79f5d319a11fd6226303ebf7a5dd1fd1913
Nicholas Hassan <nick@hassan.host>
2025-03-18 14:55:36 +1030

    podman: use type in attr name of built quadlets

--- tests/modules/services/podman-linux/integration.nix
+++ tests/modules/services/podman-linux/integration.nix
@@ -18,17 +18,17 @@
     containers = {
       "my-container" = {
         image = "my-img.image";
-        network = [ "my-net.network" "externalnet" ];
-        volumes = [ "my-vol.volume:/data" ];
+        network = [ "my-app.network" "externalnet" ];
+        volumes = [ "my-app.volume:/data" ];
       };
       "my-container-bld" = { image = "my-bld.build"; };
     };
     images."my-img" = { image = "docker.io/alpine:latest"; };
-    networks."my-net" = {
+    networks."my-app" = {
       gateway = "192.168.123.1";
       subnet = "192.168.123.0/24";
     };
-    volumes."my-vol" = {
+    volumes."my-app" = {
       device = "tmpfs";
       preserve = false;
       type = "tmpfs";
@@ -41,8 +41,8 @@
     containerFile=$configPath/podman-my-container.service
     containerBldFile=$configPath/podman-my-container-bld.service
     imageFile=$configPath/podman-my-img-image.service
-    networkFile=$configPath/podman-my-net-network.service
-    volumeFile=$configPath/podman-my-vol-volume.service
+    networkFile=$configPath/podman-my-app-network.service
+    volumeFile=$configPath/podman-my-app-volume.service
     assertFileExists $buildFile
     assertFileExists $containerFile
     assertFileExists $containerBldFile
