4207cd972233e1a6f04232ea496a018809c837d7
Austin Horstman <khaneliman12@gmail.com>
2025-03-19 13:52:47 -0500

    waylogout: remove with lib

--- modules/programs/waylogout.nix
+++ modules/programs/waylogout.nix
@@ -1,13 +1,10 @@
 { pkgs, config, lib, ... }:
-
-with lib;
-
 let cfg = config.programs.waylogout;
 in {
-  meta.maintainers = [ hm.maintainers.noodlez ];
+  meta.maintainers = [ lib.hm.maintainers.noodlez ];
 
   options.programs.waylogout = {
-    enable = mkOption {
+    enable = lib.mkOption {
       default = false;
       type = lib.types.bool;
       example = true;
@@ -16,10 +13,10 @@ in {
       '';
     };
 
-    package = mkPackageOption pkgs "waylogout" { };
+    package = lib.mkPackageOption pkgs "waylogout" { };
 
-    settings = mkOption {
-      type = with types; attrsOf (oneOf [ bool float int str ]);
+    settings = lib.mkOption {
+      type = with lib.types; attrsOf (oneOf [ bool float int str ]);
       default = { };
       description = ''
         Default arguments to {command}`waylogout`. An empty set
@@ -36,7 +33,7 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     assertions = [
       (lib.hm.assertions.assertPlatform "programs.waylogout" pkgs
         lib.platforms.linux)
@@ -44,8 +41,8 @@ in {
 
     home.packages = [ cfg.package ];
 
-    xdg.configFile."waylogout/config" = mkIf (cfg.settings != { }) {
-      text = concatStrings (mapAttrsToList (n: v:
+    xdg.configFile."waylogout/config" = lib.mkIf (cfg.settings != { }) {
+      text = lib.concatStrings (lib.mapAttrsToList (n: v:
         if v == false then
           ""
         else
