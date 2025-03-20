b2897270d9d434c2579cb5ef9b41db0e1c79f5ff
bricked <git@bricked.dev>
2025-03-20 18:08:40 +0100

    firefox: fix bookmarks backwards compatibility
    The legacy attrset option type for `firefox.profiles.<name>.bookmarks`
    was accidentally removed in 9d55428. This adds back support for this
    type by refactoring the bookmarks submodule. This also adds a new test
    ensuring this won't happen again.

--- modules/programs/firefox/profiles/bookmarks.nix
+++ modules/programs/firefox/profiles/bookmarks.nix
@@ -3,66 +3,9 @@
 with lib;
 
 let
-  bookmarkSubmodule = types.submodule ({ name, ... }: {
-    options = {
-      name = mkOption {
-        type = types.str;
-        default = name;
-        description = "Bookmark name.";
-      };
+  bookmarkTypes = import ./bookmark-types.nix { inherit lib; };
 
-      tags = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = "Bookmark tags.";
-      };
-
-      keyword = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Bookmark search keyword.";
-      };
-
-      url = mkOption {
-        type = types.str;
-        description = "Bookmark url, use %s for search terms.";
-      };
-    };
-  }) // {
-    description = "bookmark submodule";
-  };
-
-  bookmarkType = types.addCheck bookmarkSubmodule (x: x ? "url");
-
-  directoryType = types.submodule ({ name, ... }: {
-    options = {
-      name = mkOption {
-        type = types.str;
-        default = name;
-        description = "Directory name.";
-      };
-
-      bookmarks = mkOption {
-        type = types.listOf nodeType;
-        default = [ ];
-        description = "Bookmarks within directory.";
-      };
-
-      toolbar = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Make this the toolbar directory. Note, this does _not_
-          mean that this directory will be added to the toolbar,
-          this directory _is_ the toolbar.
-        '';
-      };
-    };
-  }) // {
-    description = "directory submodule";
-  };
-
-  nodeType = types.either bookmarkType directoryType;
+  inherit (bookmarkTypes) settingsType;
 
   bookmarksFile = bookmarks:
     let
@@ -141,8 +84,7 @@ in {
     };
 
     settings = mkOption {
-      type = with types;
-        coercedTo (attrsOf nodeType) attrValues (listOf nodeType);
+      type = settingsType;
       default = [ ];
       example = literalExpression ''
         [
