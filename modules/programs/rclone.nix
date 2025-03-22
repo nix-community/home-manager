7853236fae80bb625c319df8d730cad2a5584f26
Jess <jess@jessie.cafe>
2025-03-23 12:33:23 +1300

    rclone: ensure remotes have a type field

--- modules/programs/rclone.nix
+++ modules/programs/rclone.nix
@@ -17,7 +17,16 @@ in {
           options = {
             config = lib.mkOption {
               type = with lib.types;
-                attrsOf (nullOr (oneOf [ bool int float str ]));
+                let
+                  baseType = attrsOf (nullOr (oneOf [ bool int float str ]));
+
+                  # Should we verify whether type constitutes a valid remote?
+                  remoteConfigType = addCheck baseType (lib.hasAttr "type") // {
+                    name = "rcloneRemoteConfig";
+                    description =
+                      "An attribute set containing a remote type and options.";
+                  };
+                in remoteConfigType;
               default = { };
               description = ''
                 Regular configuration options as described in rclone's documentation
