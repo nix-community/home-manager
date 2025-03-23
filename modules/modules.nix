a863058feb7cd54e4cd7f5fd5e76a68ce2c4c197
Austin Horstman <khaneliman12@gmail.com>
2025-03-22 22:08:11 -0500

    skhd: add module

--- modules/modules.nix
+++ modules/modules.nix
@@ -398,6 +398,7 @@ let
     ./services/screen-locker.nix
     ./services/sctd.nix
     ./services/signaturepdf.nix
+    ./services/skhd.nix
     ./services/snixembed.nix
     ./services/spotifyd.nix
     ./services/ssh-agent.nix
