9bc685e5a888e76b7614e4e388c07fcc085e868d
Matthieu C. <886074+teto@users.noreply.github.com>
2025-03-24 03:32:20 +0100

    fixed gpgp

--- tests/modules/programs/neomutt/neomutt-with-gpg.nix
+++ tests/modules/programs/neomutt/neomutt-with-gpg.nix
@@ -17,7 +17,7 @@
   nmt.script = ''
     assertFileExists home-files/.config/neomutt/neomuttrc
     assertFileExists home-files/.config/neomutt/hm@example.com
-    assertFileContent home-files/.config/neomutt/neomuttrc ${
+    assertFileContent $(normalizeStorePaths home-files/.config/neomutt/neomuttrc) ${
       ./neomutt-expected.conf
     }
     assertFileContent home-files/.config/neomutt/hm@example.com ${
