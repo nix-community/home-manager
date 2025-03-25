d8b4ba070f5dfcb6c051c74fb31eafb58127580b
Bob van der Linden <bobvanderlinden@gmail.com>
2025-03-25 21:37:24 +0100

    mergiraf: init module (#6633)
    This adds git integration for mergiraf. It sets up git to use mergiraf
    to automatically resolve merge conflicts.

--- tests/default.nix
+++ tests/default.nix
@@ -131,6 +131,7 @@ let
       "lsd"
       "lieer"
       "mbsync"
+      "mergiraf"
       "micro"
       "mise"
       "mpv"
@@ -341,6 +342,7 @@ in import nmtSrc {
     ./modules/programs/lieer
     ./modules/programs/man
     ./modules/programs/mbsync
+    ./modules/programs/mergiraf
     ./modules/programs/micro
     ./modules/programs/mise
     ./modules/programs/mods
