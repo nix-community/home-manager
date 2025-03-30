8ce84337430492b984a7ddd73371773e8d19ad73
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-03-30 07:55:24 -0700

    granted: Add override for package (#6722)
    I would like to use the unstable version in my home-manager so let's make it possible to override.

--- modules/programs/granted.nix
+++ modules/programs/granted.nix
@@ -5,13 +5,14 @@ with lib;
 let
 
   cfg = config.programs.granted;
-  package = pkgs.granted;
 
 in {
   meta.maintainers = [ hm.maintainers.wcarlsen ];
 
   options.programs.granted = {
     enable = mkEnableOption "granted";
+
+    package = lib.mkPackageOption pkgs "granted" { };
 
     enableZshIntegration =
       lib.hm.shell.mkZshIntegrationOption { inherit config; };
@@ -21,19 +22,19 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ package ];
+    home.packages = [ cfg.package ];
 
     programs.zsh.initContent = mkIf cfg.enableZshIntegration ''
       function assume() {
         export GRANTED_ALIAS_CONFIGURED="true"
-        source ${package}/bin/assume "$@"
+        source ${cfg.package}/bin/assume "$@"
         unset GRANTED_ALIAS_CONFIGURED
       }
     '';
 
     programs.fish.functions.assume = mkIf cfg.enableFishIntegration ''
       set -x GRANTED_ALIAS_CONFIGURED "true"
-      source ${package}/share/assume.fish $argv
+      source ${cfg.package}/share/assume.fish $argv
       set -e GRANTED_ALIAS_CONFIGURED
     '';
   };
