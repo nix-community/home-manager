1e864a1178168df5cbf8a47222932ccb20100c57
Austin Horstman <khaneliman12@gmail.com>
2025-03-31 15:25:22 -0500

    gtk: refactor to support more modular customization
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/misc/gtk/gtk4-basic-settings.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "catppuccin-macchiato-blue-standard";
+      package = pkgs.catppuccin-gtk;
+    };
+    gtk4 = {
+      extraConfig = {
+        gtk-cursor-blink = false;
+        gtk-recent-files-limit = 20;
+      };
+    };
+  };
+
+  nmt.script =
+    let
+      gtk4Path = "home-files/.config/gtk-4.0";
+    in
+    ''
+      assertFileExists ${gtk4Path}/settings.ini
+
+      assertFileContent ${gtk4Path}/settings.ini \
+        ${./gtk4-basic-settings-expected.ini}
+
+      assertFileExists ${gtk4Path}/gtk.css
+    '';
+}
