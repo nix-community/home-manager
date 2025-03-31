d9915499e3080fe2afe1f717acabeb697dd21709
Austin Horstman <khaneliman12@gmail.com>
2025-03-31 15:25:22 -0500

    gtk: refactor to support more modular customization
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/misc/gtk/default.nix
+++ tests/modules/misc/gtk/default.nix
@@ -2,4 +2,5 @@
   gtk2-basic-config = ./gtk2-basic-config.nix;
   gtk2-config-file-location = ./gtk2-config-file-location.nix;
   gtk3-basic-settings = ./gtk3-basic-settings.nix;
+  gtk4-basic-settings = ./gtk4-basic-settings.nix;
 }
