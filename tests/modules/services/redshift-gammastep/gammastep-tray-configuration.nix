67eee4179650a59ee0691bd59fcad96aec815705
Evyatar Stalinsky <evyatar@stalinsky.xyz>
2025-04-05 15:04:54 +0300

    redshift/gammastep: add tray tests

---
+++ tests/modules/services/redshift-gammastep/gammastep-tray-configuration.nix
@@ -0,0 +1,25 @@
+{
+  services.gammastep = {
+    enable = true;
+    provider = "manual";
+    dawnTime = "6:00-7:45";
+    duskTime = "18:35-20:15";
+    settings = {
+      general = {
+        adjustment-method = "randr";
+        gamma = 0.8;
+      };
+      randr = { screen = 0; };
+    };
+    tray = true;
+  };
+
+  nmt.script = ''
+    assertFileContent \
+        home-files/.config/gammastep/config.ini \
+        ${./gammastep-basic-configuration-file-expected.conf}
+    assertFileContent \
+        home-files/.config/systemd/user/gammastep.service \
+        ${./gammastep-tray-configuration-expected.service}
+  '';
+}
