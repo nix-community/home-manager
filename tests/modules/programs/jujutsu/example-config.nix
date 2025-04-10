2942e2744626756595ef61b9607cfccc334860df
teto <886074+teto@users.noreply.github.com>
2025-04-11 00:28:57 +0200

    jujutsu: add ediff to the test

--- tests/modules/programs/jujutsu/example-config.nix
+++ tests/modules/programs/jujutsu/example-config.nix
@@ -7,6 +7,7 @@ in
   programs.jujutsu = {
     enable = true;
     package = config.lib.test.mkStubPackage { };
+    ediff = true;
     settings = {
       user = {
         name = "John Doe";
@@ -17,8 +18,12 @@ in
 
   nmt.script = ''
     assertFileExists 'home-files/${configDir}/jj/config.toml'
-    assertFileContent 'home-files/${configDir}/jj/config.toml' \
+    assertFileContent $(normalizeStorePaths 'home-files/${configDir}/jj/config.toml') \
       ${builtins.toFile "expected.toml" ''
+        [merge-tools.ediff]
+        merge-args = ["$left", "$right", "$base", "$output"]
+        program = "/nix/store/00000000000000000000000000000000-emacs-ediff/bin/emacs-ediff"
+
         [user]
         email = "jdoe@example.org"
         name = "John Doe"
