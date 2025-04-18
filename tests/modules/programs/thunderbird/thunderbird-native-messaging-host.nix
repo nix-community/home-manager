412eb166eb6725549c7e6b317eb48dc2bc648e39
Austin Horstman <khaneliman12@gmail.com>
2025-04-18 14:33:16 -0500

    tests/thunderbird: dont use realPkgs
    Not relevant for testing.

--- tests/modules/programs/thunderbird/thunderbird-native-messaging-host.nix
+++ tests/modules/programs/thunderbird/thunderbird-native-messaging-host.nix
@@ -1,24 +1,29 @@
 # Confirm that both Firefox and Thunderbird can be configured at the same time.
-{ lib, realPkgs, ... }:
-lib.recursiveUpdate (import ./thunderbird.nix { inherit lib realPkgs; }) {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+  nativeHostsDir =
+    if isDarwin then "Library/Mozilla/NativeMessagingHosts" else ".mozilla/native-messaging-hosts";
+in
+lib.recursiveUpdate (import ./thunderbird.nix { inherit config lib pkgs; }) {
   programs.thunderbird = {
-    nativeMessagingHosts = with realPkgs; [
-      # NOTE: this is not a real Thunderbird native host module but Firefox; no
-      # native hosts are currently packaged for nixpkgs or elsewhere, so we
-      # have to improvise. Packaging wise, Firefox and Thunderbird native hosts
-      # are identical though. The test doesn't care if the host was meant for
-      # either as long as the right paths are present in the package.
-      browserpass
+    nativeMessagingHosts = [
+      (config.lib.test.mkStubPackage {
+        name = "browserpass";
+        buildScript = ''
+          mkdir -p $out/lib/mozilla/native-messaging-hosts
+          echo test > $out/lib/mozilla/native-messaging-hosts/com.github.browserpass.native.json
+        '';
+      })
     ];
   };
 
-  nmt.script =
-    let
-      isDarwin = realPkgs.stdenv.hostPlatform.isDarwin;
-      nativeHostsDir =
-        if isDarwin then "Library/Mozilla/NativeMessagingHosts" else ".mozilla/native-messaging-hosts";
-    in
-    ''
-      assertFileExists home-files/${nativeHostsDir}/com.github.browserpass.native.json
-    '';
+  nmt.script = ''
+    assertFileExists home-files/${nativeHostsDir}/com.github.browserpass.native.json
+  '';
 }
