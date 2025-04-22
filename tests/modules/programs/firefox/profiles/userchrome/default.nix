f28afb0b77f18647395833d39280f4e76dd82f05
Austin Horstman <khaneliman12@gmail.com>
2025-04-22 13:15:13 -0500

    mkFirefoxModule: revert userChrome changes
    Reverting all the recent userChrome changes because of too many issues
    and bikeshedding.

--- tests/modules/programs/firefox/profiles/userchrome/default.nix
+++ tests/modules/programs/firefox/profiles/userchrome/default.nix
@@ -1,12 +1,15 @@
 modulePath:
-{ config, lib, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
 
   cfg = lib.getAttrFromPath modulePath config;
 
   firefoxMockOverlay = import ../../setup-firefox-mock-overlay.nix modulePath;
-
 in
 {
   imports = [ firefoxMockOverlay ];
@@ -18,36 +21,15 @@ in
         basic.isDefault = true;
         lines = {
           id = 1;
-          userChrome = ''
-            /*
-              This is a simple comment that should be written inside the `chrome/userChrome.css`
-            */
-
-            #urlbar {
-              min-width: none !important;
-              border: none !important;
-              outline: none !important;
-            }
-          '';
+          userChrome = builtins.readFile ./chrome/userChrome.css;
         };
-        path = {
+        file = {
           id = 2;
           userChrome = ./chrome/userChrome.css;
         };
-        folder = {
-          id = 3;
-          userChrome = ./chrome;
-        };
-        derivation = {
+        derivation-file = {
           id = 4;
-          userChrome = config.lib.test.mkStubPackage {
-            name = "wavefox";
-            buildScript = ''
-              mkdir -p $out
-              ln -s ${./chrome/userChrome.css} $out/userChrome.css
-              echo test > $out/README.md
-            '';
-          };
+          userChrome = pkgs.writeText "userChrome.css" (builtins.readFile ./chrome/userChrome.css);
         };
       };
     }
@@ -59,28 +41,18 @@ in
 
         assertDirectoryExists home-files/${cfg.configPath}/basic
 
-        assertPathNotExists \
-          home-files/${cfg.configPath}/lines/chrome/extraFile.css
         assertFileContent \
           home-files/${cfg.configPath}/lines/chrome/userChrome.css \
           ${./chrome/userChrome.css}
 
+        assertFileContent \
+          home-files/${cfg.configPath}/file/chrome/userChrome.css \
+          ${./chrome/userChrome.css}
+
         assertPathNotExists \
-          home-files/${cfg.configPath}/path/chrome/extraFile.css
+          home-files/${cfg.configPath}/derivation-file/chrome/extraFile.css
         assertFileContent \
-          home-files/${cfg.configPath}/path/chrome/userChrome.css \
-          ${./chrome/userChrome.css}
-
-        assertFileExists \
-          home-files/${cfg.configPath}/folder/chrome/extraFile.css
-        assertFileContent \
-          home-files/${cfg.configPath}/folder/chrome/userChrome.css \
-          ${./chrome/userChrome.css}
-
-        assertFileExists \
-          home-files/${cfg.configPath}/derivation/chrome/README.md
-        assertFileContent \
-          home-files/${cfg.configPath}/derivation/chrome/userChrome.css \
+          home-files/${cfg.configPath}/derivation-file/chrome/userChrome.css \
           ${./chrome/userChrome.css}
       '';
     }
