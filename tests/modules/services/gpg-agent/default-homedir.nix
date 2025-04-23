a4c3ce44fcd65849751caab203c2076b092e7069
Austin Horstman <khaneliman12@gmail.com>
2025-04-23 14:02:42 -0500

    gpg-agent: pinentryPackage -> pinentry.package and add pinentry.program`
    Alternative option for allowing a user to automatically configure what
    binary to use from a `pinentry` package. Previously, we always used
    `meta.mainProgram` but, there are packages that provide multiple
    binaries and this would allow flexibility for a user to override the
    default program used.

--- tests/modules/services/gpg-agent/default-homedir.nix
+++ tests/modules/services/gpg-agent/default-homedir.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  options,
   pkgs,
   ...
 }:
@@ -9,6 +10,20 @@ lib.mkIf pkgs.stdenv.isLinux {
   services.gpg-agent.enable = true;
   services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
   programs.gpg.enable = true;
+
+  test.asserts.warnings.expected =
+    let
+      renamed = {
+        pinentryPackage = "pinentry.package";
+      };
+    in
+    lib.mapAttrsToList (
+      old: new:
+      builtins.replaceStrings [ "\n" ] [ " " ] ''
+        The option `services.gpg-agent.${old}' defined in
+        ${lib.showFiles options.services.gpg-agent.${old}.files}
+        has been renamed to `services.gpg-agent.${new}'.''
+    ) renamed;
 
   nmt.script = ''
     in="${config.systemd.user.sockets.gpg-agent.Socket.ListenStream}"
