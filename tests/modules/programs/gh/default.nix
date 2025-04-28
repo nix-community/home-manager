5c54bc19daf5c29644f2f0cf1fb5e78c7d700bbf
Benedikt Rips <benedikt.rips@gmail.com>
2025-04-28 13:11:26 +0200

    gh: add `hosts` option

--- tests/modules/programs/gh/default.nix
+++ tests/modules/programs/gh/default.nix
@@ -2,5 +2,6 @@
   gh-config-file = ./config-file.nix;
   gh-credential-helper = ./credential-helper.nix;
   gh-extensions = ./extensions.nix;
+  gh-hosts-file = ./hosts-file.nix;
   gh-warnings = ./warnings.nix;
 }
