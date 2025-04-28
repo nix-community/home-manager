35312522a8b6bf3cb17c12b0b7113b43a7e670ea
Austin Horstman <khaneliman12@gmail.com>
2025-04-27 22:09:13 -0500

    pls: enableAliases -> enableShellIntegration
    Allow granular control of alias creation utilizing our shell integration
    options.

--- modules/programs/pls.nix
+++ modules/programs/pls.nix
@@ -13,9 +13,20 @@ let
     ls = "${cfg.package}/bin/pls";
     ll = "${cfg.package}/bin/pls -d perm -d user -d group -d size -d mtime -d git";
   };
-
 in
 {
+  imports =
+    let
+      msg = ''
+        'programs.pls.enableAliases' has been deprecated and replaced with integration
+        options per shell, for example, 'programs.pls.enableBashIntegration'.
+
+        Note, the default for these options is 'true' so if you want to enable the
+        aliases you can simply remove 'programs.pls.enableAliases' from your
+        configuration.'';
+    in
+    [ (lib.mkRemovedOptionModule [ "programs" "pls" "enableAliases" ] msg) ];
+
   meta.maintainers = [ lib.maintainers.arjan-s ];
 
   options.programs.pls = {
@@ -23,24 +34,28 @@ in
 
     package = lib.mkPackageOption pkgs "pls" { };
 
-    enableAliases = lib.mkEnableOption "recommended pls aliases";
+    enableBashIntegration = lib.hm.shell.mkBashIntegrationOption { inherit config; };
+
+    enableFishIntegration = lib.hm.shell.mkFishIntegrationOption { inherit config; };
+
+    enableZshIntegration = lib.hm.shell.mkZshIntegrationOption { inherit config; };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
 
-    programs.bash.shellAliases = mkIf cfg.enableAliases aliases;
+    programs.bash.shellAliases = mkIf cfg.enableBashIntegration aliases;
 
     programs.fish = lib.mkMerge [
       (mkIf (!config.programs.fish.preferAbbrs) {
-        shellAliases = mkIf cfg.enableAliases aliases;
+        shellAliases = mkIf cfg.enableFishIntegration aliases;
       })
 
       (mkIf config.programs.fish.preferAbbrs {
-        shellAbbrs = mkIf cfg.enableAliases aliases;
+        shellAbbrs = mkIf cfg.enableFishIntegration aliases;
       })
     ];
 
-    programs.zsh.shellAliases = mkIf cfg.enableAliases aliases;
+    programs.zsh.shellAliases = mkIf cfg.enableZshIntegration aliases;
   };
 }
