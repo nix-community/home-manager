76274a2130db36a6754f0bf262daac63f2afbd1e
Austin Horstman <khaneliman12@gmail.com>
2025-05-07 09:40:08 -0500

    tests/man: index.bt -> index.db
    Looks like the generated cache is now using index.db

--- tests/modules/programs/man/apropos.nix
+++ tests/modules/programs/man/apropos.nix
@@ -7,12 +7,8 @@
 
     nmt.script = ''
       assertFileExists home-files/.manpath
-
       CACHE_DIR=$(cat $TESTED/home-files/.manpath | cut --delimiter=' ' --fields=3)
-
-      if [[ ! -f "$CACHE_DIR/index.bt" ]]; then
-          fail "Expected man cache files to exist (in $CACHE_DIR) but they were not found."
-      fi
+      assertFileExists "$CACHE_DIR/index.db"
     '';
   };
 }
