882c47fa45574a91532fe06e9909d10ee89777be
awwpotato <awwpotato@voidq.com>
2025-05-10 21:40:44 -0700

    bacon: add prefs location to settings decription

--- modules/programs/bacon.nix
+++ modules/programs/bacon.nix
@@ -41,7 +41,8 @@ in
         };
       };
       description = ''
-        Bacon configuration.
+        Bacon configuration written to either {file}`Library/Application Support/org.dystroy.bacon/prefs.toml`
+        (darwin) or {file}`$XDG_CONFIG_HOME/bacon/prefs.toml`.
         For available settings see <https://dystroy.org/bacon/#global-preferences>.
       '';
     };
@@ -51,7 +52,7 @@ in
     home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
 
     home.file."${configDir}/prefs.toml" = lib.mkIf (cfg.settings != { }) {
-      source = settingsFormat.generate "prefs.toml" cfg.settings;
+      source = settingsFormat.generate "bacon-prefs" cfg.settings;
     };
   };
 }
