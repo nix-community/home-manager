de496c9ccb705ed76c1f23c2cad13e8970c37f0b
awwpotato <awwpotato@voidq.com>
2025-05-10 18:08:10 -0700

    television: add support for channels (#7026)

--- modules/programs/television.nix
+++ modules/programs/television.nix
@@ -18,6 +18,11 @@ in
     settings = lib.mkOption {
       type = tomlFormat.type;
       default = { };
+      description = ''
+        Configuration written to {file}`$XDG_CONFIG_HOME/television/config.toml`.
+        See <https://github.com/alexpasmantier/television/blob/main/.config/config.toml>
+        for the full list of options.
+      '';
       example = lib.literalExpression ''
         {
           tick_rate = 50;
@@ -31,10 +36,35 @@ in
           };
         }
       '';
+    };
+
+    channels = lib.mkOption {
+      type = lib.types.attrsOf tomlFormat.type;
+      default = { };
       description = ''
-        Configuration written to {file}`$XDG_CONFIG_HOME/television/config.toml`.
-        See <https://github.com/alexpasmantier/television/blob/main/.config/config.toml>
-        for the full list of options.
+        Each set of channels are written to
+        {file}`$XDG_CONFIG_HOME/television/NAME-channels.toml`
+
+        See <https://github.com/alexpasmantier/television/blob/main/docs/channels.md>
+        for options
+      '';
+      example = lib.literalExpression ''
+        {
+          my-custom = {
+            cable_channel = [
+              {
+                name = "git-log";
+                source_command = "git log --oneline --date=short --pretty=\"format:%h %s %an %cd\" \"$@\"";
+                preview_command = "git show -p --stat --pretty=fuller --color=always {0}";
+              }
+              {
+                name = "git-log";
+                source_command = "git log --oneline --date=short --pretty=\"format:%h %s %an %cd\" \"$@\"";
+                preview_command = "git show -p --stat --pretty=fuller --color=always {0}";
+              }
+            ];
+          };
+        }
       '';
     };
 
@@ -46,9 +76,19 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
 
-    xdg.configFile."television/config.toml" = lib.mkIf (cfg.settings != { }) {
-      source = tomlFormat.generate "config.toml" cfg.settings;
-    };
+    xdg.configFile = lib.mkMerge [
+      {
+        "television/config.toml" = lib.mkIf (cfg.settings != { }) {
+          source = tomlFormat.generate "config.toml" cfg.settings;
+        };
+      }
+      (lib.mapAttrs' (
+        name: value:
+        lib.nameValuePair "television/${name}-channels.toml" {
+          source = tomlFormat.generate "television-${name}-channels" value;
+        }
+      ) cfg.channels)
+    ];
 
     programs.bash.initExtra = lib.mkIf cfg.enableBashIntegration ''
       eval "$(${lib.getExe cfg.package} init bash)"
