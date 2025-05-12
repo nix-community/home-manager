0b24658ec09908e5a6515cacc54f29d589c8423b
awwpotato <awwpotato@voidq.com>
2025-05-12 11:08:49 -0700

    halloy: add themes option (#7043)

--- modules/programs/halloy.nix
+++ modules/programs/halloy.nix
@@ -10,11 +10,12 @@ let
     mkEnableOption
     mkPackageOption
     mkOption
+    types
     ;
 
   cfg = config.programs.halloy;
 
-  formatter = pkgs.formats.toml { };
+  tomlFormat = pkgs.formats.toml { };
 in
 {
   meta.maintainers = with lib.hm.maintainers; [ aguirre-matteo ];
@@ -23,7 +24,7 @@ in
     enable = mkEnableOption "halloy";
     package = mkPackageOption pkgs "halloy" { nullable = true; };
     settings = mkOption {
-      type = formatter.type;
+      inherit (tomlFormat) type;
       default = { };
       example = {
         "buffer.channel.topic".enabled = true;
@@ -38,12 +39,57 @@ in
         found here: <https://halloy.chat/configuration/index.html>.
       '';
     };
+    themes = mkOption {
+      type = types.attrsOf (
+        types.oneOf [
+          tomlFormat.type
+          types.lines
+          types.path
+        ]
+      );
+      default = { };
+      example = {
+        general = {
+          background = "<string>";
+          border = "<string>";
+          horizontal_rule = "<string>";
+          unread_indicator = "<string>";
+        };
+        text = {
+          primary = "<string>";
+          secondary = "<string>";
+          tertiary = "<string>";
+          success = "<string>";
+          error = "<string>";
+        };
+      };
+      description = ''
+        Each theme is written to {file}`$XDG_CONFIG_HOME/halloy/themes/NAME.toml`.
+        See <https://halloy.chat/configuration/themes/index.html> for more information.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
     home.packages = mkIf (cfg.package != null) [ cfg.package ];
-    xdg.configFile."halloy/config.toml" = mkIf (cfg.settings != { }) {
-      source = formatter.generate "halloy-config" cfg.settings;
-    };
+    xdg.configFile = lib.mkMerge [
+      {
+        "halloy/config.toml" = mkIf (cfg.settings != { }) {
+          source = tomlFormat.generate "halloy-config" cfg.settings;
+        };
+      }
+      (lib.mapAttrs' (
+        name: value:
+        lib.nameValuePair "halloy/themes/${name}.toml" {
+          source =
+            if lib.isString value then
+              pkgs.writeText "halloy-theme-${name}" value
+            else if builtins.isPath value || lib.isStorePath value then
+              value
+            else
+              tomlFormat.generate "halloy-theme-${name}" value;
+        }
+      ) cfg.themes)
+    ];
   };
 }
