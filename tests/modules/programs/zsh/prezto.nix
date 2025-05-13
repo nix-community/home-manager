5f36563a5cae05bcfbaa5e08a69d2e8d94242f61
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-05-13 23:20:40 +0100

    zsh: consider zsh.{profile,login,logout,env}Extra in prezto
    Right now the `zsh.prezto` module ignores the contents of the
    `zsh.{profile,login,logout,env}Extra` options, so it means that if you
    try to set, e.g., `zsh.profileExtra = "something";` this option will be
    (silently) ignored.
    
    This commit fixes another issue: since the main `zsh` module sets
    `home.file."${relToDotDir".zshenv"}".text` while `zsh.prezto` set the
    same file using `.source`, `zsh.prezto` would have priority so the
    environment variables from Home-Manager would not be loaded in non-NixOS
    systems. Now that we are using `.text` for both, the issue is fixed.

--- tests/modules/programs/zsh/prezto.nix
+++ tests/modules/programs/zsh/prezto.nix
@@ -1,5 +1,16 @@
 {
-  programs.zsh.prezto.enable = true;
+  programs.zsh = {
+    enable = true;
+    envExtra = "envExtra";
+    profileExtra = "profileExtra";
+    loginExtra = "loginExtra";
+    logoutExtra = "logoutExtra";
+    sessionVariables.FOO = "bar";
+    prezto = {
+      enable = true;
+      extraConfig = "configExtra";
+    };
+  };
 
   test.stubs = {
     zsh-prezto = {
@@ -10,11 +21,21 @@
         echo '# zlogin' > $out/share/zsh-prezto/runcoms/zlogin
         echo '# zlogout' > $out/share/zsh-prezto/runcoms/zlogout
         echo '# zshenv' > $out/share/zsh-prezto/runcoms/zshenv
+        echo '# zshrc' > $out/share/zsh-prezto/runcoms/zshrc
       '';
     };
   };
 
   nmt.script = ''
-    assertFileExists home-files/.zpreztorc
+    assertFileContains home-files/.zpreztorc 'configExtra'
+    assertFileContains home-files/.zprofile 'profileExtra'
+    assertFileContains home-files/.zlogin 'loginExtra'
+    assertFileContains home-files/.zlogout 'logoutExtra'
+    assertFileContains home-files/.zshenv 'envExtra'
+    # make sure we are loading the environment variables
+    assertFileContains home-files/.zshenv \
+      '. "/home/hm-user/.nix-profile/etc/profile.d/hm-session-vars.sh"'
+    assertFileContains home-files/.zshenv \
+      'export FOO="bar"'
   '';
 }
