93a2a6568672f4e524e34a628249fb69dfb557c3
Michael Hoang <enzime@users.noreply.github.com>
2025-05-13 14:43:51 +1000

    nix-darwin: use `launchctl asuser` now that activation runs as root
    https://github.com/nix-darwin/nix-darwin/pull/1341#discussion_r1989887502

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -19,7 +19,7 @@ in
       system.activationScripts.postActivation.text = lib.concatStringsSep "\n" (
         lib.mapAttrsToList (username: usercfg: ''
           echo Activating home-manager configuration for ${usercfg.home.username}
-          sudo -u ${usercfg.home.username} --set-home ${pkgs.writeShellScript "activation-${usercfg.home.username}" ''
+          launchctl asuser "$(id -u ${usercfg.home.username})" sudo -u ${usercfg.home.username} --set-home ${pkgs.writeShellScript "activation-${usercfg.home.username}" ''
             ${lib.optionalString (
               cfg.backupFileExtension != null
             ) "export HOME_MANAGER_BACKUP_EXT=${lib.escapeShellArg cfg.backupFileExtension}"}
