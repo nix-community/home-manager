34d44d7f1b7da16509538f72ec6bee121932a3e1
Damien Cassou <damien@cassou.me>
2025-05-15 07:48:19 +0200

    services.borgmatic: wrap systemd configuration within a isLinux condition

--- modules/services/borgmatic.nix
+++ modules/services/borgmatic.nix
@@ -30,57 +30,57 @@ in
     };
   };
 
-  config = lib.mkIf serviceConfig.enable {
-    assertions = [
-      (lib.hm.assertions.assertPlatform "services.borgmatic" pkgs lib.platforms.linux)
-    ];
+  config = lib.mkIf serviceConfig.enable (
+    lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isLinux {
+        systemd.user = {
+          services.borgmatic = {
+            Unit = {
+              Description = "borgmatic backup";
+              # Prevent borgmatic from running unless the machine is
+              # plugged into power:
+              ConditionACPower = true;
+            };
+            Service = {
+              Type = "oneshot";
 
-    systemd.user = {
-      services.borgmatic = {
-        Unit = {
-          Description = "borgmatic backup";
-          # Prevent borgmatic from running unless the machine is
-          # plugged into power:
-          ConditionACPower = true;
+              # Lower CPU and I/O priority:
+              Nice = 19;
+              IOSchedulingClass = "best-effort";
+              IOSchedulingPriority = 7;
+              IOWeight = 100;
+
+              Restart = "no";
+              LogRateLimitIntervalSec = 0;
+
+              # Delay start to prevent backups running during boot:
+              ExecStartPre = "${pkgs.coreutils}/bin/sleep 3m";
+
+              ExecStart = ''
+                ${pkgs.systemd}/bin/systemd-inhibit \
+                  --who="borgmatic" \
+                  --what="sleep:shutdown" \
+                  --why="Prevent interrupting scheduled backup" \
+                  ${programConfig.package}/bin/borgmatic \
+                    --stats \
+                    --verbosity -1 \
+                    --list \
+                    --syslog-verbosity 1
+              '';
+            };
+          };
+
+          timers.borgmatic = {
+            Unit.Description = "Run borgmatic backup";
+            Timer = {
+              OnCalendar = serviceConfig.frequency;
+              Persistent = true;
+              RandomizedDelaySec = "10m";
+            };
+            Install.WantedBy = [ "timers.target" ];
+          };
         };
-        Service = {
-          Type = "oneshot";
-
-          # Lower CPU and I/O priority:
-          Nice = 19;
-          IOSchedulingClass = "best-effort";
-          IOSchedulingPriority = 7;
-          IOWeight = 100;
-
-          Restart = "no";
-          LogRateLimitIntervalSec = 0;
-
-          # Delay start to prevent backups running during boot:
-          ExecStartPre = "${pkgs.coreutils}/bin/sleep 3m";
-
-          ExecStart = ''
-            ${pkgs.systemd}/bin/systemd-inhibit \
-              --who="borgmatic" \
-              --what="sleep:shutdown" \
-              --why="Prevent interrupting scheduled backup" \
-              ${programConfig.package}/bin/borgmatic \
-                --stats \
-                --verbosity -1 \
-                --list \
-                --syslog-verbosity 1
-          '';
-        };
-      };
-
-      timers.borgmatic = {
-        Unit.Description = "Run borgmatic backup";
-        Timer = {
-          OnCalendar = serviceConfig.frequency;
-          Persistent = true;
-          RandomizedDelaySec = "10m";
-        };
-        Install.WantedBy = [ "timers.target" ];
-      };
-    };
-  };
+      })
+    ]
+  );
 }
