559f6d36b32dd2180ebac40c522dd36290430fc5
Austin Horstman <khaneliman12@gmail.com>
2025-05-20 10:05:34 -0500

    news: add some more missing news items (#7097)
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ modules/misc/news/2025/03/2025-03-25_19-48-21.nix
@@ -0,0 +1,9 @@
+{
+  time = "2025-03-25T19:48:21-04:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.ripgrep-all`
+
+    Adds a module for ripgrep-all, a tool for searching text in files.
+  '';
+}
