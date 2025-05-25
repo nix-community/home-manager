bcfe9ce23f3e74b959f955525860e6ccc5336243
Clément <git@drawbu.dev>
2025-05-25 16:28:28 +0200

    mergiraf: use mergiraf cli to generate gitattributes
    Use gitattributes official supported languages list to generate
    gitattributes instead of using a wildcard, as advised in the
    [official documentation](https://mergiraf.org/usage.html). I we generate
    the git attributes, we can't relay on testing or they will break every
    so often

--- tests/modules/programs/mergiraf/basic-configuration.nix
+++ tests/modules/programs/mergiraf/basic-configuration.nix
@@ -4,6 +4,6 @@
 
   nmt.script = ''
     assertFileContent "home-files/.config/git/config" ${./mergiraf-git.conf}
-    assertFileContent "home-files/.config/git/attributes" ${./mergiraf-git-attributes.conf}
+    assertFileExists "home-files/.config/git/attributes"
   '';
 }
