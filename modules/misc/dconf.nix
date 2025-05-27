nix-community/home-manager

commit ec8205c3d7d4b19998d9b762d2c5abd2fc11faa7

dconf: set env var

modules/misc/dconf.nix
@@ -80,6 +80,7 @@ in
     {
       home.packages = [ pkgs.dconf ];
       dbus.packages = [ pkgs.dconf ];
+      home.sessionVariables.GIO_EXTRA_MODULES = "${pkgs.dconf.lib}/lib/gio/modules";
     }
     // lib.mkIf (cfg.settings != { }) {
       # Make sure the dconf directory exists.

commit 237f84319dfb67e5de5bb8e9c9f2328158a6486d

dconf: Fix Gio module variable breakage
In some setups, this would cause missing Gio modules that cause e.g.
Nautilus to not be able to view the XDG trash, and potentially other
issues.

Fixes: ec8205c3 ("dconf: set env var")
Fixes: #7143

modules/misc/dconf.nix
@@ -80,7 +80,9 @@ in
     (lib.mkIf cfg.enable {
       home.packages = [ pkgs.dconf ];
       dbus.packages = [ pkgs.dconf ];
-      home.sessionVariables.GIO_EXTRA_MODULES = "${pkgs.dconf.lib}/lib/gio/modules";
+      home.sessionVariablesExtra = ''
+        export GIO_EXTRA_MODULES="${pkgs.dconf.lib}/lib/gio/modules''${GIO_EXTRA_MODULES:+:}$GIO_EXTRA_MODULES"
+      '';
     })
     (lib.mkIf (cfg.enable && cfg.settings != { }) {
       # Make sure the dconf directory exists.

tests/modules/home-environment/session-variables.nix
@@ -9,7 +9,6 @@ let
     if [ -n "$__HM_SESS_VARS_SOURCED" ]; then return; fi
     export __HM_SESS_VARS_SOURCED=1
 
-    export GIO_EXTRA_MODULES="${pkgs.dconf}/lib/gio/modules"
     export LOCALE_ARCHIVE_2_27="${config.i18n.glibcLocales}/lib/locale/locale-archive"
     export V1="v1"
     export V2="v2-v1"
@@ -18,6 +17,7 @@ let
     export XDG_DATA_HOME="/home/hm-user/.local/share"
     export XDG_STATE_HOME="/home/hm-user/.local/state"
 
+    export GIO_EXTRA_MODULES="${pkgs.dconf}/lib/gio/modules''${GIO_EXTRA_MODULES:+:}$GIO_EXTRA_MODULES"
   '';
 
   darwinExpected = ''