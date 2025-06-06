74017e82ac441481176783f4918e2d097f13d43c
Egor Konovalov <73017521+egorkonovalov@users.noreply.github.com>
2025-06-06 23:29:38 +0200

    Update tests/modules/programs/mc/basic-configuration.nix
    Co-authored-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/mc/basic-configuration.nix
+++ tests/modules/programs/mc/basic-configuration.nix
@@ -2,16 +2,18 @@
   programs.mc = {
     enable = true;
 
-    extraConfig = ''
-      [Midnight-Commander]
-      skin=nicedark
-      show_hidden=true
-      auto_save_setup=true
-    '';
+    settings = {
+      "Midnight-Commander" = {
+         skin = "nicedark";
+         show_hidden = true;
+         auto_save_setup = true;
+      };
+    };
 
-    keymapConfig = ''
-      [panel]
-      Enter = Select
-    '';
+    keymapSettings = {
+      panel = {
+        Enter = "Select";
+      };
+    };
   };
 }
