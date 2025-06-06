56261e945705e175e7de8e80b0d0b495429313a0
Egor Konovalov <konovalov.ye@gmail.com>
2025-06-06 23:49:17 +0200

    fix formatting

--- tests/modules/programs/mc/basic-configuration.nix
+++ tests/modules/programs/mc/basic-configuration.nix
@@ -4,9 +4,9 @@
 
     settings = {
       "Midnight-Commander" = {
-         skin = "nicedark";
-         show_hidden = true;
-         auto_save_setup = true;
+        skin = "nicedark";
+        show_hidden = true;
+        auto_save_setup = true;
       };
     };
 
