eee140958aa1171183fad3dc8dc3f0cda6af6460
Robert Helgesson <robert@rycee.net>
2025-06-09 13:55:23 +0200

    home-manager: fix integration tests

--- tests/integration/standalone/home-with-symbols-init.nix
+++ tests/integration/standalone/home-with-symbols-init.nix
@@ -1,3 +1,5 @@
+{ config, pkgs, ... }:
+
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -11,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
