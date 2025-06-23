290a42cef8237d21a0084a3b9914ecb7f805a9aa
Austin Horstman <khaneliman12@gmail.com>
2025-06-22 21:33:59 -0500

    tests/yubikey-agent: reorganize darwin and linux
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/services/yubikey-agent/default.nix
+++ tests/modules/services/yubikey-agent/default.nix
@@ -1,5 +1,3 @@
 { lib, pkgs, ... }:
-
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-  yubikey-agent = ./service.nix;
-}
+(lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin (import ./darwin/default.nix))
+// (lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux (import ./linux/default.nix))
