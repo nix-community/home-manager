bf2f223691f7742eb6e7877e96332d799869d065
Austin Horstman <khaneliman12@gmail.com>
2025-06-22 21:41:50 -0500

    tests/nix-gc: reorganize darwin and linux
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/services/nix-gc-darwin/default.nix
+++
@@ -1,6 +0,0 @@
-{ lib, pkgs, ... }:
-
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
-  nix-gc = ./basic.nix;
-  darwin-nix-gc-interval-assertion = ./darwin-nix-gc-interval-assertion.nix;
-}
