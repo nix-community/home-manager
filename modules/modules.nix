47b4f85d7b531e10ac7ab487d15eab5d97d606ec
Perchun Pak <git@perchun.it>
2025-06-23 22:48:27 +0200

    programs/hyprpanel: init (#7303)
    (cherry picked from commit f5098b074051d1ae0e919adf434a60fc3f23347b)

--- modules/modules.nix
+++ modules/modules.nix
@@ -137,6 +137,7 @@ let
       ./programs/htop.nix
       ./programs/hyfetch.nix
       ./programs/hyprlock.nix
+      ./programs/hyprpanel.nix
       ./programs/i3bar-river.nix
       ./programs/i3blocks.nix
       ./programs/i3status-rust.nix
