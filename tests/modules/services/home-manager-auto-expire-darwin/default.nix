5618e7a748706dbe4f23ef11ec303070af7b5ea0
Austin Horstman <khaneliman12@gmail.com>
2025-06-22 21:38:58 -0500

    tests/home-manager-auto-expire: reorganize darwin and linux
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/services/home-manager-auto-expire-darwin/default.nix
+++
@@ -1,6 +0,0 @@
-{ lib, pkgs, ... }:
-
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
-  darwin-home-manager-autoExpire-service-basic-configuration = ./basic-configuration.nix;
-  darwin-home-manager-autoExpire-frequency-assertion = ./frequency-assertion.nix;
-}
--- tests/modules/services/home-manager-auto-expire/default.nix
+++ tests/modules/services/home-manager-auto-expire/default.nix
@@ -1,5 +1,3 @@
 { lib, pkgs, ... }:
-
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-  home-manager-auto-expire-basic-configuration = ./basic-configuration.nix;
-}
+(lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin (import ./darwin/default.nix))
+// (lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux (import ./linux/default.nix))
