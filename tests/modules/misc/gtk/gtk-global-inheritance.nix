853612ddfb7468464af5d1bedda07fe536663b37
Austin Horstman <khaneliman12@gmail.com>
2025-06-30 15:06:40 -0500

    tests/gtk: refactor and organize
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/misc/gtk/gtk-global-inheritance.nix
+++ tests/modules/misc/gtk/gtk-global-inheritance.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  # Test that global settings are inherited by all GTK versions
   gtk = {
     enable = true;
     font = {
@@ -25,33 +24,21 @@
   nmt.script = ''
     # Check GTK2 inherits global settings
     assertFileExists home-files/.gtkrc-2.0
-    assertFileRegex home-files/.gtkrc-2.0 'gtk-theme-name = "Adwaita-dark"'
-    assertFileRegex home-files/.gtkrc-2.0 'gtk-font-name = "Ubuntu 12"'
-    assertFileRegex home-files/.gtkrc-2.0 'gtk-icon-theme-name = "Adwaita"'
-    assertFileRegex home-files/.gtkrc-2.0 'gtk-cursor-theme-name = "Adwaita"'
-    assertFileRegex home-files/.gtkrc-2.0 'gtk-cursor-theme-size = 24'
+    assertFileContent home-files/.gtkrc-2.0 \
+      ${./gtk-global-inheritance-gtk2-expected.conf}
 
     # Check GTK3 inherits global settings
     assertFileExists home-files/.config/gtk-3.0/settings.ini
-    assertFileRegex home-files/.config/gtk-3.0/settings.ini 'gtk-theme-name=Adwaita-dark'
-    assertFileRegex home-files/.config/gtk-3.0/settings.ini 'gtk-font-name=Ubuntu 12'
-    assertFileRegex home-files/.config/gtk-3.0/settings.ini 'gtk-icon-theme-name=Adwaita'
-    assertFileRegex home-files/.config/gtk-3.0/settings.ini 'gtk-cursor-theme-name=Adwaita'
-    assertFileRegex home-files/.config/gtk-3.0/settings.ini 'gtk-cursor-theme-size=24'
+    assertFileContent home-files/.config/gtk-3.0/settings.ini \
+      ${./gtk-global-inheritance-gtk3-expected.ini}
 
     # Check GTK4 inherits global settings
     assertFileExists home-files/.config/gtk-4.0/settings.ini
-    assertFileRegex home-files/.config/gtk-4.0/settings.ini 'gtk-theme-name=Adwaita-dark'
-    assertFileRegex home-files/.config/gtk-4.0/settings.ini 'gtk-font-name=Ubuntu 12'
-    assertFileRegex home-files/.config/gtk-4.0/settings.ini 'gtk-icon-theme-name=Adwaita'
-    assertFileRegex home-files/.config/gtk-4.0/settings.ini 'gtk-cursor-theme-name=Adwaita'
-    assertFileRegex home-files/.config/gtk-4.0/settings.ini 'gtk-cursor-theme-size=24'
+    assertFileContent home-files/.config/gtk-4.0/settings.ini \
+      ${./gtk-global-inheritance-gtk4-expected.ini}
 
     # Check GTK4 CSS with theme import
     assertFileExists home-files/.config/gtk-4.0/gtk.css
-    assertFileRegex home-files/.config/gtk-4.0/gtk.css '@import url("file://.*/share/themes/Adwaita-dark/gtk-4.0/gtk.css")'
-
-    # Check packages are installed
-    # Package installation verified by home-manager
+    assertFileContent home-files/.config/gtk-4.0/gtk.css ${./gtk-global-inheritance-gtk4-css-expected.css}
   '';
 }
