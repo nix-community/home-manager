0e75b438c4b21aa43f4edf337e7896d87c77b97e
Austin Horstman <khaneliman12@gmail.com>
2025-07-05 00:08:39 -0500

    ci: split tests into chunks
    We have lots of tests and would like to add more. However, adding more
    testing coverage comes at the cost of a slower CI when we run them
    sequentially. This adds test outputs that are chunked however we'd like
    to tune for batch sizes. Allowing us to create a parallelized CI
    workflow.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/flake.nix
+++ tests/flake.nix
@@ -70,9 +70,47 @@
               };
             in
             lib.nameValuePair "test-all-no-big-ifd" tests.build.all;
+
+          # Create chunked test packages for better CI parallelization
+          testChunks =
+            let
+              tests = import ./. {
+                inherit pkgs;
+                # Disable big tests since this is only used for CI
+                enableBig = false;
+              };
+              allTests = lib.attrNames tests.build;
+              # Remove 'all' from the test list as it's a meta-package
+              filteredTests = lib.filter (name: name != "all") allTests;
+              # NOTE: Just a starting value, we can tweak this to find a good value.
+              targetTestsPerChunk = 250;
+              numChunks = lib.max 1 (
+                builtins.ceil ((builtins.length filteredTests) / (targetTestsPerChunk * 1.0))
+              );
+              chunkSize = builtins.ceil ((builtins.length filteredTests) / (numChunks * 1.0));
+
+              makeChunk =
+                chunkNum: testList:
+                let
+                  start = (chunkNum - 1) * chunkSize;
+                  end = lib.min (start + chunkSize) (builtins.length testList);
+                  chunkTests = lib.sublist start (end - start) testList;
+                  chunkAttrs = lib.genAttrs chunkTests (name: tests.build.${name});
+                in
+                pkgs.symlinkJoin {
+                  name = "test-chunk-${toString chunkNum}";
+                  paths = lib.attrValues chunkAttrs;
+                };
+            in
+            lib.listToAttrs (
+              lib.genList (
+                i: lib.nameValuePair "test-chunk-${toString (i + 1)}" (makeChunk (i + 1) filteredTests)
+              ) numChunks
+            );
         in
         testPackages
         // integrationTestPackages
+        // testChunks
         // (lib.listToAttrs [
           testAllNoBig
           testAllNoBigIfd
