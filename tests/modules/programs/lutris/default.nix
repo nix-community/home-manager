085335f7c103662fbe4d871f1b2cc0c61abbe7ab
Bikku <bikku+dev@slmail.me>
2025-07-10 10:56:28 +0200

    lutris: make module x86_64-linux only

--- tests/modules/programs/lutris/default.nix
+++ tests/modules/programs/lutris/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }:
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+lib.optionalAttrs (pkgs.stdenv.hostPlatform.isx86_64 && pkgs.stdenv.hostPlatform.isLinux) {
   lutris-runners = ./runners-configuration.nix;
   # lutris-wine = ./wine-configuration.nix;
   lutris-empty = ./empty.nix;
