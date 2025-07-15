e86a9d565b7d9035b5279f936856058919226dd2
Austin Horstman <khaneliman12@gmail.com>
2025-07-14 22:49:43 -0500

    tests/direnv: refactor tests
    Consolidate and increase testing scope.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/direnv/default.nix
+++ tests/modules/programs/direnv/default.nix
@@ -1,9 +1,3 @@
 {
-  direnv-bash = ./bash.nix;
-  direnv-mise = ./mise.nix;
-  direnv-nix-direnv = ./nix-direnv.nix;
-  direnv-nushell = ./nushell.nix;
-  direnv-stdlib = ./stdlib.nix;
-  direnv-stdlib-and-nix-direnv = ./stdlib-and-nix-direnv.nix;
-  direnv-silent = ./silent.nix;
+  direnv = ./basic-config.nix;
 }
