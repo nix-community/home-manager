0920ffa5713a3025f517b108d2108093b42df59f
Austin Horstman <khaneliman12@gmail.com>
2025-07-23 14:07:33 -0500

    news: add misc news entries for recent modules
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ modules/misc/news/2025/06/2025-06-10_03-33-16.nix
@@ -0,0 +1,11 @@
+{
+  time = "2025-06-10T03:33:16+00:00";
+  condition = true;
+  message = ''
+    A new module is available: 'programs.codex'.
+
+    This module provides configuration for OpenAI Codex, an AI system that
+    translates natural language to code. It powers GitHub Copilot and can
+    be used for code generation and completion tasks.
+  '';
+}
