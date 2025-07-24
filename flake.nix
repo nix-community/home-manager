aa124dfc4ec5b193ac363d8aff0bc5da42b23184
sodiboo <git@sodi.boo>
2025-07-24 02:30:05 +0200

    DROP THIS: use my branch of nixpkgs with the WIP kdl format, for now

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home Manager for Nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:sodiboo/nixpkgs/ergonomic-kdl";
 
   outputs =
     {
