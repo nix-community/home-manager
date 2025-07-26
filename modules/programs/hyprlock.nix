0daef6fbe0d71cd81967d9595a60c515a31ad034
Austin Horstman <khaneliman12@gmail.com>
2025-07-26 16:56:06 -0500

    hyprlock: update example config
    Some breaking changes from upstream
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- modules/programs/hyprlock.nix
+++ modules/programs/hyprlock.nix
@@ -59,10 +59,20 @@ in
       example = lib.literalExpression ''
         {
           general = {
-            disable_loading_bar = true;
-            grace = 300;
             hide_cursor = true;
-            no_fade_in = false;
+            ignore_empty_input = true;
+          };
+
+          animations = {
+            enabled = true;
+            fade_in = {
+              duration = 300;
+              bezier = "easeOutQuint";
+            };
+            fade_out = {
+              duration = 300;
+              bezier = "easeOutQuint";
+            };
           };
 
           background = [
