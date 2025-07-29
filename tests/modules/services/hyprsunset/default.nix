e7cf39f840aad85f91764b50d53eaa0654100d57
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-07-29 10:51:11 +0100

    hyprsunset: Add tests for transitons option
    Adds tests to ensure that the services are still created correctly and the correct deprecation warnings are shown
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/services/hyprsunset/default.nix
+++ tests/modules/services/hyprsunset/default.nix
@@ -3,4 +3,5 @@
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   hyprsunset-basic-configuration = ./basic-configuration.nix;
   hyprsunset-no-configuration = ./no-configuration.nix;
+  hyprsunset-transitions-deprecated = ./transitions-deprecated.nix;
 }
