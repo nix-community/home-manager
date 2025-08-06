4eeea7a68ff7ea6f64591561fd2f57f7fa8a801a
Austin Horstman <khaneliman12@gmail.com>
2025-08-05 22:35:50 -0500

    news: fix font config entry
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- modules/misc/news/2025/08/2025-08-05_14-17-36.nix
+++ modules/misc/news/2025/08/2025-08-05_14-17-36.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   time = "2025-08-05T19:17:36+00:00";
-  condition = config.fontconfig.enable;
+  condition = config.fonts.fontconfig.enable;
   message = ''
     The 'fontconfig' module now supports font rendering configuration.
 
