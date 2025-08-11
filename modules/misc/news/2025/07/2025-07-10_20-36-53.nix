9248ba7ce10d06ca46a17ff891892a2b7ac8cfce
Austin Horstman <khaneliman12@gmail.com>
2025-08-11 09:13:17 -0500

    news: add missing news entries for new modules
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ modules/misc/news/2025/07/2025-07-10_20-36-53.nix
@@ -0,0 +1,11 @@
+{
+  time = "2025-07-10T20:36:53+00:00";
+  condition = true;
+  message = ''
+    A new module is available: 'programs.docker-cli'.
+
+    The Docker CLI module provides configuration for the Docker command-line
+    interface, including aliases, completion settings, and credential helpers
+    for managing Docker containers and images.
+  '';
+}
