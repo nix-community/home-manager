afbcbf5101a55bcdac9e4eb543fe227d55da51b0
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-08-13 00:34:58 +0200

    glance: restart service when settings file changes

--- tests/modules/services/glance/default-settings.nix
+++ tests/modules/services/glance/default-settings.nix
@@ -4,6 +4,7 @@
   nmt.script = ''
     configFile=home-files/.config/glance/glance.yml
     serviceFile=home-files/.config/systemd/user/glance.service
+    serviceFile=$(normalizeStorePaths $serviceFile)
 
     assertFileContent $configFile ${./glance-default-config.yml}
     assertFileContent $serviceFile ${./glance.service}
