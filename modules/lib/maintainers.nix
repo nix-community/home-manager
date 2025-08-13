be8c9dca3d97c8b6403d9b5784c33b38bf86cb0b
Epix <kami@epix.icu>
2025-08-13 16:30:11 -0500

    maintainers: add epixtm

--- modules/lib/maintainers.nix
+++ modules/lib/maintainers.nix
@@ -145,6 +145,12 @@
     github = "elanora96";
     githubId = 27848938;
   };
+  epixtm = {
+    name = "Epix";
+    email = "kami@epix.icu";
+    github = "epixtm";
+    githubId = 168025607;
+  };
   fendse = {
     email = "46252070+Fendse@users.noreply.github.com";
     github = "Fendse";
