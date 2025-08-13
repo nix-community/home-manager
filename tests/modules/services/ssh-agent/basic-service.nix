94a238f9c1b84dbb299b938bee2436ce9633c3ed
Benedikt Rips <benedikt.rips@gmail.com>
2025-08-14 00:05:07 +0200

    ssh-agent: add option for the socket name

--- tests/modules/services/ssh-agent/basic-service.nix
+++ tests/modules/services/ssh-agent/basic-service.nix
@@ -1,15 +1,12 @@
-{ config, ... }:
-
 {
-  config = {
-    services.ssh-agent = {
-      enable = true;
-    };
-
-    nmt.script = ''
-      assertFileContent \
-        home-files/.config/systemd/user/ssh-agent.service \
-        ${./basic-service-expected.service}
-    '';
+  services.ssh-agent = {
+    enable = true;
+    socket = "ssh-agent/socket";
   };
+
+  nmt.script = ''
+    assertFileContent \
+      home-files/.config/systemd/user/ssh-agent.service \
+      ${./basic-service-expected.service}
+  '';
 }
