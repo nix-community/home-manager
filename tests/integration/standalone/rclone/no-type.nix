b793bf5ef61e2f92640c12f7d86365684fd3e790
Jess <jess@jessie.cafe>
2025-08-15 01:29:10 +1200

    rclone: modularize subtests

--- tests/integration/standalone/rclone/no-type.nix
+++ tests/integration/standalone/rclone/no-type.nix
@@ -1,9 +1,29 @@
+{ pkgs, ... }:
+let
+  module = pkgs.writeText "no-type-module" ''
+    {
+      programs.rclone.remotes = {
+        alices-cool-remote-v4.config = {
+          description = "this value does not have a type";
+          some-key = "value pairs";
+          another-key-value = "pair";
+        };
+      };
+    }
+  '';
+in
 {
-  programs.rclone.remotes = {
-    alices-cool-remote-v4.config = {
-      description = "this value does not have a type";
-      some-key = "value pairs";
-      another-key-value = "pair";
-    };
-  };
+  script = ''
+    with subtest("Un-typed remote"):
+      succeed_as_alice("install -m644 ${module} /home/alice/.config/home-manager/test-remote.nix")
+
+      actual = fail_as_alice("home-manager switch")
+      expected = "Activating createRcloneConfig"
+      assert expected not in actual, \
+        f"expected home-manager switch to contain {expected}, but got {actual}"
+
+      expected = "An attribute set containing a remote type and options."
+      assert expected not in actual, \
+        f"expected home-manager switch to contain {expected}, but got {actual}"
+  '';
 }
