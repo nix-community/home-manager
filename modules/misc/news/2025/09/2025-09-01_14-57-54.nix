a7a0f0ad7530959bdb797f6878726d2da85bfac7
GaspardCulis <gaspard@gasdev.fr>
2025-09-02 09:18:10 +0200

    news: Better phrasing for `services.swayosd.display` removal notice

--- modules/misc/news/2025/09/2025-09-01_14-57-54.nix
+++ modules/misc/news/2025/09/2025-09-01_14-57-54.nix
@@ -2,6 +2,6 @@
   time = "2025-09-01T12:57:54+00:00";
   condition = true;
   message = ''
-    The option 'services.swayosd.display' has been removed, it had no impact.
+    The option 'services.swayosd.display' has been removed, since the --display flag is no longer available in swayosd-server.".
   '';
 }
