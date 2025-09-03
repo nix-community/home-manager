a51a8f47eb95cf807ec589b922c61f7a59894ea6
GaspardCulis <gaspard@gasdev.fr>
2025-09-03 09:29:59 +0200

    news: Delete `services.anyrun.display` option removal entry Had a `mkRemovedOptionModule`

--- modules/misc/news/2025/09/2025-09-01_14-57-54.nix
+++
@@ -1,7 +0,0 @@
-{
-  time = "2025-09-01T12:57:54+00:00";
-  condition = true;
-  message = ''
-    The option 'services.swayosd.display' has been removed, since the --display flag is no longer available in swayosd-server.".
-  '';
-}
