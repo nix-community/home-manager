487a55d9b45db296af2a54e06dc06b532eb4a8c5
Anton Mosich <anton@mosich.at>
2025-09-05 00:16:37 +0200

    run formatter

--- modules/programs/pimsync/accounts.nix
+++ modules/programs/pimsync/accounts.nix
@@ -1,61 +1,103 @@
-{ lib, ... }:
+{ lib, config, ... }:
 
 let
   inherit (lib) types mkOption;
   dynamicType = types.nullOr (types.either types.str (types.listOf types.str));
-in {
+in
+{
   options.pimsync = {
     enable = lib.mkEnableOption "synchronization using pimsync";
 
     url = mkOption {
       type = dynamicType;
       default = null;
-      example = [ "cmd" "secret-store" "--get-caldav-url" ];
+      example = [
+        "cmd"
+        "secret-store"
+        "--get-caldav-url"
+      ];
       description = ""; # TODO: Description
     };
 
     userName = mkOption {
       type = dynamicType;
       default = null;
-      example = [ "cmd" "secret-store" "--get-caldav-username" ];
+      example = [
+        "cmd"
+        "secret-store"
+        "--get-caldav-username"
+      ];
       description = ""; # TODO: Description
     };
 
     password = mkOption {
       type = dynamicType;
       default = null;
-      example = [ "shell" "pass" "dav" "|" "head" "-1" ];
+      example = [
+        "shell"
+        "pass"
+        "dav"
+        "|"
+        "head"
+        "-1"
+      ];
+      description = ""; # TODO: Description
     };
 
-    collections = mkOption {
+    collection = mkOption {
       type = types.oneOf [
-        (types.enum [ "all" "fromLocal" "fromRemote" ])
-        (types.listOf types.str)
-        (types.listOf (types.attrsOf types.str))
+        (types.enum [
+          "all"
+          "fromLocal"
+          "fromRemote"
+        ])
+        (types.listOf (types.either types.str (types.attrsOf types.str)))
       ];
       default = "all";
-      example = [ "work" "school" ];
+      example = [
+        "work"
+        "school"
+      ];
       description = ""; # TODO: Description
     };
 
     conflictResolution = mkOption {
-      type = types.nullOr (types.oneOf [
-        (types.enum [ "keepLocal" "keepRemote" ])
-        (types.listOf types.str)
-      ]);
+      type = types.nullOr (
+        types.oneOf [
+          (types.enum [
+            "keepLocal"
+            "keepRemote"
+          ])
+          (types.listOf types.str)
+        ]
+      );
       default = null;
-      example = [ "cmd" "nvim" "-d" ];
+      example = [
+        "cmd"
+        "nvim"
+        "-d"
+      ];
       description = ""; # TODO: Description
     };
 
     onEmpty = mkOption {
-      type = types.nullOr (types.enum [ "skip" "sync" ]);
+      type = types.nullOr (
+        types.enum [
+          "skip"
+          "sync"
+        ]
+      );
       default = null;
       description = ""; # TODO: Description
     };
 
     onDelete = mkOption {
-      type = types.nullOr (types.enum [ "skip" "sync" ]);
+      type = types.nullOr (
+        types.enum [
+          "skip"
+          "sync"
+        ]
+      );
       default = null;
       description = ""; # TODO: Description
     };
