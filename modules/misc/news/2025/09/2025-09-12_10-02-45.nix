69083b3cdd2e16825d928a800bf1812baad8b7a6
Austin Horstman <khaneliman12@gmail.com>
2025-09-12 08:35:46 -0500

    jjui: init module
    Add a module for configuring the jjui program.

---
+++ modules/misc/news/2025/09/2025-09-12_10-02-45.nix
@@ -0,0 +1,9 @@
+{
+  time = "2025-09-12T15:02:45+00:00";
+  condition = true;
+  message = ''
+    A new module is available: 'programs.jjui'
+
+    jjui is a terminal user interface for jujutsu version control system.
+  '';
+}
