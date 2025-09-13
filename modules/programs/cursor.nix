bfb0a02d600470946d80f51256d6edf56eeafc61
emaiax <emaiax@fastmail.com>
2025-09-13 17:42:51 -0300

    cursor: fir module settings with updated VSCode path helpers

--- modules/programs/cursor.nix
+++ modules/programs/cursor.nix
@@ -16,6 +16,9 @@ in
       package = pkgs.code-cursor;
       packageName = "code-cursor"; # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/co/code-cursor/package.nix#L43
 
+      # configDirectory = ".cursor";
+      # userDirectory = "Cursor";
+
       overridePaths = {
         mcp = ".cursor";
       };
