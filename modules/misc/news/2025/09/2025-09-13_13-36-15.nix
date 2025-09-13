35e858cea494312ba8f4e2cda2e38eb92eb5ff37
Aguirre Matteo <aguirre-matteo@proton.me>
2025-09-13 13:38:21 -0300

    news: add cudatext entry

---
+++ modules/misc/news/2025/09/2025-09-13_13-36-15.nix
@@ -0,0 +1,13 @@
+{
+  time = "2025-09-13T16:36:15+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.cudatext`
+
+    CudaText is a cross-platform text editor, written in Object Pascal.
+    It is open source project and can be used free of charge, even for business.
+    It starts quite fast: ~0.3 sec with ~30 plugins, on Linux on CPU Intel Core
+    i3 3GHz. It is extensible by Python add-ons: plugins, linters, code tree parsers,
+    external tools. Syntax parser is feature-rich, from EControl engine.
+  '';
+}
