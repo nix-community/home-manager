3b65f9138cc10463f206ad50e4d4c344e3ec945e
Austin Horstman <khaneliman12@gmail.com>
2025-09-17 00:01:05 -0500

    tests/oh-my-posh: expand test coverage
    Cover some more scenarios with the module.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/programs/oh-my-posh/assertion-theme-config-file.nix
@@ -0,0 +1,11 @@
+{
+  programs.oh-my-posh = {
+    enable = true;
+    useTheme = "jandedobbeleer";
+    configFile = "/etc/oh-my-posh/custom.json";
+  };
+
+  test.asserts.assertions.expected = [
+    "oh-my-posh: Only one of 'settings', 'useTheme', or 'configFile' can be configured at a time."
+  ];
+}
