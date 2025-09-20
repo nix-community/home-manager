e54a8204487f00d57625e4a88f3ac1fd08323309
Austin Horstman <khaneliman12@gmail.com>
2025-09-20 11:16:30 -0500

    tests/zsh: add coverage for plugin loading optimization
    Add test assertions to verify the new array-based plugin loading
    structure generates the expected shell code patterns.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/zsh/plugins.nix
+++ tests/modules/programs/zsh/plugins.nix
@@ -26,6 +26,14 @@ in
       assertFileRegex home-files/.zshrc '^path+="/home/hm-user/.zsh/plugins/mockPlugin"$'
       assertFileRegex home-files/.zshrc '^fpath+="/home/hm-user/.zsh/plugins/mockPlugin"$'
       assertFileRegex home-files/.zshrc '^fpath+=("/home/hm-user/.zsh/plugins/mockPlugin/share/zsh/site-functions" "/home/hm-user/.zsh/plugins/mockPlugin/share/zsh/vendor-completions")$'
+
+      # Test the optimized plugin loading structure
+      assertFileContains home-files/.zshrc '# Plugin loading optimization: use array and loop instead of individual conditionals'
+      assertFileContains home-files/.zshrc 'plugins=('
+      assertFileContains home-files/.zshrc '"mockPlugin/share/mockPlugin/mockPlugin.plugin.zsh"'
+      assertFileContains home-files/.zshrc 'for plugin in "''${plugins[@]}"; do'
+      assertFileContains home-files/.zshrc '[[ -f "/home/hm-user/.zsh/plugins/$plugin" ]] && source "/home/hm-user/.zsh/plugins/$plugin"'
+      assertFileContains home-files/.zshrc 'done'
     '';
   };
 }
