004753ae6b04c4b18aa07192c1106800aaacf6c3
saygo-png <saygo.mail@proton.me>
2025-09-24 22:11:51 +0200

    home-manager: add backup overwrite option
    When using the backupFileExtension option, if the backup file exists,
    the activation process fails. This adds an option to instead overwrite
    the old backup instead of failing.

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -28,6 +28,7 @@ in
               ${lib.optionalString (
                 cfg.backupFileExtension != null
               ) "export HOME_MANAGER_BACKUP_EXT=${lib.escapeShellArg cfg.backupFileExtension}"}
+              ${lib.optionalString cfg.overwriteBackup "export HOME_MANAGER_BACKUP_OVERWRITE=1"}
               ${lib.optionalString cfg.verbose "export VERBOSE=1"}
               exec ${usercfg.home.activationPackage}/activate --driver-version ${driverVersion} >&2
             ''}
