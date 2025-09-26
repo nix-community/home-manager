fb2ae64bed1ee4997a8e350bc18f4bc5bffa670c
Aguirre Matteo <aguirre-matteo@proton.me>
2025-09-26 11:47:56 -0300

    news: add airlift entry

---
+++ modules/misc/news/2025/09/2025-09-26_11-46-57.nix
@@ -0,0 +1,11 @@
+{
+  time = "2025-09-26T14:46:57+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.airlift`
+
+    Airlift is a Command Line Interface (CLI) tool designed to provide
+    a local development environment for Apache Airflow with a simple but
+    flexible interface. It is built on top of the official Airflow Helm Chart.
+  '';
+}
