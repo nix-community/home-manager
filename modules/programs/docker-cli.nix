06bcccd80e21b075c894edc2a31f676580ac6f83
will <will.bradshaw50@gmail.com>
2025-09-28 09:45:58 +1000

    docker-cli: add docker contexts support
    Adds docker-cli.contexts support. This allows declarative configuration
    of [docker
    contexts](https://docs.docker.com/engine/manage-resources/contexts/).

--- modules/programs/docker-cli.nix
+++ modules/programs/docker-cli.nix
@@ -32,6 +32,27 @@ in
       '';
     };
 
+    contexts = mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          freeformType = jsonFormat.type;
+        }
+      );
+      default = { };
+      example = lib.literalExpression ''
+        {
+          example = {
+            Metadata = { Description = "example1"; };
+            Endpoints.docker.Host = "unix://example2";
+          };
+        }
+      '';
+      description = ''
+        Attrset of docker context configurations keyed by context name. See:
+        <https://docs.docker.com/engine/manage-resources/contexts/
+      '';
+    };
+
     settings = mkOption {
       type = jsonFormat.type;
       default = { };
@@ -62,7 +83,20 @@ in
         "${cfg.configDir}/config.json" = {
           source = jsonFormat.generate "config.json" cfg.settings;
         };
-      };
+      }
+      // lib.mapAttrs' (
+        n: ctx:
+        let
+          name = if ctx ? Name then ctx.Name else n;
+          path = "${cfg.configDir}/contexts/meta/${builtins.hashString "sha256" name}/meta.json";
+        in
+        {
+          name = path;
+          value = {
+            source = jsonFormat.generate "config.json" (ctx // { Name = name; });
+          };
+        }
+      ) cfg.contexts;
     };
   };
 }
