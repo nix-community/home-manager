20d9e7f095972444809af4ec4ec5491be62daa1b
Aguirre Matteo <aguirre-matteo@proton.me>
2025-09-29 16:24:32 -0300

    news: add aliae entry

---
+++ modules/misc/news/2025/09/2025-09-29_16-23-12.nix
@@ -0,0 +1,9 @@
+{
+  time = "2025-09-29T19:23:12+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.aliae`
+
+    Aliae is a cross shell and platform alias management tool.
+  '';
+}
