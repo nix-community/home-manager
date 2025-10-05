718a9af76ebe1e6a84c297d1a3948ed2c20899d7
Austin Horstman <khaneliman12@gmail.com>
2025-10-05 14:33:23 -0500

    tests/gcc: add tests
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/programs/gcc/with-colors.nix
@@ -0,0 +1,21 @@
+{
+  config = {
+    programs.gcc = {
+      enable = true;
+      colors = {
+        error = "01;31";
+        warning = "01;33";
+        note = "01;36";
+        caret = "01;32";
+        locus = "01";
+        quote = "01";
+      };
+    };
+
+    nmt.script = ''
+      hmEnvFile=home-path/etc/profile.d/hm-session-vars.sh
+      assertFileExists $hmEnvFile
+      assertFileRegex $hmEnvFile 'export GCC_COLORS="caret=01;32:error=01;31:locus=01:note=01;36:quote=01:warning=01;33"'
+    '';
+  };
+}
