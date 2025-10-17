b382b059efdc2f5867234cfd238d8d6f4403883a
Austin Horstman <khaneliman12@gmail.com>
2025-10-16 21:11:49 -0500

    tests/diff-so-fancy: add
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/programs/diff-so-fancy/diff-so-fancy-with-git-integration.nix
@@ -0,0 +1,23 @@
+{
+  programs.diff-so-fancy = {
+    enable = true;
+    enableGitIntegration = true;
+    markEmptyLines = false;
+    changeHunkIndicators = true;
+    stripLeadingSymbols = false;
+  };
+
+  programs.git.enable = true;
+
+  nmt.script = ''
+    assertFileExists home-files/.config/git/config
+    assertFileContains home-files/.config/git/config '[core]'
+    assertFileRegex home-files/.config/git/config 'pager = .*/diff-so-fancy.*less'
+    assertFileContains home-files/.config/git/config '[interactive]'
+    assertFileRegex home-files/.config/git/config 'diffFilter = .*/diff-so-fancy --patch'
+    assertFileContains home-files/.config/git/config '[diff-so-fancy]'
+    assertFileContains home-files/.config/git/config 'markEmptyLines = false'
+    assertFileContains home-files/.config/git/config 'changeHunkIndicators = true'
+    assertFileContains home-files/.config/git/config 'stripLeadingSymbols = false'
+  '';
+}
