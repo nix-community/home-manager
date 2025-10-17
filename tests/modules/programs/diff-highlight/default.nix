1cff2104fc15e7e9a0d38e1d059fcc6c0c39c10b
Austin Horstman <khaneliman12@gmail.com>
2025-10-16 21:11:43 -0500

    tests/diff-highlight: add
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/programs/diff-highlight/default.nix
@@ -0,0 +1,5 @@
+{
+  diff-highlight-basic = ./diff-highlight-basic.nix;
+  diff-highlight-with-git-integration = ./diff-highlight-with-git-integration.nix;
+  diff-highlight-migration = ./diff-highlight-migration.nix;
+}
