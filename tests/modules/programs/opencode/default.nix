c7feaecceb921025d73048edc1b8cdac67cc1c0d
Thierry Delafontaine <delafthi@pm.me>
2025-10-19 11:48:32 +0200

    opencode: make the `rules` option also accept a path

--- tests/modules/programs/opencode/default.nix
+++ tests/modules/programs/opencode/default.nix
@@ -1,7 +1,8 @@
 {
   opencode-settings = ./settings.nix;
   opencode-empty-settings = ./empty-settings.nix;
-  opencode-rules = ./rules.nix;
+  opencode-rules-inline = ./rules-inline.nix;
+  opencode-rules-path = ./rules-path.nix;
   opencode-empty-rules = ./empty-rules.nix;
   opencode-agents-inline = ./agents-inline.nix;
   opencode-commands-inline = ./commands-inline.nix;
