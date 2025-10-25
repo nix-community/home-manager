3aeedab2be24adfc6428c55a8365c19ca73407ae
Sami Haahtinen <ressu@ressukka.net>
2025-10-25 13:18:41 +0100

    home-manager-auto-upgrade: add flake support
    Adds the ability for the auto-upgrade service to update a Nix flake
    instead of Nix channels. Fixes #338
    
    This is controlled by a new `useFlake` boolean option. When enabled, the
    service will run `nix flake update` in the directory specified by the
    `flakeDir` option.
    
    `flakeDir` defaults to the standard Home Manager configuration directory
    (`~/.config/home-manager`), making this feature work out-of-the-box for
    most users. The path is passed to the upgrade script via an environment
    variable in the systemd service unit.

--- tests/modules/services/home-manager-auto-upgrade/default.nix
+++ tests/modules/services/home-manager-auto-upgrade/default.nix
@@ -2,4 +2,5 @@
 
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   home-manager-auto-upgrade-basic-configuration = ./basic-configuration.nix;
+  home-manager-auto-upgrade-flake-configuration = ./flake-configuration.nix;
 }
