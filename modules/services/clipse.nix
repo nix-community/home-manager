887f684ef460c86bb77e21734ab3954a869fca37
fk29g <fk29g@users.noreply.github.com>
2025-10-26 14:15:43 +0100

    clipse: fix keyBindings example syntax

--- modules/services/clipse.nix
+++ modules/services/clipse.nix
@@ -78,26 +78,26 @@ in
 
       example = lib.literalExpression ''
          {
-           "choose": "enter",
-           "clearSelected": "S",
-           "down": "down",
-           "end": "end",
-           "filter": "/",
-           "home": "home",
-           "more": "?",
-           "nextPage": "right",
-           "prevPage": "left",
-           "preview": "t",
-           "quit": "q",
-           "remove": "x",
-           "selectDown": "ctrl+down",
-           "selectSingle": "s",
-           "selectUp": "ctrl+up",
-           "togglePin": "p",
-           "togglePinned": "tab",
-           "up": "up",
-           "yankFilter": "ctrl+s"
-        }
+           "choose" = "enter";
+           "clearSelected" = "S";
+           "down" = "down";
+           "end" = "end";
+           "filter" = "/";
+           "home" = "home";
+           "more" = "?";
+           "nextPage" = "right";
+           "prevPage" = "left";
+           "preview" = "t";
+           "quit" = "q";
+           "remove" = "x";
+           "selectDown" = "ctrl+down";
+           "selectSingle" = "s";
+           "selectUp" = "ctrl+up";
+           "togglePin" = "p";
+           "togglePinned" = "tab";
+           "up" = "up";
+           "yankFilter" = "ctrl+s";
+        };
       '';
 
       description = "Custom key bindings";
