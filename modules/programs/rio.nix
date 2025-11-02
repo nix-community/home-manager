9d6e28fd32bf855d3f734ce5f2e4e2e2fc32212e
Hoang Nguyen <folliekazetani@protonmail.com>
2025-11-03 00:35:27 +0700

    rio: add support for custom themes

--- modules/programs/rio.nix
+++ modules/programs/rio.nix
@@ -28,6 +28,25 @@ in
         <https://raphamorim.io/rio/docs/#configuration-file> for options.
       '';
     };
+
+    themes = lib.mkOption {
+      type = with lib.types; attrsOf (either settingsFormat.type path);
+      default = { };
+      description = ''
+        Theme files written to {file}`$XDG_CONFIG_HOME/rio/themes/`. See
+        <https://rioterm.com/docs/config#building-your-own-theme> for
+        supported values.
+      '';
+      example = lib.literalExpression ''
+        {
+          foobar.colors = {
+            background = "#282a36";
+            green = "#50fa7b";
+            dim-green = "#06572f";
+          };
+        }
+      '';
+    };
   };
   meta.maintainers = [ lib.maintainers.otavio ];
 
@@ -41,6 +60,16 @@ in
       (lib.mkIf (cfg.settings != { }) {
         xdg.configFile."rio/config.toml".source =
           if lib.isPath cfg.settings then cfg.settings else settingsFormat.generate "rio.toml" cfg.settings;
+      })
+
+      (lib.mkIf (cfg.themes != { }) {
+        xdg.configFile = lib.mapAttrs' (
+          name: value:
+          lib.nameValuePair "rio/themes/${name}.toml" {
+            source =
+              if builtins.isPath value then value else settingsFormat.generate "rio-theme-${name}.toml" value;
+          }
+        ) cfg.themes;
       })
     ]
   );
