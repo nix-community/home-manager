3c8fd9a3a3dfc2576f023190054ee52291f5a9f2
Austin Horstman <khaneliman12@gmail.com>
2025-11-05 12:19:54 -0600

    Revert "tmpfiles: add option to purge rules' targets on change"
    This reverts commit b4350d54c2ec735bc841eb0f583eb889c8159fe9.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/misc/tmpfiles/default.nix
+++ tests/modules/misc/tmpfiles/default.nix
@@ -1,7 +1,6 @@
 {
   tmpfiles-no-rules = ./no-rules.nix;
   tmpfiles-basic-rules = ./basic-rules.nix;
-  tmpfiles-rules-with-purging = ./rules-with-purging.nix;
 
   tmpfiles-escaped-argument-warning = ./escaped-argument-warning.nix;
 }
