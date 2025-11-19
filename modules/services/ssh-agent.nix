a469b11f9e7dd4a89af6520427ddc78744d2c61f
Benedikt Rips <benedikt.rips@gmail.com>
2025-11-19 23:41:16 +0100

    ssh-agent: remove shell integration options
    It is crucial that `SSH_AUTH_SOCK` is set.

--- modules/services/ssh-agent.nix
+++ modules/services/ssh-agent.nix
@@ -36,14 +36,6 @@ in
         Set a default value for the maximum lifetime in seconds of identities added to the agent.
       '';
     };
-
-    enableBashIntegration = lib.hm.shell.mkBashIntegrationOption { inherit config; };
-
-    enableZshIntegration = lib.hm.shell.mkZshIntegrationOption { inherit config; };
-
-    enableFishIntegration = lib.hm.shell.mkFishIntegrationOption { inherit config; };
-
-    enableNushellIntegration = lib.hm.shell.mkNushellIntegrationOption { inherit config; };
   };
 
   config = lib.mkIf cfg.enable (
@@ -84,13 +76,13 @@ in
                 '';
           in
           {
-            bash.profileExtra = lib.mkIf cfg.enableBashIntegration (lib.mkBefore bashIntegration);
+            bash.profileExtra = lib.mkBefore bashIntegration;
 
-            zsh.envExtra = lib.mkIf cfg.enableZshIntegration (lib.mkBefore bashIntegration);
+            zsh.envExtra = lib.mkBefore bashIntegration;
 
-            fish.shellInit = lib.mkIf cfg.enableFishIntegration (lib.mkBefore fishIntegration);
+            fish.shellInit = lib.mkBefore fishIntegration;
 
-            nushell.extraConfig = lib.mkIf cfg.enableNushellIntegration (lib.mkBefore nushellIntegration);
+            nushell.extraConfig = lib.mkBefore nushellIntegration;
           };
       }
 
