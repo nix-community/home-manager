2217780c39169a9c77915200137550c2ef0fa974
Austin Horstman <khaneliman12@gmail.com>
2025-11-26 11:18:11 -0600

    flake.nix: lock to stable branch
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home Manager for Nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs =
     {
