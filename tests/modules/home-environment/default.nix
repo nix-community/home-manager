e7ff2ebc58ba883f0d30052ec55296c437088653
Matt Sturgeon <matt@sturgeon.me.uk>
2025-12-02 04:33:03 +0000

    home-environment: extend release check to include pkgs
    `lib` comes from the Nixpkgs used to instantiate Home Manager itself and
    cannot change within the module fixpoint. However, `pkgs` is configurable
    (via `nixpkgs.*` or `_module.args`) and may come from a different Nixpkgs
    instance from the one providing `lib`.
    
    Mismatches between Home Manager's release and the release of the `pkgs`
    instance are more common and also more likely to cause subtle issues.
    
    This change extends the release check to include `pkgs.lib.trivial.release`
    so that such mismatches can be detected and reported.

--- tests/modules/home-environment/default.nix
+++ tests/modules/home-environment/default.nix
@@ -2,4 +2,5 @@
   home-session-path = ./session-path.nix;
   home-session-search-variables = ./session-search-variables.nix;
   home-session-variables = ./session-variables.nix;
+  home-nixpkgs-release-check-pkgs = ./nixpkgs-release-check-pkgs.nix;
 }
