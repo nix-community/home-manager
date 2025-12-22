28218f1ece5836cb2fbec708904ea3a90ef766c0
Austin Horstman <khaneliman12@gmail.com>
2025-12-21 22:43:22 -0600

    tests/zsh: add more test cases for dotdir
    Adds tests for various dotDir configurations including trailing slashes
    and spaces.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/zsh/default.nix
+++ tests/modules/programs/zsh/default.nix
@@ -3,6 +3,13 @@
   zsh-aliases = ./aliases.nix;
   zsh-dotdir-absolute = import ./dotdir.nix "absolute";
   zsh-dotdir-default = import ./dotdir.nix "default";
+  zsh-dotdir-path-normalization-abs-no-slash = import ./dotdir.nix "abs-no-slash";
+  zsh-dotdir-path-normalization-abs-slash = import ./dotdir.nix "abs-slash";
+  zsh-dotdir-path-normalization-rel-no-slash = import ./dotdir.nix "rel-no-slash";
+  zsh-dotdir-path-normalization-rel-slash = import ./dotdir.nix "rel-slash";
+  zsh-dotdir-path-normalization-root-no-slash = import ./dotdir.nix "root-no-slash";
+  zsh-dotdir-path-normalization-root-slash = import ./dotdir.nix "root-slash";
+  zsh-dotdir-path-normalization-abs-space = import ./dotdir.nix "abs-space";
   zsh-dotdir-relative = import ./dotdir.nix "relative";
   zsh-dotdir-shell-variable = import ./dotdir.nix "shell-variable";
   zsh-history-ignore-pattern = ./history-ignore-pattern.nix;
