38e187fd2f9efac197e03be0c25f3ee215974144
Timothy Gallion <timbama@gmail.com>
2026-01-03 12:56:51 -0500

    podman-linux: fix tests expected results and modules
    Update the expected results to match changes in the podman generator
    and fix not importing the podman stub overlay with module import
    behind `mkIf` introduced in #6905.

--- tests/modules/services/podman-linux/build.nix
+++ tests/modules/services/podman-linux/build.nix
@@ -4,50 +4,50 @@
   pkgs,
   ...
 }:
-
-lib.mkIf config.test.enableLegacyIfd {
+{
   imports = [ ./podman-stubs.nix ];
+  config = lib.mkIf config.test.enableLegacyIfd {
+    services.podman = {
+      enable = true;
+      builds = {
+        "my-bld" = {
+          file =
+            let
+              containerFile = pkgs.writeTextFile {
+                name = "Containerfile";
+                text = ''
+                  FROM docker.io/alpine:latest
+                '';
+              };
+            in
+            "${containerFile}";
+        };
 
-  services.podman = {
-    enable = true;
-    builds = {
-      "my-bld" = {
-        file =
-          let
-            containerFile = pkgs.writeTextFile {
-              name = "Containerfile";
-              text = ''
-                FROM docker.io/alpine:latest
-              '';
-            };
-          in
-          "${containerFile}";
-      };
-
-      "my-bld-2" = {
-        file = "https://www.github.com/././Containerfile";
-        extraConfig = {
-          Build.ImageTag = [
-            "locahost/somethingelse"
-            "localhost/anothertag"
-          ];
+        "my-bld-2" = {
+          file = "https://www.github.com/././Containerfile";
+          extraConfig = {
+            Build.ImageTag = [
+              "locahost/somethingelse"
+              "localhost/anothertag"
+            ];
+          };
         };
       };
     };
+
+    test.asserts.assertions.expected = [
+      ''In 'my-bld-2' config. Build.ImageTag: '[ "locahost/somethingelse" "localhost/anothertag" ]' does not contain 'homemanager/my-bld-2'.''
+    ];
+
+    nmt.script = ''
+      configPath=home-files/.config/systemd/user
+      buildFile=$configPath/podman-my-bld-build.service
+
+      assertFileExists $buildFile
+
+      buildFile=$(normalizeStorePaths $buildFile)
+
+      assertFileContent $buildFile ${./build-expected.service}
+    '';
   };
-
-  test.asserts.assertions.expected = [
-    ''In 'my-bld-2' config. Build.ImageTag: '[ "locahost/somethingelse" "localhost/anothertag" ]' does not contain 'homemanager/my-bld-2'.''
-  ];
-
-  nmt.script = ''
-    configPath=home-files/.config/systemd/user
-    buildFile=$configPath/podman-my-bld-build.service
-
-    assertFileExists $buildFile
-
-    buildFile=$(normalizeStorePaths $buildFile)
-
-    assertFileContent $buildFile ${./build-expected.service}
-  '';
 }
