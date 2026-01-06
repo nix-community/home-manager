7b20b96390b0511cd4b50d63e6462cb67f57ca08
Robert Helgesson <robert@rycee.net>
2026-01-06 16:56:30 +0100

    ghostty: prevent service restarts
    Stopping or restarting the service will destroy open Ghostty sessions,
    potentially disrupting work. This change ensures that we leave the
    existing service untouched.
    
    Fixes #8485

--- tests/modules/programs/ghostty/default.nix
+++ tests/modules/programs/ghostty/default.nix
@@ -1,5 +1,10 @@
+{ lib, pkgs, ... }:
+
 {
-  ghostty-example-settings = ./example-settings.nix;
   ghostty-empty-settings = ./empty-settings.nix;
+  ghostty-example-settings = ./example-settings.nix;
   ghostty-example-theme = ./example-theme.nix;
+}
+// lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+  ghostty-systemd-service = ./systemd-service.nix;
 }
--- tests/modules/programs/ghostty/systemd-service.nix
+++ tests/modules/programs/ghostty/systemd-service.nix
@@ -3,7 +3,7 @@
   programs.ghostty = {
     enable = true;
     package = config.lib.test.mkStubPackage { outPath = null; };
-
+    systemd.enable = true;
     settings = {
       theme = "catppuccin-mocha";
       font-size = 10;
@@ -12,7 +12,14 @@
 
   nmt.script = ''
     servicePath=home-files/.config/systemd/user/app-com.mitchellh.ghostty.service
-    assertPathNotExists $servicePath
+    serviceOverridesPath=$servicePath.d/overrides.conf
+
+    assertFileExists $serviceOverridesPath
+    assertFileContent $serviceOverridesPath \
+      ${builtins.toFile "ghostty-service-overrides" ''
+        [Unit]
+        X-SwitchMethod=keep-old
+      ''}
 
     assertFileContent \
       home-files/.config/ghostty/config \
