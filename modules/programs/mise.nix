29223b1ad3ed753d4ff42de3e374a30bcc0b09f0
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2026-01-12 21:15:13 +0100

    mise: remove the `settings` option
    mise no longer supports the separate `settings.toml` file for settings.
    Settings should be defined in the global configuration (`globalConfig`).

--- modules/programs/mise.nix
+++ modules/programs/mise.nix
@@ -14,15 +14,20 @@ in
 
   imports =
     let
-      mkRemovedWarning =
+      mkRtxRemovedWarning =
         opt:
         (lib.mkRemovedOptionModule [ "programs" "rtx" opt ] ''
           The `rtx` package has been replaced by `mise`, please switch over to
           using the options under `programs.mise.*` instead.
         '');
-
     in
-    map mkRemovedWarning [
+    [
+      (lib.mkRenamedOptionModule
+        [ "programs" "mise" "settings" ]
+        [ "programs" "mise" "globalConfig" "settings" ]
+      )
+    ]
+    ++ map mkRtxRemovedWarning [
       "enable"
       "package"
       "enableBashIntegration"
@@ -47,38 +52,32 @@ in
       enableNushellIntegration = lib.hm.shell.mkNushellIntegrationOption { inherit config; };
 
       globalConfig = mkOption {
-        type = tomlFormat.type;
+        inherit (tomlFormat) type;
+
         default = { };
         example = lib.literalExpression ''
+          settings = {
+            disable_tools = [ "node" ];
+            experimental = true;
+            verbose = false;
+          };
+
+          tool_alias = {
+            node.versions = {
+              my_custom_node = "20";
+            };
+          };
+
           tools = {
             node = "lts";
             python = ["3.10" "3.11"];
-          };
-
-          aliases = {
-            my_custom_node = "20";
           };
         '';
         description = ''
           Config written to {file}`$XDG_CONFIG_HOME/mise/config.toml`.
 
-          See <https://mise.jdx.dev/configuration.html#global-config-config-mise-config-toml>
-          for details on supported values.
-        '';
-      };
-
-      settings = mkOption {
-        type = tomlFormat.type;
-        default = { };
-        example = lib.literalExpression ''
-          verbose = false;
-          experimental = false;
-          disable_tools = ["node"];
-        '';
-        description = ''
-          Settings written to {file}`$XDG_CONFIG_HOME/mise/settings.toml`.
-
-          See <https://mise.jdx.dev/configuration.html#settings-file-config-mise-settings-toml>
+          See <https://mise.jdx.dev/configuration.html> and
+          <https://mise.jdx.dev/configuration/settings.html>
           for details on supported values.
         '';
       };
@@ -108,10 +107,6 @@ in
     xdg.configFile = {
       "mise/config.toml" = mkIf (cfg.globalConfig != { }) {
         source = tomlFormat.generate "mise-config" cfg.globalConfig;
-      };
-
-      "mise/settings.toml" = mkIf (cfg.settings != { }) {
-        source = tomlFormat.generate "mise-settings" cfg.settings;
       };
     };
 
--- tests/modules/programs/mise/default.nix
+++ tests/modules/programs/mise/default.nix
@@ -1,6 +1,7 @@
 {
   mise-default-settings = ./default-settings.nix;
   mise-custom-settings = ./custom-settings.nix;
+  mise-custom-settings-renamed = ./custom-settings-renamed.nix;
   mise-bash-integration = ./bash-integration.nix;
   mise-zsh-integration = ./zsh-integration.nix;
   mise-fish-integration = ./fish-integration.nix;
