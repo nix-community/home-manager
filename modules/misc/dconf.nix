773170b44737c520c6f4cf6f606c65b0b21a603e
Manuel Transfeld <auipga@users.noreply.github.com>
2026-01-12 17:22:18 +0100

    dconf: remove bad words from enable.description

--- modules/misc/dconf.nix
+++ modules/misc/dconf.nix
@@ -58,7 +58,7 @@ in
         default = !pkgs.stdenv.hostPlatform.isDarwin;
         visible = false;
         description = ''
-          Whether to enable dconf settings.Add commentMore actions
+          Whether to enable dconf settings.
           Note, if you use NixOS then you must add
           `programs.dconf.enable = true`
           to your system configuration. Otherwise you will see a systemd error
