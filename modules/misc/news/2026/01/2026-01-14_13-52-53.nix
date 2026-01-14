5e109512bd64ce0ce0be2a4a7b4c4cefc4cdfa51
Aguirre Matteo <aguirre-matteo@proton.me>
2026-01-14 13:54:04 -0300

    news: add aperture entry

---
+++ modules/misc/news/2026/01/2026-01-14_13-52-53.nix
@@ -0,0 +1,10 @@
+{
+  time = "2026-01-14T16:52:53+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.aperture`
+
+    Aperture is a HTTP 402 reverse proxy that supports proxying requests for
+    gRPC (HTTP/2) and REST (HTTP/1 and HTTP/2) backends using the L402 Protocol Standard.
+  '';
+}
