bba859cd85b90dd9e4e6fd44b2af4aa64ae801a1
Jesung Yang <y.j3ms.n@gmail.com>
2026-01-15 14:32:42 +0900

    yazi: fix unintended recursive calls
    Use `command` (POSIX) and `^` (Nushell) to prevent recursive function
    calls when `cfg.shellWrapperName` is set to "yazi".
    
    Previously, if `cfg.shellWrapperName` was set to "yazi", the invocation
    of `yazi` within the shell integration function triggered the function
    itself instead of the binary. This name conflict prevents users from
    using the binary name as-is when shell integration is enabled.
    
    Hence, fix this by using shell-specific mechanisms to target the
    underlying executable, bypassing any name collisions.
    
    This aligns with the official documentation:
    - https://github.com/yazi-rs/yazi-rs.github.io/blob/2c839b37c80917ac3cf2d8c98949a392212d93ba/docs/quick-start.md?plain=1#L29
    - https://github.com/yazi-rs/yazi-rs.github.io/blob/2c839b37c80917ac3cf2d8c98949a392212d93ba/docs/quick-start.md?plain=1#L56

--- tests/modules/programs/yazi/bash-integration-enabled.nix
+++ tests/modules/programs/yazi/bash-integration-enabled.nix
@@ -10,7 +10,7 @@
     assertFileExists home-files/.bashrc
     assertFileContains home-files/.bashrc 'function yy() {'
     assertFileContains home-files/.bashrc 'local tmp="$(mktemp -t "yazi-cwd.XXXXX")"'
-    assertFileContains home-files/.bashrc 'yazi "$@" --cwd-file="$tmp"'
+    assertFileContains home-files/.bashrc 'command yazi "$@" --cwd-file="$tmp"'
     assertFileContains home-files/.bashrc 'if cwd="$(<"$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then'
     assertFileContains home-files/.bashrc 'builtin cd -- "$cwd"'
     assertFileContains home-files/.bashrc 'rm -f -- "$tmp"'
