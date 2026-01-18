5148e08046dc8c74c66b8aee4d302a47d6931b56
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-18 15:03:19 +0100

    vscode: fix argv.json path

---
+++ modules/misc/news/2026/01/2026-01-18_15-07-27.nix
@@ -0,0 +1,8 @@
+{
+  time = "2026-01-18T14:07:27+00:00";
+  condition = true;
+  message = ''
+    A new 'programs.vscode.argvSettings' option is available to configure VS
+    Code this creates the `argv.json` file.
+  '';
+}
