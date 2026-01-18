c5dfe4f6c1371549b16ea77956962675b3f6d057
Takumi <125882337+s0racat@users.noreply.github.com>
2026-01-18 20:56:51 +0900

    Fix condition for autoUpgrade in news file

--- modules/misc/news/2026/01/2026-01-13_10-11-50.nix
+++ modules/misc/news/2026/01/2026-01-13_10-11-50.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   time = "2026-01-13T01:11:50+00:00";
-  condition = config.home-manager.autoUpgrade.enable;
+  condition = config.services.home-manager.autoUpgrade.enable;
   message = ''
 
     Multiple new options are available:
