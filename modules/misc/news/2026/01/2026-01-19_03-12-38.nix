66233827127562267a3ee5aaff1a84fed74163ef
magicquark <198001825+magicquark@users.noreply.github.com>
2026-01-19 03:43:54 +0000

    pyradio: add module

---
+++ modules/misc/news/2026/01/2026-01-19_03-12-38.nix
@@ -0,0 +1,9 @@
+{
+  time = "2026-01-19T03:12:38+00:00";
+  condition = true;
+  message = ''
+    A new module `programs.pyradio` is available.
+
+    A curses based internet radio player.
+  '';
+}
