0b435f768e53db73402b52069ce75ce9c0ad0644
Austin Horstman <khaneliman12@gmail.com>
2026-01-22 19:28:28 -0600

    tests/neovim: add default/empty tests
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/neovim/default.nix
+++ tests/modules/programs/neovim/default.nix
@@ -7,4 +7,6 @@
   # waiting for a nixpkgs patch
   neovim-no-init = ./no-init.nix;
   neovim-extra-lua-init = ./extra-lua-init.nix;
+  neovim-extra-lua-default = ./extra-lua-default.nix;
+  neovim-extra-lua-empty-plugin = ./extra-lua-empty-plugin.nix;
 }
