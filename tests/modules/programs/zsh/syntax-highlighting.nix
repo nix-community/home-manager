407285ea027c1d8219c4a202667476ea916bea0e
tarzst <git.3myp3@slmail.me>
2026-01-25 15:11:44 +0530

    zsh/syntaxHighlighting: Set highlighters instead of ammending to allow disabling main

--- tests/modules/programs/zsh/syntax-highlighting.nix
+++ tests/modules/programs/zsh/syntax-highlighting.nix
@@ -7,6 +7,7 @@
       enable = true;
       package = pkgs.hello;
       highlighters = [
+        "main"
         "brackets"
         "pattern"
         "cursor"
@@ -18,7 +19,7 @@
 
   nmt.script = ''
     assertFileContains home-files/.zshrc "source ${pkgs.hello}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-    assertFileContains home-files/.zshrc "ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets pattern cursor)"
+    assertFileContains home-files/.zshrc "ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)"
     assertFileContains home-files/.zshrc "ZSH_HIGHLIGHT_STYLES[comment]='fg=#6c6c6c'"
   '';
 }
