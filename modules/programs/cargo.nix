bbdde5e7deef1734f39ffe9d6d1eaf8530135e71
N4CH723HR3R <n4ch7@n3831.net>
2026-01-26 16:03:34 +0100

    cargo: add package option

--- modules/programs/cargo.nix
+++ modules/programs/cargo.nix
@@ -5,10 +5,7 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption;
-
   tomlFormat = pkgs.formats.toml { };
-
   cfg = config.programs.cargo;
 in
 {
@@ -17,7 +14,9 @@ in
   options = {
     programs = {
       cargo = {
-        enable = mkEnableOption "management of cargo config";
+        enable = lib.mkEnableOption "management of cargo config";
+
+        package = lib.mkPackageOption pkgs "cargo" { nullable = true; };
 
         settings = lib.mkOption {
           inherit (tomlFormat) type;
@@ -33,6 +32,8 @@ in
 
   config = lib.mkIf cfg.enable {
     home = {
+      packages = lib.mkIf (cfg.package != null) [ cfg.package ];
+
       file = {
         ".cargo/config.toml" = {
           source = tomlFormat.generate "config.toml" cfg.settings;
