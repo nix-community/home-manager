c2b4212adfc9a95a70fb5be3a0e86b75faee6131
tarzst <git.3myp3@slmail.me>
2026-01-27 12:10:22 +0530

    zsh: add suffix aliases

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -156,6 +156,17 @@ in
           type = types.attrsOf types.str;
         };
 
+        shellSuffixAliases = mkOption {
+          default = { };
+          example = {
+            ps = "gv --";
+          };
+          description = ''
+            Suffix Aliases used when a file with a matching suffix is called
+            without any other commands.
+          '';
+        };
+
         dirHashes = mkOption {
           default = { };
           example = literalExpression ''
@@ -645,7 +656,7 @@ in
               ''
             ))
 
-            (lib.mkIf (aliasesStr != "" || cfg.shellGlobalAliases != { }) (
+            (lib.mkIf (aliasesStr != "" || cfg.shellGlobalAliases != { } || cfg.shellSuffixAliases != { }) (
               mkOrder 1100 (
                 (optionalString (aliasesStr != "") aliasesStr)
                 + (optionalString (cfg.shellGlobalAliases != { }) (
@@ -654,6 +665,14 @@ in
                     lib.mapAttrsToList (
                       k: v: "alias -g -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}"
                     ) cfg.shellGlobalAliases
+                  ))
+                ))
+                + (optionalString (cfg.shellSuffixAliases != { }) (
+                  optionalString (cfg.shellGlobalAliases != { } || cfg.shellAliases != { }) "\n"
+                  + (concatStringsSep "\n" (
+                    lib.mapAttrsToList (
+                      k: v: "alias -s -- ${lib.escapeShellArg k}=${lib.escapeShellArg v}"
+                    ) cfg.shellSuffixAliases
                   ))
                 ))
               )
