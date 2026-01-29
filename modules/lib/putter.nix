6ecb25fa778f8313efd7db73fcf98c4430250705
Robert Helgesson <robert@rycee.net>
2026-01-29 17:42:29 +0100

    fixup! WIP home-manager: use putter for file management

--- modules/lib/putter.nix
+++ modules/lib/putter.nix
@@ -5,9 +5,6 @@
 let
 
   inherit (lib)
-    concatMap
-    concatLists
-    mapAttrsToList
     hasPrefix
     filter
     ;
@@ -27,52 +24,18 @@ in
       fileEntries,
     }:
     let
-      # Convert a directory to a Putter configuration. Basically, this will
-      # create a file entry for each file in the directory. Any sub-directories
-      # will be handled recursively.
-      mkDirEntry =
-        f:
-        concatLists (
-          mapAttrsToList (
-            n: v:
-            let
-              f' = f // {
-                source = "${f.source}/${n}";
-                target = "${f.target}/${n}";
-              };
-            in
-            mkEntriesForType f' v
-          ) (builtins.readDir f.source)
-        );
-
-      mkEntriesForType =
-        f: t:
-        if t == "regular" || t == "symlink" then
-          mkFileEntry f
-        else if t == "directory" then
-          mkDirEntry f
-        else
-          throw "unexpected file type ${t}";
-
-      # Create a file entry for the given file.
-      mkFileEntry = f: [
-        {
-          collision.resolution = if f.force then "force" else "abort";
-          action.type = "symlink";
-          source = "${sourceBaseDirectory}/${f.target}";
-          target = (if hasPrefix "/" f.target then "" else "${targetBaseDirectory}/") + f.target;
-        }
-      ];
-
-      # Given a Home Manager file entry, produce a list of Putter entries. For
-      # recursive HM file entries, we recursively traverse the source directory
-      # and generate a Putter entry for each file we encounter.
-      mkEntries = f: if f.recursive then mkEntriesForType f "directory" else mkFileEntry f;
+      # Create a Putter entry for the given file.
+      mkEntry = f: {
+        collision.resolution = if f.force then "force" else "abort";
+        action.type = if f.recursive then "symlink_recursive" else "symlink";
+        source = "${sourceBaseDirectory}/${f.target}";
+        target = (if hasPrefix "/" f.target then "" else "${targetBaseDirectory}/") + f.target;
+      };
 
       putterJson = {
         version = "1";
         state = putterStatePath;
-        files = concatMap mkEntries (filter (f: f.enable) fileEntries);
+        files = map mkEntry (filter (f: f.enable) fileEntries);
       };
 
       putterJsonText = builtins.toJSON putterJson;
