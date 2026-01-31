5f347d27b3dd3287d4006a5563f701a1ce259107
Brenton Simpson <appsforartists@google.com>
2026-01-31 18:39:16 -0500

    material-icons: add module
    The [material-design-icons](https://github.com/google/material-design-icons)
    repository is massive.  Checking out the full repo is nearly impossible
    unless you have ample disk space, yet it contains many icons that would
    be very useful for theming.
    
    There are two attempts to package it in nixpkgs:
    
    - [material-icons](https://github.com/NixOS/nixpkgs/blob/nixos-25.11/pkgs/by-name/ma/material-icons/package.nix) is pinned to a tarball that hasn't been updated since 2020.
    - [material-symbols](https://github.com/NixOS/nixpkgs/blob/nixos-25.11/pkgs/by-name/ma/material-symbols/package.nix) includes more recent icons, but gets around the repo's size by importing only the font implementation, leaving the SVGs behind.
    
    This module takes a new approach.  Like `material-symbols`, it only
    imports the subset you use (using a blobless sparse checkout that only
    consumes ~1MB overhead in disk space).  However, it lets you specify
    which icons you're interested in, so you can use the SVGs right from
    `.nix-profile/share/icons`.  This makes it appropriate for inclusion in
    Home Manager.
    
    Since someone is already squatting on `material-design-icons` in
    `nixpkgs`, I followed the naming precedent of `material-icons`.
    
    Here's how you might use it:
    
    ```
    material-icons = {
      enable = true;
    	icons = {
    	  "scalable/launcher.svg" = {
    	    color = "white";
    	    path = "symbols/web/action_key/materialsymbolssharp/action_key_fill1_40px.svg";
    	  };
    	};
      hash = "sha256-uDJQ0UUBcEqfTri5qHzwUP0fXFIU1mzDAEm5OBtU/pc=";
    };
    ```

--- tests/default.nix
+++ tests/default.nix
@@ -184,6 +184,7 @@ import nmtSrc {
           ./modules/home-environment
           ./modules/misc/fontconfig
           ./modules/misc/manual
+          ./modules/misc/material-icons
           ./modules/misc/news
           ./modules/misc/nix
           ./modules/misc/nix-remote-build
---
+++ tests/modules/misc/material-icons/default.nix
@@ -0,0 +1,53 @@
+{
+  material-icons =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      launcherGitPath = "symbols/web/action_key/materialsymbolssharp/action_key_fill1_40px.svg";
+      disclosureTriangleGitPath = "symbols/web/arrow_right/materialsymbolssharp/arrow_right_24px.svg";
+    in
+    {
+      config = {
+        misc.material-icons = {
+          enable = true;
+          icons = {
+            "disclosure-triangle.svg" = disclosureTriangleGitPath;
+            "launcher.svg" = launcherGitPath;
+            "launcher-white.svg" = {
+              path = launcherGitPath;
+              color = "white";
+            };
+          };
+
+          groups = {
+            test-theme = {
+              color = "black";
+              icons = {
+                "launcher-black.svg" = launcherGitPath;
+                "disclosure-triangle-black.svg" = disclosureTriangleGitPath;
+              };
+            };
+          };
+          hash = "sha256-Aelq8eAw/cmbTB+j+QDM8XN1qap6aR2UeXuxktuK/rI=";
+        };
+
+        nmt.script = ''
+          assertFileExists home-path/share/icons/material/launcher.svg
+          assertFileExists home-path/share/icons/material/launcher-white.svg
+          assertFileExists home-path/share/icons/material/launcher-black.svg
+          assertFileExists home-path/share/icons/material/disclosure-triangle.svg
+          assertFileExists home-path/share/icons/material/disclosure-triangle-black.svg
+
+          assertFileRegex home-path/share/icons/material/launcher-white.svg '^<svg fill="white" '
+
+          assertFileContent home-path/share/icons/material/disclosure-triangle.svg ${pkgs.writeText "expected-disclosure-triangle-raw.svg" ''<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M400-280v-400l200 200-200 200Z"/></svg>''}
+
+          assertFileContent home-path/share/icons/material/disclosure-triangle-black.svg ${pkgs.writeText "expected-disclosure-triangle-black.svg" ''<svg fill="black" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M400-280v-400l200 200-200 200Z"/></svg>''}
+        '';
+      };
+    };
+}
