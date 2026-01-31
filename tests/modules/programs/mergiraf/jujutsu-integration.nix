d0b7a907235f72ad18beb0b28a975f451ca265d7
Vinicius Deolindo <git@vini.cat>
2026-01-31 03:21:37 -0300

    mergiraf: add jujutsu integration

---
+++ tests/modules/programs/mergiraf/jujutsu-integration.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.programs.jujutsu;
+  packageVersion = lib.getVersion cfg.package;
+
+  # jj v0.29+ deprecated support for "~/Library/Application Support" on Darwin.
+  configDir =
+    if pkgs.stdenv.isDarwin && !(lib.versionAtLeast packageVersion "0.29.0") then
+      "Library/Application Support"
+    else
+      ".config";
+in
+{
+  programs.jujutsu = {
+    enable = true;
+    package = config.lib.test.mkStubPackage { };
+  };
+  programs.mergiraf.enable = true;
+
+  nmt.script = ''
+    assertFileExists 'home-files/${configDir}/jj/config.toml'
+    assertFileContent 'home-files/${configDir}/jj/config.toml' \
+      ${builtins.toFile "expected.toml" ''
+        [ui]
+        merge-editor = "mergiraf"
+      ''}
+  '';
+}
