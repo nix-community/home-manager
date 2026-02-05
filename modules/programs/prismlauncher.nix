c809784eb6bc474f393b20ee902ab6bf73d492cd
Austin Horstman <khaneliman12@gmail.com>
2026-02-05 11:10:34 -0600

    prismlauncher: code style and lint cleanup
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- modules/programs/prismlauncher.nix
+++ modules/programs/prismlauncher.nix
@@ -10,37 +10,16 @@ let
 
   inherit (lib)
     escapeShellArg
-    getExe
     listToAttrs
     literalExpression
-    mkEnableOption
     mkIf
-    mkMerge
     mkOption
-    mkPackageOption
     types
     ;
 
   cfg = config.programs.prismlauncher;
 
-  dataDir =
-    if (isDarwin && !config.xdg.enable) then
-      "Library/Application Support/PrismLauncher"
-    else
-      "${config.xdg.dataHome}/PrismLauncher";
-
   iniFormat = pkgs.formats.ini { };
-
-  impureConfigMerger = filePath: staticSettingsFile: emptySettingsFile: ''
-    mkdir -p $(dirname '${escapeShellArg filePath}')
-
-    if [ ! -e '${escapeShellArg filePath}' ]; then
-      cat '${escapeShellArg emptySettingsFile}' > '${escapeShellArg filePath}'
-    fi
-
-    ${getExe pkgs.crudini} --merge --ini-options=nospace \
-      '${escapeShellArg filePath}' < '${escapeShellArg staticSettingsFile}'
-  '';
 in
 
 {
@@ -49,9 +28,9 @@ in
   ];
 
   options.programs.prismlauncher = {
-    enable = mkEnableOption "Prism Launcher";
+    enable = lib.mkEnableOption "Prism Launcher";
 
-    package = mkPackageOption pkgs "prismlauncher" { nullable = true; };
+    package = lib.mkPackageOption pkgs "prismlauncher" { nullable = true; };
 
     extraPackages = mkOption {
       type = types.listOf types.package;
@@ -89,26 +68,45 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = mkMerge ([ (mkIf (cfg.package != null) [ cfg.package ]) ] ++ cfg.extraPackages);
+  config =
+    let
+      dataDir =
+        if (isDarwin && !config.xdg.enable) then
+          "Library/Application Support/PrismLauncher"
+        else
+          "${config.xdg.dataHome}/PrismLauncher";
 
-    home.activation = {
-      prismlauncherConfigActivation = (
-        lib.hm.dag.entryAfter [ "linkGeneration" ] (
-          impureConfigMerger "${dataDir}/prismlauncher.cfg" (iniFormat.generate "prismlauncher-static.cfg" {
-            General = cfg.settings;
-          }) (iniFormat.generate "prismlauncher-empty.cfg" { General = { }; })
-        )
-      );
+      impureConfigMerger = filePath: staticSettingsFile: emptySettingsFile: ''
+        mkdir -p $(dirname '${escapeShellArg filePath}')
+
+        if [ ! -e '${escapeShellArg filePath}' ]; then
+          cat '${escapeShellArg emptySettingsFile}' > '${escapeShellArg filePath}'
+        fi
+
+        ${lib.getExe pkgs.crudini} --merge --ini-options=nospace \
+          '${escapeShellArg filePath}' < '${escapeShellArg staticSettingsFile}'
+      '';
+    in
+    mkIf cfg.enable {
+      home = {
+        packages = lib.mkMerge ([ (mkIf (cfg.package != null) [ cfg.package ]) ] ++ cfg.extraPackages);
+
+        activation = {
+          prismlauncherConfigActivation = lib.hm.dag.entryAfter [ "linkGeneration" ] (
+            impureConfigMerger "${dataDir}/prismlauncher.cfg" (iniFormat.generate "prismlauncher-static.cfg" {
+              General = cfg.settings;
+            }) (iniFormat.generate "prismlauncher-empty.cfg" { General = { }; })
+          );
+        };
+
+        file = mkIf (cfg.icons != [ ]) (
+          listToAttrs (
+            map (source: {
+              name = "${dataDir}/icons/${baseNameOf source}";
+              value = { inherit source; };
+            }) cfg.icons
+          )
+        );
+      };
     };
-
-    home.file = mkIf (cfg.icons != [ ]) (
-      listToAttrs (
-        map (source: {
-          name = "${dataDir}/icons/${baseNameOf source}";
-          value = { inherit source; };
-        }) cfg.icons
-      )
-    );
-  };
 }
