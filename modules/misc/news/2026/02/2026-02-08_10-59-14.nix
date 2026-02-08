08c5ce3d86efd1ad072faaffc008ab8fbe8f296e
Aguirre Matteo <aguirre-matteo@proton.me>
2026-02-08 11:01:23 -0300

    news: add lazyworktree entry

---
+++ modules/misc/news/2026/02/2026-02-08_10-59-14.nix
@@ -0,0 +1,10 @@
+{
+  time = "2026-02-08T13:59:14+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.lazyworktree`
+
+    LazyWorktree is a TUI for Git worktrees. It provides a keyboard-driven
+    workflow for creating, inspecting, and navigating worktrees within a repository.
+  '';
+}
