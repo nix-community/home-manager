c32afdf13359895dce9419e4ad00014cf805f7f9
Heitor <44377258+HeitorAugustoLN@users.noreply.github.com>
2026-02-09 14:38:15 -0300

    firefox: prepare xdg compliant configuration

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -1,8 +1,20 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  options,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkRemovedOptionModule;
 
   cfg = config.programs.firefox;
+
+  linuxConfigPath =
+    if lib.versionAtLeast config.home.stateVersion "26.11" then
+      "${config.xdg.configHome}/mozilla/firefox"
+    else
+      ".mozilla/firefox";
 
   modulePath = [
     "programs"
@@ -29,7 +41,7 @@ in
       visible = true;
 
       platforms.linux = {
-        configPath = ".mozilla/firefox";
+        configPath = linuxConfigPath;
       };
       platforms.darwin = {
         configPath = "Library/Application Support/Firefox";
@@ -57,6 +69,29 @@ in
   ];
 
   config = lib.mkIf cfg.enable {
+    warnings =
+      lib.optionals
+        (
+          pkgs.stdenv.hostPlatform.isLinux
+          && !lib.versionAtLeast config.home.stateVersion "26.11"
+          && options.programs.firefox.configPath.highestPrio >= 1500
+        )
+        [
+          ''
+            The default value of `programs.firefox.configPath` will change in a future release.
+            You are currently using the legacy default (`.mozilla/firefox`) because `home.stateVersion` is less than "26.11".
+
+            Please set `programs.firefox.configPath` explicitly to lock in your choice:
+              programs.firefox.configPath = ".mozilla/firefox";
+              # New default in 26.11+
+              programs.firefox.configPath = "''${config.xdg.configHome}/mozilla/firefox";
+
+            To migrate to the XDG path, move `~/.mozilla/firefox` to `~/.config/mozilla/firefox`.
+            Firefox keeps using the legacy path if `~/.mozilla/firefox` still exists.
+            Migrating profiles can break existing setups, so make a backup before moving.
+          ''
+        ];
+
     mozilla.firefoxNativeMessagingHosts =
       cfg.nativeMessagingHosts
       # package configured native messaging hosts (entire browser actually)
