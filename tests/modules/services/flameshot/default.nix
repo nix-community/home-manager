00e86361aa9e6a644de9afdfbc3f4245486ff99a
Austin Horstman <khaneliman12@gmail.com>
2026-02-11 23:19:57 -0600

    tests/flameshot: add darwin tests
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/services/flameshot/default.nix
+++ tests/modules/services/flameshot/default.nix
@@ -1,6 +1,12 @@
 { lib, pkgs, ... }:
 
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+{
   flameshot-empty-settings = ./empty-settings.nix;
   flameshot-example-settings = ./example-settings.nix;
+}
+// lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+  flameshot-service = ./service.nix;
+}
+// lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+  flameshot-agent = ./launchd-agent.nix;
 }
