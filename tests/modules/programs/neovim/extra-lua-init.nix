7e3cf48ace84786e066b3834f620f0610c68b496
teto <886074+teto@users.noreply.github.com>
2026-02-12 15:16:41 +0100

    wip test

--- tests/modules/programs/neovim/extra-lua-init.nix
+++ tests/modules/programs/neovim/extra-lua-init.nix
@@ -10,9 +10,14 @@
   };
 
   nmt.script = ''
-    nvimFolder="home-files/.config/nvim"
-    assertFileContent "$nvimFolder/init.lua" ${builtins.toFile "init.lua-expected" ''
+    initLua="home-files/.config/nvim/init.lua"
+    initLuaNormalized="$(normalizeStorePaths "$initLua")"
+
+    assertFileContent "$initLuaNormalized" ${builtins.toFile "init.lua-expected" ''
+      vim.g.loaded_node_provider=0;vim.g.loaded_perl_provider=0;vim.g.loaded_python_provider=0;vim.g.python3_host_prog='/nix/store/00000000000000000000000000000000-hm_nviminit.lua/bin/nvim-python3';vim.g.ruby_host_prog='/nix/store/00000000000000000000000000000000-hm_nviminit.lua/bin/nvim-ruby'
+
+
       -- initLua
-    ''}
+      ''}
   '';
 }
