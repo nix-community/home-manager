fe878ebc48e468d64ce5642f43feb49dde948904
Austin Horstman <khaneliman12@gmail.com>
2026-02-12 22:53:42 -0600

    tests/types: add suboptions doc test
    Verify we are able to extract suboptions properly with our custom lib
    extension.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/lib/types/default.nix
+++ tests/lib/types/default.nix
@@ -2,5 +2,7 @@
   lib-types-dag-submodule = ./dag-submodule.nix;
   lib-types-dag-merge = ./dag-merge.nix;
 
+  lib-types-either-suboptions-docs-lib = ./either-suboptions-docs-lib.nix;
+
   lib-types-gvariant-merge = ./gvariant-merge.nix;
 }
