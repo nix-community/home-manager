53ec57d81fef48045312693fd983db647f228f6a
mytagssma <161827049+mytagssma@users.noreply.github.com>
2026-02-17 18:14:25 +0530

    wlsunset : added duration option
    animation duration time option when using sunset/sunrise times
    
    NOT DONE: needs to set default value  and only when using sunset/sunrise times

--- modules/services/wlsunset.nix
+++ modules/services/wlsunset.nix
@@ -96,6 +96,16 @@ in
       '';
     };
 
+    duration = {
+      type = with types; int;
+      default = null;
+      example = 1800; 
+      description = ''
+        The Manual animation time in seconds.
+        Only applicable when using manual sunset/sunrise times.
+      '';
+    };
+
     systemdTarget = mkOption {
       type = with types; str;
       default = config.wayland.systemd.target;
@@ -142,6 +152,7 @@ in
               L = cfg.longitude;
               S = cfg.sunrise;
               s = cfg.sunset;
+              d = cfg.duration;
               o = cfg.output;
             };
           in
