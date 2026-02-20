ccd5351436f3312fb3e4bdb51096b4e03d44d02c
Bruno BELANYI <bruno@belanyi.fr>
2026-02-20 12:12:56 +0000

    tests: mpv: refactor stubs definition
    I am about to introduce more tests making use of the stubs, let's
    centralize them in one file.

--- tests/modules/programs/mpv/mpv-invalid-settings.nix
+++ tests/modules/programs/mpv/mpv-invalid-settings.nix
@@ -4,22 +4,12 @@
 }:
 
 {
+  imports = [ ./stubs.nix ];
+
   programs.mpv = {
     enable = true;
     package = pkgs.emptyDirectory;
     scripts = [ pkgs.mpvScript ];
-  };
-
-  test.stubs = {
-    mpv = {
-      extraAttrs.override = { ... }: pkgs.emptyDirectory;
-    };
-
-    mpvScript = {
-      extraAttrs = {
-        scriptName = "something";
-      };
-    };
   };
 
   test.asserts.assertions.expected = [
