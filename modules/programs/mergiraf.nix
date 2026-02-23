52c2ac220f606e66b1719c751181a4e2e273977a
Bruno BELANYI <bruno@belanyi.fr>
2026-02-23 17:01:17 +0000

    mergiraf: add 'enable{Git,Jujutsus}Integration'

--- modules/programs/mergiraf.nix
+++ modules/programs/mergiraf.nix
@@ -21,13 +21,37 @@ in
       enable = mkEnableOption "mergiraf";
 
       package = mkPackageOption pkgs "mergiraf" { };
+
+      enableGitIntegration = lib.mkOption {
+        type = lib.types.bool;
+        default = lib.versionOlder config.home.stateVersion "26.05";
+        defaultText = ''lib.versionOlder config.home.stateVersion "26.05"'';
+        description = ''
+          Whether to enable git integration for mergiraf.
+
+          When enabled, mergiraf will be configured as git's merge driver.
+        '';
+      };
+
+      enableJujutsuIntegration = lib.mkOption {
+        type = lib.types.bool;
+        default = lib.versionOlder config.home.stateVersion "26.05";
+        defaultText = ''lib.versionOlder config.home.stateVersion "26.05"'';
+        description = ''
+          Whether to enable jujutsu integration for mergiraf.
+
+          When enabled, mergiraf will be configured as jujutsus's merge tool.
+        '';
+      };
     };
   };
 
   config = lib.mkMerge [
     (mkIf cfg.enable {
       home.packages = [ cfg.package ];
+    })
 
+    (mkIf (cfg.enable && cfg.enableGitIntegration) {
       programs = {
         git = {
           attributes = [ "* merge=mergiraf" ];
@@ -41,7 +65,11 @@ in
             };
           };
         };
+      };
+    })
 
+    (mkIf (cfg.enable && cfg.enableJujutsuIntegration) {
+      programs = {
         jujutsu = {
           settings = {
             ui = {
