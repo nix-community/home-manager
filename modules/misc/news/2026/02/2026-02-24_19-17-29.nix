31ea32fa1465568e95ea1288ea1dd02689c77c5e
octvs <octvs@posteo.de>
2026-02-24 19:03:46 +0100

    i3-sway: allow inverse_outer on smart_gaps
    Change the boolean `smartGaps` option to an `enum` that expects common
    values of i3 and sway ("on" "off" "inverse_outer").

---
+++ modules/misc/news/2026/02/2026-02-24_19-17-29.nix
@@ -0,0 +1,9 @@
+{
+  time = "2026-02-24T18:17:29+00:00";
+  condition = true;
+  message = ''
+    The options 'xsession.windowManager.i3.config.gaps.smartGaps' and
+    'wayland.windowManager.sway.config.gaps.smartGaps' now expects either "on",
+    "off" (default) or "inverse_outer".
+  '';
+}
