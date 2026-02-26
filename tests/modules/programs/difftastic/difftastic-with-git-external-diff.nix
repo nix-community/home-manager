f3a30376bb9eb2f6f61816be7d6ed954b6d2a3b9
Ondřej Kvapil <git@viluon.me>
2026-02-26 11:01:07 +0100

    difftastic: allow repeating options
    The type was overly restrictive, see
    https://github.com/nix-community/home-manager/pull/7947/changes#r2858054388

--- tests/modules/programs/difftastic/difftastic-with-git-external-diff.nix
+++ tests/modules/programs/difftastic/difftastic-with-git-external-diff.nix
@@ -8,6 +8,10 @@
     options = {
       color = "always";
       display = "side-by-side";
+      override = [
+        "*.mill:Scala"
+        "*.yuck:Emacs Lisp"
+      ];
     };
   };
 
@@ -17,7 +21,7 @@
     assertFileExists home-files/.config/git/config
     assertFileContains home-files/.config/git/config '[diff]'
     # Should have diff.external set
-    assertFileContains home-files/.config/git/config "external = \"@difftastic@/bin/difft '--color=always' '--display=side-by-side'\""
+    assertFileContains home-files/.config/git/config "external = \"@difftastic@/bin/difft '--color=always' '--display=side-by-side' '--override=*.mill:Scala' '--override=*.yuck:Emacs Lisp'\""
     # Should NOT have difftool config when diffToolMode is explicitly false
     assertFileNotRegex home-files/.config/git/config 'tool = "difftastic"'
     assertFileNotRegex home-files/.config/git/config '\[difftool "difftastic"\]'
