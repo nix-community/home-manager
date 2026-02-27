1c3299b2bf963dd6fbd20b3bc4df91ee82963738
Austin Horstman <khaneliman12@gmail.com>
2026-02-26 21:51:01 -0600

    starship: lint cleanup
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -22,7 +22,7 @@ in
     package = lib.mkPackageOption pkgs "starship" { };
 
     settings = mkOption {
-      type = tomlFormat.type;
+      inherit (tomlFormat) type;
       default = { };
       example = lib.literalExpression ''
         {
@@ -135,45 +135,47 @@ in
 
     };
 
-    programs.bash.initExtra = mkIf cfg.enableBashIntegration (
-      lib.mkOrder 1900 ''
+    programs = {
+      bash.initExtra = mkIf cfg.enableBashIntegration (
+        lib.mkOrder 1900 ''
+          if [[ $TERM != "dumb" ]]; then
+            eval "$(${lib.getExe cfg.package} init bash --print-full-init)"
+          fi
+        ''
+      );
+
+      zsh.initContent = mkIf cfg.enableZshIntegration ''
         if [[ $TERM != "dumb" ]]; then
-          eval "$(${lib.getExe cfg.package} init bash --print-full-init)"
+          eval "$(${lib.getExe cfg.package} init zsh)"
         fi
-      ''
-    );
-
-    programs.zsh.initContent = mkIf cfg.enableZshIntegration ''
-      if [[ $TERM != "dumb" ]]; then
-        eval "$(${lib.getExe cfg.package} init zsh)"
-      fi
-    '';
-
-    programs.fish.${initFish} = mkIf cfg.enableFishIntegration ''
-      if test "$TERM" != "dumb"
-        ${lib.getExe cfg.package} init fish | source
-        ${lib.optionalString cfg.enableTransience "enable_transience"}
-      end
-    '';
-
-    programs.ion.initExtra = mkIf cfg.enableIonIntegration ''
-      if test $TERM != "dumb"
-        eval $(${lib.getExe cfg.package} init ion)
-      end
-    '';
-
-    programs.nushell = mkIf cfg.enableNushellIntegration {
-      # Unfortunately nushell doesn't allow conditionally sourcing nor
-      # conditionally setting (global) environment variables, which is why the
-      # check for terminal compatibility (as seen above for the other shells) is
-      # not done here.
-      extraConfig = ''
-        use ${
-          pkgs.runCommand "starship-nushell-config.nu" { } ''
-            ${lib.getExe cfg.package} init nu >> "$out"
-          ''
-        }
       '';
+
+      fish.${initFish} = mkIf cfg.enableFishIntegration ''
+        if test "$TERM" != "dumb"
+          ${lib.getExe cfg.package} init fish | source
+          ${lib.optionalString cfg.enableTransience "enable_transience"}
+        end
+      '';
+
+      ion.initExtra = mkIf cfg.enableIonIntegration ''
+        if test $TERM != "dumb"
+          eval $(${lib.getExe cfg.package} init ion)
+        end
+      '';
+
+      nushell = mkIf cfg.enableNushellIntegration {
+        # Unfortunately nushell doesn't allow conditionally sourcing nor
+        # conditionally setting (global) environment variables, which is why the
+        # check for terminal compatibility (as seen above for the other shells) is
+        # not done here.
+        extraConfig = ''
+          use ${
+            pkgs.runCommand "starship-nushell-config.nu" { } ''
+              ${lib.getExe cfg.package} init nu >> "$out"
+            ''
+          }
+        '';
+      };
     };
   };
 }
