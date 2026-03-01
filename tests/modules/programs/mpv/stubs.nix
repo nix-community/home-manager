366efe1ee43b3d208a581d348df4386497055cf1
Austin Horstman <khaneliman12@gmail.com>
2026-03-01 12:51:33 -0600

    tests/mpv: stub wrapper
    Pulling in external dependencies we aren't even trying to test against.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/mpv/stubs.nix
+++ tests/modules/programs/mpv/stubs.nix
@@ -1,7 +1,67 @@
 { lib, pkgs, ... }:
 
+let
+  mkMpvBuildScript =
+    {
+      scripts ? [ ],
+      extraMakeWrapperArgs ? [ ],
+    }:
+    let
+      wrapperArgMarkers = lib.concatStringsSep "\n" extraMakeWrapperArgs;
+      scriptMarkers = lib.concatMapStringsSep "\n" (
+        script: "script=${script}/share/mpv/scripts/${script.scriptName}"
+      ) scripts;
+      markers = lib.concatStringsSep "\n" (
+        lib.filter (marker: marker != "") [
+          wrapperArgMarkers
+          scriptMarkers
+        ]
+      );
+    in
+    ''
+      mkdir -p $out/bin $out/share/applications
+      echo "Name=mpv" > $out/share/applications/mpv.desktop
+
+      cat > $out/bin/mpv <<EOF
+      #!${pkgs.runtimeShell}
+      # stubbed mpv binary for home-manager tests
+      ${markers}
+      exit 0
+      EOF
+      chmod +x $out/bin/mpv
+    '';
+in
 {
   test.stubs = {
+    mpv = {
+      name = "mpv";
+      outPath = null;
+      buildScript = mkMpvBuildScript { };
+
+      extraAttrs = rec {
+        override =
+          {
+            scripts ? [ ],
+            extraMakeWrapperArgs ? [ ],
+          }:
+          let
+            pkg =
+              pkgs.runCommandLocal "mpv"
+                {
+                  pname = "mpv";
+                  meta.mainProgram = "mpv";
+                }
+                (mkMpvBuildScript {
+                  inherit scripts extraMakeWrapperArgs;
+                });
+          in
+          pkg
+          // {
+            inherit override;
+          };
+      };
+    };
+
     mpv-unwrapped = {
       name = "mpv-unwrapped";
       outPath = null;
