b4b0063f5f6b6758f736c6ca3d0341dda113cb11
Bruno BELANYI <bruno@belanyi.fr>
2026-03-06 15:58:30 +0000

    man: add 'cache.generateAtRuntime'

--- modules/programs/man.nix
+++ modules/programs/man.nix
@@ -7,6 +7,16 @@
 let
   inherit (lib) mkOption types;
   cfg = config.programs.man;
+
+  # Generate a directory containing installed packages' manpages.
+  manualPages = pkgs.buildEnv {
+    name = "man-paths";
+    paths = config.home.packages;
+    pathsToLink = [ "/share/man" ];
+    extraOutputsToInstall = [ "man" ];
+    ignoreCollisions = true;
+  };
+
 in
 {
   imports = [
@@ -66,6 +76,19 @@ in
             this option.
           '';
         };
+
+        generateAtRuntime = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Whether to generate the manual page index caches at runtime using
+            a systemd service.
+
+            ::: {.note}
+            This is currently only supported on Linux.
+            :::
+          '';
+        };
       };
     };
   };
@@ -73,6 +96,20 @@ in
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
+        assertions = [
+          {
+            assertion =
+              let
+                runtimeCache = (cfg.cache.enable && cfg.cache.generateAtRuntime);
+                isLinux = lib.elem pkgs.stdenv.hostPlatform.system lib.platforms.linux;
+              in
+              runtimeCache -> isLinux;
+            message = ''
+              `programs.man.cache.generateAtRuntime` is only supported on Linux.
+            '';
+          }
+        ];
+
         warnings = lib.optional (
           cfg.cache.enable && cfg.package == null
         ) "programs.man.cache.enable has no effect when programs.man.package is null";
@@ -85,17 +122,8 @@ in
         # This is mostly copy/pasted/adapted from NixOS' documentation.nix.
         home.file.".manpath".text =
           let
-            # Generate a directory containing installed packages' manpages.
-            manualPages = pkgs.buildEnv {
-              name = "man-paths";
-              paths = config.home.packages;
-              pathsToLink = [ "/share/man" ];
-              extraOutputsToInstall = [ "man" ];
-              ignoreCollisions = true;
-            };
-
             # Generate a database of all manpages in ${manualPages}.
-            manualCache =
+            manualCache' =
               pkgs.runCommandLocal "man-cache"
                 {
                   nativeBuildInputs = [ cfg.package ];
@@ -109,11 +137,54 @@ in
                   mandb -C man.conf --no-straycats --create \
                     ${manualPages}/share/man
                 '';
+            manualCache =
+              if (!cfg.cache.generateAtRuntime) then
+                manualCache'
+              else
+                # FIXME: relies on this path being correct
+                "${config.xdg.cacheHome}/man/hm-mandb";
+
           in
           ''
             MANDB_MAP ${config.home.profileDirectory}/share/man ${manualCache}
           ''
           + lib.optionalString (cfg.extraConfig != "") "\n${cfg.extraConfig}";
+      })
+      (lib.mkIf (cfg.cache.enable && cfg.cache.generateAtRuntime && cfg.package != null) {
+        systemd.user.services.mandb = {
+          Unit = {
+            Description = ""; # FIXME
+            Documentation = "man:mandb(8)";
+            X-Restart-Triggers = [
+              manualPages
+            ];
+          };
+          Service = {
+            ExecStart =
+              pkgs.writeShellApplication
+                {
+                  runtimeInputs = [
+                    cfg.package
+                    pkgs.rsync
+                  ];
+                }
+                ''
+                  rsync \
+                    --checksum --recursive --copy-links --delete --no-times --no-perms --chmod=+w \
+                    ${manualPages}/share/man/ "$CACHE_DIRECTORY/hm-manpages"
+
+                  echo "MANDB_MAP $CACHE_DIRECTORY/hm-manpages $CACHE_DIRECTORY/hm-mandb" \
+                    > "$RUNTIME_DIRECTORY/man.conf"
+
+                  mandb -C "$RUNTIME_DIRECTORY/man.conf" -q
+                '';
+            CacheDirectory = "man";
+            RuntimeDirectory = "mandb";
+            BindReadOnlyPaths = [ "/dev/null:/etc/man_db.conf" ]; # mandb will still read /etc/man_db.conf if it exists, even when setting -C path/to/config.conf
+            ProtectSystem = "strict";
+          };
+          Install.WantedBy = [ "default.target" ];
+        };
       })
     ]
   );
