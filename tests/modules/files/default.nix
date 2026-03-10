6d4d7f746f99a3eaed49e5b719cc4d1b8a2d3bb1
Robert Helgesson <robert@rycee.net>
2026-03-10 14:50:38 +0100

    files: handle overlapping file targets
    Specifically, if a user has specified a recursive target directory we
    also allow the user to separately specify a file under that directory
    even if the recursive directory already contains that file. In other
    words, it should be possible to override a file introduced by a
    recursively linked directory.
    
    More concretely, with this change we allow a configuration such as
    
    ```nix
    home.file = {
      "foo" = {
        source = ./foo;
        recursive = true;
      };
      "foo/bar".source = lib.mkOutOfStoreSymlink ./foo/bar;
    };
    ```

--- tests/modules/files/default.nix
+++ tests/modules/files/default.nix
@@ -3,6 +3,7 @@
   files-executable = ./executable.nix;
   files-hidden-source = ./hidden-source.nix;
   files-out-of-store-symlink = ./out-of-store-symlink.nix;
+  files-override-recursive = ./override-recursive.nix;
   files-source-with-spaces = ./source-with-spaces.nix;
   files-target-conflict = ./target-conflict.nix;
   files-target-with-shellvar = ./target-with-shellvar.nix;
