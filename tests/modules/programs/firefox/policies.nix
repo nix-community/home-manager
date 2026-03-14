85366b6aebf9c94d3f6a6a9beec6bd073bd5c438
Mikilio <official.mikilio@gmail.com>
2026-03-14 11:46:02 +0100

    mkFirefox: add policy to accept added extensions
    This commit adds extensions added via
    profiles.<name>.globalExtensions to the policy.
    In consequence the extensions are added without complaint and don't need
    to be enabled manually.
    
    Co-authored-by: khaneliman <khaneliman@users.noreply.github.com>

--- tests/modules/programs/firefox/policies.nix
+++ tests/modules/programs/firefox/policies.nix
@@ -11,6 +11,16 @@ let
   firefoxMockOverlay = import ./setup-firefox-mock-overlay.nix modulePath;
 
   darwinPath = "Applications/${cfg.darwinAppName}.app/Contents/Resources";
+
+  uBlockStubPkg = config.lib.test.mkStubPackage {
+    name = "ublock-origin-dummy";
+    extraAttrs.addonId = "uBlock0@raymondhill.net";
+  };
+
+  privacyBadgerStubPkg = config.lib.test.mkStubPackage {
+    name = "privacy-badger-dummy";
+    extraAttrs.addonId = "jid1-MnnxcxisBPnSXQ@jetpack";
+  };
 in
 {
   imports = [ firefoxMockOverlay ];
@@ -22,6 +32,16 @@ in
       policies = {
         BlockAboutConfig = true;
       };
+      globalExtensions = [
+        uBlockStubPkg
+        {
+          package = privacyBadgerStubPkg;
+          settings = {
+            default_area = "menupanel";
+            private_browsing = true;
+          };
+        }
+      ];
       package = pkgs.${cfg.wrappedPackageName}.override {
         extraPolicies = {
           DownloadDirectory = "/foo";
@@ -61,6 +81,31 @@ in
 
           if [[ $downloadDirectory_actual_value != "\"/foo\"" ]]; then
             fail "Expected '${config_file}' to set 'policies.DownloadDirectory' to \"/foo\""
+          fi
+
+          ublock_installation_mode="$($jq --arg extid "${uBlockStubPkg.addonId}" ".policies.ExtensionSettings[\$extid].installation_mode" ${config_file})"
+
+          if [[ $ublock_installation_mode != "\"force_installed\"" ]]; then
+            fail "Expected '${config_file}' to force-install ${uBlockStubPkg.addonId}"
+          fi
+
+          ublock_install_url="$($jq --arg extid "${uBlockStubPkg.addonId}" ".policies.ExtensionSettings[\$extid].install_url" ${config_file})"
+          expected_ublock_install_url="\"file://${uBlockStubPkg}/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/${uBlockStubPkg.addonId}.xpi\""
+
+          if [[ $ublock_install_url != "$expected_ublock_install_url" ]]; then
+            fail "Expected '${config_file}' to set install_url for ${uBlockStubPkg.addonId}"
+          fi
+
+          privacy_badger_private_browsing="$($jq --arg extid "${privacyBadgerStubPkg.addonId}" ".policies.ExtensionSettings[\$extid].private_browsing" ${config_file})"
+
+          if [[ $privacy_badger_private_browsing != "true" ]]; then
+            fail "Expected '${config_file}' to preserve custom settings for ${privacyBadgerStubPkg.addonId}"
+          fi
+
+          privacy_badger_default_area="$($jq --arg extid "${privacyBadgerStubPkg.addonId}" ".policies.ExtensionSettings[\$extid].default_area" ${config_file})"
+
+          if [[ $privacy_badger_default_area != "\"menupanel\"" ]]; then
+            fail "Expected '${config_file}' to preserve default_area for ${privacyBadgerStubPkg.addonId}"
           fi
         '';
     }
