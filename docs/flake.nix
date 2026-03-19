a7ec5440474cdcc9939cc67c320f01aea0a2f195
Austin Horstman <khaneliman12@gmail.com>
2026-03-19 10:35:31 -0500

    docs: match parent flake nixpkgs channel
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- docs/flake.nix
+++ docs/flake.nix
@@ -2,7 +2,7 @@
   description = "Support developing Home Manager documentation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     scss-reset = {
       url = "github:andreymatin/scss-reset/1.4.2";
       flake = false;
