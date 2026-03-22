cf28e3b318f9b746ce100532fffa0df83f749441
mikaeladev <mikaeladev@icloud.com>
2026-03-22 11:38:25 +0000

    qt: add kvantum options

--- tests/modules/misc/qt/default.nix
+++ tests/modules/misc/qt/default.nix
@@ -1,5 +1,7 @@
 {
   qt-basic = ./qt-basic.nix;
+  qt-kvantum-settings = ./qt-kvantum-settings.nix;
+  qt-kvantum-themes = ./qt-kvantum-themes.nix;
   qt-platform-theme-gtk = ./qt-platform-theme-gtk.nix;
   qt-platform-theme-gtk3 = ./qt-platform-theme-gtk3.nix;
   qt-platform-theme-gnome = ./qt-platform-theme-gnome.nix;
