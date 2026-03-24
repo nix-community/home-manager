53797f71b9c91e2454fdd0e496f271ad1416db14
Max Cabrajac <38819272+maxcabrajac@users.noreply.github.com>
2026-03-23 21:44:06 -0300

    news: fix entry formatting
    The `"` string delimiter prevents leading spaces from being removed from the string.

--- modules/misc/news/2026/02/2026-02-13_14-17-15.nix
+++ modules/misc/news/2026/02/2026-02-13_14-17-15.nix
@@ -2,8 +2,8 @@
 {
   time = "2026-02-13T20:17:15+00:00";
   condition = config.programs.yazi.enable;
-  message = "
-      The option `programs.yazi.shellWrapperName` default has changed from `yy` to `y`
-      to align with the recommendation from upstream.
-    ";
+  message = ''
+    The option `programs.yazi.shellWrapperName` default has changed from `yy` to `y`
+    to align with the recommendation from upstream.
+  '';
 }
