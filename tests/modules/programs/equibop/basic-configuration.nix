294b048a9f9d3bb8a8c4ee5e837c41ca4b29881f
Perchun Pak <git@perchun.it>
2026-03-25 12:23:42 +0100

    equibop: init

---
+++ tests/modules/programs/equibop/basic-configuration.nix
@@ -0,0 +1,57 @@
+{
+  config = {
+    programs.equibop = {
+      enable = true;
+      settings = {
+        tray = false;
+        minimizeToTray = false;
+        hardwareAcceleration = true;
+        customTitleBar = false;
+        staticTitle = true;
+        discordBranch = "stable";
+      };
+      equicord = {
+        themes.my_theme = ''
+          .privateChannels_f0963d::after {
+            content: "";
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            z-index: 1000;
+            background: linear-gradient(to bottom, transparent 85%, var(--base00));
+            pointer-events: none;
+          }
+        '';
+        settings = {
+          autoUpdate = false;
+          autoUpdateNotification = false;
+          notifyAboutUpdates = false;
+          useQuickCss = true;
+          disableMinSize = true;
+          plugins = {
+            MessageLogger = {
+              enabled = true;
+              ignoreSelf = true;
+            };
+            FakeNitro.enabled = true;
+          };
+        };
+      };
+    };
+
+    nmt.script = ''
+      configDir=home-files/.config/equibop
+      assertFileExists $configDir/settings.json
+      assertFileContent $configDir/settings.json \
+        ${./basic-settings.json}
+      assertFileExists $configDir/settings/settings.json
+      assertFileContent $configDir/settings/settings.json \
+        ${./basic-equicord-settings.json}
+      assertFileExists $configDir/themes/my_theme.css
+      assertFileContent $configDir/themes/my_theme.css \
+        ${./basic-theme.css}
+    '';
+  };
+}
