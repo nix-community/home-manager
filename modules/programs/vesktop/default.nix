17f8fa436957e413683b3e09994e0839f78ae0c2
Perchun Pak <git@perchun.it>
2026-03-25 12:23:36 +0100

    mkVesktopLikeModule: init

---
+++ modules/programs/vesktop/default.nix
@@ -0,0 +1,30 @@
+{ lib, config, ... }:
+let
+  mkVesktopLikeModule = import ./mkVesktopLikeModule.nix;
+  cfg = config.programs.vesktop;
+in
+{
+  imports = [
+    (mkVesktopLikeModule {
+      moduleName = "vesktop";
+      cordModuleName = "vencord";
+      allConfigOptionsLink = "https://github.com/Vencord/Vesktop/blob/main/src/shared/settings.d.ts";
+      allCordConfigOptionsLink = "https://github.com/Vendicated/Vencord/blob/main/src/api/Settings.ts";
+      installPackage = false;
+      maintainers = with lib.maintainers; [
+        Flameopathic
+        LilleAila
+      ];
+    })
+  ];
+
+  options.programs.vesktop = {
+    vencord.useSystem = lib.mkEnableOption "Vencord package from Nixpkgs";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      (cfg.package.override { withSystemVencord = cfg.vencord.useSystem; })
+    ];
+  };
+}
