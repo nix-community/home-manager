4b1be5c38be350ee9452a4847945ce71d950dc31
Austin Horstman <khaneliman12@gmail.com>
2026-03-26 16:18:11 -0500

    vicinae: move lib to separate file
    Not used internally and cleaner to separate
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- modules/programs/vicinae/default.nix
+++ modules/programs/vicinae/default.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.programs.vicinae;
+  vicinaeLib = import ./lib.nix { inherit lib pkgs; };
 
   jsonFormat = pkgs.formats.json { };
   tomlFormat = pkgs.formats.toml { };
@@ -170,60 +171,7 @@ in
       }
     ];
 
-    lib.vicinae = {
-      mkExtension =
-        {
-          name,
-          src,
-        }:
-        (pkgs.buildNpmPackage {
-          inherit name src;
-          inherit (pkgs.importNpmLock) npmConfigHook;
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out
-            cp -r /build/.local/share/vicinae/extensions/${name}/* $out/
-
-            runHook postInstall
-          '';
-          npmDeps = pkgs.importNpmLock { npmRoot = src; };
-        });
-
-      mkRayCastExtension =
-        {
-          name,
-          src ? null,
-          rev ? null,
-          sha256 ? null,
-        }:
-        let
-          resolvedSrc =
-            if src != null then
-              src
-            else
-              pkgs.fetchgit {
-                inherit rev sha256;
-                url = "https://github.com/raycast/extensions";
-                sparseCheckout = [ "/extensions/${name}" ];
-              }
-              + "/extensions/${name}";
-        in
-        pkgs.buildNpmPackage {
-          inherit name;
-          inherit (pkgs.importNpmLock) npmConfigHook;
-          src = resolvedSrc;
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out
-            cp -r /build/.config/raycast/extensions/${name}/* $out/
-
-            runHook postInstall
-          '';
-          npmDeps = pkgs.importNpmLock { npmRoot = resolvedSrc; };
-        };
-    };
+    lib.vicinae = vicinaeLib;
 
     home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
 
