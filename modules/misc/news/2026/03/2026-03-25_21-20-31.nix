7f59d0e888f307fd8c688d3b1d917c8927880c83
oneorseveralcats <170012754+oneorseveralcats@users.noreply.github.com>
2026-03-27 00:28:18 -0400

    aria2p: add module

---
+++ modules/misc/news/2026/03/2026-03-25_21-20-31.nix
@@ -0,0 +1,10 @@
+{
+  time = "2026-03-26T01:20:31+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.aria2p`.
+
+    Aria2p is a tui client for aria2c. Note: aria2c must
+    be running with the "enable-rpc" setting.
+  '';
+}
