efef5e81d5c4fc280eea629e8d645124751f2cc7
Yiheng He <yiheng.he@proton.me>
2026-03-31 08:49:45 +0800

    awww: rename swww to awww
    Upstream project name has changed

--- tests/modules/services/awww/default.nix
+++ tests/modules/services/awww/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }:
 
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-  swww-graphical-session-target = ./swww-graphical-session-target.nix;
+  awww-graphical-session-target = ./awww-graphical-session-target.nix;
 }
