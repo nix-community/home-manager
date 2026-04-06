d1ccb5c9db8b543ab0c90aa4b9c503847d0e36e7
jtrrll <jacksonterrill3@gmail.com>
2026-04-06 00:31:24 +0000

    retroarch: add core settings

--- tests/modules/programs/retroarch/default.nix
+++ tests/modules/programs/retroarch/default.nix
@@ -1,5 +1,15 @@
 { lib, pkgs, ... }:
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-  retroarch-cores = ./cores.nix;
-  retroarch-settings = ./settings.nix;
-}
+lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux (
+  lib.pipe (builtins.readDir ./by-name) [
+    (lib.filterAttrs (_: kind: kind == "regular"))
+    (lib.mapAttrs' (
+      name: _:
+      lib.nameValuePair "retroarch-${lib.removeSuffix ".nix" name}" {
+        imports = [
+          (./by-name + "/${name}")
+          ./stubs.nix
+        ];
+      }
+    ))
+  ]
+)
