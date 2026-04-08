dc0668145d1a0e0ca702773fe4cdce0c3085786f
Veselin Ivanov <me@veselyn.com>
2026-04-08 17:02:27 +0300

    neovim: make existing extra-lua-* tests pass

--- tests/modules/programs/neovim/extra-lua-default.nix
+++ tests/modules/programs/neovim/extra-lua-default.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   imports = [ ./stubs.nix ];
 
@@ -5,6 +6,13 @@
 
   nmt.script = ''
     nvimFolder="home-files/.config/nvim"
-    assertPathNotExists "$nvimFolder/init.lua"
+    initLua="home-files/.config/nvim/init.lua"
+    initLuaNormalized="$(normalizeStorePaths "$initLua")"
+
+    assertFileContent "$initLuaNormalized" ${builtins.toFile "init.lua-expected" (
+      lib.trim ''
+        vim.g.loaded_node_provider=0;vim.g.loaded_perl_provider=0;vim.g.loaded_ruby_provider=0;vim.g.python3_host_prog='/nix/store/00000000000000000000000000000000-nvim-host-python3/bin/nvim-python3'
+      ''
+    )}
   '';
 }
