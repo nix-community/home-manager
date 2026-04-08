1e731f81e741fbca191d21967249dea62512e6f1
Austin Horstman <khaneliman12@gmail.com>
2026-04-08 12:15:03 -0500

    vicinae: guard refresh-apps activation

--- modules/programs/vicinae/default.nix
+++ modules/programs/vicinae/default.nix
@@ -205,7 +205,7 @@ in
       home.activation.vicinae-refresh-apps = lib.mkIf (cfg.package != null) (
         lib.hm.dag.entryAfter [ "installPackages" ] ''
           verboseEcho "Refreshing the vicinae app list"
-          run --silence ${lib.getExe config.programs.vicinae.package} deeplink vicinae://launch/core/refresh-apps
+          run --silence ${lib.getExe config.programs.vicinae.package} deeplink vicinae://launch/core/refresh-apps || verboseEcho "Failed to refresh the vicinae app list"
         ''
       );
 
