4b55e0fa8411edc150fefc3cc873a1b9ad399c9d
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-07 22:09:37 -0400

    Updated tests to match

--- tests/modules/programs/inkscape/inkscape-keymap-custom-xml.nix
+++ tests/modules/programs/inkscape/inkscape-keymap-custom-xml.nix
@@ -1,20 +1,30 @@
 {
   programs.inkscape = {
     enable = true;
-    keymapXml = ''
-      <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-      <keys name="default">
-        <bind key="F2" action="node-tool" display="true"/>
-        <bind key="F1" action="select-tool" display="true"/>
-      </keys>
-    '';
+    keymap = {
+      keys = {
+        "@name" = "default";
+        bind = [
+          {
+            "@key" = "F2";
+            "@action" = "node-tool";
+            "@display" = "true";
+          }
+          {
+            "@key" = "F1";
+            "@action" = "select-tool";
+            "@display" = "true";
+          }
+        ];
+      };
+    };
   };
 
   nmt.script = ''
     keysFile=home-files/.config/inkscape/keys/default.xml
 
     assertFileExists "$keysFile"
-    assertFileContains "$keysFile" 'action="node-tool"'
-    assertFileContains "$keysFile" 'action="select-tool"'
+    assertFileContains "$keysFile" 'node-tool'
+    assertFileContains "$keysFile" 'select-tool'
   '';
 }
