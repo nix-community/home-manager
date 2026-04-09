f919edb2c78bf5d18705d6354f5bea8a9f19f821
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-09 18:23:25 +0200

    xdg: add support for local bin path Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: I045f07415467ca6d409a6f4d52e753866a6a6964

--- tests/modules/misc/xdg/default.nix
+++ tests/modules/misc/xdg/default.nix
@@ -4,6 +4,8 @@
   xdg-mime-disabled = ./mime-disabled.nix;
   xdg-autostart = ./autostart.nix;
   xdg-autostart-readonly = ./autostart-readonly.nix;
+  xdg-local-bin-in-path = ./local-bin-in-path.nix;
+  xdg-local-bin-not-in-path = ./local-bin-not-in-path.nix;
   xdg-user-dirs-mixed = ./user-dirs-mixed.nix;
   xdg-user-dirs-null = ./user-dirs-null.nix;
   xdg-user-dirs-short = ./user-dirs-short.nix;
