4b521be704d3d23a3a64a94289884f0e8cbc360e
Rachit Kumar Verma <rachitve6h2g@users.noreply.github.com>
2026-04-14 14:59:44 +0530

    dprint: modify news for dprint

--- modules/misc/news/2026/04/2026-04-14_14-50-21.nix
+++ modules/misc/news/2026/04/2026-04-14_14-50-21.nix
@@ -1,11 +1,14 @@
-{ config, pkgs, ... }:
-{
+_: {
   time = "2026-04-14T09:20:21+00:00";
   # condition = pkgs.stdenv.hostPlatform.isLinux;
   # condition = config.programs.neovim.enable;
   condition = true;
   # if behavior changed, explain how to restore previous behavior.
   message = ''
-    PLACEHOLDER
+    A new module is available: 'programs.dprint'.
+
+    dprint is a code formatter written in rust.
+    It allows formatting toml, markdown, yaml, and many other filetypes.
+    See https://dprint.dev/ for more.
   '';
 }
