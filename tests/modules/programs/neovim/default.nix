74d746dbcc6a9c4bdab244625e67b164ebc93878
Veselin Ivanov <me@veselyn.com>
2026-04-17 16:33:08 +0200

    neovim: assert init.lua doesn't exist when sideloadInitLua is true

--- tests/modules/programs/neovim/default.nix
+++ tests/modules/programs/neovim/default.nix
@@ -4,8 +4,7 @@
   neovim-runtime = ./runtime.nix;
   neovim-wrapper-args = ./wrapper-args.nix;
 
-  # waiting for a nixpkgs patch
-  neovim-no-init-vim = ./no-init.nix;
+  neovim-no-init-lua = ./no-init-lua.nix;
   neovim-extra-lua-init = ./extra-lua-init.nix;
   neovim-extra-lua-default = ./extra-lua-default.nix;
   neovim-extra-lua-empty-plugin = ./extra-lua-empty-plugin.nix;
