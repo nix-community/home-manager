1e9bdc6d35c917f1f2f9f6adddfee7ad9c33a245
Austin Horstman <khaneliman12@gmail.com>
2026-04-19 22:31:51 -0500

    swaync: update style documentation link
    Closes #9098

--- modules/services/swaync.nix
+++ modules/services/swaync.nix
@@ -46,7 +46,7 @@ in
       '';
       description = ''
         CSS style of the bar. See
-        <https://github.com/ErikReider/SwayNotificationCenter/blob/main/src/style.css>
+        <https://github.com/ErikReider/SwayNotificationCenter?tab=readme-ov-file#configuring>
         for the documentation.
 
         If the value is set to a path literal, then the path will be used as the CSS file.
