eecbec5bb339e4600c6266551f01905830681a73
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-22 17:29:39 -0400

    news: add entry for programs.qalculate

---
+++ modules/misc/news/2026/04/2026-04-22_17-29-21.nix
@@ -0,0 +1,10 @@
+{
+  time = "2026-04-22T17:29:21+00:00";
+  condition = true;
+  message = ''
+    A new module is available: 'programs.qalculate'. Qalculate! is a
+    multi-purpose desktop calculator that is simple to use but
+    provides power and versatility normally reserved for complicated
+    math packages.
+  '';
+}
