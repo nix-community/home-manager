746593945193a15b2d55561cb64747f2617cadf7
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-22 17:23:59 -0400

    news: add entry for imapgoose modules

---
+++ modules/misc/news/2026/04/2026-04-22_17-23-04.nix
@@ -0,0 +1,14 @@
+{
+  time = "2026-04-22T17:23:04+00:00";
+  condition = true;
+  message = ''
+    Two new modules are available:
+
+      - 'programs.imapgoose' and
+      - 'services.imapgoose'.
+
+    ImapGoose is a modern, high-performance daemon designed to keep
+    local Maildir mailboxes in continuous, bidirectional
+    synchronization with an IMAP server.
+  '';
+}
--- tests/modules/services/imapgoose/linux-configuration.nix
+++ tests/modules/services/imapgoose/linux-configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   imports = [ ../../accounts/email-test-accounts.nix ];
 
