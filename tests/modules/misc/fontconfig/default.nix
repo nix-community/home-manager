f0bf2d0a067ab19b18b713d072e89d0a7b138e8a
Benedikt Rips <benedikt.rips@gmail.com>
2026-04-26 23:10:27 +0200

    fontconfig: add test for default fonts

--- tests/modules/misc/fontconfig/default.nix
+++ tests/modules/misc/fontconfig/default.nix
@@ -3,6 +3,7 @@
   fontconfig-single-font-package = ./single-font-package.nix;
   fontconfig-multiple-font-packages = ./multiple-font-packages.nix;
 
+  fontconfig-default-fonts = ./default-fonts.nix;
   fontconfig-default-rendering = ./default-rendering.nix;
   fontconfig-custom-rendering = ./custom-rendering.nix;
   fontconfig-extra-config-files = ./extra-config-files.nix;
