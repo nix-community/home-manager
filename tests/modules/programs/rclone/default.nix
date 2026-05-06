fd3a68e6405635af0922b3733185046dcec6f837
jiezhuzzz <jiezzz@duck.com>
2026-05-05 21:33:33 -0500

    rclone tests: cross-platform basic-configuration gating
    Lets basic-configuration run on both Linux and Darwin with
    platform-specific assertions. Mount/serve generation tests stay
    Linux-only for now.

--- tests/modules/programs/rclone/default.nix
+++ tests/modules/programs/rclone/default.nix
@@ -1,6 +1,8 @@
 { lib, pkgs, ... }:
-lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+{
   rclone-basic-configuration = ./basic-configuration.nix;
+}
+// lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   rclone-mount-service-generation = ./mount-service-generation.nix;
   rclone-serve-service-generation = ./serve-service-generation.nix;
 }
