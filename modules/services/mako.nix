81f459fc883212dd12c74395be5d2d4f1ac4a964
davidaf3 <david.alvarez.fidalgo@gmail.com>
2026-05-07 23:20:15 +0200

    mako: allow null package
    Make `services.mako.package` nullable and add the corresponding checks.

--- modules/services/mako.nix
+++ modules/services/mako.nix
@@ -99,7 +99,7 @@ in
 
   options.services.mako = {
     enable = mkEnableOption "mako";
-    package = mkPackageOption pkgs "mako" { };
+    package = mkPackageOption pkgs "mako" { nullable = true; };
     settings = mkOption {
       type = lib.types.attrsOf (
         lib.types.oneOf [
@@ -151,12 +151,16 @@ in
       (lib.hm.assertions.assertPlatform "services.mako" pkgs lib.platforms.linux)
     ];
 
-    home.packages = [ cfg.package ];
+    home.packages = lib.optionals (cfg.package != null) [ cfg.package ];
 
-    dbus.packages = [ cfg.package ];
+    dbus.packages = lib.optionals (cfg.package != null) [ cfg.package ];
 
     xdg.configFile."mako/config" = mkIf (cfg.settings != { } || cfg.extraConfig != "") {
-      onChange = "${cfg.package}/bin/makoctl reload || true";
+      onChange =
+        let
+          makoctl = if cfg.package != null then lib.getExe' cfg.package "makoctl" else "makoctl";
+        in
+        "${makoctl} reload || true";
       text = generateConfig cfg.settings;
     };
   };
