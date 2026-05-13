61fe28b1ec62b2800896e7aec4f77eddafc9cf03
kira <67888720+koffydrop@users.noreply.github.com>
2026-05-12 21:38:25 -0300

    discord: add option for different config locations
    Different discord branches use different config locations, but home-manager
    uses `${configDir}/discord` unconditionally. This adds an option that changes
    that to `${configDir}/${configName}`.

---
+++ tests/modules/programs/discord/non-default-config-dir.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  config = {
+    programs.discord = {
+      enable = true;
+      configName = "discordcanary";
+      settings.DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+    };
+
+    nmt.script =
+      let
+        configDir =
+          if pkgs.stdenv.hostPlatform.isLinux then
+            "home-files/.config/discordcanary"
+          else
+            "home-files/Library/Application Support/discordcanary";
+      in
+      ''
+        assertFileExists "${configDir}/settings.json"
+        assertFileContent "${configDir}/settings.json" \
+          ${./basic-settings.json}
+      '';
+  };
+}
