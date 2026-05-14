541add2e70b378595fcd158b4765a0a5acbc6e0b
Ilan Joselevich <personal@ilanjoselevich.com>
2026-05-14 20:20:19 +0300

    ssh-tpm-agent: add extraArgs option

--- tests/modules/services/ssh-tpm-agent/default.nix
+++ tests/modules/services/ssh-tpm-agent/default.nix
@@ -2,6 +2,7 @@
 
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   ssh-tpm-agent-as-ssh-agent-proxy = ./as-ssh-agent-proxy.nix;
+  ssh-tpm-agent-extra-args = ./extra-args.nix;
   ssh-tpm-agent-sshAuthSock = ./ssh-auth-sock.nix;
   ssh-tpm-agent-standalone = ./standalone.nix;
 }
