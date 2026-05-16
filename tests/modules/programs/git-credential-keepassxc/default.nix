bcb774cfc3268120cd61808629f9aa7dad3750a2
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-16 15:53:58 +0200

    git-credential-keepassxc: fix misplaced parentheses

--- tests/modules/programs/git-credential-keepassxc/default.nix
+++ tests/modules/programs/git-credential-keepassxc/default.nix
@@ -1,4 +1,5 @@
 {
   git-credential-keepassxc-default-config = ./default-config.nix;
   git-credential-keepassxc-custom-groups = ./custom-groups.nix;
+  git-credential-keepassxc-custom-hosts = ./custom-hosts.nix;
 }
