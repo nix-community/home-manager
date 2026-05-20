8982f45135b038f2d8b559f7f9ae55db6883baaa
Austin Horstman <khaneliman12@gmail.com>
2026-05-20 09:22:55 -0500

    lib/strings: add isPathLike helper
    (cherry picked from commit d33c97ec1b3f26d6a2d4d6ec0246af48a1a2259d)

--- modules/lib/strings.nix
+++ modules/lib/strings.nix
@@ -87,4 +87,11 @@ rec {
   # Returns true for strings like `SCREAMING_SNAKE_CASE`, `SOME_CONSTANT`.
   # Must be all uppercase letters/numbers, with words separated by single underscores.
   isScreamingSnakeCase = str: builtins.match "^[A-Z0-9]+(_[A-Z0-9]+)*$" str != null;
+
+  # Check if the content is path-like (a path, a Nix store path string, or a derivation).
+  isPathLike =
+    content:
+    lib.isPath content
+    || (builtins.isString content && lib.hasPrefix "${builtins.storeDir}/" content)
+    || lib.isDerivation content;
 }
