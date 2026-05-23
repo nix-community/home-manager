f15c764b1449f45c9034b2f67873f8c4164ea21c
Léana 江 <leana.jiang+git@icloud.com>
2026-05-23 13:19:27 +0200

    password-store: improve warning message

--- tests/modules/programs/password-store/old-default-path.nix
+++ tests/modules/programs/password-store/old-default-path.nix
@@ -5,10 +5,10 @@
 
   test.asserts.warnings.expected = [
     ''
-      The default value of `programs.password-store.settings` has changed from `{ PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store"; }` to `{ }`.
-      You are currently using the legacy default (`{ PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store"; }`) because `home.stateVersion` is less than "25.11".
+      The default value of `programs.password-store.settings` has changed from `{ PASSWORD_STORE_DIR = "''${config.xdg.dataHome}/password-store"; }` to `{ }`.
+      You are currently using the legacy default (`{ PASSWORD_STORE_DIR = "''${config.xdg.dataHome}/password-store"; }`) because `home.stateVersion` is less than "25.11".
       To silence this warning and keep legacy behavior, set:
-        programs.password-store.settings = { PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store"; };
+        programs.password-store.settings = { PASSWORD_STORE_DIR = "''${config.xdg.dataHome}/password-store"; };
       To adopt the new default behavior, set:
         programs.password-store.settings = { };
     ''
