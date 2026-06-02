07c723c3fe06c8e24d76882184e8bdee0073ba34
Austin Horstman <khaneliman12@gmail.com>
2026-06-01 22:47:50 -0500

    zsh: fix oh-my-zsh custom path expansion
    Preserve shell-variable paths for programs.zsh.oh-my-zsh.custom while
    still escaping values for a double-quoted shell assignment.
    
    This restores runtime expansion for values like $HOME/extra/zsh, which
    otherwise became a literal path after the previous escapeShellArg
    change.

--- tests/modules/programs/zsh/default.nix
+++ tests/modules/programs/zsh/default.nix
@@ -20,6 +20,7 @@
   zsh-history-path-zdotdir-variable = import ./history-path.nix "zdotdir-variable";
   zsh-history-substring-search = ./history-substring-search.nix;
   zsh-legacy-warning = ./legacy-warning.nix;
+  zsh-oh-my-zsh-custom = ./oh-my-zsh-custom.nix;
   zsh-plugins-completions-renamed = ./plugins-completions-renamed.nix;
   zsh-siteFunctions-mkcd = ./siteFunctions-mkcd.nix;
   zsh-plugins = ./plugins.nix;
