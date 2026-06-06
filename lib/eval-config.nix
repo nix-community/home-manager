2e14269e04a02f48738bd553d802e557216b8e65
Ben Baldwin <benbaldwin000@gmail.com>
2026-06-06 11:19:43 -0700

    lib: separate eval config file
    Move the `homeManagerConfiguration` function into its own file to allow
    importing without passing an unused `lib` argument.

---
+++ lib/eval-config.nix
@@ -0,0 +1,33 @@
+{
+  check ? true,
+  extraSpecialArgs ? { },
+  lib ? pkgs.lib,
+  modules ? [ ],
+  pkgs,
+  minimal ? false,
+}:
+import ../modules {
+  inherit
+    check
+    extraSpecialArgs
+    lib
+    pkgs
+    minimal
+    ;
+  configuration = {
+    imports = modules ++ [
+      {
+        programs.home-manager.path = builtins.path {
+          path = ../.;
+          name = "source";
+        };
+      }
+    ];
+
+    nixpkgs = {
+      config = lib.mkDefault pkgs.config;
+
+      inherit (pkgs) overlays;
+    };
+  };
+}
