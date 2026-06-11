652f75468661f94b73361f8acbe0a84d4ac312be
Austin Horstman <khaneliman12@gmail.com>
2026-06-11 10:08:19 -0500

    difftastic: support difftool-only git integration
    Splitting git diff tools into the separate programs.difftastic module
    made it impossible to register difftastic as a git difftool without also
    forcing it as diff.external, since the boolean git.diffToolMode only
    toggled between "external" and "external + difftool".
    
    Replace git.diffToolMode with a git.mode enum ("external", "difftool",
    "both") so difftastic can be wired as the difftool only, leaving
    `git diff` untouched. The deprecated boolean is migrated automatically
    via mkChangedOptionModule (true -> "both", false -> "external"),
    preserving existing behavior.
    
    Fixes #8592

--- tests/modules/programs/difftastic/difftastic-with-git-both.nix
+++ tests/modules/programs/difftastic/difftastic-with-git-both.nix
@@ -3,7 +3,7 @@
     enable = true;
     git = {
       enable = true;
-      mode = "difftool";
+      mode = "both";
     };
     options = {
       color = "always";
@@ -15,11 +15,11 @@
 
   nmt.script = ''
     assertFileExists home-files/.config/git/config
-    # Should have difftool config only, leaving `git diff` untouched
+    assertFileContains home-files/.config/git/config '[diff]'
+    # Should have BOTH diff.external AND difftool config when mode is "both"
+    assertFileContains home-files/.config/git/config "external = \"@difftastic@/bin/difft '--color=always' '--display=side-by-side'\""
     assertFileContains home-files/.config/git/config 'tool = "difftastic"'
     assertFileContains home-files/.config/git/config '[difftool "difftastic"]'
     assertFileContains home-files/.config/git/config "cmd = \"@difftastic@/bin/difft '--color=always' '--display=side-by-side' \$LOCAL \$REMOTE\""
-    # Should NOT have diff.external when mode is "difftool"
-    assertFileNotRegex home-files/.config/git/config 'external = .*/difft'
   '';
 }
