c6f60caed22d316cf31d218ceba9d0f6b52fb159
khaneliman <khaneliman@users.noreply.github.com>
2026-06-22 03:52:31 +0000

    deploy: db97e414d5457538f21bb332b972e13c609cbeba

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-39721307.js',
+                path_to_root + 'searchindex-9d7a8e91.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
