nix-community/home-manager

commit 74b38179ac0820af3ea4a240ff12668fb2b8c72e

kanshi: Reload on config change

modules/services/kanshi.nix
@@ -16,6 +16,8 @@ let
 
   cfg = config.services.kanshi;
 
+  configPath = "kanshi/config";
+
   directivesTag = types.attrTag {
     profile = mkOption {
       type = profileModule;
@@ -346,7 +348,7 @@ in
       {
         home.packages = [ cfg.package ];
 
-        xdg.configFile."kanshi/config" =
+        xdg.configFile.${configPath} =
           let
             generatedConfigStr =
               if cfg.profiles == { } && cfg.extraConfig == "" then directivesStr else oldDirectivesStr;
@@ -361,11 +363,15 @@ in
             PartOf = cfg.systemdTarget;
             Requires = cfg.systemdTarget;
             After = cfg.systemdTarget;
+            X-Reload-Triggers = lib.mkIf (cfg.settings != { }) [
+              "${config.xdg.configFile.${configPath}.source}"
+            ];
           };
 
           Service = {
             Type = "simple";
             ExecStart = "${cfg.package}/bin/kanshi";
+            ExecReload = "${lib.getExe' cfg.package "kanshictl"} reload";
             Restart = "always";
           };
 

commit 8ec37d316e60a3ef8b0dea85719c81e8610d8fc9

kanshi: fix check for empty settings
Fixes regression introduced by me in 74b3817.

modules/services/kanshi.nix
@@ -363,7 +363,7 @@ in
             PartOf = cfg.systemdTarget;
             Requires = cfg.systemdTarget;
             After = cfg.systemdTarget;
-            X-Reload-Triggers = lib.mkIf (cfg.settings != { }) [
+            X-Reload-Triggers = lib.mkIf (cfg.settings != [ ]) [
               "${config.xdg.configFile.${configPath}.source}"
             ];
           };