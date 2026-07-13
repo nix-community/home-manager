fb3154441032d07cdcb45ae574d0e1da77599f17
jtrrll <jacksonterrill3@gmail.com>
2026-07-13 11:50:48 -0400

    retroarch: add per-core options and overrides
    Add per-core configuration under programs.retroarch.cores.<name>:
    
    - options: core options merged into retroarch-core-options.cfg
    - settingsOverrides: retroarch.cfg overrides
    - inputRemaps: input remap files
    - perContentDirectory.<dir> and perGame.<game>: the same, scoped to a
      content directory or game
    
    The attribute name is used as the core's directory name, so it must
    match the core's library_name as reported by RetroArch.

--- modules/programs/retroarch.nix
+++ modules/programs/retroarch.nix
@@ -18,6 +18,105 @@ let
       "Library/Application Support/RetroArch"
     else
       "${lib.removePrefix "${config.home.homeDirectory}/" config.xdg.configHome}/retroarch";
+
+  # RetroArch reads directory settings at runtime, so they must be absolute.
+  absoluteConfigDir = "${config.home.homeDirectory}/${configDir}";
+
+  mkKeyValueOption =
+    description:
+    lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = { };
+      inherit description;
+    };
+
+  overrideOptions = {
+    options = {
+      settingsOverrides = mkKeyValueOption ''
+        Overrides of global `retroarch.cfg` settings, written as a `.cfg`
+        override file. Only the settings that differ from the global
+        configuration need to be specified.
+
+        See <https://docs.libretro.com/guides/overrides/> for details.
+      '';
+
+      inputRemaps = mkKeyValueOption ''
+        Input remappings, written as a `.rmp` remap file under the
+        `config/remaps` directory. These override the input bindings from
+        `retroarch.cfg` when the corresponding content is loaded.
+
+        See <https://docs.libretro.com/guides/overrides/> for details.
+      '';
+    };
+  };
+
+  coreType = lib.types.submodule (
+    { name, ... }:
+    {
+      imports = [ overrideOptions ];
+      options = {
+        enable = lib.mkEnableOption "RetroArch core";
+
+        package = lib.mkPackageOption pkgs [ "libretro" name ] { };
+
+        options = mkKeyValueOption ''
+          Core options that configure this core's behavior.
+
+          These are merged with the options of all other cores into the
+          global `retroarch-core-options.cfg`. Keys are typically prefixed
+          with the core's name to avoid collisions.
+
+          These are the settings found under `Quick Menu -> Options`.
+
+          See <https://docs.libretro.com/guides/core-list/> for available
+          options.
+        '';
+
+        perContentDirectory = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.submodule overrideOptions);
+          default = { };
+          description = ''
+            Overrides applied when loading content from a particular directory.
+            The attribute name is the content directory name. These take
+            precedence over the core-level overrides.
+
+            See <https://docs.libretro.com/guides/overrides/> for details.
+          '';
+        };
+
+        perGame = lib.mkOption {
+          type = lib.types.attrsOf (
+            lib.types.submodule {
+              imports = [ overrideOptions ];
+              options = {
+                options = mkKeyValueOption ''
+                  Game-specific core options, written as a `.opt` file. These
+                  take precedence over the core's `options`.
+
+                  See <https://docs.libretro.com/guides/overrides/> for
+                  details.
+                '';
+              };
+            }
+          );
+          default = { };
+          description = ''
+            Overrides applied when loading a particular game. The attribute
+            name is the game (content) name. These take precedence over the
+            per-content-directory overrides.
+
+            See <https://docs.libretro.com/guides/overrides/> for details.
+          '';
+        };
+      };
+    }
+  );
+
+  mkFileIfPresent =
+    path: attrs:
+    lib.optionalAttrs (attrs != { }) {
+      "${configDir}/${path}".text = toKeyValue attrs;
+    };
 in
 {
   meta.maintainers = [
@@ -40,24 +139,18 @@ in
     };
 
     cores = lib.mkOption {
-      type = lib.types.attrsOf (
-        lib.types.submodule (
-          { name, ... }:
-          {
-            options = {
-              enable = lib.mkEnableOption "RetroArch core";
-              package = lib.mkPackageOption pkgs [ "libretro" name ] { };
-            };
-          }
-        )
-      );
+      type = lib.types.attrsOf coreType;
       default = { };
       example = lib.literalExpression ''
         {
-          mgba.enable = true;  # Uses pkgs.libretro.mgba
-          snes9x = {
+          mGBA.enable = true;  # Uses pkgs.libretro.mgba
+          "Snes9x - Current" = {
             enable = true;
-            package = pkgs.libretro.snes9x2010;
+            package = pkgs.libretro.snes9x;
+            options = {
+              snes9x_aspect = "4:3";
+              snes9x_region = "auto";
+            };
           };
           custom-core = {
             enable = true;
@@ -67,6 +160,10 @@ in
       '';
       description = ''
         RetroArch cores to enable. You can provide custom core packages.
+
+        The attribute name must match the core's directory name as reported by
+        RetroArch (its `library_name`), since it is used to locate the
+        per-core options and override files under `config/<name>`.
       '';
     };
 
@@ -94,14 +191,46 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.retroarch.settings.config_save_on_exit = lib.mkDefault "false";
+    programs.retroarch = {
+      settings = {
+        config_save_on_exit = lib.mkDefault "false";
+        game_specific_options = lib.mkDefault "true";
+        global_core_options = lib.mkDefault "true";
+        remap_save_on_exit = lib.mkDefault "false";
+        rgui_config_directory = lib.mkDefault "${absoluteConfigDir}/config";
+        input_remapping_directory = lib.mkDefault "${absoluteConfigDir}/config/remaps";
+      };
 
-    programs.retroarch.finalPackage = cfg.package.wrapper {
-      cores = lib.mapAttrsToList (_: core: core.package) enabledCores;
+      finalPackage = cfg.package.wrapper {
+        cores = lib.mapAttrsToList (_: core: core.package) enabledCores;
+      };
     };
 
-    home.packages = [ cfg.finalPackage ];
+    home = {
+      packages = [ cfg.finalPackage ];
 
-    home.file."${configDir}/retroarch.cfg".text = toKeyValue cfg.settings;
+      file = {
+        "${configDir}/retroarch.cfg".text = toKeyValue cfg.settings;
+        "${configDir}/retroarch-core-options.cfg".text = toKeyValue (
+          lib.concatMapAttrs (_: core: core.options) enabledCores
+        );
+      }
+      // lib.concatMapAttrs (
+        name: core:
+        mkFileIfPresent "config/${name}/${name}.cfg" core.settingsOverrides
+        // mkFileIfPresent "config/remaps/${name}/${name}.rmp" core.inputRemaps
+        // lib.concatMapAttrs (
+          contentDir: scope:
+          mkFileIfPresent "config/${name}/${contentDir}.cfg" scope.settingsOverrides
+          // mkFileIfPresent "config/remaps/${name}/${contentDir}.rmp" scope.inputRemaps
+        ) core.perContentDirectory
+        // lib.concatMapAttrs (
+          game: scope:
+          mkFileIfPresent "config/${name}/${game}.opt" scope.options
+          // mkFileIfPresent "config/${name}/${game}.cfg" scope.settingsOverrides
+          // mkFileIfPresent "config/remaps/${name}/${game}.rmp" scope.inputRemaps
+        ) core.perGame
+      ) enabledCores;
+    };
   };
 }
