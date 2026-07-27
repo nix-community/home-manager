0fa868c16b1dce5a8d54f4aeef49df0072321433
mikaeladev <mikaeladev@icloud.com>
2026-07-27 21:02:12 +0100

    gram: add module

---
+++ tests/modules/programs/gram/default.nix
@@ -0,0 +1,12 @@
+{
+  gram-asserts-packages = ./asserts/packages.nix;
+  gram-asserts-snippets = ./asserts/snippets.nix;
+
+  gram-configs-empty = ./configs/empty.nix;
+  gram-configs-settings = ./configs/settings.nix;
+  gram-configs-snippets = ./configs/snippets.nix;
+  gram-configs-themes = ./configs/themes.nix;
+
+  gram-extensions-fallback = ./extensions/fallback.nix;
+  gram-extensions-wrapper = ./extensions/wrapper.nix;
+}
