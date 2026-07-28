52361c6571b40f0fdefa016b407959215a7eed8b
Austin Horstman <khaneliman12@gmail.com>
2026-07-28 06:20:33 -0500

    radio-active: honor package and settings options
    Use the configured package for installation and player-specific patching, while preserving configuration generation when package is null. Generate any declared INI section and correct the settings example to match the nested section type.

--- tests/modules/programs/radio-active/AppConfig_player_vlc.nix
+++ tests/modules/programs/radio-active/AppConfig_player_vlc.nix
@@ -1,14 +1,41 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
   programs.radio-active = {
     enable = true;
+
+    package = config.lib.test.mkStubPackage {
+      name = "radio-active-custom";
+      buildScript = ''
+        mkdir -p radioactive "$out/bin" "$out/share/radio-active"
+        echo 'self.exe_path = which(self.program_name)' > radioactive/vlc.py
+
+        runHook postPatch
+
+        cp radioactive/vlc.py "$out/share/radio-active/vlc.py"
+        touch "$out/bin/radio-active-custom"
+      '';
+      extraAttrs.postPatch = null;
+    };
 
     settings.AppConfig.player = "vlc";
   };
 
   nmt.script = ''
+    assertFileExists home-path/bin/radio-active-custom
+    assertFileContent home-path/share/radio-active/vlc.py \
+    ${builtins.toFile "expected.radio-active_vlc.py" ''
+      self.exe_path = "${lib.getExe pkgs.vlc}"
+    ''}
+
     assertFileExists home-files/.config/radio-active/configs.ini
     assertFileContent home-files/.config/radio-active/configs.ini \
-    ${builtins.toFile "expected.player_mpv.radio-active_configs.ini" ''
+    ${builtins.toFile "expected.player_vlc.radio-active_configs.ini" ''
       [AppConfig]
       player=vlc
     ''}
--- tests/modules/programs/radio-active/aliases.nix
+++ tests/modules/programs/radio-active/aliases.nix
@@ -1,13 +1,38 @@
 {
   programs.radio-active = {
     enable = true;
+    package = null;
 
     aliases = {
       "Deep House Lounge" = "http://198.15.94.34:8006/stream";
     };
+
+    settings.Favorites = {
+      station = "Deep House Lounge";
+      volume = 42;
+    };
+  };
+
+  test.stubs.radio-active = {
+    name = "radio-active-default";
+    outPath = null;
+    buildScript = ''
+      mkdir -p "$out/bin"
+      touch "$out/bin/radio-active"
+    '';
   };
 
   nmt.script = ''
+    assertPathNotExists home-path/bin/radio-active
+
+    assertFileExists home-files/.config/radio-active/configs.ini
+    assertFileContent home-files/.config/radio-active/configs.ini \
+    ${builtins.toFile "expected.radio-active_favorites.ini" ''
+      [Favorites]
+      station=Deep House Lounge
+      volume=42
+    ''}
+
     assertFileExists home-files/.radio-active-alias
     assertFileContent home-files/.radio-active-alias \
     ${builtins.toFile "expected.radio-active_aliases.ini" ''
