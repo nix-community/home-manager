9451f7833e5cadaa37b825cdd874dde630a3b6a9
teto <teto@users.noreply.github.com>
2026-07-28 10:38:23 +0000

    deploy: b8324779d178e484b54cf7c202f87ec8c500a3d3

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d53f95b6.js',
+                path_to_root + 'searchindex-6f428da6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
