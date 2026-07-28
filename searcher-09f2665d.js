428b9244442e0a2cca6dd069efe0057a772e6588
khaneliman <khaneliman@users.noreply.github.com>
2026-07-28 20:17:43 +0000

    deploy: 3d25fa94cf355cf7da64c77bb9c3c15806486329

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2cd8ae4b.js',
+                path_to_root + 'searchindex-cd2b47c9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
