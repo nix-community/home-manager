2076765a5b402d30f3282fa028d7f951197b5999
rycee <rycee@users.noreply.github.com>
2026-08-13 06:43:25 +0000

    deploy: 0668fdbc15bd602e463cab37ff64346021193652

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-59c3bfb9.js',
+                path_to_root + 'searchindex-f1e44f99.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
