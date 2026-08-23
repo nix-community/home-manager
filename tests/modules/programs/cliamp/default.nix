d1faef32f5e6ab3cb34ada5e030cbceb52307775
Rachit Kumar Verma <rachitverma1122@gmail.com>
2026-08-23 10:28:55 +0530

    module/cliamp

---
+++ tests/modules/programs/cliamp/default.nix
@@ -0,0 +1,6 @@
+{
+  cliamp-empty-config = ./empty-config.nix;
+  cliamp-example-config = ./example-config.nix;
+  cliamp-example-theme = ./example-theme.nix;
+  cliamp-example-radios = ./example-radios.nix;
+}
