ef2cfc4a8a2ac8381271055cf9478606cf0503ea
DPDmancul <davide.peressoni@tuta.io>
2026-08-24 16:03:42 +0200

    copyq: add settings

--- tests/modules/services/copyq/default.nix
+++ tests/modules/services/copyq/default.nix
@@ -3,4 +3,5 @@
 lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   copyq-basic-configuration = ./basic-configuration.nix;
   copyq-dont-force-x = ./dont-force-x-configuration.nix;
+  copyq-settings = ./settings.nix;
 }
