3cc4e1ff84005bb6958e82fc0f892ed228b94a86
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-25 23:39:47 -0400

    firefox: apply configPath to package wrapper
    This is a counterpart for: https://github.com/NixOS/nixpkgs/pull/556611
    
    When configPath is overridden, if wrapper supports the new appDataDir
    argument, we pass configPath's absolute form to tell the app to look for
    its datadir in the correct directory.
    
    This will become necessary on macOS 27+ where Firefox that is not signed
    by Mozilla has no access to its default datadir.

--- tests/modules/programs/firefox/default.nix
+++ tests/modules/programs/firefox/default.nix
@@ -4,6 +4,17 @@
   "firefox-bookmarks-legacy-attrset-warning" = ./bookmarks-legacy-attrset-warning.nix;
   "firefox-config-path-explicit-legacy" = ./config-path-explicit-legacy.nix;
   "firefox-config-path-explicit-xdg" = ./config-path-explicit-xdg.nix;
+  "firefox-config-path-wrapper-absolute" = import ./config-path-wrapper.nix {
+    absolute = true;
+    configPath = "Library/Application Support/org.nixos.firefox";
+  };
+  "firefox-config-path-wrapper-relative" = import ./config-path-wrapper.nix {
+    configPath = "Library/Application Support/org.nixos.firefox";
+  };
+  "firefox-config-path-wrapper-unsupported" = import ./config-path-wrapper.nix {
+    configPath = "Library/Application Support/org.nixos.firefox";
+    supportsAppDataDir = false;
+  };
   "firefox-config-path-xdg-default" = ./config-path-xdg-default.nix;
   "firefox-config-path-warning" = ./config-path-warning.nix;
   "firefox-multiple-derivatives" = ./multiple-derivatives.nix;
