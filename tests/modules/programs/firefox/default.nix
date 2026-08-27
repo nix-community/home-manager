a47d49712f518a867f6617cfb72d648c3cc4418a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-27 18:51:59 -0400

    firefox: set non-default configPath on darwin
    On macOS 27+, wrapped package will have no Mozilla signature and will
    fail to access the default datadir. Move users with recent stateVersion
    to a non-default datadir recommended by:
    
    https://github.com/NixOS/nixpkgs/pull/556611
    
    Users with existing profiles under default datadir will have to migrate
    them to a different location on macOS 27 upgrade. Migrating existing
    directory is out of scope for Home Manager.

--- tests/modules/programs/firefox/default.nix
+++ tests/modules/programs/firefox/default.nix
@@ -4,6 +4,19 @@
   "firefox-bookmarks-legacy-attrset-warning" = ./bookmarks-legacy-attrset-warning.nix;
   "firefox-config-path-explicit-legacy" = ./config-path-explicit-legacy.nix;
   "firefox-config-path-explicit-xdg" = ./config-path-explicit-xdg.nix;
+  "firefox-config-path-darwin-default-current" = import ./config-path-darwin-default.nix {
+    expectedDarwinPath = "Library/Application Support/org.nixos.firefox";
+    stateVersion = "26.11";
+  };
+  "firefox-config-path-darwin-default-legacy" = import ./config-path-darwin-default.nix {
+    expectedDarwinPath = "Library/Application Support/Firefox";
+    stateVersion = "26.05";
+  };
+  "firefox-config-path-darwin-default-null-package" = import ./config-path-darwin-default.nix {
+    expectedDarwinPath = "Library/Application Support/Firefox";
+    packageIsNull = true;
+    stateVersion = "26.11";
+  };
   "firefox-config-path-wrapper-absolute" = import ./config-path-wrapper.nix {
     absolute = true;
     configPath = "Library/Application Support/org.nixos.firefox";
