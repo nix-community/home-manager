8ce05abd8f051e4b50e7c764c7808ee4ed005e9f
glmlm <91877885+glmlm@users.noreply.github.com>
2026-09-01 18:07:29 +0900

    wayland.windowManager.niri: add test for `package = null` and `enableDefaultConfig`
    Ensure that the module correctly triggers an assertion when `package` is
    set to `null` while `enableDefaultConfig` is enabled.

--- tests/modules/services/niri/default.nix
+++ tests/modules/services/niri/default.nix
@@ -4,4 +4,5 @@ lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   niri-empty-config = ./niri-empty-config.nix;
   niri-example-settings = ./niri-example-settings.nix;
   niri-default-config = ./niri-default-config.nix;
+  niri-default-config-package-null = ./niri-default-config-package-null.nix;
 }
