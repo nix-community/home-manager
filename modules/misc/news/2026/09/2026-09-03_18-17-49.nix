aa308770461dcf22c333a5e8a31c8bddbde5bee9
Rachit Kumar Verma <rachitverma1122@gmail.com>
2026-09-03 18:16:21 +0530

    ripasso: add module
    Ripasso-cursive is a TUI for pass-cli, built on Rust, with a ncurses
    interface.

---
+++ modules/misc/news/2026/09/2026-09-03_18-17-49.nix
@@ -0,0 +1,10 @@
+{
+  time = "2026-09-03T12:47:49+00:00";
+  condition = true;
+  message = ''
+    A new module is available: `programs.ripasso`
+
+    Ripasso is a TUI for pass-cli built in Rust.
+    See <https://github.com/cortex/ripasso/> for more details.
+  '';
+}
