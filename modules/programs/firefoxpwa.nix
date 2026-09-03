13e318f11f2f3ffc5b7fc7a2846a92fa8690a709
Antonio Spadaro <nix+home-manager@ilovelinux.dev>
2026-09-03 12:44:14 +0200

    firefoxpwa: improve support for non-PWA websites
    Now `manifestUrl` is nullable and null by default. When used, it defaults
    to a data URL with an empty JSON object.
    
    See https://github.com/nix-community/home-manager/issues/9818 and
    https://ilovelinux.dev/posts/install-non-pwa-websites-in-pwasforfirefox-with-home-manager/
    for the rationale.
    
    Co-Authored-By: Austin Horstman <khaneliman12@gmail.com>

--- modules/programs/firefoxpwa.nix
+++ modules/programs/firefoxpwa.nix
@@ -122,8 +122,9 @@ in
                           example = "https://developer.mozilla.org/";
                         };
                         manifestUrl = lib.mkOption {
-                          type = lib.types.str;
-                          description = "URL of the site's web app manifest.";
+                          type = with lib.types; nullOr str;
+                          default = null;
+                          description = "URL of the site's web app manifest. Omit or set to `null` when installing a non-PWA website.";
                           example = "https://developer.mozilla.org/manifest.f42880861b394dd4dc9b.json";
                         };
                         desktopEntry = {
@@ -168,11 +169,18 @@ in
                         settings = {
                           ulid = name;
                           profile = profile.name;
-                          config = {
-                            inherit (config) name;
-                            document_url = config.url;
-                            manifest_url = config.manifestUrl;
-                          };
+                          config =
+                            let
+                              # A data url with an empty base64-encoded JSON object.
+                              # This is usually used for non-PWA websites.
+                              # See: https://github.com/nix-community/home-manager/issues/9818
+                              empty_manifest_url = "data:application/manifest+json;base64,e30=";
+                            in
+                            {
+                              inherit (config) name;
+                              document_url = config.url;
+                              manifest_url = lib.defaultTo empty_manifest_url config.manifestUrl;
+                            };
                           manifest = {
                             inherit (config) name;
                             start_url = config.url;
