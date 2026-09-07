0fdc3f0f778a5ccf0ad3c8fe89703a827a2146bd
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-09-07 09:42:59 +0200

    fixup! hyprland: verify generated config

--- modules/services/window-managers/hyprland/lib.nix
+++ modules/services/window-managers/hyprland/lib.nix
@@ -57,7 +57,7 @@ let
   mkCheckPhase = package: ''
     export XDG_RUNTIME_DIR=$(mktemp -d)
     trap "rm -rf $XDG_RUNTIME_DIR" EXIT
-    ${lib.getExec package} --verify-config --config "$target"
+    ${lib.getExe package} --verify-config --config "$target"
   '';
 in
 {
