44563307d1334b5471c74a1fe1eb9e5e0bcdc200
sensura-san <gohzhixuan2006@gmail.com>
2026-09-25 22:35:58 +0800

    hyfetch: add platform-dependent path support Now supports the correct filepath for macOS systems, which hyfetch on macOS reads from by default. Hyfetch test cases are updated to cover this.

--- modules/programs/hyfetch.nix
+++ modules/programs/hyfetch.nix
@@ -8,6 +8,9 @@ let
   cfg = config.programs.hyfetch;
 
   jsonFormat = pkgs.formats.json { };
+
+  configDir =
+    if pkgs.stdenv.hostPlatform.isDarwin then "Library/Application Support" else config.xdg.configHome;
 in
 {
   meta.maintainers = [ lib.hm.maintainers.lilyinstarlight ];
@@ -27,13 +30,21 @@ in
           mode = "horizontal";
         };
       };
-      description = "JSON config for HyFetch";
+      description = ''
+        JSON configuration for HyFetch.
+
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/hyfetch.json` on Linux or
+        {file}`~/Library/Application Support/hyfetch.json` on Darwin.
+      '';
     };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
-    xdg.configFile."hyfetch.json" = lib.mkIf (cfg.settings != { }) {
+
+    home.file."${configDir}/hyfetch.json" = {
+      enable = cfg.settings != { };
       source = jsonFormat.generate "hyfetch.json" cfg.settings;
     };
   };
--- tests/modules/programs/hyfetch/default.nix
+++ tests/modules/programs/hyfetch/default.nix
@@ -1,4 +1,7 @@
 {
-  hyfetch-settings = ./settings.nix;
-  hyfetch-empty-settings = ./empty-settings.nix;
+  hyfetch-settings = import ./settings.nix { };
+  hyfetch-settings-no-xdg = import ./settings.nix { xdgEnable = false; };
+  hyfetch-settings-custom-xdg = import ./settings.nix { configDir = "custom-config"; };
+  hyfetch-empty-settings = import ./empty-settings.nix { };
+  hyfetch-empty-settings-no-xdg = import ./empty-settings.nix { xdgEnable = false; };
 }
